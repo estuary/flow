a3a2c4446b4b675c3ac69795d297bd9aa0dffc36
Johnny Graettinger <johnny531@gmail.com>
2020-04-25 22:16:57 -0500

    nodejs: WIP getting lambda printing working in some form

--- src/derive/runtime/nodejs.rs
+++ src/derive/runtime/nodejs.rs
@@ -1,23 +1,30 @@
 use crate::catalog::Error;
 use rusqlite::{params as sql_params, Connection as DB};
 use std::io::Write;
+use serde_json::Value;
 
 pub fn compile_bundle(db: &DB) -> Result<(), Error> {
     let mut w = std::io::stdout();
 
     // Write out bootstraps.
-    let mut stmt = db.prepare_cached("SELECT id, body FROM nodejs_lambdas WHERE is_bootstrap")?;
+    let mut stmt = db.prepare(
+        "SELECT derivation_id, JSON_GROUP_ARRAY(expression)
+                FROM nodejs_expressions WHERE type = 'bootstrap';")?;
     let mut rows = stmt.query(sql_params![])?;
 
     write!(w, "let bootstrap_lambdas : BootstrapLambdaMap = {{\n")?;
     while let Some(row) = rows.next()? {
-        let (id, body): (i64, String) = (row.get(0)?, row.get(1)?);
-        write!(w, "\t{}: {},\n", id, body)?;
+        let (id, expressions): (i64, Value) = (row.get(0)?, row.get(1)?);
+        let expressions : Vec<String> = serde_json::from_value(expressions)?;
+        let expressions = expressions.join(", ");
+        write!(w, "\t{}: [{}],\n", id, expressions)?;
     }
     write!(w, "}};\n\n")?;
 
     // Write out transforms.
-    let mut stmt = db.prepare_cached("SELECT id, body FROM nodejs_lambdas WHERE is_transform")?;
+    let mut stmt = db.prepare(
+        "SELECT lambda_id, expression
+                FROM nodejs_expressions WHERE type = 'transform';")?;
     let mut rows = stmt.query(sql_params![])?;
 
     write!(w, "let transform_lambdas : TransformLambdaMap = {{\n")?;
@@ -26,14 +33,6 @@ pub fn compile_bundle(db: &DB) -> Result<(), Error> {
         write!(w, "\t{}: {},\n", id, body)?;
     }
     write!(w, "}};\n\n")?;
-
-    /*
-    write!(w, "let bootstraps : BootstrapMap = {{\n")?;
-    for (id, body, _) in bootstraps {
-        write!(w, "\t{}: {},\n", id, body)?;
-    }
-    write!(w, "}};\n")?;
-    */
 
     Ok(())
 }
