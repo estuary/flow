366d42ba86e8eeafce0dfe35a31faf9bfc40dd94
Johnny Graettinger <johnny@estuary.dev>
2020-07-15 13:34:44 -0400

    labels: remove WorkerRing label
    On further thought, I realized that the present and historical ring
    configurations should be stored in the catalog. Let's have just one
    joint for updating the configuration of the Shard at runtime, and
    that joint is updating the catalog URL.
    
    Derivation and WorkerIndex are *maybe* useful as labels, but they're
    also 100% derive-able from the ShardID, and perhaps that should be
    the only representation...

--- go/labels/labels.go
+++ go/labels/labels.go
@@ -22,27 +22,6 @@ const (
 	CatalogURL = "estuary.dev/catalog-url"
 	// Derivation is the name of the Estuary collection to be derived.
 	Derivation = "estuary.dev/derivation"
-	// WorkerRing is the topology under which source collection documents
-	// are shuffled to derivation workers, which can (slowly) change over time
-	// as worker parallelism is increased or decreased. The value of this label
-	// is an array of unsigned integers which defines a mapping from the Clock
-	// time of a message UUID to the total number of workers.
-	//
-	// The rationale for synchronizing over message Clocks is that it allows for
-	// atomic cut-overs by all workers from an old graph topology to an updated
-	// one, so long as all workers are notified in advance: eg the cut-over
-	// timestamp is sufficiently far in the future that the topology has propagated
-	// to all processes. Workers use only the message Clock for this, never their
-	// own measure of time (which could drift between workers).
-	//
-	// The label value is defined as:
-	//   [num-workers, +delta-seconds, updated-num-workers, ...]
-	//
-	// For example, value [4, 1000, 8, 1000, 6] means:
-	//  - Within interval [0, 1000), there are 4 workers.
-	//  - Within interval [1000, 2000), there are 8 workers (scaled up).
-	//  - Within interval [2000, ...), there are 6 workers (scaled down).
-	WorkerRing = "estuary.dev/worker-ring"
 	// Index of this Shard within the topology of workers for Derivation.
 	WorkerIndex = "estuary.dev/worker-index"
 )
