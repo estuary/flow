4d5364b4bfbc4e60063e70475ea7369486ed55c6
Johnny Graettinger <johnny@estuary.dev>
2020-09-17 12:44:36 -0400

    derive: add FlowConsumer interface, and implementing derive.App

---
+++ go/flow/interfaces.go
@@ -0,0 +1,26 @@
+package flow
+
+import (
+	pb "go.gazette.dev/core/broker/protocol"
+	"go.gazette.dev/core/consumer"
+	pc "go.gazette.dev/core/consumer/protocol"
+	"go.gazette.dev/core/consumer/recoverylog"
+	"go.gazette.dev/core/message"
+)
+
+// FlowConsumer is the interface implemented by the flow derivation
+// and materialization consumer application runtimes.
+type FlowConsumer interface {
+	consumer.Store
+
+	// TODO - move to consumer.Store.
+	BuildHints() (recoverylog.FSMHints, error)
+
+	BeginTxn(consumer.Shard) error
+	ConsumeMessage(consumer.Shard, message.Envelope, *message.Publisher) error
+	FinalizeTxn(consumer.Shard, *message.Publisher) error
+	FinishedTxn(consumer.Shard, consumer.OpFuture)
+
+	StartReadingMessages(consumer.Shard, pc.Checkpoint, chan<- consumer.EnvelopeOrError)
+	ReplayRange(_ consumer.Shard, _ pb.Journal, begin, end pb.Offset) message.Iterator
+}
