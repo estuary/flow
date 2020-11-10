5377279785399e3036828bb228c912014a2c6aed
Phil Fried <phil@estuary.dev>
2020-11-10 14:03:02 -0500

    Refactor CSV ingestion value parsing

--- go/ingest/ws_api_test.go
+++ go/ingest/ws_api_test.go
@@ -1,9 +1,11 @@
 package ingest
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	pf "github.com/estuary/flow/go/protocol"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/require"
 	pb "go.gazette.dev/core/broker/protocol"
@@ -130,6 +132,163 @@ func testCSVConversionError(t *testing.T, addr string) {
 	require.Equal(t, `processing frame: failed to parse '32.32' (of column: i) into [integer]: strconv.ParseInt: parsing "32.32": invalid syntax`, whoops)
 }
 
+func testCSVConvertRequiredNullable(t *testing.T, addr string) {
+	var valid = []string{
+		"theKey,string,boolean,integer,number,object,array,null",
+		"someKey,,,,,,,",
+	}
+
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/required-nullable", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, valid)
+	require.Equal(t, "", whoops)
+	require.Equal(t, 1, processed)
+}
+
+func testCSVMissingMustExistNullable(t *testing.T, addr string) {
+	var valid = []string{
+		"theKey,string,boolean,integer,number,object,array,null",
+		"someKey,,,",
+	}
+
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/required-nullable", nil)
+	require.NoError(t, err)
+
+	var whoops, _ = runWebsocket(t, c, valid)
+	require.Equal(t, "processing frame: row omits column 7 ('null'), which must exist", whoops)
+}
+
+func testCSVOptionalMultipleTypes(t *testing.T, addr string) {
+	var rows = []string{
+		"theKey,stringOrInt,intOrNum,boolOrString",
+		"a,55,66,true",
+		"b,\"77\",\"1.23\",\"false\"", // Even though these are quoted, they will not import as strings
+		"c,a real string,0,moar strings",
+		"d", // columns may be undefined (but not null)
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, rows)
+	require.Equal(t, "", whoops)
+	require.Equal(t, 4, processed)
+}
+
+func testCSVValueFailsValidation(t *testing.T, addr string) {
+	// 0 is parseable as an int, but the document will fail validation because it's not within the correct range
+	var rows = []string{
+		"theKey,intDifferentRanges",
+		"a,0",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, _ = runWebsocket(t, c, rows)
+	require.Contains(t, whoops, "document is invalid")
+}
+
+func testCSVHeaderMissingRequiredField(t *testing.T, addr string) {
+	// header is missing theKey
+	var rows = []string{
+		"string,bool,int,number",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optionals", nil)
+	require.NoError(t, err)
+
+	var whoops, _ = runWebsocket(t, c, rows)
+	require.Equal(t, "processing frame: Header does not include any field that maps to the location: '/theKey', which is required to exist by the collection schema", whoops)
+}
+
+func testCSVNumOrIntOrNull(t *testing.T, addr string) {
+	var rows = []string{
+		"theKey,intOrNumOrNull",
+		"a,55.55",
+		"b,\"77\"",
+		"c,\"\"",
+		"d",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, rows)
+	require.Equal(t, "", whoops)
+	require.Equal(t, 4, processed)
+}
+
+func testCSVOptionalObjectsAndArrays(t *testing.T, addr string) {
+	var rows = []string{
+		"theKey,intOrObjectOrNull,boolOrArrayOrNull",
+		"a,55,true",
+		"b,\"77\",\"true\"",
+		// These will be null values
+		"c,\"\",\"\"",
+		"d,,",
+		"e",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, rows)
+	require.Equal(t, "", whoops)
+	require.Equal(t, 5, processed)
+}
+
+func testCSVUnsupportedArray(t *testing.T, addr string) {
+	var rows = []string{
+		"theKey,boolOrArrayOrNull",
+		"a,[]",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, rows)
+	var expectedErr = "processing frame: failed to parse '[]' (of column: boolOrArrayOrNull) into [array boolean null]: unspported type 'array'"
+	require.Equal(t, expectedErr, whoops)
+	require.Equal(t, 0, processed)
+}
+
+func testCSVUnsupportedObject(t *testing.T, addr string) {
+	var rows = []string{
+		"theKey,intOrObjectOrNull",
+		"a,{}",
+	}
+	var dialer = websocket.Dialer{
+		Subprotocols: []string{wsCSVProtocol},
+	}
+	var c, _, err = dialer.Dial("ws://"+addr+"/ingest/weird-types/optional-multi-types", nil)
+	require.NoError(t, err)
+
+	var whoops, processed = runWebsocket(t, c, rows)
+	var expectedErr = "processing frame: failed to parse '{}' (of column: intOrObjectOrNull) into [integer null object]: unspported type 'object'"
+	require.Equal(t, expectedErr, whoops)
+	require.Equal(t, 0, processed)
+}
+
 func testCSVEmptyBody(t *testing.T, addr string) {
 	var dialer = websocket.Dialer{
 		Subprotocols: []string{wsCSVProtocol},
@@ -139,6 +298,48 @@ func testCSVEmptyBody(t *testing.T, addr string) {
 
 	var whoops, _ = runWebsocket(t, c, []string{})
 	require.Equal(t, "client closed the connection without sending any documents", whoops)
+}
+
+func testCSVTypeConversions(t *testing.T) {
+	var doExtractValueTest = func(types []string, input string, expectedValue interface{}, expectedError error) {
+		var projection = pf.Projection{
+			Ptr:   "/the/ptr",
+			Field: "theField",
+			Inference: &pf.Inference{
+				Types: types,
+			},
+		}
+		var mapping = newFieldMapping(&projection)
+		var value, err = mapping.extractValue(input)
+
+		if expectedValue == nil {
+			require.Nil(t, value)
+		} else {
+			require.Equal(t, expectedValue, value)
+		}
+		if expectedError == nil {
+			require.Nil(t, err)
+		} else {
+			require.Equal(t, expectedError, err)
+		}
+	}
+	var allTypes = []string{"integer", "string", "object", "number", "array", "boolean", "null"}
+
+	// empty input always gets parsed as an explicit null if null is in the list of allowed types
+	doExtractValueTest(allTypes, "", nil, nil)
+	doExtractValueTest(allTypes, "3", uint64(3), nil)
+	doExtractValueTest(allTypes, "1.234", 1.234, nil)
+	doExtractValueTest(allTypes, "true", true, nil)
+	doExtractValueTest(allTypes, "canary", "canary", nil)
+
+	doExtractValueTest([]string{"boolean"}, "", nil, fmt.Errorf("value cannot be null"))
+	doExtractValueTest([]string{"string", "null"}, "", nil, nil)
+
+	var typesBesidesNull = []string{"integer", "number", "array", "object", "boolean", "string"}
+	// If null is not in the allowed types, but string is, then an empty input gets parsed to an
+	// empty string
+	doExtractValueTest(typesBesidesNull, "", "", nil)
+	doExtractValueTest(typesBesidesNull, "asdf", "asdf", nil)
 }
 
 func testJSONSimple(t *testing.T, addr string) {
