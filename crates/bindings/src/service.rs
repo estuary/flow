4138e4f41a52b9026b97c2cdb1042dc8b307740a
Johnny Graettinger <johnny@estuary.dev>
2020-12-03 00:49:50 -0500

    bindings: add crate and Go module with Service inter-op mechanism
    A Service looks like a bidirectional channel, but is one which is
    synchronously polled to send and receive framed message code and data
    payloads. It's intended as a fundamental building block for building
    bidirectional in-process streaming services that interoperate between
    Go and Rust -- an interface which is a heck of a lot faster than gRPC.
    
    It achieves zero-copy semantics by pushing Go []byte buffers down
    into CGO invocations, and surfacing Rust-owned []byte arenas and
    Frame payloads. There are some rules callers need to adhere to,
    such as not scribbling "sent" memory until Poll() is called, and
    not referencing Rust-owned memory returned by Poll() after _another_
    Poll() call is made.
    
    It amortizes the CGO overhead by vectorizing dispatch. On the Rust
    side, some common functions are provided to monomorphize appropriate
    bindings for an implementation of a Service trait.
    
    Lies, damn lies, and benchmarks:
    
    This shows a variety of stride patterns (number of sent messages
    per Poll() call), some of which are deliberately worst case.
    
    It's comparing the actual "upper case" service with a no-op service
    that has the same setup & moving parts, but avoids the actual CGO
    invocation itself.
    
    Comparative benchmarks with equivalent "naive" CGO and "pure" Go
    Services are included. These are also zero-copy / return owned
    memory.
    
        $ go test ./go/bindings/ -bench='.' -benchtime 3s
        goos: linux
        goarch: amd64
        pkg: github.com/estuary/flow/go/bindings
        BenchmarkUpperService/cgo-1-24          43781984                79.1 ns/op
        BenchmarkUpperService/noop-1-24         270172464               13.3 ns/op
        BenchmarkUpperService/cgo-3-24          48714016                72.2 ns/op
        BenchmarkUpperService/noop-3-24         368625391                9.70 ns/op
        BenchmarkUpperService/cgo-4-24          91550871                36.8 ns/op
        BenchmarkUpperService/noop-4-24         363729646               10.1 ns/op
        BenchmarkUpperService/cgo-11-24         77817141                45.2 ns/op
        BenchmarkUpperService/noop-11-24        369495175               9.76 ns/op
        BenchmarkUpperService/cgo-15-24         83527456                42.2 ns/op
        BenchmarkUpperService/noop-15-24        382336641                9.84 ns/op
        BenchmarkUpperService/cgo-17-24         135539218               27.0 ns/op
        BenchmarkUpperService/noop-17-24        423105061                8.66 ns/op
        BenchmarkUpperService/cgo-31-24         100000000               32.6 ns/op
        BenchmarkUpperService/noop-31-24        406740273                9.10 ns/op
        BenchmarkUpperService/cgo-32-24         151226794               23.7 ns/op
        BenchmarkUpperService/noop-32-24        423966495                8.67 ns/op
        BenchmarkUpperService/cgo-63-24         129356660               27.7 ns/op
        BenchmarkUpperService/noop-63-24        426944920                8.82 ns/op
        BenchmarkUpperService/cgo-137-24        150434529               23.9 ns/op
        BenchmarkUpperService/noop-137-24               441497529                8.39 ns/op
        BenchmarkUpperService/cgo-426-24                155104615               23.3 ns/op
        BenchmarkUpperService/noop-426-24               452368488                8.30 ns/op
        BenchmarkUpperServiceNaive-24                   34605169               104 ns/op
        BenchmarkUpperServiceGo-24                      278482276               12.8 ns/op
        PASS
        ok      github.com/estuary/flow/go/bindings     111.130s

---
+++ crates/bindings/src/service.rs
@@ -0,0 +1,233 @@
+/// Service is a trait implemented by services which may be called from Go.
+pub trait Service {
+    /// Error type returned by Service invocations.
+    type Error: std::error::Error;
+    /// Create a new instance of the Service.
+    fn create() -> Self;
+    /// Invoke with the given op-code & |data| payload.
+    /// It extends |arena| with any returned []byte data, and pushes output messages onto |out|.
+    fn invoke(
+        &mut self,
+        code: u32,
+        data: &[u8],
+        arena: &mut Vec<u8>,
+        out: &mut Vec<Out>,
+    ) -> Result<(), Self::Error>;
+}
+
+/// Output frame produced by a Service.
+#[repr(C)]
+pub struct Out {
+    /// Service-defined response code.
+    pub code: u32,
+    /// Begin data offset into the Channel arena.
+    pub begin: u32,
+    /// End data offset into the Channel arena.
+    pub end: u32,
+}
+
+/// InN is a variadic input which invokes itself against a Service.
+pub trait InN {
+    fn invoke<S: Service>(
+        self: &Self,
+        svc: &mut S,
+        arena: &mut Vec<u8>,
+        out: &mut Vec<Out>,
+    ) -> Result<(), S::Error>;
+}
+
+/// Input frame produced from CGO, which is a single service invocation.
+/// 16 bytes, or 1/4 of a typical cache line.
+#[repr(C)]
+pub struct In1 {
+    data_ptr: *const u8,
+    data_len: u32,
+    code: u32,
+}
+
+impl InN for In1 {
+    #[inline]
+    fn invoke<S: Service>(
+        self: &Self,
+        svc: &mut S,
+        arena: &mut Vec<u8>,
+        out: &mut Vec<Out>,
+    ) -> Result<(), S::Error> {
+        svc.invoke(
+            self.code,
+            unsafe { std::slice::from_raw_parts(self.data_ptr, self.data_len as usize) },
+            arena,
+            out,
+        )
+    }
+}
+
+/// Four invocations, composed into one struct.
+/// 64 bytes, or one typical cache line.
+#[repr(C)]
+pub struct In4 {
+    in0: In1,
+    in1: In1,
+    in2: In1,
+    in3: In1,
+}
+
+impl InN for In4 {
+    #[inline]
+    fn invoke<S: Service>(
+        self: &Self,
+        svc: &mut S,
+        arena: &mut Vec<u8>,
+        out: &mut Vec<Out>,
+    ) -> Result<(), S::Error> {
+        self.in0.invoke(svc, arena, out)?;
+        self.in1.invoke(svc, arena, out)?;
+        self.in2.invoke(svc, arena, out)?;
+        self.in3.invoke(svc, arena, out)
+    }
+}
+
+/// Sixteen invocations, composed into one struct.
+/// 256 bytes, or four typical cache lines.
+#[repr(C)]
+pub struct In16 {
+    in0: In4,
+    in1: In4,
+    in2: In4,
+    in3: In4,
+}
+
+impl InN for In16 {
+    #[inline]
+    fn invoke<S: Service>(
+        self: &Self,
+        svc: &mut S,
+        arena: &mut Vec<u8>,
+        out: &mut Vec<Out>,
+    ) -> Result<(), S::Error> {
+        self.in0.invoke(svc, arena, out)?;
+        self.in1.invoke(svc, arena, out)?;
+        self.in2.invoke(svc, arena, out)?;
+        self.in3.invoke(svc, arena, out)
+    }
+}
+
+/// Opaque pointer for a Service instance in the ABI.
+#[repr(C)]
+pub struct ServiceImpl {
+    _private: [u8; 0],
+}
+
+/// Channel is shared between CGO and Rust, and holds details
+/// about the language interconnect.
+#[repr(C)]
+pub struct Channel {
+    // Opaque service pointer.
+    svc_impl: *mut ServiceImpl,
+
+    // Output memory arena, exposed to CGO.
+    arena_ptr: *mut u8,
+    arena_len: usize,
+    arena_cap: usize,
+
+    // Output frame codes & arena offsets, exposed to CGO.
+    out_ptr: *mut Out,
+    out_len: usize,
+    out_cap: usize,
+
+    // Final error returned by the Service.
+    err_ptr: *mut u8,
+    err_len: usize,
+    err_cap: usize,
+}
+
+/// Create a new Service instance, wrapped in an owning Channel.
+/// This is intended to be monomorphized by each Service implementation,
+/// and exposed via cbindgen.  See the UpperCase service for an example.
+#[inline]
+pub fn create<S: Service>() -> *mut Channel {
+    let svc_impl = Box::new(S::create());
+    let svc_impl = Box::leak(svc_impl) as *mut S as *mut ServiceImpl;
+
+    let ch = Box::new(Channel {
+        svc_impl,
+        arena_ptr: 0 as *mut u8,
+        arena_len: 0,
+        arena_cap: 0,
+        out_ptr: 0 as *mut Out,
+        out_len: 0,
+        out_cap: 0,
+        err_ptr: 0 as *mut u8,
+        err_len: 0,
+        err_cap: 0,
+    });
+    Box::leak(ch)
+}
+
+/// Invoke a Service with one input.
+/// This is intended to be monomorphized by each Service implementation,
+/// and exposed via cbindgen.  See the UpperCase service for an example.
+#[inline]
+pub fn invoke<S: Service, I: InN>(ch: *mut Channel, i: I) {
+    let ch = unsafe { &mut *ch };
+
+    if ch.err_cap != 0 {
+        return; // If an error has been set, further invocations are no-ops.
+    }
+
+    let mut arena = unsafe { Vec::<u8>::from_raw_parts(ch.arena_ptr, ch.arena_len, ch.arena_cap) };
+    let mut out = unsafe { Vec::<Out>::from_raw_parts(ch.out_ptr, ch.out_len, ch.out_cap) };
+    let mut err_str = unsafe { String::from_raw_parts(ch.err_ptr, ch.err_len, ch.err_cap) };
+    let svc_impl = unsafe { &mut *(ch.svc_impl as *mut S) };
+
+    let r = i.invoke(svc_impl, &mut arena, &mut out);
+    if let Err(err) = r {
+        // Set terminal error string.
+        err_str = format!("{:?}", err);
+    }
+
+    ch.arena_ptr = arena.as_mut_ptr();
+    ch.arena_cap = arena.capacity();
+    ch.arena_len = arena.len();
+    std::mem::forget(arena);
+
+    ch.out_ptr = out.as_mut_ptr();
+    ch.out_cap = out.capacity();
+    ch.out_len = out.len();
+    std::mem::forget(out);
+
+    ch.err_ptr = err_str.as_mut_ptr();
+    ch.err_cap = err_str.capacity();
+    ch.err_len = err_str.len();
+    std::mem::forget(err_str);
+}
+
+/// Drop a Service and its Channel.
+/// This is intended to be monomorphized by each Service implementation,
+/// and exposed via cbindgen.  See the UpperCase service for an example.
+#[inline]
+pub fn drop<S: Service>(ch: *mut Channel) {
+    let Channel {
+        // Opaque service pointer.
+        svc_impl,
+
+        // Output frame codes & arena offsets, exposed to CGO.
+        arena_ptr,
+        arena_len,
+        arena_cap,
+
+        out_ptr,
+        out_len,
+        out_cap,
+
+        err_ptr,
+        err_len,
+        err_cap,
+    } = *unsafe { Box::from_raw(ch) };
+
+    // Drop svc_impl, arena, and out.
+    unsafe { Box::from_raw(svc_impl as *mut S) };
+    unsafe { Vec::<u8>::from_raw_parts(arena_ptr, arena_len, arena_cap) };
+    unsafe { Vec::<Out>::from_raw_parts(out_ptr, out_len, out_cap) };
+    unsafe { String::from_raw_parts(err_ptr, err_len, err_cap) };
+}
---
+++ crates/bindings/src/upper_case.rs
@@ -0,0 +1,109 @@
+use crate::service::{self, Channel, Service, ServiceImpl};
+use std::io::Write;
+
+/// UpperCase is an example Service that returns its inputs as upper-case
+/// ASCII, along with a running sum of the number of bytes upper-cased
+/// (returned with each response code).
+pub struct UpperCase {
+    sum_length: u32,
+}
+
+impl Service for UpperCase {
+    type Error = std::io::Error;
+
+    fn create() -> Self {
+        Self { sum_length: 0 }
+    }
+
+    fn invoke(
+        &mut self,
+        _code: u32,
+        data: &[u8],
+        arena: &mut Vec<u8>,
+        out: &mut Vec<service::Out>,
+    ) -> Result<(), Self::Error> {
+        if data == b"whoops!" {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "whoops!"));
+        }
+
+        let begin = arena.len() as u32;
+        arena.extend(data.iter().map(u8::to_ascii_uppercase));
+        self.sum_length += data.len() as u32;
+
+        out.push(service::Out {
+            code: self.sum_length,
+            begin,
+            end: arena.len() as u32,
+        });
+
+        Ok(())
+    }
+}
+
+// Define cbindgen <=> CGO hooks for driving the UpperCase service.
+
+#[no_mangle]
+pub extern "C" fn upper_case_create() -> *mut Channel {
+    service::create::<UpperCase>()
+}
+#[no_mangle]
+pub extern "C" fn upper_case_invoke1(ch: *mut Channel, i: service::In1) {
+    service::invoke::<UpperCase, _>(ch, i)
+}
+#[no_mangle]
+pub extern "C" fn upper_case_invoke4(ch: *mut Channel, i: service::In4) {
+    service::invoke::<UpperCase, _>(ch, i)
+}
+#[no_mangle]
+pub extern "C" fn upper_case_invoke16(ch: *mut Channel, i: service::In16) {
+    service::invoke::<UpperCase, _>(ch, i)
+}
+#[no_mangle]
+pub extern "C" fn upper_case_drop(ch: *mut Channel) {
+    service::drop::<UpperCase>(ch)
+}
+
+/// UpperCaseNaive is not part of UpperCase's service interface.
+/// It's here for comparative benchmarking with a more traditional CGO call style.
+
+struct UpperCaseNaive {
+    sum_length: u32,
+    arena: Vec<u8>,
+}
+
+#[no_mangle]
+pub extern "C" fn create_upper_case_naive() -> *mut ServiceImpl {
+    Box::leak(Box::new(UpperCaseNaive {
+        sum_length: 0,
+        arena: Vec::new(),
+    })) as *mut UpperCaseNaive as *mut ServiceImpl
+}
+
+#[no_mangle]
+pub extern "C" fn upper_case_naive(
+    svc: *mut ServiceImpl,
+    _code: u32,
+    in_ptr: *const u8,
+    in_len: u32,
+    out_ptr: &mut *const u8,
+    out_len: &mut u32,
+) -> u32 {
+    let svc = unsafe { &mut *(svc as *mut UpperCaseNaive) };
+    let in_ = unsafe { std::slice::from_raw_parts(in_ptr, in_len as usize) };
+
+    svc.arena.clear();
+
+    let code = if in_ == b"whoops!" {
+        let err = std::io::Error::new(std::io::ErrorKind::Other, "whoops!");
+        write!(svc.arena, "{:?}", err).unwrap();
+        std::u32::MAX
+    } else {
+        svc.arena.extend(in_.iter().map(u8::to_ascii_uppercase));
+        svc.sum_length += in_.len() as u32;
+        svc.sum_length
+    };
+
+    *out_ptr = svc.arena.as_ptr();
+    *out_len = svc.arena.len() as u32;
+    code
+}
