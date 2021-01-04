1cea6ae6b783bd69defb024d754810a147edaf34
Phil Fried <phil@estuary.dev>
2021-01-04 12:28:33 -0500

    Test materialization spec validation

---
+++ go/materialize/driver/validate_test.go
@@ -0,0 +1,91 @@
+package driver
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/bradleyjkemp/cupaloy"
+	"github.com/estuary/flow/go/flow"
+
+	//pf "github.com/estuary/flow/go/protocols/flow"
+	pm "github.com/estuary/flow/go/protocols/materialize"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidations(t *testing.T) {
+	var tempdir, err = ioutil.TempDir("", "validate-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	cat, err := flow.NewCatalog("../../../catalog.db", tempdir)
+	require.NoError(t, err)
+
+	var collections = []string{
+		"optionals",
+		"required-nullable",
+		"optional-multi-types",
+	}
+	for _, name := range collections {
+		var collectionName = fmt.Sprintf("weird-types/%s", name)
+		t.Run(fmt.Sprintf("NewSqlProjections-%s", name), func(t *testing.T) {
+			collection, err := cat.LoadCollection(collectionName)
+			require.NoError(t, err)
+
+			var constraints = ValidateNewSqlProjections(&collection)
+			cupaloy.SnapshotT(t, constraints)
+		})
+	}
+
+	t.Run("MatchesExisting", func(t *testing.T) {
+		testMatchesExisting(t, cat)
+	})
+}
+
+func testMatchesExisting(t *testing.T, catalog *flow.Catalog) {
+	existingCollection, err := catalog.LoadCollection("weird-types/optionals")
+	require.NoError(t, err)
+	var existingFields = pm.FieldSelection{
+		Keys:     []string{"theKey"},
+		Values:   []string{"string", "bool", "int"},
+		Document: "flow_document",
+	}
+	var existingSpec = MaterializationSpec{
+		CollectionSpec: existingCollection,
+		Fields:         existingFields,
+	}
+
+	// Load a new copy of the same collection, which we'll modify and use as the "proposed"
+	proposedCollection, err := catalog.LoadCollection("weird-types/optionals")
+	require.NoError(t, err)
+	// int projection is changing type to "string", which should result in unsatisfiable
+	// constraint
+	var intProjection = proposedCollection.GetProjection("int")
+	intProjection.Inference.Types = []string{"string"}
+	// string projection is going from optional to requried, which should be allowed
+	var stringProjection = proposedCollection.GetProjection("string")
+	stringProjection.Inference.MustExist = true
+
+	var constraints = ValidateMatchesExisting(&existingSpec, &proposedCollection)
+	var req = []string{"theKey", "string", "bool", "flow_document"}
+	for _, field := range req {
+		var constraint, ok = constraints[field]
+		require.True(t, ok, "constraint must be present for field '%s'", field)
+		require.Equal(t, pm.Constraint_FIELD_REQUIRED, constraint.Type)
+	}
+	var intConstraint, ok = constraints["int"]
+	require.True(t, ok, "missing constraint for 'int' field")
+	require.Equal(t, pm.Constraint_UNSATISFIABLE, intConstraint.Type)
+
+	numConstraint, ok := constraints["number"]
+	require.True(t, ok, "missing constraint for 'number' field")
+	require.Equal(t, pm.Constraint_FIELD_FORBIDDEN, numConstraint.Type)
+
+	var proposedSpec = MaterializationSpec{
+		CollectionSpec: proposedCollection,
+		Fields:         existingFields,
+	}
+	var constraintsError = ValidateSelectedFields(constraints, &proposedSpec)
+	require.Error(t, constraintsError)
+}
--- go/protocols/flow/collection_extensions_test.go
+++ go/protocols/flow/collection_extensions_test.go
@@ -10,7 +10,6 @@ func TestIsSingleScalarType(t *testing.T) {
 	var scalars = []string{"string", "boolean", "integer", "number"}
 
 	var truthy []Inference
-
 	for _, scalar := range scalars {
 		var basic = Inference{
 			MustExist: true,
@@ -39,6 +38,41 @@ func TestIsSingleScalarType(t *testing.T) {
 
 		var result = projection.IsSingleScalarType()
 		require.True(t, result, "expected projection to be a single scalar type %v", projection)
+	}
+
+	var falsey = []Inference{
+		{
+			MustExist: true,
+			Types:     []string{"string", "int", "null"},
+		},
+		{
+			MustExist: true,
+			// This is just documenting the behavior. We could definitely allow this in the future,
+			// but we already collapse these into a single type during catalog builds, so we're not
+			// likely to ever see this at runtime.
+			Types: []string{"int", "number"},
+		},
+		{
+			MustExist: true,
+			Types:     []string{"boolean", "int"},
+		},
+		{
+			MustExist: true,
+			Types:     []string{},
+		},
+		{
+			MustExist: true,
+			Types:     []string{"null"},
+		},
+	}
+	for _, inference := range falsey {
+		var projection = Projection{
+			Field:     "canary",
+			Ptr:       "/foo/bar",
+			Inference: &inference,
+		}
+		var result = projection.IsSingleScalarType()
+		require.False(t, result, "expected projection to not be a single scalar type: %v", projection)
 	}
 
 }
