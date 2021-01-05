edf1cffc6a6b858bfaf70ec996c7be0cf75e2495
Phil Fried <phil@estuary.dev>
2021-01-05 15:56:25 -0500

    Shorten go function receiver names

--- go/protocols/materialize/materialize_extensions.go
+++ go/protocols/materialize/materialize_extensions.go
@@ -2,8 +2,8 @@ package materialize
 
 // IsForbidden returns true if the constraint type forbids inclusion in a materialization. This will
 // return true for FIELD_FORBIDDEN and UNSATISFIABLE, and false for any other constraint type.
-func (constraintType *Constraint_Type) IsForbidden() bool {
-	switch *constraintType {
+func (m *Constraint_Type) IsForbidden() bool {
+	switch *m {
 	case Constraint_FIELD_FORBIDDEN, Constraint_UNSATISFIABLE:
 		return true
 	default:
