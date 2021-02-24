3230a9309be4a463d0461a0e0c68004fd9dc29cc
Johnny Graettinger <johnny@estuary.dev>
2021-02-24 15:57:58 -0500

    materialize: add sql2 with alternative implementation against new protocol
    This liberally copies from `sql` package, with a bunch of refactoring.
    It exposes a new callback-centric API for running driver transactions.

---
+++ go/materialize/driver/sql2/tables.go
@@ -0,0 +1,64 @@
+package sql
+
+const (
+	// DefaultGazetteCheckpoints is the default table for checkpoints.
+	DefaultGazetteCheckpoints = "gazette_checkpoints"
+	// DefaultFlowMaterializations is the default table for materialization specs.
+	DefaultFlowMaterializations = "flow_materializations"
+)
+
+// GazetteCheckpointsTable returns the Table description for the table that holds the checkpoint
+// and nonce values for each materialization shard.
+func GazetteCheckpointsTable(name string) *Table {
+	return &Table{
+		Name:        name,
+		IfNotExists: true,
+		Comment:     "This table holds journal checkpoints, which Flow manages in order to ensure exactly-once updates for materializations",
+		Columns: []Column{
+			{
+				Name:       "shard_fqn",
+				Comment:    "The id of the consumer shard. Note that a single collection may have multiple consumer shards materializing it, and each will have a separate checkpoint.",
+				PrimaryKey: true,
+				Type:       STRING,
+				NotNull:    true,
+			},
+			{
+				Name:    "fence",
+				Comment: "This nonce is used to uniquely identify unique process assignments of a shard and prevent them from conflicting.",
+				Type:    INTEGER,
+				NotNull: true,
+			},
+			{
+				Name:    "checkpoint",
+				Comment: "Opaque checkpoint of the Flow consumer shard",
+				Type:    BINARY,
+			},
+		},
+	}
+}
+
+// FlowMaterializationsTable returns the Table description for the table that holds the
+// MaterializationSpec that corresponds to each target table. This state is used both for sql
+// generation and for validation.
+func FlowMaterializationsTable(name string) *Table {
+	return &Table{
+		Name:        name,
+		IfNotExists: true,
+		Comment:     "This table is the source of truth for all materializations into this system.",
+		Columns: []Column{
+			{
+				Name:       "table_name",
+				Comment:    "The name of the target table of the materialization, which may or may not include a schema and catalog prefix",
+				PrimaryKey: true,
+				Type:       STRING,
+				NotNull:    true,
+			},
+			{
+				Name:    "spec",
+				Comment: "Specification of the materialization, encoded as protobuf.",
+				Type:    OBJECT,
+				NotNull: true,
+			},
+		},
+	}
+}
---
+++ go/materialize/driver/sqlite/sqlite.go
@@ -0,0 +1,51 @@
+package sql
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	sqlDriver "github.com/estuary/flow/go/materialize/driver/sql2"
+	"github.com/estuary/flow/go/protocols/flow"
+)
+
+// NewSQLiteDriver creates a new Driver for sqlite.
+func NewSQLiteDriver() sqlDriver.Driver {
+	return sqlDriver.Driver{
+		NewEndpoint: func(ctx context.Context, et flow.EndpointType, config json.RawMessage) (*sqlDriver.Endpoint, error) {
+			var parsed struct {
+				Path  string
+				Table string
+			}
+
+			if err := json.Unmarshal(config, &parsed); err != nil {
+				return nil, fmt.Errorf("parsing SQLite configuration: %w", err)
+			}
+			if parsed.Path == "" {
+				return nil, fmt.Errorf("expected SQLite database configuration `path`")
+			}
+			if parsed.Table == "" {
+				return nil, fmt.Errorf("expected SQLite database configuration `table`")
+			}
+
+			db, err := sql.Open("sqlite3", parsed.Path)
+			if err != nil {
+				return nil, fmt.Errorf("opening SQLite database: %w", err)
+			}
+
+			var endpoint = &sqlDriver.Endpoint{
+				Context:      ctx,
+				EndpointType: et,
+				DB:           db,
+				Generator:    sqlDriver.SQLiteSQLGenerator(),
+			}
+			endpoint.Tables.Target = parsed.Table
+			endpoint.Tables.Checkpoints = sqlDriver.DefaultGazetteCheckpoints
+			endpoint.Tables.Specs = sqlDriver.DefaultFlowMaterializations
+
+			return endpoint, nil
+		},
+		RunTransactions: sqlDriver.RunSQLTransactions,
+	}
+}
