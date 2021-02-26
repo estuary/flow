915f23ab692960aec76636fc73fd6daf8ca2f21d
Johnny Graettinger <johnny@estuary.dev>
2021-02-26 09:54:53 -0500

    sqlite: support "opening" remote DBs by mangling to local path
    This is a more ergonomic experience for running `flowctl develop`
    against a remote catalog.
    
    Tested via `flowctl develop` of:
      https://raw.githubusercontent.com/estuary/flow/sqlite-handling/examples/flow.yaml

--- go/materialize/driver/sqlite/sqlite.go
+++ go/materialize/driver/sqlite/sqlite.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/url"
+	"strings"
 
 	sqlDriver "github.com/estuary/flow/go/materialize/driver/sql2"
 	"github.com/estuary/flow/go/protocols/flow"
@@ -29,9 +31,25 @@ func NewSQLiteDriver() *sqlDriver.Driver {
 				return nil, fmt.Errorf("expected SQLite database configuration `table`")
 			}
 
+			if u, err := url.Parse(parsed.Path); err != nil {
+				return nil, fmt.Errorf("parsing path %q: %w", parsed.Path, err)
+			} else if !u.IsAbs() {
+				return nil, fmt.Errorf("path %q is not absolute", parsed.Path)
+			} else if u.Scheme == "file" {
+				// We can directly pass file:// schemes to SQLite.
+			} else {
+				// Path is absolute and non-local (e.x. https://some/database.db).
+				// Mangle to turn into a file opened relative to the current directory.
+				var parts = append([]string{u.Host}, strings.Split(u.Path, "/")...)
+				parsed.Path = strings.Join(parts, "_")
+			}
+
 			db, err := sql.Open("sqlite3", parsed.Path)
+			if err == nil {
+				err = db.PingContext(ctx)
+			}
 			if err != nil {
-				return nil, fmt.Errorf("opening SQLite database: %w", err)
+				return nil, fmt.Errorf("opening SQLite database %q: %w", parsed.Path, err)
 			}
 
 			var endpoint = &sqlDriver.Endpoint{
