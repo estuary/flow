2e20aa45ea702ccc5681a6bf0de67d438307916d
Phil Fried <phil@estuary.dev>
2021-03-10 12:54:17 -0500

    avoid lint error when there are no derivations

--- flow_generated/flow/routes.ts
+++ flow_generated/flow/routes.ts
@@ -5,6 +5,8 @@ export type Document = unknown;
 // Lambda is a relaxed signature implemented by all Flow transformation lambdas.
 export type Lambda = (source: Document, register?: Document, previous?: Document) => Document[];
 
+// "Use" imported modules, even if they're empty, to satisfy compiler and linting.
+export type __interfaces_module = interfaces.__module;
 // Import derivation classes from their implementation modules.
 import {
     TestingIntStrings,
