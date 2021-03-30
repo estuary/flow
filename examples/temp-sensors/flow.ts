6cfebdc699dab8ea78042f55cd71fd0727a69e38
dyaffe <dyaffe1@gmail.com>
2021-03-30 13:37:30 -0600

    Updating the sensors example to break it into two tutorials.  First, we'll show off reduction annotations and remote URL's, then registers and testing.
    Adding foursquare and other files

--- examples/temp-sensors/flow.ts
+++ examples/temp-sensors/flow.ts
@@ -1,84 +1,85 @@
 import { collections, interfaces, registers } from 'flow/modules';
 
-// Implementation for derivation examples/temp-sensors/flow.yaml#/collections/temperature~1average-by-location/derivation.
+// Implementation for derivation examples/temp-sensors/flow.yaml#/collections/temperature~1averageByLocation/derivation.
 export class TemperatureAverageByLocation implements interfaces.TemperatureAverageByLocation {
-
-    // Updates the register value for each reading document.
-    readingsUpdate(
-        source: collections.TemperatureReadings,
-    ): registers.TemperatureAverageByLocation[] {
-      // When updating the register, these values will be 'reduced' into the current value of the
-      // register according to annotations on the register schema.
-      return [{
-        // Will increment the number of readings by 1, since that field uses a `sum` strategy
-        numReadings: 1,
-        // Will add this reading's value to `totalC` since that field also uses a `sum` strategy
-        totalC: source.tempC,
-        // Since this field has a `maximize` strategy, `lastReading` will only be modified if
-        // `source.timestamp` is more recent than the current value.
-        lastReading: source.timestamp,
-
-        // The minimize and maximize reduction strategies will take care of determining the final
-        // values by comparing the values in the register against these ones.
-        minTempC: source.tempC,
-        maxTempC: source.tempC,
-      }]
-    }
-
-    // For each reading, publish an update to the average temperature for the location.
-    readingsPublish(
-        source: collections.TemperatureReadings,
-        register: registers.TemperatureAverageByLocation,
-        _previous: registers.TemperatureAverageByLocation,
-    ): collections.TemperatureAverageByLocation[] {
-      return [publishAvgTemp(source.sensorId, register)]
-    }
-
-    // sensorsUpdate updates the register state for a given sensor
-    sensorsUpdate(
+    avgTempLocationSensorsUpdate(
         source: collections.TemperatureSensors,
     ): registers.TemperatureAverageByLocation[] {
-      // Updates the register with the location name, but does not add to the number of readings
-      return [{ locationName: source.locationName }]
+        // Update the register when a new location arrives.
+        return [{ locationName: source.locationName }];
     }
-
-    // sensorPublish may publish a document when the sensor is updated. This will allow updates to
-    // sensor locationNames to be plumbed through.
-    sensorsPublish(
+    avgTempLocationSensorsPublish(
         source: collections.TemperatureSensors,
         register: registers.TemperatureAverageByLocation,
         previous: registers.TemperatureAverageByLocation,
     ): collections.TemperatureAverageByLocation[] {
-      // Only publish a document if the locationName has changed AND there's been prior readings from
-      // this sensor. This is just an optimization, since we know that locationName is the only field
-      // we're populating from the sensor record. You could remove this conditional and always
-      // publish a new document, and you'd get exactly the same end result (just with an additional
-      // document in your cloud storage bucket).
-      if (register.locationName === previous.locationName || !register.lastReading) {
-        return []
-      } else {
-        return [publishAvgTemp(source.id, register)]
-      }
+        // If we have a reading for a new location, update the collection.  Else, don't update it
+        // but keep it around in the register for when a reading arrives.
+        if (register.numReadings) {
+            var avg = Math.round(register.totalC! / register.numReadings! * 100) / 100.0;
+            return [{
+                sensorId: source.id,
+                numReadings: register.numReadings,
+                avgC: avg,
+                totalC: register.totalC,
+                minTempC: register.minTempC,
+                maxTempC: register.maxTempC,
+                lastReading: register.lastReading,
+                locationName: source.locationName
+            }];
+        } else {
+            return []
+        }
+    }
+    avgTempLocationTempsUpdate(
+        source: collections.TemperatureAverageTemps,
+    ): registers.TemperatureAverageByLocation[] {
+        // Update the register with stats when a new reading comes in.  This can be used later
+        // if a location arrives in for this sensor to ensure a fully reactive join.
+        return [{
+            numReadings: source.numReadings,
+            totalC: source.totalC,
+            minTempC: source.minTempC,
+            maxTempC: source.maxTempC,
+            lastReading: source.lastReading,
+        }];
+    }
+    avgTempLocationTempsPublish(
+        source: collections.TemperatureAverageTemps,
+        register: registers.TemperatureAverageByLocation,
+        previous: registers.TemperatureAverageByLocation,
+    ): collections.TemperatureAverageByLocation[] {
+        var avg = Math.round(source.totalC! / source.numReadings! * 100) / 100.0;
+        // Always update the collection when a new reading comes in.
+        return [{
+            sensorId: source.sensorId,
+            numReadings: source.numReadings,
+            avgC: avg,
+            totalC: source.totalC,
+            minTempC: source.minTempC,
+            maxTempC: source.maxTempC,
+            lastReading: source.lastReading,
+            locationName: register.locationName
+        }];
     }
 }
 
-// This function is called from both `readingsPublish` and `sensorsPublish` to produce the joined
-// and aggregated result document.
-function publishAvgTemp(sensorId: number, register: registers.TemperatureAverageByLocation):
-  collections.TemperatureAverageByLocation {
-
-  // Calculate the average, rounding to 2 decimal places
-  // The ! at the ends of expressions here are a non-null assertions, because the register schema
-  // does not require these properties to be present. We know these to be non-null because this
-  // function is only called if we've seen at least one reading. An alternative would be to provide
-  // default values for the `initial` register value.
-  var avg = Math.round(register.totalC! / register.numReadings! * 100) / 100.0
-  return {
-    sensorId: sensorId,
-    locationName: register.locationName || null,
-    averageTempC: avg,
-    minTempC: register.minTempC!,
-    maxTempC: register.maxTempC!,
-    lastReading: register.lastReading!,
-  }
+// Implementation for derivation examples/temp-sensors/flow.yaml#/collections/temperature~1averageTemps/derivation.
+export class TemperatureAverageTemps implements interfaces.TemperatureAverageTemps {
+    averageTempsPublish(
+        source: collections.TemperatureReadings,
+        register: registers.TemperatureAverageTemps,
+        previous: registers.TemperatureAverageTemps,
+    ): collections.TemperatureAverageTemps[] {
+        // This will execute on every reading, so pass in 1 reading for this document.  Reduction annotations
+        // will handle ensuring temps and other fields are correctly minimized, maximized, or summed.
+        return [{
+            sensorId: source.sensorId,
+            numReadings: 1,
+            totalC: source.tempC,
+            minTempC: source.tempC,
+            maxTempC: source.tempC,
+            lastReading: source.timestamp
+        }]
+    }
 }
