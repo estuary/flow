b614ce1c60673ac5f7bdf4482f3d88dc3244cf5e
Johnny Graettinger <johnny@estuary.dev>
2021-04-07 09:15:29 -0400

    crates/derive: re-configuration support & schema validation
    DeriveAPI and CombineAPI now support dynamic re-configuration at
    transaction boundaries. This is accomplished by first refactoring out
    ownership of catalog entities (like a schema index or URL) from the
    long-lived Register type, and instead passing these parameters in with
    each call.
    
    Combiner is similarly refactored for parity, though that factoring isn't as
    strictly necessary today, since Combiner is currently re-initialized with each
    re-configuration. It may not be in the future, though, if Combiner is extended
    to support spilling to disk, etc.
    
    The CombineAPI and DeriveAPI then respond to re-configurations by
    rebuilding all per-configuration state, and retaining
    cross-configuration states (currently just Registers).
    
    ExtractAPI is updated to perform schema validation if requested, and
    DeriveAPI removes support for this.

--- crates/derive/src/pipeline/mod.rs
+++ crates/derive/src/pipeline/mod.rs
@@ -9,21 +9,18 @@ use futures::{stream::FuturesOrdered, StreamExt};
 use invocation::Invocation;
 use itertools::Itertools;
 use protocol::{
-    cgo,
+    cgo, consumer,
     flow::{self, derive_api},
     message_flags,
 };
 use serde_json::Value;
-use std::marker::PhantomData;
 use std::task::{Context, Poll};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    Json(#[from] serde_json::Error),
-    #[error(transparent)]
     SchemaIndex(#[from] json::schema::index::Error),
-    #[error("register reduction error")]
+    #[error("register error")]
     RegisterErr(#[from] registers::Error),
     #[error("lambda returned fewer rows than expected")]
     TooFewRows,
@@ -33,25 +30,37 @@ pub enum Error {
     Combiner(#[from] combiner::Error),
     #[error("failed to open registers RocksDB")]
     Rocks(#[from] rocksdb::Error),
-    #[error(transparent)]
-    Url(#[from] url::ParseError),
+    #[error("invalid collection schema {:?}", .schema)]
+    CollectionSchema {
+        schema: String,
+        #[source]
+        source: url::ParseError,
+    },
+    #[error("invalid register schema {:?}", .schema)]
+    RegisterSchema {
+        schema: String,
+        #[source]
+        source: url::ParseError,
+    },
+    #[error("invalid register initial JSON")]
+    RegisterJson(#[source] serde_json::Error),
     #[error("failed to invoke update lambda")]
     UpdateInvocationError(#[source] anyhow::Error),
     #[error("failed to invoke publish lambda")]
     PublishInvocationError(#[source] anyhow::Error),
     #[error("failed to parse lambda invocation response")]
     LambdaParseError(#[source] serde_json::Error),
-
-    // TODO remove me
-    #[error("source document validation error: {0:#}")]
-    SourceValidation(doc::FailedValidation),
 }
 
-pub struct Pipeline<'e> {
+pub struct Pipeline {
     // Collection being derived.
     collection: flow::CollectionSpec,
     // Transforms of the derivation.
     transforms: Vec<flow::TransformSpec>,
+    // Schema against which registers must validate.
+    registers_schema: url::Url,
+    // Initial value of registers which have not yet been written.
+    registers_initial: Value,
     // Models of update & publish Invocations for each transform. These are kept
     // pristine, and cloned to produce working instances given to new Blocks.
     updates_model: Vec<Invocation>,
@@ -67,44 +76,29 @@ pub struct Pipeline<'e> {
     // Registers updated by "update" lambdas.
     // Pipeline owns Registers, but it's pragmatically public due to
     // unrelated usages in derive_api (clearing registers & checkpoints).
-    pub registers: registers::Registers,
+    registers: registers::Registers,
     // Combiner of derived documents.
     combiner: combiner::Combiner,
     // Partitions to extract when draining the Combiner.
     partitions: Vec<doc::Pointer>,
-    // This instance cannot outlive the RocksDB environment it uses.
-    _env: PhantomData<&'e rocksdb::Env>,
-
-    // TODO(johnny): remove me to the extraction API.
+    // Validator for register validations.
     validator: doc::Validator<'static>,
-    source_schemas: Vec<url::Url>,
 }
 
-impl<'e> Pipeline<'e> {
-    // Build a pipeline from a Config.
-    pub fn from_config(cfg: derive_api::Config) -> Result<Self, Error> {
-        // Re-hydrate a &rocksdb::Env from a provided memory address.
-        let env_ptr = cfg.rocksdb_env_memptr as usize;
-        let env: &rocksdb::Env = unsafe { std::mem::transmute(&env_ptr) };
-
-        // Re-hydrate a &'static SchemaIndex from a provided memory address.
-        let schema_index_memptr = cfg.schema_index_memptr as usize;
-        let schema_index: &doc::SchemaIndex = unsafe { std::mem::transmute(schema_index_memptr) };
-
-        Self::from_config_and_parts(cfg, env, schema_index)
-    }
-
-    fn from_config_and_parts(
+impl Pipeline {
+    pub fn from_config_and_parts(
         cfg: derive_api::Config,
-        env: &'e rocksdb::Env,
-        schema_index: &'static doc::SchemaIndex,
+        registers: registers::Registers,
+        block_id: usize,
     ) -> Result<Self, Error> {
         let derive_api::Config {
             derivation,
-            local_dir,
-            rocksdb_env_memptr,
             schema_index_memptr,
         } = cfg;
+
+        // Re-hydrate a &'static SchemaIndex from a provided memory address.
+        let schema_index_memptr = schema_index_memptr as usize;
+        let schema_index: &doc::SchemaIndex = unsafe { std::mem::transmute(schema_index_memptr) };
 
         let flow::DerivationSpec {
             collection,
@@ -117,10 +111,8 @@ impl<'e> Pipeline<'e> {
 
         tracing::debug!(
             ?collection,
-            ?local_dir,
             ?register_initial_json,
             ?register_schema_uri,
-            ?rocksdb_env_memptr,
             ?schema_index_memptr,
             ?transforms,
             "building from config"
@@ -137,35 +129,16 @@ impl<'e> Pipeline<'e> {
             })
             .unzip();
 
-        let first_block = Block::new(1, &updates_model, &publishes_model);
-
-        // Open RocksDB and build Registers.
-        let mut rocks_opts = rocksdb::Options::default();
-        rocks_opts.create_if_missing(true);
-        rocks_opts.create_missing_column_families(true);
-        rocks_opts.set_env(&env);
-
-        let rocks = rocksdb::DB::open_cf(
-            &rocks_opts,
-            local_dir,
-            [
-                rocksdb::DEFAULT_COLUMN_FAMILY_NAME,
-                super::registers::REGISTERS_CF,
-            ]
-            .iter(),
-        )?;
-
-        let registers = super::registers::Registers::new(
-            rocks,
-            schema_index,
-            &url::Url::parse(&register_schema_uri)?,
-            serde_json::from_str(&register_initial_json)?,
-        );
+        let first_block = Block::new(block_id, &updates_model, &publishes_model);
 
         // Build Combiner.
+        let collection_schema =
+            url::Url::parse(&collection.schema_uri).map_err(|source| Error::CollectionSchema {
+                schema: collection.schema_uri.clone(),
+                source,
+            })?;
         let combiner = combiner::Combiner::new(
-            schema_index,
-            &url::Url::parse(&collection.schema_uri)?,
+            collection_schema,
             collection
                 .key_ptrs
                 .iter()
@@ -189,23 +162,20 @@ impl<'e> Pipeline<'e> {
             })
             .collect();
 
-        // TODO remove me to extraction API.
+        let registers_schema =
+            url::Url::parse(&register_schema_uri).map_err(|source| Error::RegisterSchema {
+                schema: register_schema_uri.clone(),
+                source,
+            })?;
+        let registers_initial =
+            serde_json::from_str(&register_initial_json).map_err(Error::RegisterJson)?;
         let validator = doc::Validator::new(schema_index);
-        let source_schemas = transforms
-            .iter()
-            .map(|tf| {
-                url::Url::parse(
-                    &tf.shuffle
-                        .as_ref()
-                        .map(|s| s.source_schema_uri.clone())
-                        .unwrap_or_default(),
-                )
-            })
-            .collect::<Result<Vec<_>, _>>()?;
 
         Ok(Self {
             collection,
             transforms,
+            registers_initial,
+            registers_schema,
             updates_model,
             publishes_model,
             next: first_block,
@@ -215,10 +185,44 @@ impl<'e> Pipeline<'e> {
             registers,
             combiner,
             partitions,
-            _env: PhantomData,
             validator,
-            source_schemas,
         })
+    }
+
+    // Consume this Pipeline, returning its Registers and next Block ID.
+    // This may only be called in between transactions, after draining.
+    pub fn into_inner(self) -> (registers::Registers, usize) {
+        assert_eq!(self.next.num_bytes, 0);
+        assert!(self.trampoline.is_empty());
+
+        (self.registers, self.next.id)
+    }
+
+    // Delegate to load the last persisted checkpoint.
+    pub fn last_checkpoint(&self) -> Result<consumer::Checkpoint, registers::Error> {
+        assert_eq!(self.next.num_bytes, 0);
+        assert!(self.trampoline.is_empty());
+
+        self.registers.last_checkpoint()
+    }
+
+    // Delegate to clear held registers.
+    pub fn clear_registers(&mut self) -> Result<(), registers::Error> {
+        assert_eq!(self.next.num_bytes, 0);
+        assert!(self.trampoline.is_empty());
+
+        self.registers.clear()
+    }
+
+    // Delegate to prepare the checkpoint for commit.
+    pub fn prepare(
+        &mut self,
+        checkpoint: protocol::consumer::Checkpoint,
+    ) -> Result<(), registers::Error> {
+        assert_eq!(self.next.num_bytes, 0);
+        assert!(self.trampoline.is_empty());
+
+        self.registers.prepare(checkpoint)
     }
 
     // Resolve a trampoline task.
@@ -229,8 +233,9 @@ impl<'e> Pipeline<'e> {
     // Add a source document to the Pipeline, and return true if it caused the
     // current Block to flush (and false otherwise).
     //
-    // TODO(johnny): this only errors due to document validations, which are
-    // being moved out of the derive API. Switch to -> bool when able.
+    // Currently this never returns an error, but it may in the future if an
+    // invocation performed deeper processing of the input |body|
+    // (e.x. by deserializing into Deno V8 types).
     pub fn add_source_document(
         &mut self,
         header: derive_api::DocHeader,
@@ -246,15 +251,6 @@ impl<'e> Pipeline<'e> {
         let flags = uuid.producer_and_flags & message_flags::MASK;
 
         if flags != message_flags::ACK_TXN {
-            // TODO(johnny): Move source schema validation to read-time, avoiding this parsing.
-            doc::Validation::validate(
-                &mut self.validator,
-                &self.source_schemas[transform_index as usize],
-                serde_json::from_slice(body)?,
-            )?
-            .ok()
-            .map_err(Error::SourceValidation)?;
-
             self.next
                 .add_source(transform_index as usize, packed_key, body);
         }
@@ -396,14 +392,19 @@ impl<'e> Pipeline<'e> {
             };
 
             let publish = &mut block.publishes[*tf_ind as usize];
-            publish.begin_register(self.registers.read(key));
+            publish.begin_register(self.registers.read(key, &self.registers_initial));
 
             // If we have deltas to apply, reduce them and assemble into
             // a future publish invocation body.
             if !deltas.is_empty() {
-                self.registers
-                    .reduce(key, deltas.into_iter(), tf.rollback_on_register_conflict)?;
-                publish.end_register(Some(self.registers.read(key)));
+                self.registers.reduce(
+                    key,
+                    &self.registers_schema,
+                    &self.registers_initial,
+                    deltas.into_iter(),
+                    &mut self.validator,
+                )?;
+                publish.end_register(Some(self.registers.read(key, &self.registers_initial)));
             } else {
                 publish.end_register(None);
             }
@@ -448,7 +449,7 @@ impl<'e> Pipeline<'e> {
             };
 
             for doc in derived_docs {
-                self.combiner.combine_right(doc)?;
+                self.combiner.combine_right(doc, &mut self.validator)?;
             }
         }
         tracing::trace!(combiner = ?self.combiner, "combined documents");
