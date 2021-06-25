7132c5fb3f5ff797d1859517d882c37fa8c87e68
Johnny Graettinger <johnny@estuary.dev>
2021-06-25 09:59:47 -0400

    go/capture: update stubs for bindings change

--- go/capture/driver/airbyte/driver.go
+++ go/capture/driver/airbyte/driver.go
@@ -2,10 +2,42 @@ package airbyte
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	pc "github.com/estuary/flow/go/protocols/capture"
+	pf "github.com/estuary/flow/go/protocols/flow"
 )
+
+// sourceConfig is the configuration for Airbyte source connectors.
+// It must match the one defined for the source specs (flow.yaml) in Rust.
+type sourceConfig struct {
+	Image  string
+	Config json.RawMessage
+}
+
+// Validate the configuration.
+func (c sourceConfig) Validate() error {
+	if c.Image == "" {
+		return fmt.Errorf("expected `image`")
+	}
+	return nil
+}
+
+// streamConfig is the configuration for Airbyte source streams.
+type streamConfig struct {
+	Stream    string
+	Namespace string
+}
+
+// Validate the configuration.
+func (c streamConfig) Validate() error {
+	if c.Stream == "" {
+		return fmt.Errorf("expected `stream`")
+	}
+	// Namespace is optional.
+	return nil
+}
 
 // driver implements the pm.DriverServer interface.
 type driver struct{}
@@ -22,9 +54,26 @@ func (driver) Discover(ctx context.Context, req *pc.DiscoverRequest) (*pc.Discov
 }
 
 func (driver) Validate(ctx context.Context, req *pc.ValidateRequest) (*pc.ValidateResponse, error) {
-	return &pc.ValidateResponse{
-		ResourcePath: []string{"placeholder"},
-	}, nil
+	if err := req.Validate(); err != nil {
+		return nil, fmt.Errorf("validating request: %w", err)
+	}
+
+	var source = new(sourceConfig)
+	if err := pf.UnmarshalStrict(req.EndpointSpecJson, source); err != nil {
+		return nil, fmt.Errorf("parsing connector configuration: %w", err)
+	}
+
+	var resp = new(pc.ValidateResponse)
+	for _, binding := range req.Bindings {
+		var stream = new(streamConfig)
+		if err := pf.UnmarshalStrict(binding.ResourceSpecJson, stream); err != nil {
+			return nil, fmt.Errorf("parsing stream configuration: %w", err)
+		}
+		resp.Bindings = append(resp.Bindings, &pc.ValidateResponse_Binding{
+			ResourcePath: []string{stream.Stream},
+		})
+	}
+	return resp, nil
 }
 
 func (driver) Capture(*pc.CaptureRequest, pc.Driver_CaptureServer) error {
