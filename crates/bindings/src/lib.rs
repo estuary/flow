fc6765805ea827402cb1a492fcdb88a0a26c8f2c
Johnny Graettinger <johnny531@gmail.com>
2021-09-13 20:47:55 +0000

    bindings: RUST_LOG goes to stderr rather than stdout

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -14,6 +14,7 @@ pub fn setup_env_tracing() {
     SUBSCRIBE.call_once(|| {
         let subscriber = tracing_subscriber::FmtSubscriber::builder()
             .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_writer(std::io::stderr)
             .finish();
         tracing::subscriber::set_global_default(subscriber).unwrap();
     });
