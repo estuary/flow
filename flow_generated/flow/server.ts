2e9623cb892841c333f1143c419131f9a18419c6
Johnny Graettinger <johnny531@gmail.com>
2021-09-14 14:18:48 +0000

    node worker: switch to HTTP/1 over unix domain sockets
    We're seeing lockups over the H/2 UDS transport, where Go is
    attempting to both write and read to the file descriptor
    (according to /debug/pprof/goroutines) but is making no progress.
    
    It appears that NodeJS is hitting a condition where it fails to
    service the descriptor after running a while at load?
    
    Issue #226

--- flow_generated/flow/server.ts
+++ flow_generated/flow/server.ts
@@ -1,4 +1,4 @@
-import * as h2 from 'http2';
+import * as http from 'http';
 
 import { routes, Document, Lambda } from './routes';
 
@@ -13,24 +13,21 @@ export class Server {
     }
 
     start(): void {
-        const server = h2.createServer();
-        server.on('stream', this._processStream.bind(this));
+        const server = http.createServer(this._processStream.bind(this));
         server.on('error', console.error);
         server.listen({ path: this.listenPath });
     }
 
-    _processStream(req: h2.ServerHttp2Stream, headers: h2.IncomingHttpHeaders): void {
+    _processStream(req: http.IncomingMessage, resp: http.ServerResponse): void {
         const malformed = (msg: string): void => {
-            req.respond({
-                ':status': 400,
-                'content-type': 'text/plain',
-            });
-            req.end(msg + '\n'); // Send message & EOF.
+            resp.setHeader('content-type', 'text/plain');
+            resp.writeHead(400);
+            resp.end(msg + '\n'); // Send message & EOF.
         };
 
-        const path = headers[':path'];
+        const path = req.url;
         if (path === undefined) {
-            return malformed('expected :path header');
+            return malformed('expected url');
         }
 
         const lambda: Lambda | undefined = routes[path];
@@ -64,21 +61,16 @@ export class Server {
             Promise.all(futures)
                 .then((rows: Document[][]) => {
                     const body = Buffer.from(JSON.stringify(rows), 'utf8');
-
-                    req.respond({
-                        ':status': 200,
-                        'content-type': 'application/json',
-                        'content-length': body.length,
-                    });
-                    req.end(body);
+                    resp.setHeader('Content-Length', body.length);
+                    resp.setHeader('Content-Type', 'application/json');
+                    resp.writeHead(200);
+                    resp.end(body);
                 })
                 .catch((err: Error) => {
                     // Send |err| to peer, and log to console.
-                    req.respond({
-                        ':status': 400,
-                        'content-type': 'text/plain',
-                    });
-                    req.end(`${err.name}: (${err.message})\n`);
+                    resp.setHeader('content-type', 'text/plain');
+                    resp.writeHead(400);
+                    resp.end(`${err.name}: (${err.message})\n`);
                     console.error(err);
                 });
         });
