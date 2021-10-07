a4f9bdffe1838ddeacac6907d04cf18b5de17506
Johnny <johnny@estuary.dev>
2021-10-07 22:43:46 +0000

    go/labels: refactor and fix partition encoding
    Remove `estuary.dev/logs-collection` and `estuary.dev/stats-collection`
    labels. We'll compute these dynamically from task names.
    
    Consolidate / refactor routines for working with label-encodings of
    logical partition values.
    
    Fix partition values to use query encoding rather than path encoding.
    Path encoding produces outputs which are invalid for label values.
    Add new tests on partition encoding behaviors and edge cases.
    
    Rework routines for naming journals & shards to instead focus on
    suffixes of journals/shards, which are intended to be post-fixed onto a
    template, and to be pure functions over current LabelSets.
    
    Issue #250

--- go/labels/labels_test.go
+++ go/labels/labels_test.go
@@ -3,139 +3,38 @@ package labels
 import (
 	"testing"
 
-	pf "github.com/estuary/protocols/flow"
 	"github.com/stretchr/testify/require"
-	pb "go.gazette.dev/core/broker/protocol"
+	"go.gazette.dev/core/labels"
 )
 
-func TestRangeSpecParsingCases(t *testing.T) {
-	// Case: success.
-	require.Equal(t, pf.RangeSpec{
-		KeyBegin:    0xccddeeff,
-		KeyEnd:      0xff00aabb,
-		RClockBegin: 1 << 31,
-		RClockEnd:   (1 << 32) - 1,
-	}, MustParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, "ccddeeff",
-		KeyEnd, "ff00aabb",
-		RClockBegin, "80000000",
-		RClockEnd, "ffffffff")))
-
-	// Case: key is malformed hex.
-	var _, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, "000000zz",
-		KeyEnd, KeyEndMax,
-		RClockBegin, RClockBeginMin,
-		RClockEnd, RClockEndMax))
-	require.Contains(t, err.Error(), "decoding hex-encoded label "+KeyBegin+": strconv.ParseUint: parsing \"000000zz\": invalid syntax")
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyBeginMin,
-		KeyEnd, "000000zz",
-		RClockBegin, RClockBeginMin,
-		RClockEnd, RClockEndMax))
-	require.Contains(t, err.Error(), "decoding hex-encoded label "+KeyEnd+": strconv.ParseUint: parsing \"000000zz\": invalid syntax")
-
-	// Case: clock is malformed hex.
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyBeginMin,
-		KeyEnd, KeyEndMax,
-		RClockBegin, "000000zz",
-		RClockEnd, RClockEndMax))
-	require.Contains(t, err.Error(), "decoding hex-encoded label "+RClockBegin+": strconv.ParseUint: parsing \"000000zz\": invalid syntax")
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyBeginMin,
-		KeyEnd, KeyEndMax,
-		RClockBegin, RClockBeginMin,
-		RClockEnd, "000000zz"))
-	require.Contains(t, err.Error(), "decoding hex-encoded label "+RClockEnd+": strconv.ParseUint: parsing \"000000zz\": invalid syntax")
-
-	// Case: clock is wrong length.
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyBeginMin,
-		KeyEnd, KeyEndMax,
-		RClockBegin, RClockBeginMin+"00",
-		RClockEnd, RClockEndMax))
-	require.Contains(t, err.Error(), "expected "+RClockBegin+" to be a 4-byte, hex encoded integer; got 0000000000")
-
-	// Case: missing labels.
-	_, err = ParseRangeSpec(pb.LabelSet{})
-	require.EqualError(t, err, "missing required label: "+KeyBegin)
-
-	// Case: parses okay, but invalid range.
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyEndMax,
-		KeyEnd, KeyBeginMin,
-		RClockBegin, RClockBeginMin,
-		RClockEnd, RClockEndMax))
-	require.EqualError(t, err, "expected KeyBegin <= KeyEnd (ffffffff vs 00000000)")
-	_, err = ParseRangeSpec(pb.MustLabelSet(
-		KeyBegin, KeyBeginMin,
-		KeyEnd, KeyEndMax,
-		RClockBegin, RClockEndMax,
-		RClockEnd, RClockBeginMin))
-	require.EqualError(t, err, "expected RClockBegin <= RClockEnd (ffffffff vs 00000000)")
-}
-
-func TestRoundTripRangeSpecToLabels(t *testing.T) {
-	var range_ = pf.RangeSpec{
-		KeyBegin:    0xccddeeff,
-		KeyEnd:      0xff00aabb,
-		RClockBegin: 0xaabbaabb,
-		RClockEnd:   0xddeeddee,
+func TestRuntimeLabels(t *testing.T) {
+	var cases = []struct {
+		label  string
+		expect bool
+	}{
+		{"other", false},
+		{Collection, false},
+		{FieldPrefix + "One", true},
+		{FieldPrefix + "two", true},
+		{KeyBegin, true},
+		{KeyEnd, true},
+		{LogLevel, false},
+		{RClockBegin, true},
+		{RClockEnd, true},
+		{SplitSource, true},
+		{SplitTarget, true},
+		{TaskCreated, true},
+		{TaskName, false},
+		{TaskType, false},
+		{labels.ContentType, false},
+		{labels.Instance, false},
+		{labels.ManagedBy, false},
+		{labels.MessageSubType, false},
+		{labels.MessageType, false},
+		{labels.Region, false},
+		{labels.Tag, false},
 	}
-	var labels = EncodeRange(range_, pb.MustLabelSet("other", "label"))
-
-	require.Equal(t, pb.MustLabelSet(
-		KeyBegin, "ccddeeff",
-		KeyEnd, "ff00aabb",
-		RClockBegin, "aabbaabb",
-		RClockEnd, "ddeeddee",
-		"other", "label",
-	), labels)
-
-	var recovered, err = ParseRangeSpec(labels)
-	require.NoError(t, err)
-
-	require.Equal(t, range_, recovered)
-}
-
-func TestShardIDs(t *testing.T) {
-	var labels = pb.MustLabelSet(
-		TaskName, "some/thing",
-		TaskType, TaskTypeMaterialization,
-	)
-	labels = EncodeRange(pf.RangeSpec{
-		KeyBegin:    0,
-		KeyEnd:      123456,
-		RClockBegin: 0,
-		RClockEnd:   78910,
-	}, labels)
-
-	var id, err = BuildShardID(labels)
-	require.NoError(t, err)
-	require.Equal(t, "materialize/some/thing/00000000-00000000", id.String())
-
-	labels = pb.MustLabelSet(
-		TaskName, "other/thing",
-		TaskType, TaskTypeDerivation,
-	)
-	labels = EncodeRange(pf.RangeSpec{
-		KeyBegin:    0x123,
-		KeyEnd:      0x456,
-		RClockBegin: 0x789,
-		RClockEnd:   0x1011,
-	}, labels)
-
-	id, err = BuildShardID(labels)
-	require.NoError(t, err)
-	require.Equal(t, "derivation/other/thing/00000123-00000789", id.String())
-
-	// Error cases:
-	labels.SetValue(TaskType, "other")
-	_, err = BuildShardID(labels)
-	require.EqualError(t, err, "unexpected estuary.dev/task-type: other")
-
-	labels.Remove(KeyBegin)
-	_, err = BuildShardID(labels)
-	require.EqualError(t, err, "expected one estuary.dev/key-begin: []")
+	for _, c := range cases {
+		require.Equal(t, IsRuntimeLabel(c.label), c.expect)
+	}
 }
---
+++ go/labels/partitions.go
@@ -0,0 +1,121 @@
+package labels
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/estuary/protocols/fdb/tuple"
+	pf "github.com/estuary/protocols/flow"
+)
+
+// EncodePartitionValue appends an encoding of |value| into the
+// []byte slice, returning the result. Encoded values are suitable
+// for embedding within Journal names as well as label values.
+func EncodePartitionValue(b []byte, value tuple.TupleElement) []byte {
+	switch v := value.(type) {
+	case nil:
+		return append(b, "null"...)
+	case bool:
+		if v {
+			return append(b, "true"...)
+		}
+		return append(b, "false"...)
+	case uint64:
+		return strconv.AppendUint(b, v, 10)
+	case int64:
+		return strconv.AppendInt(b, v, 10)
+	case int:
+		return strconv.AppendInt(b, int64(v), 10)
+	case string:
+		// Label values have a pretty restrictive set of allowed non-letter
+		// or digit characters. Use URL query escapes to encode an arbitrary
+		// string value into a label-safe (and name-safe) representation.
+		return append(b, url.QueryEscape(v)...)
+	default:
+		panic(fmt.Sprintf("invalid element type: %#v", value))
+	}
+}
+
+// EncodePartitionLabels adds encoded |fields| and corresponding |values|
+// into the given LabelSet. |fields| must be in sorted order and have the
+// same length as |values|, or EncodePartitionLabels panics.
+func EncodePartitionLabels(fields []string, values tuple.Tuple, set pf.LabelSet) pf.LabelSet {
+	if len(fields) != len(values) {
+		panic("fields and values have different lengths")
+	}
+	for i := range fields {
+		if i > 0 && fields[i] <= fields[i-1] {
+			panic("fields are not in sorted order")
+		}
+		set.AddValue(
+			FieldPrefix+fields[i], // |fields| are already restricted to label-safe values.
+			string(EncodePartitionValue(nil, values[i])),
+		)
+	}
+	return set
+}
+
+// PartitionSuffix returns the Journal name suffix that's implied by the LabelSet.
+// This suffix is appended to the collection name to form a complete journal name.
+func PartitionSuffix(set pf.LabelSet) (string, error) {
+	var name strings.Builder
+
+	// We're relying on the fact that labels are always in lexicographic order.
+	for _, l := range set.Labels {
+		if !strings.HasPrefix(l.Name, FieldPrefix) {
+			continue
+		}
+
+		name.WriteString(l.Name[len(FieldPrefix):]) // Field without label prefix.
+		name.WriteByte('=')
+		name.WriteString(l.Value)
+		name.WriteByte('/')
+	}
+	name.WriteString("pivot=")
+
+	// Pluck singleton label value we expect to exist.
+	var keyBegin, err = valueOf(set, KeyBegin)
+	if err != nil {
+		return "", err
+	}
+
+	// As a prettified special case, and for historical reasons, we represent the
+	// KeyBeginMin value of "00000000" as just "00". This is safe because "00"
+	// will naturally order before all other splits, as "00000000" would.
+	// All other key splits are their normal EncodeHexU32Label encodings.
+	if keyBegin == KeyBeginMin {
+		name.WriteString("00")
+	} else {
+		name.WriteString(keyBegin)
+	}
+
+	return name.String(), nil
+}
+
+// ShardSuffix is the suffix of a ShardID that's implied by the LabelSet.
+// This suffix is appended to the tasks's base name to form a complete ShardID.
+func ShardSuffix(set pf.LabelSet) (string, error) {
+	// Pluck singleton label values we expect to exist.
+	var (
+		keyBegin, err1    = valueOf(set, KeyBegin)
+		rclockBegin, err2 = valueOf(set, RClockBegin)
+	)
+	for _, err := range []error{err1, err2} {
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return fmt.Sprintf("%s-%s", keyBegin, rclockBegin), nil
+}
+
+func valueOf(set pf.LabelSet, name string) (string, error) {
+	var v = set.ValuesOf(name)
+	if len(v) != 1 {
+		return "", fmt.Errorf("expected one %s: %v", name, v)
+	} else {
+		return v[0], nil
+	}
+}
