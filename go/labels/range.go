5337cf19ccf9d742397dc2c557e9d3397c074e1a
Johnny <johnny@estuary.dev>
2021-10-07 22:43:46 +0000

    go/labels: refactor and fix partition encoding
    Remove `estuary.dev/logs-collection` and `estuary.dev/stats-collection`
    labels. We'll compute these dynamically from task names.
    
    Consolidate / refactor routines for working with label-encodings of
    logical partition values.
    
    Fix partition values to use query encoding rather than path encoding.
    Path encoding produces outputs which are invalid for label values.
    Add new tests on partition encoding behaviors and edge cases.
    
    Rework routines for naming journals & shards to instead focus on
    suffixes of journals/shards, which are intended to be post-fixed onto a
    template, and to be pure functions over current LabelSets.
    
    Issue #250

---
+++ go/labels/range.go
@@ -0,0 +1,69 @@
+package labels
+
+import (
+	"fmt"
+	"strconv"
+
+	pf "github.com/estuary/protocols/flow"
+)
+
+// EncodeRange encodes the RangeSpec into the given LabelSet,
+// which is then returned.
+func EncodeRange(range_ pf.RangeSpec, set pf.LabelSet) pf.LabelSet {
+	set = EncodeHexU32Label(KeyBegin, range_.KeyBegin, set)
+	set = EncodeHexU32Label(KeyEnd, range_.KeyEnd, set)
+	set = EncodeHexU32Label(RClockBegin, range_.RClockBegin, set)
+	set = EncodeHexU32Label(RClockEnd, range_.RClockEnd, set)
+	return set
+}
+
+// ParseRangeSpec extracts a RangeSpec from its associated labels.
+func ParseRangeSpec(set pf.LabelSet) (pf.RangeSpec, error) {
+	if kb, err := ParseHexU32Label(KeyBegin, set); err != nil {
+		return pf.RangeSpec{}, err
+	} else if ke, err := ParseHexU32Label(KeyEnd, set); err != nil {
+		return pf.RangeSpec{}, err
+	} else if cb, err := ParseHexU32Label(RClockBegin, set); err != nil {
+		return pf.RangeSpec{}, err
+	} else if ce, err := ParseHexU32Label(RClockEnd, set); err != nil {
+		return pf.RangeSpec{}, err
+	} else {
+		var out = pf.RangeSpec{
+			KeyBegin:    kb,
+			KeyEnd:      ke,
+			RClockBegin: cb,
+			RClockEnd:   ce,
+		}
+		return out, out.Validate()
+	}
+}
+
+// MustParseRangeSpec parses a RangeSpec from the labels, and panics on an error.
+func MustParseRangeSpec(set pf.LabelSet) pf.RangeSpec {
+	if s, err := ParseRangeSpec(set); err != nil {
+		panic(err)
+	} else {
+		return s
+	}
+}
+
+// EncodeHexU32Label encodes label |name| as a hex-encoded uint32
+// |value| into the provided |LabelSet|, which is returned.
+func EncodeHexU32Label(name string, value uint32, set pf.LabelSet) pf.LabelSet {
+	set.SetValue(name, fmt.Sprintf("%08x", value))
+	return set
+}
+
+// ParseHexU32Label parses label |name|, a hex-encoded uint32, from the LabelSet.
+// It returns an error if the label value is malformed.
+func ParseHexU32Label(name string, set pf.LabelSet) (uint32, error) {
+	if l := set.ValuesOf(name); len(l) != 1 {
+		return 0, fmt.Errorf("missing required label: %s", name)
+	} else if len(l[0]) != 8 {
+		return 0, fmt.Errorf("expected %s to be a 4-byte, hex encoded integer; got %v", name, l[0])
+	} else if b, err := strconv.ParseUint(l[0], 16, 32); err != nil {
+		return 0, fmt.Errorf("decoding hex-encoded label %s: %w", name, err)
+	} else {
+		return uint32(b), nil
+	}
+}
