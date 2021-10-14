2cbe4313499a3ac508a4ec988bc156263fc2f0e3
Johnny <johnny@estuary.dev>
2021-10-14 05:12:08 +0000

    crates/models: hoist former `sources` models into `models`
    Move to the crate root.

---
+++ crates/models/src/derivations.rs
@@ -0,0 +1,208 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_value, json, Value};
+use std::time::Duration;
+use tuple_vec_map;
+
+use super::{Collection, Lambda, PartitionSelector, Schema, ShardTemplate, Shuffle, Transform};
+
+/// A derivation specifies how a collection is derived from other
+/// collections. A collection without a derivation is a "captured"
+/// collection, into which documents are directly ingested.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Derivation {
+    /// # Register configuration of this derivation.
+    #[serde(default)]
+    pub register: Register,
+    /// # Transforms which make up this derivation.
+    #[serde(default, with = "tuple_vec_map")]
+    #[schemars(schema_with = "transforms_schema")]
+    pub transform: Vec<(Transform, TransformDef)>,
+    /// # Template for shards of this derivation task.
+    #[serde(default)]
+    pub shards: ShardTemplate,
+}
+
+/// Registers are the internal states of a derivation, which can be read and
+/// updated by all of its transformations. They're an important building
+/// block for joins, aggregations, and other complex stateful workflows.
+///
+/// Registers are implemented using JSON-Schemas, often ones with reduction
+/// annotations. When reading source documents, every distinct shuffle key
+/// by which the source collection is read is mapped to a corresponding
+/// register value (or, if no shuffle key is defined, the source collection's
+/// key is used instead).
+///
+/// Then, an "update" lambda of the transformation produces updates which
+/// are reduced into the register, and a "publish" lambda reads the current
+/// (and previous, if updated) register value.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Register {
+    /// # Schema which validates and reduces register documents.
+    pub schema: Schema,
+    /// # Initial value of a keyed register which has never been updated.
+    /// If not specified, the default is "null".
+    #[serde(default = "value_null")]
+    pub initial: Value,
+}
+
+fn value_null() -> Value {
+    Value::Null
+}
+
+impl Default for Register {
+    fn default() -> Self {
+        Register {
+            schema: Schema::Bool(true),
+            initial: Value::Null,
+        }
+    }
+}
+
+/// A Transform reads and shuffles documents of a source collection,
+/// and processes each document through either one or both of a register
+/// "update" lambda and a derived document "publish" lambda.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[schemars(example = "TransformDef::example")]
+pub struct TransformDef {
+    /// # Source collection read by this transform.
+    pub source: TransformSource,
+    /// # Priority applied to documents processed by this transform.
+    /// When all transforms are of equal priority, Flow processes documents
+    /// according to their associated publishing time, as encoded in the
+    /// document UUID.
+    ///
+    /// However, when one transform has a higher priority than others,
+    /// then *all* ready documents are processed through the transform
+    /// before *any* documents of other transforms are processed.
+    #[serde(default)]
+    pub priority: u32,
+    /// # Delay applied to documents processed by this transform.
+    /// Delays are applied as an adjustment to the UUID clock encoded within each
+    /// document, which is then used to impose a relative ordering of all documents
+    /// read by this derivation. This means that read delays are applied in a
+    /// consistent way, even when back-filling over historical documents. When caught
+    /// up and tailing the source collection, delays also "gate" documents such that
+    /// they aren't processed until the current wall-time reflects the delay.
+    #[serde(default, with = "humantime_serde")]
+    #[schemars(schema_with = "super::duration_schema")]
+    pub read_delay: Option<Duration>,
+    /// # Shuffle by which source documents are mapped to registers.
+    /// If empty, the key of the source collection is used.
+    #[serde(default)]
+    #[schemars(default = "Shuffle::example")]
+    pub shuffle: Option<Shuffle>,
+    /// # Update that maps a source document into register updates.
+    #[serde(default)]
+    #[schemars(default = "Update::example")]
+    pub update: Option<Update>,
+    /// # Publish that maps a source document and registers into derived documents of the collection.
+    #[serde(default)]
+    #[schemars(default = "Publish::example")]
+    pub publish: Option<Publish>,
+}
+
+impl TransformDef {
+    pub fn example() -> Self {
+        from_value(json!({
+            "source": TransformSource::example(),
+            "publish": Publish::example(),
+            "update": null,
+        }))
+        .unwrap()
+    }
+}
+
+/// Update lambdas take a source document and transform it into one or more
+/// register updates, which are then reduced into the associated register by
+/// the runtime. For example these register updates might update counters,
+/// or update the state of a "join" window.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[schemars(example = "Update::example")]
+pub struct Update {
+    /// # Lambda invoked by the update.
+    pub lambda: Lambda,
+}
+
+impl Update {
+    pub fn example() -> Self {
+        from_value(json!({
+            "lambda": Lambda::example_typescript(),
+        }))
+        .unwrap()
+    }
+}
+
+/// Publish lambdas take a source document, a current register and
+/// (if there is also an "update" lambda) a previous register, and transform
+/// them into one or more documents to be published into a derived collection.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[schemars(example = "Publish::example")]
+pub struct Publish {
+    /// # Lambda invoked by the publish.
+    pub lambda: Lambda,
+}
+
+impl Publish {
+    pub fn example() -> Self {
+        from_value(json!({
+            "lambda": Lambda::example_typescript(),
+        }))
+        .unwrap()
+    }
+}
+
+/// TransformSource defines a transformation source collection and how it's read.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[schemars(example = "TransformSource::example")]
+pub struct TransformSource {
+    /// # Name of the collection to be read.
+    pub name: Collection,
+    /// # Optional JSON-Schema to validate against the source collection.
+    /// All data in the source collection is already validated against the
+    /// schema of that collection, so providing a source schema is only used
+    /// for _additional_ validation beyond that.
+    ///
+    /// This is useful in building "Extract Load Transform" patterns,
+    /// where a collection is captured with minimal schema applied (perhaps
+    /// because it comes from an uncontrolled third party), and is then
+    /// progressively verified as collections are derived.
+    /// If None, the principal schema of the collection is used instead.
+    #[serde(default)]
+    #[schemars(default = "Schema::example_relative")]
+    pub schema: Option<Schema>,
+    /// # Selector over partition of the source collection to read.
+    #[serde(default)]
+    #[schemars(default = "PartitionSelector::example")]
+    pub partitions: Option<PartitionSelector>,
+}
+
+impl TransformSource {
+    pub fn example() -> Self {
+        Self {
+            name: Collection::new("source/collection"),
+            schema: None,
+            partitions: None,
+        }
+    }
+}
+
+fn transforms_schema(_: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+    from_value(json!({
+        "type": "object",
+        "patternProperties": {
+            Transform::schema_pattern(): {
+                "$ref": "#/definitions/Transform",
+            },
+        },
+        "additionalProperties": false,
+        "default": [{"nameOfTransform": {"source": {"name": "a/source/collection"}}}],
+    }))
+    .unwrap()
+}
---
+++ crates/models/src/materializations.rs
@@ -0,0 +1,154 @@
+use protocol::flow::EndpointType;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::collections::BTreeMap;
+
+use super::{Collection, Field, Object, PartitionSelector, RelativeUrl, ShardTemplate};
+
+/// A Materialization binds a Flow collection with an external system & target
+/// (e.x, a SQL table) into which the collection is to be continuously materialized.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct MaterializationDef {
+    /// # Endpoint to materialize into.
+    pub endpoint: MaterializationEndpoint,
+    /// # Bound collections to materialize into the endpoint.
+    pub bindings: Vec<MaterializationBinding>,
+    /// # Template for shards of this materialization task.
+    #[serde(default)]
+    pub shards: ShardTemplate,
+}
+
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields)]
+#[schemars(example = "MaterializationBinding::example")]
+pub struct MaterializationBinding {
+    /// # Endpoint resource to materialize into.
+    pub resource: Object,
+    /// # Name of the collection to be materialized.
+    pub source: Collection,
+    /// # Selector over partitions of the source collection to read.
+    #[serde(default)]
+    #[schemars(default = "PartitionSelector::example")]
+    pub partitions: Option<PartitionSelector>,
+    /// # Selected projections for this materialization.
+    #[serde(default)]
+    pub fields: MaterializationFields,
+}
+
+impl MaterializationBinding {
+    pub fn example() -> Self {
+        Self {
+            resource: json!({"table": "a_table"}).as_object().unwrap().clone(),
+            source: Collection::new("source/collection"),
+            partitions: None,
+            fields: MaterializationFields::default(),
+        }
+    }
+}
+
+/// MaterializationFields defines a selection of projections to materialize,
+/// as well as optional per-projection, driver-specific configuration.
+#[derive(Serialize, Deserialize, Debug, JsonSchema, Clone)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[schemars(example = "MaterializationFields::example")]
+pub struct MaterializationFields {
+    /// # Fields to include.
+    /// This supplements any recommended fields, where enabled.
+    /// Values are passed through to the driver, e.x. for customization
+    /// of the driver's schema generation or runtime behavior with respect
+    /// to the field.
+    #[serde(default)]
+    pub include: BTreeMap<Field, Object>,
+    /// # Fields to exclude.
+    /// This removes from recommended projections, where enabled.
+    #[serde(default)]
+    pub exclude: Vec<Field>,
+    /// # Should recommended projections for the endpoint be used?
+    pub recommended: bool,
+}
+
+impl MaterializationFields {
+    pub fn example() -> Self {
+        MaterializationFields {
+            include: vec![(Field::new("added"), Object::new())]
+                .into_iter()
+                .collect(),
+            exclude: vec![Field::new("removed")],
+            recommended: true,
+        }
+    }
+}
+
+impl Default for MaterializationFields {
+    fn default() -> Self {
+        Self {
+            include: BTreeMap::new(),
+            exclude: Vec::new(),
+            recommended: true,
+        }
+    }
+}
+
+/// An Endpoint connector used for Flow materializations.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub enum MaterializationEndpoint {
+    /// # A Flow sink.
+    FlowSink(FlowSinkConfig),
+    /// # A SQLite database.
+    Sqlite(SqliteConfig),
+    /// # A Snowflake database.
+    /// Deprecated. Use the connector docker image instead.
+    Snowflake(SnowflakeConfig),
+}
+
+impl MaterializationEndpoint {
+    pub fn endpoint_type(&self) -> EndpointType {
+        match self {
+            Self::FlowSink(_) => EndpointType::FlowSink,
+            Self::Sqlite(_) => EndpointType::Sqlite,
+            Self::Snowflake(_) => EndpointType::Snowflake,
+        }
+    }
+}
+
+/// Flow sink connector specification.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+pub struct FlowSinkConfig {
+    /// # Image of the connector.
+    pub image: String,
+    /// # Configuration of the connector.
+    pub config: Object,
+}
+
+/// Sqlite endpoint configuration.
+/// Compare to https://github.com/mattn/go-sqlite3#connection-string
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+pub struct SqliteConfig {
+    /// Preserve and pass-through all configuration.
+    /// Some fields are explicit below, to benefit from JSON-Schema generation.
+    #[serde(flatten)]
+    pub extra: Object,
+    /// # Path of the database, relative to this catalog source.
+    pub path: RelativeUrl,
+}
+
+/// Snowflake endpoint configuration.
+/// Compare to https://pkg.go.dev/github.com/snowflakedb/gosnowflake#Config
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+pub struct SnowflakeConfig {
+    /// Preserve and pass-through all configuration.
+    /// Some fields are explicit below, to benefit from JSON-Schema generation.
+    #[serde(flatten)]
+    pub extra: Object,
+
+    pub account: String,   // Account name
+    pub user: String,      // Username
+    pub password: String,  // Password (requires User)
+    pub database: String,  // Database name
+    pub schema: String,    // Schema
+    pub warehouse: String, // Warehouse
+    pub role: String,      // Role
+}
