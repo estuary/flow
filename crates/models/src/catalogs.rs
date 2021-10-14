0dfb48803ea8e9b0a5f477e48bd363fd2c0a15fb
Johnny <johnny@estuary.dev>
2021-10-14 05:12:08 +0000

    crates/models: hoist former `sources` models into `models`
    Move to the crate root.

---
+++ crates/models/src/catalogs.rs
@@ -0,0 +1,91 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_value, json, Value};
+use std::collections::BTreeMap;
+
+use super::{
+    Capture, CaptureDef, Collection, CollectionDef, Materialization, MaterializationDef,
+    RelativeUrl, StorageMapping, Test, TestStep,
+};
+
+/// Each catalog source defines a portion of a Flow Catalog, by defining
+/// collections, derivations, tests, and materializations of the Catalog.
+/// Catalog sources may reference and import other sources, in order to
+/// collections and other entities that source defines.
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Catalog {
+    /// # JSON-Schema against which the Catalog is validated.
+    #[serde(default, rename = "$schema")]
+    pub _schema: Option<String>,
+    /// # Import other Flow catalog sources.
+    /// By importing another Flow catalog source, the collections, schemas, and derivations
+    /// it defines become usable within this Catalog source. Each import is an absolute URI,
+    /// or a URI which is relative to this source location.
+    #[serde(default)]
+    #[schemars(example = "Catalog::example_import")]
+    pub import: Vec<RelativeUrl>,
+    /// # NPM package dependencies of the Catalog.
+    /// Dependencies are included when building the catalog's build NodeJS
+    /// package, as {"package-name": "version"}. I.e. {"moment": "^2.24"}.
+    ///
+    /// Version strings can take any form understood by NPM.
+    /// See https://docs.npmjs.com/files/package.json#dependencies
+    #[serde(default)]
+    #[schemars(default = "Catalog::default_node_dependencies")]
+    pub npm_dependencies: BTreeMap<String, String>,
+    /// # Collections defined by this Catalog.
+    #[serde(default)]
+    #[schemars(example = "Catalog::example_collections")]
+    pub collections: BTreeMap<Collection, CollectionDef>,
+    /// # Materializations of this Catalog.
+    #[serde(default)]
+    pub materializations: BTreeMap<Materialization, MaterializationDef>,
+    /// # Captures of this Catalog.
+    #[serde(default)]
+    pub captures: BTreeMap<Capture, CaptureDef>,
+    // Tests of the catalog, indexed by name.
+    #[serde(default)]
+    #[schemars(default = "Catalog::default_test")]
+    #[schemars(example = "Catalog::example_test")]
+    pub tests: BTreeMap<Test, Vec<TestStep>>,
+    // # Storage mappings of this Catalog.
+    #[serde(default)]
+    #[schemars(example = "StorageMapping::example")]
+    pub storage_mappings: Vec<StorageMapping>,
+}
+
+impl Catalog {
+    /// Build a root JSON schema for the Catalog model.
+    pub fn root_json_schema() -> schemars::schema::RootSchema {
+        let settings = schemars::gen::SchemaSettings::draft2019_09();
+        let generator = schemars::gen::SchemaGenerator::new(settings);
+        generator.into_root_schema_for::<Self>()
+    }
+
+    fn default_node_dependencies() -> BTreeMap<String, String> {
+        from_value(json!({"a-npm-package": "^1.2.3"})).unwrap()
+    }
+    fn default_test() -> Value {
+        json!({"Test that fob quips ipsum": []})
+    }
+    fn example_import() -> Vec<RelativeUrl> {
+        vec![
+            RelativeUrl::example_relative(),
+            RelativeUrl::example_absolute(),
+        ]
+    }
+    fn example_collections() -> BTreeMap<Collection, CollectionDef> {
+        vec![(Collection::example(), CollectionDef::example())]
+            .into_iter()
+            .collect()
+    }
+    fn example_test() -> Value {
+        json!({
+            "Test that fob quips ipsum": [
+                TestStep::example_ingest(),
+                TestStep::example_verify(),
+            ]
+        })
+    }
+}
--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,3 +1,52 @@
+use serde_json::{from_value, json};
+
 pub mod build;
-pub mod names;
 pub mod tables;
+
+mod captures;
+mod catalogs;
+mod collections;
+mod derivations;
+mod journals;
+mod labels;
+mod materializations;
+mod references;
+mod schemas;
+mod shards;
+mod shuffles;
+mod tests;
+
+pub use captures::{AirbyteSourceConfig, CaptureBinding, CaptureDef, CaptureEndpoint};
+pub use catalogs::Catalog;
+pub use collections::{CollectionDef, Projection};
+pub use derivations::{Derivation, Publish, Register, TransformDef, TransformSource, Update};
+pub use journals::{
+    BucketType, CompressionCodec, FragmentTemplate, JournalTemplate, StorageMapping, Store,
+};
+pub use materializations::{
+    FlowSinkConfig, MaterializationBinding, MaterializationDef, MaterializationEndpoint,
+    MaterializationFields, SnowflakeConfig, SqliteConfig,
+};
+pub use references::{
+    Capture, Collection, CompositeKey, Field, JsonPointer, Materialization, Prefix, RelativeUrl,
+    Test, Transform,
+};
+pub use schemas::Schema;
+pub use shards::ShardTemplate;
+pub use shuffles::{Lambda, PartitionSelector, Shuffle};
+pub use tests::{TestStep, TestStepIngest, TestStepVerify};
+
+/// Object is an alias for a JSON object.
+pub type Object = serde_json::Map<String, serde_json::Value>;
+
+fn duration_schema(_: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+    from_value(json!({
+        "type": ["string", "null"],
+        "pattern": "^\\d+(s|m|h)$"
+    }))
+    .unwrap()
+}
+
+fn is_false(b: &bool) -> bool {
+    !*b
+}
