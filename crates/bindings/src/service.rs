df0e6280cb352e62323981a48994379e18235e9e
Phil Fried <phil@estuary.dev>
2021-10-18 10:42:28 -0400

    Add log configuration to Rust Service bindings
    This adds the basic framework for configuring logging for Rust services
    that are called from Go. The Service interface is modified such that a
    log level filter and a log destination filedescriptor are passed during
    the creation of each service instance. Each time the Service is invoked,
    the logs for that invocation will be written to the filedscriptor that
    was passed during service creation. This lays the groundwork for logs
    from the Rust side to be collected and associated with the specific
    tasks they relate to.
    
    Error logging is done generically in the Rust `Channel`. This is a
    departure from what's normally considered the best practice of logging
    errors at the point where they're handled. The reason for this is that
    it allows for JSON serialization of errors to the logs, while allowing
    the `err_str` in the channel to remain as a more brief human-readable
    representation. The reality at this point is that the "human-readable"
    representation is still using the Rust `Debug` representation, which is
    quite verbose. But having the full details logged will enable us to trim
    those down in a future PR.
    
    The tracing crate does not yet support serialization of nested fields
    as JSON objects, and so errors are currently serialized to JSON as set
    as a string field. Gross, I know. But the tracing project plans to
    support nested JSON objects soon, so it didn't seem worth the effort to
    figure out an alternative approach.

--- crates/bindings/src/service.rs
+++ crates/bindings/src/service.rs
@@ -1,4 +1,5 @@
-pub use protocol::cgo::{Out, Service};
+use crate::logging::{new_thread_local_subscriber, FileWriter};
+pub use protocol::cgo::{self, Out, Service};
 
 /// InN is a variadic input which invokes itself against a Service.
 pub trait InN {
@@ -113,16 +114,42 @@ pub struct Channel {
     err_ptr: *mut u8,
     err_len: usize,
     err_cap: usize,
+
+    // The tracing Dispatch that will be used for logging when this channel is used to invoke a
+    // service. Dispatch is the type-erased form that is used by the tracing crate. Once the
+    // channel is created, we no longer care about the specific implementation of the Subscriber.
+    // The representation here is just a plain pointer so the the Dispatch type doesn't need to be
+    // defined in libbindings.h.
+    log_subscriber: *mut u8,
 }
 
 /// Create a new Service instance, wrapped in an owning Channel.
 /// This is intended to be monomorphized by each Service implementation,
 /// and exposed via cbindgen.  See the UpperCase service for an example.
+/// All logs will be written to the `log_dest_file_descriptor`. This file descriptor will *not* be
+/// closed when the service is destroyed. It is the caller's responsibility to close it if and when
+/// desired. If the `log_dest_file_descriptor` is <= 0, then logging will be disabled entirely for
+/// this service. Each service must use a unique `log_dest_file_descriptor` to avoid interleaved
+/// logs making the JSON output unparseable.
 #[inline]
-pub fn create<S: Service>() -> *mut Channel {
-    // Use service creation as a common entry hook through which we can
-    // install global tracing and logging.
-    crate::setup_env_tracing();
+pub fn create<S: Service>(log_level_filter: i32, log_dest_file_descriptor: i32) -> *mut Channel {
+    // Use service creation as a common entry hook through which we can install global tracing and
+    // logging. The global subscriber that's initialized here will only be used as a fallback for
+    // logs that are produced from other threads, so this is really just a bit of insurance to make
+    // sure we don't miss anything important.
+    crate::logging::setup_env_tracing();
+
+    // Now initialize the subscriber that will forward all the logs for this service to the
+    // `log_dest_file_descriptor`, if logging is enabled. If disabled, then we'll set a no-op
+    // subscriber.
+    let subscriber = if log_dest_file_descriptor > 0 {
+        new_thread_local_subscriber(log_level_filter, unsafe {
+            FileWriter::new(log_dest_file_descriptor)
+        })
+    } else {
+        tracing::Dispatch::none()
+    };
+    let dispatch = Box::into_raw(Box::new(subscriber));
 
     let svc_impl = Box::new(S::create());
     let svc_impl = Box::leak(svc_impl) as *mut S as *mut ServiceImpl;
@@ -138,6 +165,7 @@ pub fn create<S: Service>() -> *mut Channel {
         err_ptr: 0 as *mut u8,
         err_len: 0,
         err_cap: 0,
+        log_subscriber: dispatch as *mut u8,
     });
     Box::leak(ch)
 }
@@ -158,7 +186,26 @@ pub fn invoke<S: Service, I: InN>(ch: *mut Channel, i: I) {
     let mut err_str = unsafe { String::from_raw_parts(ch.err_ptr, ch.err_len, ch.err_cap) };
     let svc_impl = unsafe { &mut *(ch.svc_impl as *mut S) };
 
-    let r = i.invoke(svc_impl, &mut arena, &mut out);
+    let dispatch = unsafe { &*(ch.log_subscriber as *mut tracing::Dispatch) };
+    let r = tracing::dispatcher::with_default(dispatch, || {
+        let result = i.invoke(svc_impl, &mut arena, &mut out);
+        if let Err(err) = &result {
+            // Errors get passed to the go side as a simple string, but we'd ideally like to
+            // include JSON representation of the error in the logs. So we always log errors here
+            // on the Rust side so that logs can include all the gory details, but the error
+            // that's returned to Go has the more human-readable representation.
+            //
+            // For now, we are serializing errors to a JSON string and setting them as the error
+            // field. Yes, this is gross because that field will be a string containing json
+            // instead of an actual json object. But the tracing project is planning to support
+            // serialization of nested objects soon, so we should be able to switch this with an
+            // impl that will serialize the error as a json object.
+            // see: https://github.com/tokio-rs/tracing/issues/1570
+            let err_json = serde_json::to_string(err).expect("serializing error cannot fail");
+            tracing::error!(error = err_json.as_str(), "{}", err);
+        }
+        result
+    });
     if let Err(err) = r {
         // Set terminal error string.
         err_str = format!("{:?}", anyhow::Error::new(err));
@@ -201,11 +248,15 @@ pub fn drop<S: Service>(ch: *mut Channel) {
         err_ptr,
         err_len,
         err_cap,
+
+        log_subscriber,
+        ..
     } = *unsafe { Box::from_raw(ch) };
 
-    // Drop svc_impl, arena, and out.
+    // Drop svc_impl, arena, out, and tracing subscriber.
     unsafe { Box::from_raw(svc_impl as *mut S) };
     unsafe { Vec::<u8>::from_raw_parts(arena_ptr, arena_len, arena_cap) };
     unsafe { Vec::<Out>::from_raw_parts(out_ptr, out_len, out_cap) };
     unsafe { String::from_raw_parts(err_ptr, err_len, err_cap) };
+    unsafe { Box::from_raw(log_subscriber) };
 }
