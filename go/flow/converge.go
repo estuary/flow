5af14be5d226573df555278a0f5e5a3250c4bc3a
Johnny <johnny@estuary.dev>
2021-10-29 22:02:48 +0000

    go/flow: add routines for generalized shard & journal convergence
    Routines take a current state (as journal / shard listings) and a
    desired state (as splits), and return insert/update/delete change
    operations which, once applied, bring the data plane into convergence.
    
    Also refactor the shard StartSplit workflow into these routines.
    
    Bump Gazette to bring in a ShardSpec.Equal implementation,
    as well as new flags exposing testing knobs we'll use in `flowctl`.
    
    Issue #276

---
+++ go/flow/converge.go
@@ -0,0 +1,360 @@
+package flow
+
+import (
+	"context"
+	"fmt"
+	"math"
+
+	"github.com/estuary/flow/go/labels"
+	pf "github.com/estuary/protocols/flow"
+	"go.gazette.dev/core/broker/client"
+	pb "go.gazette.dev/core/broker/protocol"
+	"go.gazette.dev/core/consumer"
+	pc "go.gazette.dev/core/consumer/protocol"
+)
+
+// ListShardsRequest builds a ListRequest of the Task's shards.
+func ListShardsRequest(task pf.Task) pc.ListRequest {
+	return pc.ListRequest{
+		Selector: pb.LabelSelector{
+			Include: pb.MustLabelSet(
+				labels.TaskName, task.TaskName(),
+				labels.TaskType, taskType(task),
+			),
+		},
+	}
+}
+
+// ListRecoveryLogsRequest builds a ListRequest of the Tasks's recovery logs.
+func ListRecoveryLogsRequest(task pf.Task) pb.ListRequest {
+	return pb.ListRequest{
+		Selector: pb.LabelSelector{
+			Include: pb.MustLabelSet(
+				labels.TaskName, task.TaskName(),
+				labels.TaskType, taskType(task),
+			),
+		},
+	}
+}
+
+// ListPartitionsRequest builds a ListRequest of the collection's partitions.
+func ListPartitionsRequest(collection *pf.CollectionSpec) pb.ListRequest {
+	return pb.ListRequest{
+		Selector: pf.LabelSelector{
+			Include: pb.MustLabelSet(labels.Collection, collection.Collection.String()),
+		},
+	}
+}
+
+// MapPartitionsToCurrentSplits passes through current labels of existing partitions.
+func MapPartitionsToCurrentSplits(partitions []pb.ListResponse_Journal) []pf.LabelSet {
+	var out []pf.LabelSet
+	for _, p := range partitions {
+		out = append(out, p.Spec.LabelSet)
+	}
+	return out
+}
+
+// MapShardsToCurrentOrInitialSplits passes through current labels of existing shards.
+// If no shards exist, then initial label splits are returned which evenly subdivide
+// the key range into |initialSplits| chunks.
+func MapShardsToCurrentOrInitialSplits(shards []pc.ListResponse_Shard, initialSplits int) []pf.LabelSet {
+	var out []pf.LabelSet
+
+	if len(shards) != 0 {
+		for _, s := range shards {
+			out = append(out, s.Spec.LabelSet)
+		}
+		return out
+	}
+
+	for p := 0; p != initialSplits; p++ {
+		out = append(out,
+			labels.EncodeRange(pf.RangeSpec{
+				KeyBegin:    uint32((1 << 32) * (p + 0) / initialSplits),
+				KeyEnd:      uint32(((1 << 32) * (p + 1) / initialSplits) - 1),
+				RClockBegin: 0,
+				RClockEnd:   math.MaxUint32,
+			}, pf.LabelSet{}),
+		)
+	}
+
+	return out
+}
+
+// MapShardToSplit maps a single shard contained in the |shards| response to a
+// desired split state, where the shard is evenly subdivided on either key or
+// r-clock (depending on the value of |splitOnKey|).
+func MapShardToSplit(task pf.Task, shards []pc.ListResponse_Shard, splitOnKey bool) ([]pf.LabelSet, error) {
+	if len(shards) != 1 {
+		return nil, fmt.Errorf("expected exactly one shard in the response")
+	}
+	var parent = shards[0].Spec
+
+	// Confirm the shard doesn't have a current split.
+	if l := parent.LabelSet.ValuesOf(labels.SplitSource); len(l) != 0 {
+		return nil, fmt.Errorf("shard %s is already splitting from source %s", parent.Id, l[0])
+	}
+	if l := parent.LabelSet.ValuesOf(labels.SplitTarget); len(l) != 0 {
+		return nil, fmt.Errorf("shard %s is already splitting into target %s", parent.Id, l[0])
+	}
+
+	// Pick a split point of the parent range, which will divide the future
+	// LHS & RHS children.
+	var parentRange, err = labels.ParseRangeSpec(parent.LabelSet)
+	if err != nil {
+		return nil, fmt.Errorf("parsing range spec: %w", err)
+	}
+	var lhsRange, rhsRange = parentRange, parentRange
+
+	if splitOnKey {
+		var pivot = uint32((uint64(parentRange.KeyBegin) + uint64(parentRange.KeyEnd) + 1) / 2)
+		lhsRange.KeyEnd, rhsRange.KeyBegin = pivot-1, pivot
+	} else {
+		var pivot = uint32((uint64(parentRange.RClockBegin) + uint64(parentRange.RClockEnd) + 1) / 2)
+		lhsRange.RClockEnd, rhsRange.RClockBegin = pivot-1, pivot
+	}
+
+	// Deep-copy parent labels for the desired LHS / RHS updates.
+	var lhs = pf.LabelSet{Labels: append([]pf.Label(nil), parent.Labels...)}
+	var rhs = pf.LabelSet{Labels: append([]pf.Label(nil), parent.Labels...)}
+
+	rhs = labels.EncodeRange(rhsRange, rhs)
+
+	// We don't update the |lhs| range at this time.
+	// That will happen when the |rhs| shard finishes playback
+	// and completes the split workflow.
+
+	// Determine what the RHS child's shard ID will be.
+	rhsSuffix, err := labels.ShardSuffix(rhs)
+	if err != nil {
+		return nil, fmt.Errorf("building RHS shard suffix: %w", err)
+	}
+	var rhsId = task.TaskShardTemplate().Id.String() + "/" + rhsSuffix
+
+	// Mark the parent & child specs as having an in-progress split.
+	lhs.SetValue(labels.SplitTarget, rhsId)
+	rhs.SetValue(labels.SplitSource, parent.Id.String())
+
+	return []pf.LabelSet{lhs, rhs}, nil
+}
+
+// CollectionChanges compares a CollectionSpec and |desiredSplits| with the
+// collection's |curPartitions|, and appends proposed JournalChanges
+// which bring the current state into consistency with the desired state.
+func CollectionChanges(
+	collection *pf.CollectionSpec,
+	curPartitions []pb.ListResponse_Journal,
+	desiredSplits []pf.LabelSet,
+	into []pb.ApplyRequest_Change,
+) ([]pb.ApplyRequest_Change, error) {
+	var idx = make(map[pf.Journal]*pb.ListResponse_Journal, len(curPartitions))
+	for i := range curPartitions {
+		idx[curPartitions[i].Spec.Name] = &curPartitions[i]
+	}
+
+	for _, d := range desiredSplits {
+		var next, err = BuildPartitionSpec(collection.PartitionTemplate, d)
+		if err != nil {
+			return nil, fmt.Errorf("building journal spec: %w", err)
+		}
+
+		var cur, ok = idx[next.Name]
+		if ok && cur == nil {
+			return nil, fmt.Errorf("duplicate desired partition journal %s", next.Name)
+		} else if ok {
+			idx[next.Name] = nil
+
+			if !next.Equal(&cur.Spec) {
+				into = append(into, pb.ApplyRequest_Change{
+					Upsert:            next,
+					ExpectModRevision: cur.ModRevision,
+				})
+			}
+		} else {
+			into = append(into, pb.ApplyRequest_Change{
+				Upsert:            next,
+				ExpectModRevision: 0, // Expected to not exist.
+			})
+		}
+	}
+
+	// Journals still in |idx| were not in |desired| and must be removed.
+	for _, cur := range idx {
+		if cur == nil {
+			continue // Already in |updates|.
+		}
+
+		into = append(into, pb.ApplyRequest_Change{
+			Delete:            cur.Spec.Name,
+			ExpectModRevision: cur.ModRevision,
+		})
+	}
+
+	return into, nil
+}
+
+// TaskChanges compares a Task and |desiredSplits| with the tasks's |curShards|
+// and |curRecoveryLogs|, and appends proposed shard and journal changes
+// which bring the current state into consistency with the desired state.
+func TaskChanges(
+	task pf.Task,
+	curShards []pc.ListResponse_Shard,
+	curRecoveryLogs []pb.ListResponse_Journal,
+	desiredSplits []pf.LabelSet,
+	intoShards []pc.ApplyRequest_Change,
+	intoJournals []pb.ApplyRequest_Change,
+) ([]pc.ApplyRequest_Change, []pb.ApplyRequest_Change, error) {
+
+	var shardIdx = make(map[pc.ShardID]*pc.ListResponse_Shard, len(curShards))
+	var logIdx = make(map[pb.Journal]*pb.ListResponse_Journal, len(curRecoveryLogs))
+
+	for i := range curShards {
+		shardIdx[curShards[i].Spec.Id] = &curShards[i]
+	}
+	for i := range curRecoveryLogs {
+		logIdx[curRecoveryLogs[i].Spec.Name] = &curRecoveryLogs[i]
+	}
+
+	for _, d := range desiredSplits {
+		var nextShard, err = BuildShardSpec(task.TaskShardTemplate(), d)
+		if err != nil {
+			return nil, nil, fmt.Errorf("building shard spec: %w", err)
+		}
+		var nextLog = BuildRecoverySpec(task.TaskRecoveryLogTemplate(), nextShard)
+
+		var curShard, ok = shardIdx[nextShard.Id]
+		if ok && curShard == nil {
+			return nil, nil, fmt.Errorf("duplicate desired shard %s", nextShard.Id)
+		} else if ok {
+			shardIdx[nextShard.Id] = nil
+
+			if !nextShard.Equal(&curShard.Spec) {
+				intoShards = append(intoShards, pc.ApplyRequest_Change{
+					Upsert:            nextShard,
+					ExpectModRevision: curShard.ModRevision,
+				})
+			}
+		} else {
+			intoShards = append(intoShards, pc.ApplyRequest_Change{
+				Upsert:            nextShard,
+				ExpectModRevision: 0, // Expected to not exist.
+			})
+		}
+
+		curLog, ok := logIdx[nextLog.Name]
+		if ok && curLog == nil {
+			panic("duplicate recovery log; cannot be reached, because it's also a duplicate shard")
+		} else if ok {
+			logIdx[nextLog.Name] = nil
+
+			if !nextLog.Equal(&curLog.Spec) {
+				intoJournals = append(intoJournals, pb.ApplyRequest_Change{
+					Upsert:            nextLog,
+					ExpectModRevision: curLog.ModRevision,
+				})
+			}
+		} else {
+			intoJournals = append(intoJournals, pb.ApplyRequest_Change{
+				Upsert:            nextLog,
+				ExpectModRevision: 0, // Expected to not exist.
+			})
+		}
+	}
+
+	// Shards still in |shardIdx| were not in |desired| and must be removed.
+	for _, cur := range shardIdx {
+		if cur == nil {
+			continue // Already in |shardOut.Updates|.
+		}
+
+		intoShards = append(intoShards, pc.ApplyRequest_Change{
+			Delete:            cur.Spec.Id,
+			ExpectModRevision: cur.ModRevision,
+		})
+	}
+
+	// Journals still in |logIdx| were not in |desired| and must be removed.
+	for _, cur := range logIdx {
+		if cur == nil {
+			continue // Already in |logOut.Updates|.
+		}
+
+		intoJournals = append(intoJournals, pb.ApplyRequest_Change{
+			Delete:            cur.Spec.Name,
+			ExpectModRevision: cur.ModRevision,
+		})
+	}
+
+	return intoShards, intoJournals, nil
+}
+
+// ActivationChanges enumerates all shard and journal changes required to bring
+// a current data-plane state into consistency with the desired state of each
+// of the specified, activated collections and tasks.
+func ActivationChanges(
+	ctx context.Context,
+	jc pb.JournalClient,
+	sc pc.ShardClient,
+	collections []*pf.CollectionSpec,
+	tasks []pf.Task,
+	initialTaskSplits int,
+) ([]pc.ApplyRequest_Change, []pb.ApplyRequest_Change, error) {
+
+	var shards []pc.ApplyRequest_Change
+	var journals []pb.ApplyRequest_Change
+
+	// TODO(johnny): We could parallelize this by scattering / gathering list requests.
+
+	for _, collection := range collections {
+		var resp, err = client.ListAllJournals(ctx, jc, ListPartitionsRequest(collection))
+		if err != nil {
+			return nil, nil, fmt.Errorf("listing partitions of %s: %w", collection.Collection, err)
+		}
+
+		var desired = MapPartitionsToCurrentSplits(resp.Journals)
+		journals, err = CollectionChanges(collection, resp.Journals, desired, journals)
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("processing collection %s: %w", collection.Collection, err)
+		}
+	}
+
+	for _, task := range tasks {
+		var shardsReq = ListShardsRequest(task)
+		var logsReq = ListRecoveryLogsRequest(task)
+
+		shardsResp, err := consumer.ListShards(ctx, sc, &shardsReq)
+		if err != nil {
+			return nil, nil, fmt.Errorf("listing shards of %s: %w", task.TaskName(), err)
+		}
+		logsResp, err := client.ListAllJournals(ctx, jc, logsReq)
+		if err != nil {
+			return nil, nil, fmt.Errorf("listing recovery logs of %s: %w", task.TaskName(), err)
+		}
+
+		var desired = MapShardsToCurrentOrInitialSplits(shardsResp.Shards, initialTaskSplits)
+		shards, journals, err = TaskChanges(
+			task, shardsResp.Shards, logsResp.Journals, desired, shards, journals)
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("processing task %s: %w", task.TaskName(), err)
+		}
+	}
+
+	return shards, journals, nil
+}
+
+// taskType returns the label matching this Task.
+func taskType(task pf.Task) string {
+	switch task.(type) {
+	case *pf.CaptureSpec:
+		return labels.TaskTypeCapture
+	case *pf.DerivationSpec:
+		return labels.TaskTypeDerivation
+	case *pf.MaterializationSpec:
+		return labels.TaskTypeMaterialization
+	default:
+		panic(task)
+	}
+}
---
+++ go/flow/converge_test.go
@@ -0,0 +1,420 @@
+package flow
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/bradleyjkemp/cupaloy"
+	"github.com/estuary/flow/go/bindings"
+	"github.com/estuary/flow/go/labels"
+	"github.com/estuary/protocols/catalog"
+	"github.com/estuary/protocols/fdb/tuple"
+	pf "github.com/estuary/protocols/flow"
+	"github.com/stretchr/testify/require"
+	pb "go.gazette.dev/core/broker/protocol"
+	pc "go.gazette.dev/core/consumer/protocol"
+	"google.golang.org/grpc"
+)
+
+func TestConvergence(t *testing.T) {
+	var args = bindings.BuildArgs{
+		Context:  context.Background(),
+		FileRoot: "./testdata",
+		BuildAPI_Config: pf.BuildAPI_Config{
+			BuildId:    "fixture",
+			Directory:  t.TempDir(),
+			Source:     "file:///specs_test.flow.yaml",
+			SourceType: pf.ContentType_CATALOG_SPEC,
+		}}
+	require.NoError(t, bindings.BuildCatalog(args))
+
+	var collection *pf.CollectionSpec
+	var derivation *pf.DerivationSpec
+
+	require.NoError(t, catalog.Extract(args.OutputPath(), func(db *sql.DB) (err error) {
+		if collection, err = catalog.LoadCollection(db, "example/collection"); err != nil {
+			return err
+		}
+		derivation, err = catalog.LoadDerivation(db, "example/derivation")
+		return err
+	}))
+
+	// Create many partition, shard, and recovery log fixtures.
+
+	partitionSpec1, err := BuildPartitionSpec(collection.PartitionTemplate,
+		labels.EncodePartitionLabels(
+			collection.PartitionFields, tuple.Tuple{true, "a-val"},
+			pb.MustLabelSet(
+				labels.KeyBegin, fmt.Sprintf("%08x", 0x10000000),
+				labels.KeyEnd, fmt.Sprintf("%08x", 0x3fffffff),
+			)))
+	require.NoError(t, err)
+
+	partitionSpec2, err := BuildPartitionSpec(collection.PartitionTemplate,
+		labels.EncodePartitionLabels(
+			collection.PartitionFields, tuple.Tuple{true, "a-val"},
+			pb.MustLabelSet(
+				labels.KeyBegin, fmt.Sprintf("%08x", 0x40000000),
+				labels.KeyEnd, fmt.Sprintf("%08x", 0x5fffffff),
+			)))
+	require.NoError(t, err)
+
+	partitionSpec3, err := BuildPartitionSpec(collection.PartitionTemplate,
+		labels.EncodePartitionLabels(
+			collection.PartitionFields, tuple.Tuple{false, "other-val"},
+			pb.MustLabelSet(
+				labels.KeyBegin, labels.KeyBeginMin,
+				labels.KeyEnd, labels.KeyEndMax,
+			)))
+	require.NoError(t, err)
+
+	shardSpec1, err := BuildShardSpec(derivation.ShardTemplate,
+		labels.EncodeRange(pf.RangeSpec{
+			KeyBegin:    0x10000000,
+			KeyEnd:      0x2fffffff,
+			RClockBegin: 0x60000000,
+			RClockEnd:   0x9fffffff,
+		}, pf.LabelSet{}),
+	)
+	require.NoError(t, err)
+	logSpec1 := BuildRecoverySpec(derivation.RecoveryLogTemplate, shardSpec1)
+
+	shardSpec2, err := BuildShardSpec(derivation.ShardTemplate,
+		labels.EncodeRange(pf.RangeSpec{
+			KeyBegin:    0x30000000,
+			KeyEnd:      0x3fffffff,
+			RClockBegin: 0x60000000,
+			RClockEnd:   0x7fffffff,
+		}, pf.LabelSet{}),
+	)
+	require.NoError(t, err)
+	logSpec2 := BuildRecoverySpec(derivation.RecoveryLogTemplate, shardSpec2)
+
+	shardSpec3, err := BuildShardSpec(derivation.ShardTemplate,
+		labels.EncodeRange(pf.RangeSpec{
+			KeyBegin:    0x30000000,
+			KeyEnd:      0x3fffffff,
+			RClockBegin: 0x80000000,
+			RClockEnd:   0x9fffffff,
+		}, pf.LabelSet{}),
+	)
+	require.NoError(t, err)
+	logSpec3 := BuildRecoverySpec(derivation.RecoveryLogTemplate, shardSpec3)
+
+	var allPartitions = []pb.ListResponse_Journal{
+		{Spec: *partitionSpec1, ModRevision: 11},
+		{Spec: *partitionSpec2, ModRevision: 22},
+		{Spec: *partitionSpec3, ModRevision: 33},
+	}
+	var allShards = []pc.ListResponse_Shard{
+		{Spec: *shardSpec1, ModRevision: 44},
+		{Spec: *shardSpec2, ModRevision: 55},
+		{Spec: *shardSpec3, ModRevision: 66},
+	}
+	var allLogs = []pb.ListResponse_Journal{
+		{Spec: *logSpec1, ModRevision: 77},
+		{Spec: *logSpec2, ModRevision: 88},
+		{Spec: *logSpec3, ModRevision: 99},
+	}
+
+	t.Run("list-shards-request", func(t *testing.T) {
+		var out = ListShardsRequest(derivation)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("list-recovery-logs-request", func(t *testing.T) {
+		var out = ListRecoveryLogsRequest(derivation)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("list-partitions-request", func(t *testing.T) {
+		var out = ListPartitionsRequest(collection)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("partitions-to-current-splits", func(t *testing.T) {
+		var out = MapPartitionsToCurrentSplits(allPartitions)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("shards-to-current-splits", func(t *testing.T) {
+		var out = MapShardsToCurrentOrInitialSplits(allShards, 100)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("shards-to-initial-splits", func(t *testing.T) {
+		var out = MapShardsToCurrentOrInitialSplits(nil, 4)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("shard-split-on-key", func(t *testing.T) {
+		var out, err = MapShardToSplit(derivation, allShards[0:1], true)
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("shard-split-on-r-clock", func(t *testing.T) {
+		var out, err = MapShardToSplit(derivation, allShards[1:2], false)
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("shard-split-errors", func(t *testing.T) {
+		var shard, err = BuildShardSpec(derivation.ShardTemplate,
+			labels.EncodeRange(pf.RangeSpec{
+				KeyEnd:    0x10000000,
+				RClockEnd: 0x10000000,
+			}, pf.LabelSet{}))
+		require.NoError(t, err)
+
+		// Case: already has a split-source.
+		shard.LabelSet.SetValue(labels.SplitSource, "whoops")
+		_, err = MapShardToSplit(derivation, []pc.ListResponse_Shard{{Spec: *shard}}, false)
+		require.EqualError(t, err,
+			"shard derivation/example/derivation/00000000-00000000 is already splitting from source whoops")
+
+		// Case: already has a split-target.
+		shard.LabelSet.Remove(labels.SplitSource)
+		shard.LabelSet.SetValue(labels.SplitTarget, "whoops")
+
+		_, err = MapShardToSplit(derivation, []pc.ListResponse_Shard{{Spec: *shard}}, false)
+		require.EqualError(t, err,
+			"shard derivation/example/derivation/00000000-00000000 is already splitting into target whoops")
+
+		// Case: expects exactly one shard.
+		_, err = MapShardToSplit(derivation, nil, false)
+		require.EqualError(t, err, "expected exactly one shard in the response")
+		_, err = MapShardToSplit(derivation, allShards, false)
+		require.EqualError(t, err, "expected exactly one shard in the response")
+	})
+
+	var desiredPartitions = MapPartitionsToCurrentSplits(allPartitions)
+
+	// aj & as are empty fixtures which verify that routines are correctly
+	// appending into the passed-in values.
+	var aj = []pb.ApplyRequest_Change{{Delete: "journal-prelude"}}
+	var as = []pc.ApplyRequest_Change{{Delete: "shard-prelude"}}
+
+	t.Run("collection-has-no-changes", func(t *testing.T) {
+		var out, err = CollectionChanges(collection, allPartitions, desiredPartitions, aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("collection-insertion", func(t *testing.T) {
+		var out, err = CollectionChanges(collection, allPartitions[1:], desiredPartitions, aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("collection-deletion", func(t *testing.T) {
+		var out, err = CollectionChanges(collection, allPartitions, desiredPartitions[1:], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("collection-update", func(t *testing.T) {
+		var modified = append([]pb.ListResponse_Journal(nil), allPartitions...)
+		modified[0].Spec.Replication = 42
+
+		var out, err = CollectionChanges(collection, modified, desiredPartitions, aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, out)
+	})
+
+	t.Run("collection-duplicate", func(t *testing.T) {
+		var duplicated = append(desiredPartitions, desiredPartitions[0])
+
+		var _, err = CollectionChanges(collection, allPartitions, duplicated, aj[:1])
+		require.EqualError(t, err,
+			"duplicate desired partition journal example/collection/a_bool=true/a_str=a-val/pivot=10000000")
+	})
+
+	var desiredTasks = MapShardsToCurrentOrInitialSplits(allShards, 1234)
+
+	t.Run("task-has-no-changes", func(t *testing.T) {
+		var shards, journals, err = TaskChanges(derivation, allShards, allLogs, desiredTasks, as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-shard-insertion", func(t *testing.T) {
+		var shards, journals, err = TaskChanges(derivation, allShards[1:], allLogs, desiredTasks, as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-log-insertion", func(t *testing.T) {
+		var shards, journals, err = TaskChanges(derivation, allShards, allLogs[1:], desiredTasks, as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-shard-deletion", func(t *testing.T) {
+		var shards, journals, err = TaskChanges(derivation, allShards, allLogs[1:], desiredTasks[1:], as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-log-deletion", func(t *testing.T) {
+		var shards, journals, err = TaskChanges(derivation, allShards[1:], allLogs, desiredTasks[1:], as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-shard-update", func(t *testing.T) {
+		var modified = append([]pc.ListResponse_Shard(nil), allShards...)
+		modified[0].Spec.HotStandbys = 42
+
+		var shards, journals, err = TaskChanges(derivation, modified, allLogs, desiredTasks, as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-log-update", func(t *testing.T) {
+		var modified = append([]pb.ListResponse_Journal(nil), allLogs...)
+		modified[0].Spec.Replication = 42
+
+		var shards, journals, err = TaskChanges(derivation, allShards, modified, desiredTasks, as[:1], aj[:1])
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("task-duplicate", func(t *testing.T) {
+		var duplicated = append(desiredTasks, desiredTasks[0])
+
+		var _, _, err = TaskChanges(derivation, allShards, allLogs, duplicated, as[:1], aj[:1])
+		require.EqualError(t, err,
+			"duplicate desired shard derivation/example/derivation/10000000-60000000")
+	})
+
+	t.Run("activate-empty-cluster", func(t *testing.T) {
+		var ctx = context.Background()
+		var jc = &mockJournals{}
+		var sc = &mockShards{}
+
+		var shards, journals, err = ActivationChanges(ctx, jc, sc, []*pf.CollectionSpec{collection}, []pf.Task{derivation}, 2)
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+
+	t.Run("activate-has-no-changes", func(t *testing.T) {
+		var ctx = context.Background()
+		var jc = &mockJournals{
+			collections: map[string]*pb.ListResponse{
+				collection.Collection.String(): {Journals: allPartitions},
+			},
+			logs: map[string]*pb.ListResponse{
+				derivation.TaskName(): {Journals: allLogs},
+			},
+		}
+		var sc = &mockShards{
+			tasks: map[string]*pc.ListResponse{
+				derivation.TaskName(): {Shards: allShards},
+			},
+		}
+
+		var shards, journals, err = ActivationChanges(ctx, jc, sc, []*pf.CollectionSpec{collection}, []pf.Task{derivation}, 2)
+		require.NoError(t, err)
+		cupaloy.SnapshotT(t, shards, journals)
+	})
+}
+
+type mockJournals struct {
+	collections map[string]*pb.ListResponse
+	logs        map[string]*pb.ListResponse
+}
+
+type mockShards struct {
+	tasks map[string]*pc.ListResponse
+}
+
+func (jc *mockJournals) List(ctx context.Context, in *pb.ListRequest, opts ...grpc.CallOption) (*pb.ListResponse, error) {
+	var out *pb.ListResponse
+
+	if name := in.Selector.Include.ValueOf(labels.Collection); name != "" {
+		if r, ok := jc.collections[name]; ok {
+			out = r
+		} else {
+			out = new(pb.ListResponse)
+		}
+	}
+
+	if name := in.Selector.Include.ValueOf(labels.TaskName); name != "" {
+		if r, ok := jc.logs[name]; ok {
+			out = r
+		} else {
+			out = new(pb.ListResponse)
+		}
+	}
+
+	if out == nil {
+		return nil, fmt.Errorf("bad request")
+	}
+
+	out.Header = pb.Header{
+		Etcd:  pb.Header_Etcd{ClusterId: 1, MemberId: 2, Revision: 3, RaftTerm: 4},
+		Route: pb.Route{Primary: -1},
+	}
+	for i := range out.Journals {
+		out.Journals[i].Route = pb.Route{Primary: -1}
+	}
+	return out, nil
+}
+
+func (sc *mockShards) List(ctx context.Context, in *pc.ListRequest, opts ...grpc.CallOption) (*pc.ListResponse, error) {
+	var out *pc.ListResponse
+
+	if name := in.Selector.Include.ValueOf(labels.TaskName); name != "" {
+		if r, ok := sc.tasks[name]; ok {
+			out = r
+		} else {
+			out = new(pc.ListResponse)
+		}
+	}
+
+	if out == nil {
+		return nil, fmt.Errorf("bad request")
+	}
+
+	out.Header = pb.Header{
+		Etcd:  pb.Header_Etcd{ClusterId: 1, MemberId: 2, Revision: 3, RaftTerm: 4},
+		Route: pb.Route{Primary: -1},
+	}
+	for i := range out.Shards {
+		out.Shards[i].Route = pb.Route{Primary: -1}
+	}
+	return out, nil
+}
+
+var _ pb.JournalClient = &mockJournals{}
+
+func (jc *mockJournals) Apply(ctx context.Context, in *pb.ApplyRequest, opts ...grpc.CallOption) (*pb.ApplyResponse, error) {
+	panic("not implemented")
+}
+func (jc *mockJournals) Read(ctx context.Context, in *pb.ReadRequest, opts ...grpc.CallOption) (pb.Journal_ReadClient, error) {
+	panic("not implemented")
+}
+func (jc *mockJournals) Append(ctx context.Context, opts ...grpc.CallOption) (pb.Journal_AppendClient, error) {
+	panic("not implemented")
+}
+func (jc *mockJournals) Replicate(ctx context.Context, opts ...grpc.CallOption) (pb.Journal_ReplicateClient, error) {
+	panic("not implemented")
+}
+func (jc *mockJournals) ListFragments(ctx context.Context, in *pb.FragmentsRequest, opts ...grpc.CallOption) (*pb.FragmentsResponse, error) {
+	panic("not implemented")
+}
+
+var _ pc.ShardClient = &mockShards{}
+
+func (sc *mockShards) Stat(ctx context.Context, in *pc.StatRequest, opts ...grpc.CallOption) (*pc.StatResponse, error) {
+	panic("not implemented")
+}
+func (sc *mockShards) Apply(ctx context.Context, in *pc.ApplyRequest, opts ...grpc.CallOption) (*pc.ApplyResponse, error) {
+	panic("not implemented")
+}
+func (sc *mockShards) GetHints(ctx context.Context, in *pc.GetHintsRequest, opts ...grpc.CallOption) (*pc.GetHintsResponse, error) {
+	panic("not implemented")
+}
