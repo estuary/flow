6983ec50619dc79b1092915be4a2e3acf4015cd7
Jixiang Jiang <jixiang@estuary.dev>
2021-12-09 09:20:44 -0500

    Improvements to Materializations ElasticSearch (#80)
    * data ingest script
    
    * test infra change and s3-parquet
    
    * elasticsearch tests
    
    * hook with github workflow
    
    * change elastic search jsonschema descriptions
    
    * add GetFieldDocString interface commends
    
    * check scanner.Err
    
    * new comment

--- run.go
+++ run.go
@@ -12,38 +12,85 @@ import (
 const ProgramName = "schema-builder"
 
 // DateSpec configures a date field in elastic search schema.
-// https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-date-format.html
 type DateSpec struct {
 	Format string `json:"format"`
 }
 
+// GetFieldDocString implements the jsonschema.customSchemaGetFieldDocString interface.
+func (DateSpec) GetFieldDocString(fieldName string) string {
+	switch fieldName {
+	case "Format":
+		return "Format of the date. " +
+			"See https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-date-format.html."
+	default:
+		return ""
+	}
+}
+
 // KeywordSpec configures a keyword field for elastic search schema.
 type KeywordSpec struct {
-	//https://www.elastic.co/guide/en/elasticsearch/reference/current/ignore-above.html
 	IgnoreAbove int `json:"ignore_above"`
+}
+
+// GetFieldDocString implements the jsonschema.customSchemaGetFieldDocString interface.
+func (KeywordSpec) GetFieldDocString(fieldName string) string {
+	switch fieldName {
+	case "IgnoreAbove":
+		return "Strings longer than the ignore_above setting will not be indexed or stored. " +
+			"See https://www.elastic.co/guide/en/elasticsearch/reference/current/ignore-above.html"
+	default:
+		return ""
+	}
 }
 
 // TextSpec configures a text field for elastic search schema.
 type TextSpec struct {
-	// Whether or not to specify the field as text/keyword dual field.
-	DualKeyword bool `json:"dual_keyword"`
+	DualKeyword        bool `json:"dual_keyword"`
+	KeywordIgnoreAbove int  `json:"keyword_ignore_above"`
+}
 
-	// https://www.elastic.co/guide/en/elasticsearch/reference/current/ignore-above.html
-	// effective only if DualKeyword is enabled.
-	KeywordIgnoreAbove int `json:"keyword_ignore_above"`
+// GetFieldDocString implements the jsonschema.customSchemaGetFieldDocString interface.
+func (TextSpec) GetFieldDocString(fieldName string) string {
+	switch fieldName {
+	case "DualKeyword":
+		return "Whether or not to specify the field as text/keyword dual field."
+
+	case "KeywordIgnoreAbove":
+		return "Effective only if DualKeyword is enabled. Strings longer than the ignore_above setting will not be indexed or stored. " +
+			"See https://www.elastic.co/guide/en/elasticsearch/reference/current/ignore-above.html"
+	default:
+		return ""
+	}
 }
 
 // ElasticFieldType specifies the type to override the field with.
 type ElasticFieldType struct {
-	// The elastic search field data types.
-	// Supported types include: boolean, date, double, geo_point, geo_shape, keyword, long, null, text
+	// A snake_case string corresponding to a enum type of ESBasicType
+	// defined in src/elastic_search_data_types.rs
 	FieldType string `json:"field_type"`
-	// Spec of the date field, effective if field_type is "date"
-	DateSpec DateSpec `json:"date_spec,omitempty"`
-	// Spec of the keyword field, effective if field_type if "keyword"
+
+	DateSpec    DateSpec    `json:"date_spec,omitempty"`
 	KeywordSpec KeywordSpec `json:"keyword_spec,omitempty"`
-	// Spec of the text field, effective if FieldType is "text"
-	TextSpec TextSpec `json:"text_spec,omitempty"`
+	TextSpec    TextSpec    `json:"text_spec,omitempty"`
+}
+
+// GetFieldDocString implements the jsonschema.customSchemaGetFieldDocString interface.
+func (ElasticFieldType) GetFieldDocString(fieldName string) string {
+	switch fieldName {
+	case "FieldType":
+		return "The elastic search field data types. " +
+			"Supported types include: boolean, date, double, geo_point, geo_shape, keyword, long, null, text."
+	case "DateSpec":
+		return "Spec of the date field, effective if field_type is 'date'. " +
+			"See https://www.elastic.co/guide/en/elasticsearch/reference/current/date.html"
+	case "KeywordSpec":
+		return "Spec of the keyword field, effective if field_type is 'keyword'. " +
+			"See https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html"
+	case "TextSpec":
+		return "Spec of the text field, effective if field_type is 'text'."
+	default:
+		return ""
+	}
 }
 
 // MarshalJSON provides customized marshalJSON of ElasticFieldType
@@ -76,10 +123,20 @@ func (e ElasticFieldType) MarshalJSON() ([]byte, error) {
 // FieldOverride specifies which field in the resulting elastic search schema
 // and how it is overridden.
 type FieldOverride struct {
-	// A '/'-delimitated json pointer to the location of the overridden field.
-	Pointer string `json:"pointer"`
-	// The overriding elastic search data type of the field.
-	EsType ElasticFieldType `json:"es_type"`
+	Pointer string           `json:"pointer"`
+	EsType  ElasticFieldType `json:"es_type"`
+}
+
+// GetFieldDocString implements the jsonschema.customSchemaGetFieldDocString interface.
+func (FieldOverride) GetFieldDocString(fieldName string) string {
+	switch fieldName {
+	case "Pointer":
+		return "A '/'-delimitated json pointer to the location of the overridden field."
+	case "EsType":
+		return "The overriding elastic search data type of the field."
+	default:
+		return ""
+	}
 }
 
 // Input provides the input data for schema builder.
