657df486ff7b372657f3ac8ef7ddbe15540f6faf
Alex Burkhart <alex@estuary.dev>
2022-01-12 16:57:21 -0500

    Add axum hello world

--- crates/control/src/main.rs
+++ crates/control/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tower::{limit::ConcurrencyLimitLayer, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+
+async fn hello_world() -> &'static str {
+    "Hello World"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(hello_world)).layer(
+        ServiceBuilder::new()
+            .layer(TraceLayer::new_for_http())
+            .layer(ConcurrencyLimitLayer::new(64)),
+    );
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
