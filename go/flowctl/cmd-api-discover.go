ef963ebda314a8cb5fc949ea3fe63ba292e60133
Johnny Graettinger <johnny@estuary.dev>
2022-01-14 10:42:42 -0500

    flowctl: add `api discover` and enhance `discover`
    flowctl api discover invokes a capture's Discovery RPC and returns
    its response as either json or protobuf.
    
    flowctl discover is refactored to use `api spec` and `api discover`
    to fetch capture specifications and bindings. It's also updated
    to stage out schemas and endpoint configuration as separate files
    in a user-controlled directory. Its endpoint configuration stubs
    are also updated thread through default values.
    
    Issue #335

---
+++ go/flowctl/cmd-api-discover.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/estuary/flow/go/capture"
+	"github.com/estuary/flow/go/flow/ops"
+	pc "github.com/estuary/protocols/capture"
+	pf "github.com/estuary/protocols/flow"
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/sirupsen/logrus"
+	pb "go.gazette.dev/core/broker/protocol"
+	mbp "go.gazette.dev/core/mainboilerplate"
+	"gopkg.in/yaml.v3"
+)
+
+type apiDiscover struct {
+	Log         mbp.LogConfig         `group:"Logging" namespace:"log" env-namespace:"LOG"`
+	Diagnostics mbp.DiagnosticsConfig `group:"Debug" namespace:"debug" env-namespace:"DEBUG"`
+	Image       string                `long:"image" required:"true" description:"Docker image of the connector to use"`
+	Network     string                `long:"network" default:"host" description:"The Docker network that connector containers are given access to."`
+	Config      string                `long:"config" description:"Path to the connector endpoint configuration"`
+	Output      string                `long:"output" choice:"json" choice:"proto" default:"json"`
+}
+
+func (cmd apiDiscover) execute(ctx context.Context) (*pc.DiscoverResponse, error) {
+	var config, err = readConfig(cmd.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	spec, err := json.Marshal(struct {
+		Image  string          `json:"image"`
+		Config json.RawMessage `json:"config"`
+	}{
+		Image:  cmd.Image,
+		Config: config,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := capture.NewDriver(ctx, pf.EndpointType_AIRBYTE_SOURCE, spec, cmd.Network, ops.StdLogger())
+	if err != nil {
+		return nil, fmt.Errorf("building client: %w", err)
+	}
+
+	resp, err := client.Discover(ctx,
+		&pc.DiscoverRequest{
+			EndpointType:     pf.EndpointType_AIRBYTE_SOURCE,
+			EndpointSpecJson: spec,
+		})
+
+	return resp, err
+}
+
+func (cmd apiDiscover) Execute(_ []string) error {
+	defer mbp.InitDiagnosticsAndRecover(cmd.Diagnostics)()
+	mbp.InitLog(cmd.Log)
+
+	logrus.WithFields(logrus.Fields{
+		"config":    cmd,
+		"version":   mbp.Version,
+		"buildDate": mbp.BuildDate,
+	}).Info("flowctl configuration")
+	pb.RegisterGRPCDispatcher("local")
+
+	var resp, err = cmd.execute(context.Background())
+	if err != nil {
+		return err
+	}
+
+	if cmd.Output == "json" {
+		return (&jsonpb.Marshaler{}).Marshal(os.Stdout, resp)
+	} else if cmd.Output == "proto" {
+		var b, err = resp.Marshal()
+		if err != nil {
+			return err
+		}
+		_, err = os.Stdout.Write(b)
+		return err
+	} else {
+		panic(cmd.Output)
+	}
+}
+
+func readConfig(path string) (raw json.RawMessage, err error) {
+	var iface interface{}
+
+	if r, err := os.Open(path); err != nil {
+		return nil, fmt.Errorf("opening config: %w", err)
+	} else if err = yaml.NewDecoder(r).Decode(&iface); err != nil {
+		return nil, fmt.Errorf("decoding config: %w", err)
+	} else if raw, err = json.Marshal(iface); err != nil {
+		return nil, fmt.Errorf("encoding JSON config: %w", err)
+	}
+
+	return raw, nil
+}
