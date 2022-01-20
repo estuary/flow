8e1cf1872160762cce34b8051e3120400dfa0ad5
Alex Burkhart <alex@estuary.dev>
2022-01-20 18:43:32 -0500

    Add better handling of shutdown signals
    Docker sends a SIGTERM and this wasn't being picked up. The server would
    shutdown when run locally, but not when run inside Docker.

---
+++ crates/control/src/shutdown.rs
@@ -0,0 +1,27 @@
+use tracing::info;
+
+/// Sets up a termination signal handlers. The Future returned by this async
+/// function will resolve when one of these handlers is triggered.
+pub(crate) async fn signal() {
+    use tokio::signal;
+
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Shutdown signal received, starting graceful shutdown");
+}
