6bc561eebb5f1fe44f5c2c0b7b51a7e08ad29f73
Alex Burkhart <alex@estuary.dev>
2022-01-21 17:25:58 -0500

    Extract routes module

--- crates/control/src/lib.rs
+++ crates/control/src/lib.rs
@@ -2,22 +2,20 @@ use std::fmt::Debug;
 use std::future::Future;
 use std::net::TcpListener;
 
-use axum::Json;
 use axum::{routing::get, Router};
-use chrono::Utc;
-use serde::{Deserialize, Serialize};
 use tower::{limit::ConcurrencyLimitLayer, ServiceBuilder};
 use tower_http::trace::TraceLayer;
 
 pub mod config;
+mod routes;
 mod shutdown;
 
 pub fn run(
     listener: TcpListener,
 ) -> anyhow::Result<impl Future<Output = Result<(), hyper::Error>>> {
     let app = Router::new()
-        .route("/health_check", get(health_check))
-        .route("/connectors", get(list_connectors))
+        .route("/health_check", get(routes::health_check))
+        .route("/connectors", get(routes::list_connectors))
         .layer(
             ServiceBuilder::new()
                 .layer(TraceLayer::new_for_http())
@@ -29,38 +27,4 @@ pub fn run(
         .with_graceful_shutdown(shutdown::signal());
 
     Ok(server)
-}
-
-async fn health_check() -> String {
-    format!("{}", Utc::now())
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "snake_case")]
-enum ConnectorType {
-    Source,
-    Materialization,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Connector {
-    description: String,
-    image: String,
-    name: String,
-    owner: String,
-    r#type: ConnectorType,
-    tags: Vec<String>,
-}
-
-async fn list_connectors() -> Json<Vec<Connector>> {
-    let connectors = vec![Connector {
-        description: "A flood of greetings.".to_owned(),
-        image: "ghcr.io/estuary/source-hello-world".to_owned(),
-        name: "source-hello-world".to_owned(),
-        owner: "Estuary".to_owned(),
-        r#type: ConnectorType::Source,
-        tags: vec!["dev".to_owned()],
-    }];
-
-    Json(connectors)
 }
