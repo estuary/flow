1e522091f688cc4ef94dd108a4a55b4ea3ca1249
Alex Burkhart <alex@estuary.dev>
2022-01-28 16:30:56 -0500

    Add endpoint to create connectors
    This allows us to manage connectors without mucking about in the
    database directly.
    
    This will be hidden from non-Estuary users behind auth...when we have
    an authN and authZ system. Until then, it's only accessible to us
    anyway.

--- crates/control/src/controllers/connectors.rs
+++ crates/control/src/controllers/connectors.rs
@@ -5,11 +5,25 @@ use hyper::StatusCode;
 use sqlx::PgPool;
 
 use crate::controllers::Payload;
+use crate::models::connectors::CreateConnector;
 use crate::repo::connectors as connectors_repo;
 
 pub async fn index(Extension(db): Extension<PgPool>) -> impl IntoResponse {
     match connectors_repo::fetch_all(&db).await {
         Ok(connectors) => (StatusCode::OK, Json(Payload::Data(connectors))),
+        Err(error) => (
+            StatusCode::UNPROCESSABLE_ENTITY,
+            Json(Payload::Error(error.to_string())),
+        ),
+    }
+}
+
+pub async fn create(
+    Extension(db): Extension<PgPool>,
+    Json(input): Json<CreateConnector>,
+) -> impl IntoResponse {
+    match connectors_repo::insert(&db, input).await {
+        Ok(connector) => (StatusCode::CREATED, Json(Payload::Data(connector))),
         Err(error) => (
             StatusCode::UNPROCESSABLE_ENTITY,
             Json(Payload::Error(error.to_string())),
