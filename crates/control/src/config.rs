dd850f777c3ab373de41436150d25d4581c26515
Alex Burkhart <alex@estuary.dev>
2022-02-02 14:08:33 -0500

    Ditch SQLx offline mode
    The way offline mode works feels like an unnecessary stumbling block.
    It's necessary to provide the sqlx macros with a database connection OR
    the offline cache of all the queries in the system. Either way can solve
    the bootstrapping problem. The docker build already has a database
    connection available, so that isn't actually a problem. However,
    remembering to update the `sqlx-data.json` file every time the app
    changes *is* quite annoying.
    
    So, rather than fighting against sqlx's desire for a DATABASE_URL env
    var, embrace it. It knows how to read a .env file, so use that as the
    source of truth for this value. The app configuration system will read
    this value and parse it to allow overriding any specific components
    individually. The user doesn't need to worry about it, the database
    always connects to the database it should. No cache needs to be updated
    with every commit.

--- crates/control/src/config.rs
+++ crates/control/src/config.rs
@@ -4,6 +4,7 @@ use serde::Deserialize;
 pub mod app_env;
 
 pub use app_env::app_env;
+use tracing::info;
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
@@ -44,7 +45,11 @@ impl DatabaseSettings {
 pub fn settings() -> &'static Settings {
     static SETTINGS: OnceCell<Settings> = OnceCell::new();
 
-    SETTINGS.get_or_init(|| load_settings().expect("Failed to load settings"))
+    SETTINGS.get_or_init(|| {
+        load_dotenv(".env");
+        load_dotenv(&format!(".env.{}", app_env().as_str()));
+        load_settings().expect("Failed to load settings")
+    })
 }
 
 fn load_settings() -> Result<Settings, config::ConfigError> {
@@ -60,8 +65,76 @@ fn load_settings() -> Result<Settings, config::ConfigError> {
     // Load app_env-specific settings
     config.merge(config::File::from(config_dir.join(app_env().as_str())).required(true))?;
 
+    // Parse the DATABASE_URL env var for database settings
+    merge_database_url(&mut config).expect("to parse the database url");
+
     // Load settings from ENV_VARs
-    config.merge(config::Environment::with_prefix("CONTROL").separator("_"))?;
+    config.merge(config::Environment::with_prefix("CONTROL").separator("__"))?;
 
     config.try_into()
+}
+
+/// If we have a .env file to load. In production, we'll be loading k8s secrets
+/// into the env, so we won't have a .env file.
+fn load_dotenv(filename: &str) {
+    match dotenv::from_filename(filename) {
+        Ok(path) => {
+            info!("Loaded env from {}.", path.to_str().unwrap_or(filename));
+        }
+        Err(dotenv::Error::LineParse(contents, line_num)) => {
+            panic!(
+                "Found a {} file, but it was malformed on Line {}: {}",
+                filename, line_num, contents
+            )
+        }
+        Err(e) => {
+            info!("No {} file found: {}", filename, e)
+        }
+    };
+}
+
+/// SQLx expects the DATABASE_URL in the env, and this makes constructing it in
+/// the config files a bit difficult. The `query!` macros read from the env or a
+/// .env file exclusively, and if it isn't defined the project fails to compile.
+///
+/// Rather than duplicating the config, let's use the .env files to store the
+/// DATABASE_URL and merge them into our regular settings. There's one single
+/// place to define it, but users access the value like any other setting.
+///
+/// Any component of the DATABASE_URL can be overridden using a specific env var.
+/// * ex. `CONTROL_DATABASE__DB_NAME=foobar`
+fn merge_database_url(config: &mut config::Config) -> anyhow::Result<()> {
+    let db_str = match std::env::var("DATABASE_URL") {
+        Ok(url) => url,
+        Err(_e) => {
+            // If there's no DATABASE_URL set by the environment, that's not an
+            // error. We'll use the config files or specific env vars instead.
+            return Ok(());
+        }
+    };
+
+    let url = url::Url::parse(&db_str)?;
+
+    if !url.username().is_empty() {
+        config.set_default("database.username", url.username())?;
+    }
+
+    if let Some(pw) = url.password() {
+        config.set_default("database.password", pw)?;
+    }
+
+    if let Some(host) = url.host_str() {
+        config.set_default("database.host", host)?;
+    }
+
+    if let Some(port) = url.port() {
+        config.set_default("database.port", port.to_string())?;
+    }
+
+    let path = url.path();
+    if !path.is_empty() && path.starts_with('/') && !(&path[1..]).contains('/') {
+        config.set_default("database.db_name", &path[1..])?;
+    }
+
+    Ok(())
 }
