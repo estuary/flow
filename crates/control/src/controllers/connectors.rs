6552439fda8f235003693dd2536a6deaa177f97a
Alex Burkhart <alex@estuary.dev>
2022-02-03 13:10:05 -0500

    Clean up controller error handling
    Now that we can automatically render errors as appropriate responses, we
    can use `?` to reduce the boilerplate in our http handlers.

--- crates/control/src/controllers/connectors.rs
+++ crates/control/src/controllers/connectors.rs
@@ -6,43 +6,32 @@ use hyper::StatusCode;
 use sqlx::PgPool;
 
 use crate::controllers::Payload;
+use crate::error::AppError;
 use crate::models::connectors::CreateConnector;
 use crate::models::Id;
 use crate::repo::connector_images as images_repo;
 use crate::repo::connectors as connectors_repo;
 
-pub async fn index(Extension(db): Extension<PgPool>) -> impl IntoResponse {
-    match connectors_repo::fetch_all(&db).await {
-        Ok(connectors) => (StatusCode::OK, Json(Payload::Data(connectors))),
-        Err(error) => (
-            StatusCode::UNPROCESSABLE_ENTITY,
-            Json(Payload::Error(error.to_string())),
-        ),
-    }
+pub async fn index(Extension(db): Extension<PgPool>) -> Result<impl IntoResponse, AppError> {
+    let connectors = connectors_repo::fetch_all(&db).await?;
+
+    Ok((StatusCode::OK, Json(Payload::Data(connectors))))
 }
 
 pub async fn create(
     Extension(db): Extension<PgPool>,
     Json(input): Json<CreateConnector>,
-) -> impl IntoResponse {
-    match connectors_repo::insert(&db, input).await {
-        Ok(connector) => (StatusCode::CREATED, Json(Payload::Data(connector))),
-        Err(error) => (
-            StatusCode::UNPROCESSABLE_ENTITY,
-            Json(Payload::Error(error.to_string())),
-        ),
-    }
+) -> Result<impl IntoResponse, AppError> {
+    let connector = connectors_repo::insert(&db, input).await?;
+
+    Ok((StatusCode::CREATED, Json(Payload::Data(connector))))
 }
 
 pub async fn images(
     Extension(db): Extension<PgPool>,
     Path(connector_id): Path<Id>,
-) -> impl IntoResponse {
-    match images_repo::fetch_all_for_connector(&db, connector_id).await {
-        Ok(images) => (StatusCode::OK, Json(Payload::Data(images))),
-        Err(error) => (
-            StatusCode::UNPROCESSABLE_ENTITY,
-            Json(Payload::Error(error.to_string())),
-        ),
-    }
+) -> Result<impl IntoResponse, AppError> {
+    let images = images_repo::fetch_all_for_connector(&db, connector_id).await?;
+
+    Ok((StatusCode::OK, Json(Payload::Data(images))))
 }
