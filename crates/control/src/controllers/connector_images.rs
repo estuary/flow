05030ef21b16bf583cfbc53a8d328b3138cbd19b
Alex Burkhart <alex@estuary.dev>
2022-02-03 13:10:05 -0500

    Clean up controller error handling
    Now that we can automatically render errors as appropriate responses, we
    can use `?` to reduce the boilerplate in our http handlers.

--- crates/control/src/controllers/connector_images.rs
+++ crates/control/src/controllers/connector_images.rs
@@ -5,28 +5,21 @@ use hyper::StatusCode;
 use sqlx::PgPool;
 
 use crate::controllers::Payload;
+use crate::error::AppError;
 use crate::models::connector_images::CreateConnectorImage;
 use crate::repo::connector_images as images_repo;
 
-pub async fn index(Extension(db): Extension<PgPool>) -> impl IntoResponse {
-    match images_repo::fetch_all(&db).await {
-        Ok(connectors) => (StatusCode::OK, Json(Payload::Data(connectors))),
-        Err(error) => (
-            StatusCode::UNPROCESSABLE_ENTITY,
-            Json(Payload::Error(error.to_string())),
-        ),
-    }
+pub async fn index(Extension(db): Extension<PgPool>) -> Result<impl IntoResponse, AppError> {
+    let images = images_repo::fetch_all(&db).await?;
+
+    Ok((StatusCode::OK, Json(Payload::Data(images))))
 }
 
 pub async fn create(
     Extension(db): Extension<PgPool>,
     Json(input): Json<CreateConnectorImage>,
-) -> impl IntoResponse {
-    match images_repo::insert(&db, input).await {
-        Ok(connector) => (StatusCode::CREATED, Json(Payload::Data(connector))),
-        Err(error) => (
-            StatusCode::UNPROCESSABLE_ENTITY,
-            Json(Payload::Error(error.to_string())),
-        ),
-    }
+) -> Result<impl IntoResponse, AppError> {
+    let image = images_repo::insert(&db, input).await?;
+
+    Ok((StatusCode::CREATED, Json(Payload::Data(image))))
 }
