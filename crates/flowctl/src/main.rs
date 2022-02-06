82a793bc8544ee92a965c95f47d9da46cca68aa2
Phil Fried <phil@estuary.dev>
2022-02-06 17:43:18 -0500

    Integrate schemalate into flowctl-rs, plus a few
    - Migrate schemalate from separate binaries to internal subcommands.
    - Handle --version in flowctl-rs, which is determined in Makefile and
      passed as the `FLOW_VERSION` environment variable.
    - Remove usages of `or_bail` in the combine subcommand, in favor of just
      returning an error. The rationale being that using `or_bail` would
      make composition of subcommands needlessly painful.

--- crates/flowctl/src/main.rs
+++ crates/flowctl/src/main.rs
@@ -3,10 +3,15 @@ mod external;
 
 use clap::Parser;
 use external::{exec_go_flowctl, ExternalSubcommand};
+use flow_cli_common::{init_logging, LogArgs, NoArgs};
+
+// Note that the FLOW_VERSION referenced below is set in Makefile, but there is a default also
+// provided in .cargo/config
 
 /// flowctl is a CLI for interacting with Flow data planes (and soon, control planes).
 #[derive(Debug, Parser)]
-struct Cli {
+#[clap(name = "flowctl-rs", version = env!("FLOW_VERSION"))]
+struct Flowctl {
     #[clap(subcommand)]
     subcommand: Subcommand,
 }
@@ -20,25 +25,47 @@ enum Subcommand {
     External(ExternalSubcommand),
 }
 
+/// A wrapper type for internal subcommand arguments, which defines logging arguments that apply to
+/// all internal subcommands.
+#[derive(Debug, clap::Args)]
+pub struct InternalSubcommandArgs<T: clap::Args + std::fmt::Debug> {
+    #[clap(flatten)]
+    log_args: LogArgs,
+
+    #[clap(flatten)]
+    args: T,
+}
+
 #[derive(Debug, clap::Subcommand)]
 #[clap(rename_all = "kebab-case")]
 enum InternalSubcommand {
     /// Reduce JSON documents by key and print the results to stdout
-    Combine(combine::CombineArgs),
+    Combine(InternalSubcommandArgs<combine::CombineArgs>),
+
+    /// Generates TypeScript types from JSON Schemas
+    SchemaToTypescript(InternalSubcommandArgs<schemalate::typescript::Args>),
+    /// Generates Markdown documentation for the given JSON schema
+    SchemaToMarkdown(InternalSubcommandArgs<NoArgs>),
 }
 
 fn main() -> Result<(), anyhow::Error> {
     // calling parse will automatically handle --help and --version flags that were provided as
     // top-level arguments. If it does, then it will exit(0) automatically. This will not be the
     // case for external subcommands, though, as they handle their own --help and --version flags.
-    let cli = Cli::parse();
+    let cli = Flowctl::parse();
     run_subcommand(cli.subcommand)
 }
 
 fn run_subcommand(subcommand: Subcommand) -> Result<(), anyhow::Error> {
+    use InternalSubcommand::*;
+    use Subcommand::*;
+
     match subcommand {
-        Subcommand::Internal(internal) => execute_internal_subcommand(internal),
-        Subcommand::External(external) => {
+        Internal(Combine(args)) => run_internal(args, combine::run),
+        Internal(SchemaToTypescript(args)) => run_internal(args, schemalate::typescript::run),
+        Internal(SchemaToMarkdown(args)) => run_internal(args, schemalate::markdown::run),
+
+        External(external) => {
             let (subcommand, external_args) = external.into_subcommand_and_args();
             let err = exec_go_flowctl(subcommand, &external_args);
             Err(err.into())
@@ -46,8 +73,20 @@ fn run_subcommand(subcommand: Subcommand) -> Result<(), anyhow::Error> {
     }
 }
 
-fn execute_internal_subcommand(subcommand: InternalSubcommand) -> Result<(), anyhow::Error> {
-    match subcommand {
-        InternalSubcommand::Combine(args) => combine::run(args),
+fn run_internal<T, F>(
+    subcommand_args: InternalSubcommandArgs<T>,
+    run_fn: F,
+) -> Result<(), anyhow::Error>
+where
+    T: clap::Args + std::fmt::Debug,
+    F: FnOnce(T) -> Result<(), anyhow::Error>,
+{
+    let InternalSubcommandArgs { log_args, args } = subcommand_args;
+
+    init_logging(&log_args);
+    let result = run_fn(args);
+    if let Err(err) = result.as_ref() {
+        tracing::error!(error = ?err, "subcommand failed");
     }
+    result
 }
--- crates/schemalate/src/lib.rs
+++ crates/schemalate/src/lib.rs
@@ -1,1 +1,9 @@
+//! JSON Schema + Translate = Schemalate
+//! Contains modules for generating various things from JSON schemas.
+
+/// Generates Markdown documentation of the fields in a schema.
+pub mod markdown;
+
+/// Generates Typescript types that serialize/deserialize into/from JSON that validates against the
+/// schema.
 pub mod typescript;
