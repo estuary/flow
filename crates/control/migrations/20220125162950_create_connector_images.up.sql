1eec92640928b3ba33ce809f2fb139be3bbf0c59
Alex Burkhart <alex@estuary.dev>
2022-02-07 17:25:19 -0500

    Rename images.image/sha256 => name/digest
    Again, just slightly better terminology. Changing it now is easier than
    later.
    
    Also removes the image+tag index, which we *may* add back, but may allow
    this. We need to discuss further.

--- crates/control/migrations/20220125162950_create_connector_images.up.sql
+++ crates/control/migrations/20220125162950_create_connector_images.up.sql
@@ -2,12 +2,11 @@ CREATE TABLE connector_images (
   "id" BIGINT NOT NULL DEFAULT id_generator(),
   PRIMARY KEY (id),
   "connector_id" BIGINT NOT NULL REFERENCES connectors("id"),
-  "image" TEXT NOT NULL,
+  "name" TEXT NOT NULL,
   "tag" TEXT NOT NULL,
-  "sha256" TEXT NOT NULL,
+  "digest" TEXT NOT NULL,
   "created_at" TIMESTAMPTZ NOT NULL,
   "updated_at" TIMESTAMPTZ NOT NULL
 );
 
-CREATE INDEX idx_connector_images_image ON connector_images("image");
-CREATE UNIQUE INDEX idx_connector_images_image_tag ON connector_images("image", "tag");
+CREATE INDEX idx_connector_images_image ON connector_images("name");
