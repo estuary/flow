f15d2ef94b5b1bca881ec0f8c2b6c32f7e75bd68
Alex Burkhart <alex@estuary.dev>
2022-02-09 17:31:33 -0500

    Move tests to the right file
    Doh!

--- crates/control/tests/it/connector_images.rs
+++ crates/control/tests/it/connector_images.rs
@@ -107,3 +107,165 @@ async fn connector_images_create_test() {
         ".data.attributes.updated_at" => "[datetime]",
     });
 }
+
+#[tokio::test]
+async fn connectors_show_test() {
+    let db = support::test_db_pool(support::function_name!())
+        .await
+        .expect("Failed to acquire a database connection");
+    let server_address = spawn_app(db.clone())
+        .await
+        .expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+
+    let connector = insert_connector(
+        &db,
+        CreateConnector {
+            description: "A flood greetings.".to_owned(),
+            name: "Hello World".to_owned(),
+            maintainer: "Estuary Technologies".to_owned(),
+            r#type: ConnectorType::Source,
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let image = insert(
+        &db,
+        CreateConnectorImage {
+            connector_id: connector.id,
+            name: "ghcr.io/estuary/source-hello-world".to_owned(),
+            digest: "15751ba960870e5ba233ebfe9663fe8a236c8ce213b43fbf4cccc4e485594600".to_owned(),
+            tag: "01fb856".to_owned(),
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let response = client
+        .get(format!(
+            "http://{}/connector_images/{}",
+            server_address, image.id
+        ))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+    let redactor = Redactor::default()
+        .redact(connector.id, "c1")
+        .redact(image.id, "i1");
+    assert_json_snapshot!(redactor.response_json(response).await.unwrap(), {
+        ".data.attributes.created_at" => "[datetime]",
+        ".data.attributes.updated_at" => "[datetime]",
+    });
+}
+
+#[tokio::test]
+async fn connectors_spec_test() {
+    let db = support::test_db_pool(support::function_name!())
+        .await
+        .expect("Failed to acquire a database connection");
+    let server_address = spawn_app(db.clone())
+        .await
+        .expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+
+    let connector = insert_connector(
+        &db,
+        CreateConnector {
+            description: "A flood greetings.".to_owned(),
+            name: "Hello World".to_owned(),
+            maintainer: "Estuary Technologies".to_owned(),
+            r#type: ConnectorType::Source,
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let image = insert(
+        &db,
+        CreateConnectorImage {
+            connector_id: connector.id,
+            name: "ghcr.io/estuary/source-hello-world".to_owned(),
+            digest: "15751ba960870e5ba233ebfe9663fe8a236c8ce213b43fbf4cccc4e485594600".to_owned(),
+            tag: "01fb856".to_owned(),
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let response = client
+        .get(format!(
+            "http://{}/connector_images/{}/spec",
+            server_address, image.id
+        ))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+    let redactor = Redactor::default()
+        .redact(connector.id, "c1")
+        .redact(image.id, "i1");
+    assert_json_snapshot!(redactor.response_json(response).await.unwrap(), {
+        ".data.id" => "[nonce]",
+        ".data.attributes.created_at" => "[datetime]",
+        ".data.attributes.updated_at" => "[datetime]",
+    });
+}
+
+#[tokio::test]
+async fn connectors_discovery_test() {
+    let db = support::test_db_pool(support::function_name!())
+        .await
+        .expect("Failed to acquire a database connection");
+    let server_address = spawn_app(db.clone())
+        .await
+        .expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+
+    let connector = insert_connector(
+        &db,
+        CreateConnector {
+            description: "A flood greetings.".to_owned(),
+            name: "Hello World".to_owned(),
+            maintainer: "Estuary Technologies".to_owned(),
+            r#type: ConnectorType::Source,
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let image = insert(
+        &db,
+        CreateConnectorImage {
+            connector_id: connector.id,
+            name: "ghcr.io/estuary/source-hello-world".to_owned(),
+            digest: "15751ba960870e5ba233ebfe9663fe8a236c8ce213b43fbf4cccc4e485594600".to_owned(),
+            tag: "01fb856".to_owned(),
+        },
+    )
+    .await
+    .expect("to insert test data");
+
+    let response = client
+        .post(format!(
+            "http://{}/connector_images/{}/discovery",
+            server_address, image.id
+        ))
+        .json(&serde_json::json!({"greetings": 10}))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+    let redactor = Redactor::default()
+        .redact(connector.id, "c1")
+        .redact(image.id, "i1");
+    assert_json_snapshot!(redactor.response_json(response).await.unwrap(), {
+        ".data.id" => "[nonce]",
+        ".data.attributes.created_at" => "[datetime]",
+        ".data.attributes.updated_at" => "[datetime]",
+    });
+}
