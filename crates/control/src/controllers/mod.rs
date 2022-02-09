c82e31e3fda216322d8ce5e7d3fad6300ae8239b
Alex Burkhart <alex@estuary.dev>
2022-02-09 17:26:16 -0500

    Cleanup dead code

--- crates/control/src/controllers/mod.rs
+++ crates/control/src/controllers/mod.rs
@@ -2,14 +2,3 @@ pub mod connector_images;
 pub mod connectors;
 pub mod health_check;
 pub mod json_api;
-
-/// Simple wrapper to differentiate metadata from primary data.
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub enum Payload<D> {
-    Data(D),
-    Error(String),
-}
-
-// Temporary re-export.
-pub use json_api::RawJson;
