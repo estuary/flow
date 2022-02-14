f6ea0ff75f4b17d2cf724ef630815cae72b84fea
Alex Burkhart <alex@estuary.dev>
2022-02-14 14:04:49 -0500

    Simplify application configuration
    * Removes hierarchical config files. I'm going to add a command line
    flag for pointing at a specific config file. This is how end-users
    (non-Flow-devs) will launch the control plane. These settings can still
    be overridden by the environment, but let's simplify this for now.
    * Settings must be explicitly loaded from a specific config file before
    being accessed. This just makes this contract more explicit.
    * Ditches `control_test` database. We can clone each test's database
    schema from the `control_development` database. This simplifies the
    subsequent DATABASE_URL handling.
    * Assumes the DATABASE_URL is present in the environment. There is only
    one "local" database to worry about now, so there's no need to switch
    between values. Use direnv to load this value automatically.

--- crates/control/src/startup.rs
+++ crates/control/src/startup.rs
@@ -6,8 +6,9 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use tower::limit::ConcurrencyLimitLayer;
 use tower_http::trace::TraceLayer;
+use tracing::info;
 
-use crate::config::DatabaseSettings;
+use crate::config::{self, DatabaseSettings};
 use crate::cors;
 use crate::routes::routes;
 use crate::shutdown;
@@ -16,6 +17,12 @@ pub fn run(
     listener: TcpListener,
     db: PgPool,
 ) -> anyhow::Result<impl Future<Output = Result<(), hyper::Error>>> {
+    info!("Running in {} mode", config::app_env().as_str());
+    info!(
+        "Listening on http://{}",
+        config::settings().application.address()
+    );
+
     let app = routes()
         .layer(cors::cors_layer())
         .layer(TraceLayer::new_for_http())
