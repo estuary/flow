b25c3e3e7d8aaa42511a0631101cc60ede611bf5
Alex Burkhart <alex@estuary.dev>
2022-02-14 16:11:03 -0500

    Use clap to parse log arguments in dev mode
    I'd accidentally removed the support for tracing. We should use the same
    methodology for setting up logging as the rest of the project.

--- crates/control/src/main.rs
+++ crates/control/src/main.rs
@@ -1,9 +1,18 @@
+use clap::Parser;
+use flow_cli_common::LogArgs;
 use std::net::TcpListener;
 
-use control::config::app_env::{self, AppEnv};
-
 use control::config;
+use control::config::app_env::{self, AppEnv};
 use control::startup;
+
+/// Runs the control plane api server in development mode.
+#[derive(Debug, Parser)]
+#[clap(author, name = "control", version = "dev", about)]
+struct Args {
+    #[clap(flatten)]
+    pub log_args: LogArgs,
+}
 
 /// Runs the development server. This sets a few defaults:
 /// * Runs in `AppEnv::Development` mode
@@ -14,6 +23,9 @@ use control::startup;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     app_env::force_env(AppEnv::Development);
+
+    let args = Args::parse();
+    flow_cli_common::init_logging(&args.log_args);
 
     let settings = config::load_settings("config/development.toml")?;
     let listener = TcpListener::bind(settings.application.address())?;
