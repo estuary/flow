6c0f02ffffbd707c7fd623ca6cb1d9d8a4906bbb
Phil Fried <phil@estuary.dev>
2022-02-14 10:05:53 -0500

    Add logs subcommmand to flowctl-rs
    Adds the `logs` subcommand for reading task logs from flowctl-rs.
    This is implemented as an alias for `flowctl journals read` with a label
    selector that's generated for the task(s) identified in the arguments.
    This also involved a significant refactoring of the flowctl and
    flow_cli_common crates around the execution of external binaries.

--- crates/flowctl/src/main.rs
+++ crates/flowctl/src/main.rs
@@ -1,91 +1,11 @@
-mod combine;
-mod external;
-
 use clap::Parser;
-use external::{exec_flow_schemalate, exec_go_flowctl, ExternalArgs, FlowctlGoSubcommand};
-use flow_cli_common::{init_logging, LogArgs};
+use flowctl::{run_subcommand, Flowctl};
 
-// Note that the FLOW_VERSION referenced below is set in Makefile, but there is a default also
-// provided in .cargo/config
-
-/// flowctl is a CLI for interacting with Flow data planes (and soon, control planes).
-#[derive(Debug, Parser)]
-#[clap(author, name = "flowctl-rs", version = env!("FLOW_VERSION"))]
-struct Flowctl {
-    #[clap(subcommand)]
-    subcommand: Subcommand,
-}
-
-#[derive(Debug, clap::Subcommand)]
-#[clap(rename_all = "kebab-case")]
-enum Subcommand {
-    #[clap(flatten)]
-    Internal(InternalSubcommand),
-    #[clap(flatten)]
-    FlowctlGo(FlowctlGoSubcommand),
-    /// Tools for generating various things from JSON schemas
-    Schemalate(ExternalArgs),
-}
-
-/// A wrapper type for internal subcommand arguments, which defines logging arguments that apply to
-/// all internal subcommands.
-#[derive(Debug, clap::Args)]
-pub struct InternalSubcommandArgs<T: clap::Args + std::fmt::Debug> {
-    #[clap(flatten)]
-    log_args: LogArgs,
-
-    #[clap(flatten)]
-    args: T,
-}
-
-#[derive(Debug, clap::Subcommand)]
-#[clap(rename_all = "kebab-case")]
-enum InternalSubcommand {
-    /// Reduce JSON documents by key and print the results to stdout
-    Combine(InternalSubcommandArgs<combine::CombineArgs>),
-}
-
-fn main() -> Result<(), anyhow::Error> {
+fn main() {
     // calling parse will automatically handle --help and --version flags that were provided as
     // top-level arguments. If it does, then it will exit(0) automatically. This will not be the
     // case for external subcommands, though, as they handle their own --help and --version flags.
     let cli = Flowctl::parse();
-    run_subcommand(cli.subcommand)
-}
-
-fn run_subcommand(subcommand: Subcommand) -> Result<(), anyhow::Error> {
-    use InternalSubcommand::*;
-    use Subcommand::*;
-
-    match subcommand {
-        Internal(Combine(args)) => run_internal(args, combine::run),
-
-        FlowctlGo(external) => {
-            let (subcommand, external_args) = external.into_subcommand_and_args();
-            let err = exec_go_flowctl(subcommand, &external_args);
-            Err(err.into())
-        }
-        Schemalate(args) => {
-            let err = exec_flow_schemalate(&args);
-            Err(err.into())
-        }
-    }
-}
-
-fn run_internal<T, F>(
-    subcommand_args: InternalSubcommandArgs<T>,
-    run_fn: F,
-) -> Result<(), anyhow::Error>
-where
-    T: clap::Args + std::fmt::Debug,
-    F: FnOnce(T) -> Result<(), anyhow::Error>,
-{
-    let InternalSubcommandArgs { log_args, args } = subcommand_args;
-
-    init_logging(&log_args);
-    let result = run_fn(args);
-    if let Err(err) = result.as_ref() {
-        tracing::error!(error = ?err, "subcommand failed");
-    }
-    result
+    flow_cli_common::run_cli_main(cli.subcommand, run_subcommand);
+    unreachable!();
 }
