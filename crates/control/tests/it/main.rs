a25daa9b222880fed2c30d557b546a817adb793b
Alex Burkhart <alex@estuary.dev>
2022-02-14 14:04:49 -0500

    Simplify application configuration
    * Removes hierarchical config files. I'm going to add a command line
    flag for pointing at a specific config file. This is how end-users
    (non-Flow-devs) will launch the control plane. These settings can still
    be overridden by the environment, but let's simplify this for now.
    * Settings must be explicitly loaded from a specific config file before
    being accessed. This just makes this contract more explicit.
    * Ditches `control_test` database. We can clone each test's database
    schema from the `control_development` database. This simplifies the
    subsequent DATABASE_URL handling.
    * Assumes the DATABASE_URL is present in the environment. There is only
    one "local" database to worry about now, so there's no need to switch
    between values. Use direnv to load this value automatically.

--- crates/control/tests/it/main.rs
+++ crates/control/tests/it/main.rs
@@ -16,14 +16,12 @@ mod support;
 #[ctor]
 fn setup() {
     app_env::force_env(AppEnv::Test);
-
-    let test_db = support::TestDatabase::new();
-
-    // Dropping the database may not be possible, as it may not yet exist, but this is okay.
-    let _ = test_db.drop();
+    control::config::load_settings("config/test.toml").expect("to load configuration file");
 
     // Setup will create the database and run all migrations.
-    test_db.setup().expect("To setup the database");
+    support::TestDatabase::new()
+        .setup()
+        .expect("To setup the database");
 }
 
 /// Teardown runs exactly once after all tests have run. This allows the test
