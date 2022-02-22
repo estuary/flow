4c70eef04ebb2a6033a0fc8072db014c0b1fd4c2
Alex Burkhart <alex@estuary.dev>
2022-02-22 17:22:29 -0500

    Extract url_for helper
    This was unintentionally duplicated as I was working on the last PR. It
    doesn't really have a great home yet, but this is better than it was.
    
    Axum is getting typesafe routing in the next release, so I'm hesitant to
    go too far down this road until we upgrade to that.

--- crates/control/src/controllers/mod.rs
+++ crates/control/src/controllers/mod.rs
@@ -3,3 +3,14 @@ pub mod connector_images;
 pub mod connectors;
 pub mod health_check;
 pub mod json_api;
+
+/// Generates an absolute url to the path based on the application address.
+pub fn url_for(path: impl AsRef<str>) -> String {
+    use crate::config::settings;
+
+    format!(
+        "http://{}{}",
+        settings().application.address(),
+        path.as_ref()
+    )
+}
