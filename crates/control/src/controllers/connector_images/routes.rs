b2948e26a82aa886a2e3458ea55ca5efc41ec468
Alex Burkhart <alex@estuary.dev>
2022-02-22 17:22:29 -0500

    Extract url_for helper
    This was unintentionally duplicated as I was working on the last PR. It
    doesn't really have a great home yet, but this is better than it was.
    
    Axum is getting typesafe routing in the next release, so I'm hesitant to
    go too far down this road until we upgrade to that.

--- crates/control/src/controllers/connector_images/routes.rs
+++ crates/control/src/controllers/connector_images/routes.rs
@@ -1,25 +1,21 @@
-use crate::config::settings;
+use crate::controllers::url_for;
 use crate::models::Id;
 
 pub fn index() -> String {
-    prefixed("/connector_images")
+    url_for("/connector_images")
 }
 
 pub fn show(image_id: Id) -> String {
-    prefixed(format!("/connector_images/{}", image_id.to_string()))
+    url_for(format!("/connector_images/{}", image_id.to_string()))
 }
 
 pub fn spec(image_id: Id) -> String {
-    prefixed(format!("/connector_images/{}/spec", image_id.to_string()))
+    url_for(format!("/connector_images/{}/spec", image_id.to_string()))
 }
 
 pub fn discovery(connector_id: Id) -> String {
-    prefixed(format!(
+    url_for(format!(
         "/connector_images/{}/discovery",
         connector_id.to_string()
     ))
-}
-
-fn prefixed(path: impl Into<String>) -> String {
-    format!("http://{}{}", settings().application.address(), path.into())
 }
