9e05953ff10197e4c0b715a8cdf5fa43d0d54dbf
Jixiang Jiang <jixiang@estuary.dev>
2022-02-22 09:21:37 -0500

    network-proxy changes to work with connector-proxy

--- crates/network-proxy/src/main.rs
+++ crates/network-proxy/src/main.rs
@@ -1,21 +1,29 @@
 pub mod interface;
 pub mod sshforwarding;
 pub mod errors;
-pub mod logging;
 pub mod networkproxy;
 
+use errors::Error;
+use flow_cli_common::{init_logging, LogArgs, LogFormat};
 use std::io::{self, Write};
 
 use interface::NetworkProxyConfig;
-use logging::{init_tracing, Must};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    init_tracing();
-    let proxy_config: NetworkProxyConfig = serde_json::from_reader(io::stdin()).or_bail("Failed to deserialize network proxy config.");
+    init_logging(&LogArgs{level: "info".to_string(), format: Some(LogFormat::Json)});
+    if let Err(err) = run().await.as_ref() {
+        tracing::error!(error = ?err, "network proxy failed.");
+        std::process::exit(1);
+    }
+    Ok(())
+}
+
+async fn run() -> Result<(), Error> {
+    let proxy_config: NetworkProxyConfig = serde_json::from_reader(io::stdin())?;
     let mut proxy = proxy_config.new_proxy();
 
-    proxy.prepare().await.or_bail("Failed to prepare network proxy.");
+    proxy.prepare().await?;
 
     // Write "READY" to stdio to unblock Go logic.
     // The current workflow assumes that
@@ -24,9 +32,9 @@ async fn main() -> io::Result<()> {
     // If either of the assumptions is invalid for any new proxy type, the READY-logic need to be moved to a separate task, which
     //    sends out the "READY" signal after making sure the network proxy is started and working properly.
     println!("READY");
-    io::stdout().flush().or_bail("Failed flushing output.");
+    io::stdout().flush()?;
 
-    proxy.start_serve().await.or_bail("Failed to start proxy service.");
+    proxy.start_serve().await?;
 
     Ok(())
 }
