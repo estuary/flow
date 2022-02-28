ff55d663c2cf96d8372499b46565e7ee8d486f4b
Jixiang Jiang <jixiang@estuary.dev>
2022-02-28 16:46:23 -0500

    RawValue

--- crates/connector_proxy/src/libs/network_proxy.rs
+++ crates/connector_proxy/src/libs/network_proxy.rs
@@ -3,6 +3,7 @@ use crate::libs::json::{create_root_schema, remove_subobject};
 use network_proxy::interface::NetworkProxyConfig;
 
 use schemars::schema::{RootSchema, Schema};
+use serde_json::value::RawValue;
 use tokio::sync::oneshot::{self, Receiver};
 use tokio::time::timeout;
 
@@ -10,10 +11,12 @@ pub struct NetworkProxy {}
 pub const NETWORK_PROXY_KEY: &str = "networkProxy";
 
 impl NetworkProxy {
-    pub fn extend_endpoint_schema(endpoint_spec_schema_str: &str) -> Result<String, Error> {
+    pub fn extend_endpoint_schema(
+        endpoint_spec_schema: Box<RawValue>,
+    ) -> Result<Box<RawValue>, Error> {
         let network_proxy_schema = create_root_schema::<NetworkProxyConfig>();
 
-        let mut modified_schema: RootSchema = serde_json::from_str(endpoint_spec_schema_str)?;
+        let mut modified_schema: RootSchema = serde_json::from_str(endpoint_spec_schema.get())?;
         if let Some(ref mut o) = &mut modified_schema.schema.object {
             if o.as_ref().properties.contains_key(NETWORK_PROXY_KEY) {
                 return Err(Error::DuplicatedKeyError(NETWORK_PROXY_KEY));
@@ -23,7 +26,9 @@ impl NetworkProxy {
                 Schema::Object(network_proxy_schema.schema),
             );
         }
-        serde_json::to_string_pretty(&modified_schema).map_err(Into::into)
+
+        let json = serde_json::to_string_pretty(&modified_schema)?;
+        RawValue::from_string(json).map_err(Into::into)
     }
 
     // Start the network proxy. The receiver rx will be dropped to indicate the network proxy
@@ -53,18 +58,18 @@ impl NetworkProxy {
     }
 
     pub async fn consume_network_proxy_config(
-        endpoint_spec_json_str: &str,
-    ) -> Result<String, Error> {
-        if endpoint_spec_json_str.is_empty() {
-            return Ok(endpoint_spec_json_str.to_string());
+        endpoint_spec_json: Box<RawValue>,
+    ) -> Result<Box<RawValue>, Error> {
+        if endpoint_spec_json.get().is_empty() {
+            return Ok(endpoint_spec_json);
         }
 
-        let endpoint_spec_json = serde_json::from_str(endpoint_spec_json_str)?;
-        let (network_proxy_config, endpoint_spec_json) =
-            remove_subobject(endpoint_spec_json, NETWORK_PROXY_KEY);
+        let endpoint_spec = serde_json::from_str(endpoint_spec_json.get())?;
+        let (network_proxy_config, endpoint_spec) =
+            remove_subobject(endpoint_spec, NETWORK_PROXY_KEY);
 
         let network_proxy_config: NetworkProxyConfig = match network_proxy_config {
-            None => return Ok(endpoint_spec_json_str.to_string()),
+            None => return Ok(endpoint_spec_json),
             Some(c) => serde_json::from_value(c)?,
         };
 
@@ -78,6 +83,7 @@ impl NetworkProxy {
 
         tracing::info!("network proxy started.");
 
-        serde_json::to_string_pretty(&endpoint_spec_json).map_err(Into::into)
+        let json = serde_json::to_string_pretty(&endpoint_spec)?;
+        RawValue::from_string(json).map_err(Into::into)
     }
 }
