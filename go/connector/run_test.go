1a5c8fa7509b549ef7c6a9e9928673fdc01cea5a
Jixiang Jiang <jixiang@estuary.dev>
2022-03-02 11:47:51 -0500

    remove image proxy

--- go/connector/run_test.go
+++ go/connector/run_test.go
@@ -252,3 +252,31 @@ func TestStderrCapture(t *testing.T) {
 	s.Write(bytes.Repeat([]byte("x"), maxStderrBytes))
 	require.Equal(t, maxStderrBytes, s.buffer.Len())
 }
+
+func TestRustBinDir(t *testing.T) {
+	os.Setenv("FLOW_BINARY_DIR", "/test_rustbin")
+	require.Equal(t, getRustBinDir(), "/test_rustbin")
+	os.Unsetenv("FLOW_BINARY_DIR")
+	require.Equal(t, getRustBinDir(), "/usr/local/bin")
+}
+
+func TestPrepareFlowConnectorProxyBinary(t *testing.T) {
+	var tmpRustBinDir, err = ioutil.TempDir("", "test-rustbin")
+	require.NoError(t, err)
+	err = os.WriteFile(filepath.Join(tmpRustBinDir, "flow-connector-proxy"), []byte("random content"), 0777)
+	require.NoError(t, err)
+	defer os.Remove(tmpRustBinDir)
+
+	os.Setenv("FLOW_BINARY_DIR", tmpRustBinDir)
+
+	var connectorProxyPath string
+	connectorProxyPath, err = prepareFlowConnectorProxyBinary(tmpRustBinDir)
+	require.NoError(t, err)
+	bytes, err := os.ReadFile(connectorProxyPath)
+	require.NoError(t, err)
+	require.Equal(t, "random content", string(bytes))
+}
+
+func copyFlowConnectorProxyBinary(tmpRustBinDir string) {
+	panic("unimplemented")
+}
