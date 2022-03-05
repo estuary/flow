823c61a1448e7b03bd3656bf20a7fa21a7bc52ef
Jixiang Jiang <jixiang@estuary.dev>
2022-03-05 09:42:12 -0500

    first implementation of connector proxy with airbyte
    stop and cont
    
    airbyte discover
    
    spec / validate / apply capture requests
    
    airbyte pull
    
    airbyte discover
    
    various improvements
    
    check runtime protocol
    
    check runtime protocol
    
    ready signal from stderr
    
    makefile more deps
    
    try_stream! for error processing
    
    update Cargo.lock
    
    Makefile change
    
    improve airbyte source logic
    
    ldconfig
    
    XXtesting
    
    move test to common stage
    
    NsMerge
    
    tweaks
    
    update Cargo.lock
    
    use libc instead of nix
    
    skip null field in specresult
    
    update control plane snapshots
    
    delayed process stop itself
    
    setup go in stage 2
    
    tweaks
    
    using /var/tmp
    
    send SpecRequest
    
    synthetic chekcpoint
    
    convert_ to adapt_
    
    debug - replacing sigcont with stdin
    
    simplify delay waiting logic
    
    remove a TODO

--- crates/connector_proxy/src/apis.rs
+++ crates/connector_proxy/src/apis.rs
@@ -1,8 +1,19 @@
-use crate::errors::Error;
 use bytes::Bytes;
 use clap::ArgEnum;
 use futures_core::stream::Stream;
 use std::pin::Pin;
+
+// The protocol used by FlowRuntime to speak with connector-proxy.
+// There are two ways to infer the protocol.
+// 1. From the proxy command passed in from FlowRuntime to the connector proxy.
+// 2. From the connector image labels and tags.
+// The proxy raises an error if both are inconsistent.
+#[derive(Debug, strum_macros::Display, ArgEnum, PartialEq, Clone)]
+#[strum(serialize_all = "snake_case")]
+pub enum FlowRuntimeProtocol {
+    Capture,
+    Materialize,
+}
 
 // Flow Capture operations defined in
 // https://github.com/estuary/flow/blob/master/go/protocols/capture/capture.proto
@@ -29,39 +40,6 @@ pub enum FlowMaterializeOperation {
     Transactions,
 }
 
-// To be used as a trait bound for interceptors.
-pub trait FlowOperation {}
-impl FlowOperation for FlowCaptureOperation {}
-impl FlowOperation for FlowMaterializeOperation {}
-
 // An interceptor modifies the request/response streams between Flow runtime and the connector.
 // InterceptorStream defines the type of input and output streams handled by interceptors.
 pub type InterceptorStream = Pin<Box<dyn Stream<Item = std::io::Result<Bytes>> + Send + Sync>>;
-
-// The generic param "T" below is bounded by FlowOperation.
-// A converter is a function that contains the specific stream-handling logic of an interceptor.
-type ConverterFn<T> = Box<dyn Fn(&T, InterceptorStream) -> Result<InterceptorStream, Error>>;
-// An intercept is characterized by a pair of converters, corresponding to the handling logic of request and response streams, respectively.
-pub type RequestResponseConverterPair<T> = (ConverterFn<T>, ConverterFn<T>);
-pub trait Interceptor<T: FlowOperation> {
-    fn get_converters() -> RequestResponseConverterPair<T> {
-        (
-            Box::new(|_op, stream| Ok(stream)),
-            Box::new(|_op, stream| Ok(stream)),
-        )
-    }
-}
-
-// Two converter pairs can be composed together to form a new converter pair.
-pub fn compose<T: 'static + FlowOperation>(
-    a: RequestResponseConverterPair<T>,
-    b: RequestResponseConverterPair<T>,
-) -> RequestResponseConverterPair<T> {
-    let (req_a, resp_a) = a;
-    let (req_b, resp_b) = b;
-    (
-        Box::new(move |op, stream| (req_b)(op, (req_a)(op, stream)?)),
-        // Response conversions are applied in the reverse order of the request conversions.
-        Box::new(move |op, stream| (resp_a)(op, (resp_b)(op, stream)?)),
-    )
-}
--- crates/connector_proxy/src/libs/mod.rs
+++ crates/connector_proxy/src/libs/mod.rs
@@ -1,5 +1,6 @@
+pub mod airbyte_catalog;
 pub mod command;
-pub mod image_config;
+pub mod image_inspect;
 pub mod json;
 pub mod network_proxy;
 pub mod protobuf;
