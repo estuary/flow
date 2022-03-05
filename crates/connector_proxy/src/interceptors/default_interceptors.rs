e5bcd923b8c996fd576f1555ecb4ebc46af8dfee
Jixiang Jiang <jixiang@estuary.dev>
2022-03-05 09:42:12 -0500

    with signalcont

--- crates/connector_proxy/src/interceptors/default_interceptors.rs
+++ crates/connector_proxy/src/interceptors/default_interceptors.rs
@@ -1,6 +1,59 @@
-use crate::apis::{FlowCaptureOperation, FlowMaterializeOperation, Interceptor};
+use crate::apis::{FlowCaptureOperation, FlowMaterializeOperation, Interceptor, InterceptorStream};
+use crate::errors::Error;
+use crate::libs::command::resume_process;
+
 pub struct DefaultFlowCaptureInterceptor {}
-impl Interceptor<FlowCaptureOperation> for DefaultFlowCaptureInterceptor {}
+impl Interceptor<FlowCaptureOperation> for DefaultFlowCaptureInterceptor {
+    fn convert_command_args(
+        &self,
+        op: &FlowCaptureOperation,
+        args: Vec<String>,
+    ) -> Result<Vec<String>, Error> {
+        let mut new_args = vec![op.to_string()];
+        new_args.extend_from_slice(&args);
+        Ok(new_args)
+    }
+
+    fn convert_request(
+        &self,
+        pid: Option<u32>,
+        _op: &FlowCaptureOperation,
+        stream: InterceptorStream,
+    ) -> Result<InterceptorStream, Error> {
+        match pid {
+            None => Err(Error::MissingPid),
+            Some(pid) => {
+                resume_process(pid);
+                Ok(stream)
+            }
+        }
+    }
+}
 
 pub struct DefaultFlowMaterializeInterceptor {}
-impl Interceptor<FlowMaterializeOperation> for DefaultFlowMaterializeInterceptor {}
+impl Interceptor<FlowMaterializeOperation> for DefaultFlowMaterializeInterceptor {
+    fn convert_command_args(
+        &self,
+        op: &FlowMaterializeOperation,
+        args: Vec<String>,
+    ) -> Result<Vec<String>, Error> {
+        let mut new_args = vec![op.to_string()];
+        new_args.extend_from_slice(&args);
+        Ok(new_args)
+    }
+
+    fn convert_request(
+        &self,
+        pid: Option<u32>,
+        op: &FlowMaterializeOperation,
+        stream: InterceptorStream,
+    ) -> Result<InterceptorStream, Error> {
+        match pid {
+            None => Err(Error::MissingPid),
+            Some(pid) => {
+                resume_process(pid);
+                Ok(stream)
+            }
+        }
+    }
+}
