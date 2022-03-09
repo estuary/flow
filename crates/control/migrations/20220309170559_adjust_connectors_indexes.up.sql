53644dfba3605faa1075ca5ff48c3cc05cef15bd
Alex Burkhart <alex@estuary.dev>
2022-03-09 13:10:47 -0500

    Adjust indexes on connectors table
    When I originally wrote up the first migration, I put the unique index
    on the wrong key. It needs to be unique on (type, name), rather than
    just (name). This allows us to name both a Source and a Materialization
    the same thing (eg. "Postgres"), since they will always be displayed
    in-context with their type.

---
+++ crates/control/migrations/20220309170559_adjust_connectors_indexes.up.sql
@@ -0,0 +1,5 @@
+DROP INDEX idx_connectors_name;
+DROP INDEX idx_connectors_type_name;
+
+CREATE INDEX idx_connectors_name ON connectors("name");
+CREATE UNIQUE INDEX idx_connectors_type_name ON connectors("type", "name");
