1ed6c557107268c4b5c90a6671f989078a59ff68
Alex Burkhart <alex@estuary.dev>
2022-03-10 13:07:31 -0500

    Use request parameter to generate entity names
    This adds a `name` field to the request body of the discovery endpoint.
    This will be used as the name of the discovered capture entity, but also
    to generate the other entity names (the collections, the schemas, etc.).

--- crates/control/src/controllers/connector_images.rs
+++ crates/control/src/controllers/connector_images.rs
@@ -7,7 +7,6 @@ use models::Object;
 use crate::context::AppContext;
 
 use crate::error::AppError;
-use crate::middleware::sessions::CurrentAccount;
 use crate::models::connector_images::{ConnectorImage, NewConnectorImage};
 use crate::models::id::Id;
 use crate::models::names::CatalogName;
@@ -52,21 +51,40 @@ pub async fn spec(
     Ok((StatusCode::OK, view::spec(connector, image, spec)))
 }
 
+#[derive(Debug, Deserialize)]
+pub struct DiscoveryInput {
+    /// The desired name of the Capture. This is used to generate all the
+    /// related resource names as well.
+    name: CatalogName,
+    /// The endpoint configuration for the source connector.
+    config: Object,
+}
+
+impl DiscoveryInput {
+    pub fn discovery_options(&self) -> Result<DiscoveryOptions, anyhow::Error> {
+        // TODO: replace this with real validations.
+        let (prefix, name) = self
+            .name
+            .split_once('/')
+            .ok_or_else(|| anyhow::anyhow!("invalid name"))?;
+
+        Ok(DiscoveryOptions {
+            capture_name: CatalogName::new(name),
+            catalog_prefix: CatalogName::new(prefix),
+        })
+    }
+}
+
 pub async fn discovery(
     Extension(ctx): Extension<AppContext>,
     Path(image_id): Path<Id<ConnectorImage>>,
-    CurrentAccount(current_account): CurrentAccount,
-    Json(config): Json<Object>,
+    Json(input): Json<DiscoveryInput>,
 ) -> Result<impl IntoResponse, AppError> {
     let image = images_repo::fetch_one(ctx.db(), image_id).await?;
     let connector = connectors_repo::fetch_one(ctx.db(), image.connector_id).await?;
+    let opts = input.discovery_options()?;
 
-    let opts = DiscoveryOptions {
-        catalog_name: CatalogName::new(&connector.codename()),
-        catalog_prefix: current_account.name,
-    };
-
-    let discovered_catalog = connectors::discover(connector, image, config, opts).await?;
+    let discovered_catalog = connectors::discover(connector, image, input.config, opts).await?;
 
     Ok((StatusCode::OK, view::discovery(discovered_catalog)))
 }
