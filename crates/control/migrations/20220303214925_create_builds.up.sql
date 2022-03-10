2cb5fa93a812b6927283da571ffbcab8da79572b
Johnny Graettinger <johnny@estuary.dev>
2022-03-10 14:45:22 -0500

    control: advisory locks => SELECT ... WITH UPDATE SKIP LOCKED
    Also add an index on builds.account_id.

--- crates/control/migrations/20220303214925_create_builds.up.sql
+++ crates/control/migrations/20220303214925_create_builds.up.sql
@@ -7,6 +7,9 @@ CREATE TABLE builds (
   "updated_at" TIMESTAMPTZ NOT NULL
 );
 
+-- Index for identifying builds of a given account.
+CREATE INDEX builds_account_id ON builds USING BTREE (account_id);
+
 -- Index for efficiently identifying builds that are queued,
 -- which is a small subset of the overall builds that exist.
 CREATE UNIQUE INDEX builds_id_where_queued ON builds USING BTREE (id)
