5093c3b282444d8fbe9d03abeead2fc7493c5f3c
Johnny Graettinger <johnny@estuary.dev>
2022-03-10 20:07:06 -0500

    control: builder cleanups from review
    * Use new url_for pattern
    * State::Done => State::Success
    * Terminology: "task" => "job" for background jobs of the control plane.

--- crates/control/src/controllers/builds/routes.rs
+++ crates/control/src/controllers/builds/routes.rs
@@ -1,14 +1,10 @@
-use crate::config::settings;
+use crate::controllers::url_for;
 use crate::models::{builds::Build, id::Id};
 
 pub fn index() -> String {
-    prefixed("/builds")
+    url_for("/builds")
 }
 
 pub fn show(build_id: Id<Build>) -> String {
-    prefixed(format!("/builds/{}", build_id.to_string()))
-}
-
-fn prefixed(path: impl Into<String>) -> String {
-    format!("http://{}{}", settings().application.address(), path.into())
+    url_for(format!("/builds/{}", build_id.to_string()))
 }
