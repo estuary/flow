34878f2f90b0b19c4fcea5f09556f1772c9f8e7c
Jixiang Jiang <jixiang@estuary.dev>
2022-03-21 10:46:43 -0400

    all_of

--- crates/connector_proxy/src/libs/json.rs
+++ crates/connector_proxy/src/libs/json.rs
@@ -1,5 +1,9 @@
-use schemars::{schema::RootSchema, JsonSchema};
-use serde_json::Value;
+use crate::errors::Error;
+use schemars::{
+    schema::{ObjectValidation, RootSchema, Schema, SchemaObject, SubschemaValidation},
+    JsonSchema,
+};
+use serde_json::{value::RawValue, Value};
 
 // Create the RootSchema given datatype T.
 pub fn create_root_schema<T: JsonSchema>() -> RootSchema {
@@ -18,4 +22,154 @@ pub fn remove_subobject(mut v: Value, key: &str) -> (Option<Value>, Value) {
     }
 
     (sub_object, v)
+}
+
+// Extend the endpoint schema (of a connector) with the interceptor schema. The resulting schema allows the connector configuration JSON
+// object to accept an additional subobject in the field specified by interceptor_object_field, in addition to all the other configs of the connector.
+pub fn extend_endpoint_schema(
+    endpoint_spec_schema: Box<RawValue>,
+    interceptor_object_field: String,
+    interceptor_schema: RootSchema,
+) -> Result<Box<RawValue>, Error> {
+    let mut interceptor_schema_object_validation = ObjectValidation::default();
+    interceptor_schema_object_validation.properties.insert(
+        interceptor_object_field,
+        Schema::Object(interceptor_schema.schema),
+    );
+
+    let mut interceptor_schema_object = SchemaObject::default();
+    // interceptor_schema_object validates a JSON object that contains a subobject of
+    // { interceptor_object_field: { interceptor config object } }
+    interceptor_schema_object.object = Some(Box::new(interceptor_schema_object_validation));
+
+    // If the endpoint schema is already in `all_of` structure, just append the interceptor shcema object into the list.
+    let mut extended_schema: RootSchema = serde_json::from_str(endpoint_spec_schema.get())?;
+    if let Some(ref mut subschemas) = extended_schema.schema.subschemas {
+        if let Some(ref mut all_of) = subschemas.all_of {
+            all_of.push(Schema::Object(interceptor_schema_object));
+            return RawValue::from_string(serde_json::to_string_pretty(&extended_schema)?)
+                .map_err(Into::into);
+        }
+    }
+
+    // Construct a new schema object with "all_of" structure.
+    let connector_schema_object = extended_schema.schema;
+    let all_of = vec![
+        Schema::Object(connector_schema_object),
+        Schema::Object(interceptor_schema_object),
+    ];
+    let mut subschema_validation = SubschemaValidation::default();
+    subschema_validation.all_of = Some(all_of);
+
+    extended_schema.schema = SchemaObject::default();
+    extended_schema.schema.subschemas = Some(Box::new(subschema_validation));
+
+    RawValue::from_string(serde_json::to_string_pretty(&extended_schema)?).map_err(Into::into)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde::Deserialize;
+    use serde_json::json;
+
+    #[derive(Deserialize, JsonSchema)]
+    #[serde(deny_unknown_fields)]
+    pub struct TestInterceptorConfigA {
+        #[allow(dead_code)]
+        interceptor_name_a: String,
+    }
+
+    #[derive(Deserialize, JsonSchema)]
+    #[serde(deny_unknown_fields)]
+    pub struct TestInterceptorConfigB {
+        #[allow(dead_code)]
+        interceptor_name_b: String,
+    }
+
+    fn verify_extended_endpoint_schema(endpoint_schema: String) {
+        let interceptor_schema_a = create_root_schema::<TestInterceptorConfigA>();
+        let interceptor_schema_b = create_root_schema::<TestInterceptorConfigB>();
+
+        let valid_config_instance_1 = json!({
+            "connector_name": "connector",
+            "interceptor_config_a": {
+                "interceptor_name_a": "interceptor_a"
+            },
+            "interceptor_config_b": {
+                "interceptor_name_b": "interceptor_b"
+            }
+        });
+
+        let valid_config_instance_2 = json!({
+            "connector_name": "connector"
+        });
+
+        let config_instance_bad_connector_config = json!({
+            "connector_name_bad": "connector"
+        });
+
+        let config_instance_bad_interceptor_config = json!({
+            "connector_name": "connector",
+            "interceptor_config_a": {
+                "interceptor_name_bad": "interceptor"
+            }
+        });
+
+        let extended_schema_json = extend_endpoint_schema(
+            extend_endpoint_schema(
+                RawValue::from_string(endpoint_schema).unwrap(),
+                "interceptor_config_a".to_string(),
+                interceptor_schema_a,
+            )
+            .unwrap(),
+            "interceptor_config_b".to_string(),
+            interceptor_schema_b,
+        )
+        .unwrap()
+        .to_string();
+
+        let compiled_schema = jsonschema::JSONSchema::options()
+            .compile(&serde_json::from_str(&extended_schema_json).unwrap())
+            .unwrap();
+
+        assert!(compiled_schema.validate(&valid_config_instance_1).is_ok());
+        assert!(compiled_schema.validate(&valid_config_instance_2).is_ok());
+        assert!(compiled_schema
+            .validate(&config_instance_bad_connector_config)
+            .is_err());
+        assert!(compiled_schema
+            .validate(&config_instance_bad_interceptor_config)
+            .is_err());
+    }
+
+    #[test]
+    fn test_extend_endpoint_schema() {
+        let endpoint_schema_a = json!({
+            "type": "object",
+            "required": ["connector_name"],
+            "properties": {
+                "connector_name": {"type": "string"}
+            },
+        })
+        .to_string();
+
+        verify_extended_endpoint_schema(endpoint_schema_a);
+
+        let endpoint_schema_b = json!({
+            "$ref": "#/definitions/nested",
+            "definitions": {
+              "nested": {
+                  "type": "object",
+                  "required": ["connector_name"],
+                  "properties": {
+                      "connector_name": {"type": "string"}
+                  },
+              }
+            }
+        })
+        .to_string();
+
+        verify_extended_endpoint_schema(endpoint_schema_b);
+    }
 }
--- crates/connector_proxy/src/libs/network_proxy.rs
+++ crates/connector_proxy/src/libs/network_proxy.rs
@@ -1,34 +1,24 @@
 use crate::errors::Error;
-use crate::libs::json::{create_root_schema, remove_subobject};
+use crate::libs::json::{create_root_schema, extend_endpoint_schema, remove_subobject};
 use network_proxy::interface::NetworkProxyConfig;
 
-use schemars::schema::{RootSchema, Schema};
 use serde_json::value::RawValue;
 use tokio::sync::oneshot::{self, Receiver};
 use tokio::time::timeout;
 
 pub struct NetworkProxy {}
-pub const NETWORK_PROXY_KEY: &str = "networkProxy";
+pub const NETWORK_PROXY_KEY: &str = "flowNetworkProxy";
 
 impl NetworkProxy {
     pub fn extend_endpoint_schema(
         endpoint_spec_schema: Box<RawValue>,
     ) -> Result<Box<RawValue>, Error> {
         let network_proxy_schema = create_root_schema::<NetworkProxyConfig>();
-
-        let mut modified_schema: RootSchema = serde_json::from_str(endpoint_spec_schema.get())?;
-        if let Some(ref mut o) = &mut modified_schema.schema.object {
-            if o.as_ref().properties.contains_key(NETWORK_PROXY_KEY) {
-                return Err(Error::DuplicatedKeyError(NETWORK_PROXY_KEY));
-            }
-            o.as_mut().properties.insert(
-                NETWORK_PROXY_KEY.to_string(),
-                Schema::Object(network_proxy_schema.schema),
-            );
-        }
-
-        let json = serde_json::to_string_pretty(&modified_schema)?;
-        RawValue::from_string(json).map_err(Into::into)
+        extend_endpoint_schema(
+            endpoint_spec_schema,
+            NETWORK_PROXY_KEY.to_string(),
+            network_proxy_schema,
+        )
     }
 
     // Start the network proxy. The receiver rx will be dropped to indicate the network proxy
