554282c7fb84bdb7d7a4b6caa16193ccce980c3d
Johnny Graettinger <johnny@estuary.dev>
2022-04-22 02:34:05 +0000

    examples/: update for explicit TypeScript modules
    These are mechanical changes, mostly adding existing TypeScript modules
    as explicit modules of their corresponding derivations. In some cases
    modules were split up because they were used by multiple derivations.
    
    To generate these, all modules were initially deleted to generate stubs,
    and then implementations were re-added through a merge tool.

--- examples/derive-patterns/join-outer.flow.ts
+++ examples/derive-patterns/join-outer.flow.ts
@@ -1,19 +1,11 @@
-import { collections, interfaces, registers } from 'flow/modules';
+import { IDerivation, Document, Register, FromIntsSource, FromStringsSource } from 'flow/patterns/outer-join';
 
-// Implementation for derivation derive-patterns/join-outer.flow.yaml#/collections/patterns~1outer-join/derivation.
-export class PatternsOuterJoin implements interfaces.PatternsOuterJoin {
-    fromIntsPublish(
-        source: collections.PatternsInts,
-        _register: registers.PatternsOuterJoin,
-        _previous: registers.PatternsOuterJoin,
-    ): collections.PatternsOuterJoin[] {
+// Implementation for derivation examples/derive-patterns/join-outer.flow.yaml#/collections/patterns~1outer-join/derivation.
+export class Derivation implements IDerivation {
+    fromIntsPublish(source: FromIntsSource, _register: Register, _previous: Register): Document[] {
         return [{ Key: source.Key, LHS: source.Int }];
     }
-    fromStringsPublish(
-        source: collections.PatternsStrings,
-        _register: registers.PatternsOuterJoin,
-        _previous: registers.PatternsOuterJoin,
-    ): collections.PatternsOuterJoin[] {
+    fromStringsPublish(source: FromStringsSource, _register: Register, _previous: Register): Document[] {
         return [{ Key: source.Key, RHS: [source.String] }];
     }
 }
--- examples/shopping/purchases.flow.ts
+++ examples/shopping/purchases.flow.ts
@@ -1,15 +1,11 @@
-import { collections, interfaces, registers } from 'flow/modules';
+import { IDerivation, Document, Register, CartsSource, PurchaseActionsSource } from 'flow/examples/shopping/purchases';
 
 // Implementation for derivation examples/shopping/purchases.flow.yaml#/collections/examples~1shopping~1purchases/derivation.
-export class ExamplesShoppingPurchases implements interfaces.ExamplesShoppingPurchases {
-    cartsUpdate(source: collections.ExamplesShoppingCarts): registers.ExamplesShoppingPurchases[] {
+export class Derivation implements IDerivation {
+    cartsUpdate(source: CartsSource): Register[] {
         return [source];
     }
-    purchaseActionsPublish(
-        source: collections.ExamplesShoppingCartPurchaseRequests,
-        register: registers.ExamplesShoppingPurchases,
-        _previous: registers.ExamplesShoppingPurchases,
-    ): collections.ExamplesShoppingPurchases[] {
+    purchaseActionsPublish(source: PurchaseActionsSource, register: Register, _previous: Register): Document[] {
         return [
             {
                 userId: register.userId,
