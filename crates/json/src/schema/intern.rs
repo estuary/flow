0d39911b87eaefeff53bc2019f7c75e1f878967e
Mahdi Dibaiee <mdibaiee@pm.me>
2022-04-25 12:43:27 +0000

    json/schema: use bit_set to solve the 64 field limit in schema

--- crates/json/src/schema/intern.rs
+++ crates/json/src/schema/intern.rs
@@ -1,8 +1,9 @@
+use bit_set::BitSet;
 use fxhash::FxHashMap as HashMap;
 use thiserror;
 
 /// Set of interned strings.
-pub type Set = u64;
+pub type Set = BitSet;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -30,13 +31,14 @@ impl Table {
     /// An Error is returned only if the str overflows Set's capacity to
     /// represent all interned strings (determined by the number of bits
     /// of the Set type).
-    pub fn intern(&mut self, s: &str) -> Result<Set, Error> {
+    pub fn intern(&mut self, s: &str) -> Result<&Set, Error> {
         let l = self.m.len();
         if l == MAX_TABLE_SIZE {
             return Err(Error::Overflow);
         }
-        let id = (1 as Set) << (l as Set);
-        Ok(*self.m.entry(s.to_owned()).or_insert(id))
+        let mut id = BitSet::new();
+        id.insert(l);
+        Ok(self.m.entry(s.to_owned()).or_insert(id.clone()))
     }
 
     /// Freeze the table, indicating no further strings will be interned.
@@ -49,8 +51,8 @@ impl Table {
     /// is zero-valued.
     pub fn lookup(&self, s: &str) -> Set {
         match self.m.get(s) {
-            Some(&v) => v,
-            None => 0,
+            Some(v) => v.clone(),
+            None => BitSet::new(),
         }
     }
 }
@@ -63,16 +65,31 @@ mod test {
     fn test_intern_and_lookup_with_fixtures() {
         let mut t = Table::new();
 
-        assert_eq!(t.intern("hello").unwrap(), 0b0001 as Set);
-        assert_eq!(t.intern("world").unwrap(), 0b0010 as Set);
-        assert_eq!(t.intern("hello").unwrap(), 0b0001 as Set);
-        assert_eq!(t.intern("there").unwrap(), 0b0100 as Set);
-        assert_eq!(t.intern("now").unwrap(), 0b1000 as Set);
+        assert_eq!(
+            t.intern("hello").unwrap(),
+            &BitSet::from_bytes(&[0b1_000_0000])
+        );
+        assert_eq!(
+            t.intern("world").unwrap(),
+            &BitSet::from_bytes(&[0b0_100_0000])
+        );
+        assert_eq!(
+            t.intern("hello").unwrap(),
+            &BitSet::from_bytes(&[0b1_000_0000])
+        );
+        assert_eq!(
+            t.intern("there").unwrap(),
+            &BitSet::from_bytes(&[0b0_010_0000])
+        );
+        assert_eq!(
+            t.intern("now").unwrap(),
+            &BitSet::from_bytes(&[0b0_001_0000])
+        );
 
-        assert_eq!(t.lookup("there"), 0b0100 as Set);
-        assert_eq!(t.lookup("world"), 0b0010 as Set);
-        assert_eq!(t.lookup("not found"), 0b0 as Set);
+        assert_eq!(t.lookup("there"), BitSet::from_bytes(&[0b0_010_0000]));
+        assert_eq!(t.lookup("world"), BitSet::from_bytes(&[0b0_100_0000]));
+        assert_eq!(t.lookup("not found"), BitSet::from_bytes(&[0b0]));
     }
 }
 
-const MAX_TABLE_SIZE: usize = std::mem::size_of::<Set>() * 8;
+const MAX_TABLE_SIZE: usize = 256;
