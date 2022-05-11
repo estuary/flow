33060c558e9072b852844193c25a39ad21db5abd
Phil <phil@estuary.dev>
2022-05-10 22:00:39 -0400

    Add 'advanced' JSON schema annotation
    We use JSON schemas to describe the structure of forms in our UI. When
    rendering forms for configurations, it's common to want to collapse or
    hide certain "advanced" options so that users aren't overwhelmed by
    irrelevant input fields. This adds support for the `advanced` JSON
    schema annotation, which expresses this intent.
    
    Additionally, this applies the `advanced` annotation to the
    `networkTunneling` configuration that's injected by the connector proxy,
    so that it will be collapsed by default.

--- crates/network-tunnel/src/interface.rs
+++ crates/network-tunnel/src/interface.rs
@@ -4,14 +4,29 @@ use super::sshforwarding::{SshForwarding, SshForwardingConfig};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, JsonSchema, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
-#[schemars(
-    title = "Network Tunneling",
-    description = "Connect to systems on a private network using tunneling."
-)]
 pub enum NetworkTunnelConfig {
     SshForwarding(SshForwardingConfig),
+}
+
+// manually implement JsonSchema so that we can add the 'advanced' annotation, which causes this
+// configuration form to be collapsed by default in the UI.
+impl JsonSchema for NetworkTunnelConfig {
+    fn schema_name() -> String {
+        "NetworkTunnelConfig".to_owned()
+    }
+
+    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        let ssh_forwarding = gen.subschema_for::<SshForwardingConfig>();
+        serde_json::from_value(serde_json::json!({
+            "title": "Network Tunneling",
+            "description": "Setup a network tunnel to access systems on a private network",
+            "advanced": true,
+            "oneOf": [ssh_forwarding],
+        }))
+        .unwrap()
+    }
 }
 
 impl NetworkTunnelConfig {
@@ -25,6 +40,26 @@ impl NetworkTunnelConfig {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn network_tunnel_config_has_advanced_annotation() {
+        let schema = schemars::schema_for!(NetworkTunnelConfig);
+        // assert on the serialized form of the schema, to prove that it's actually serialized
+        let as_json = serde_json::to_value(schema.clone()).unwrap();
+        assert_eq!(
+            Some(&serde_json::Value::Bool(true)),
+            as_json.pointer("/advanced"),
+            "actual: {}",
+            as_json,
+        );
+
+        assert!(as_json.pointer("/oneOf").is_some());
+        assert!(as_json.pointer("/oneOf").unwrap().is_array());
+        assert_eq!(
+            as_json.pointer("/oneOf").unwrap().as_array().unwrap().len(),
+            1
+        );
+    }
 
     #[test]
     fn test_network_config_parse_without_tunnel_type() {
