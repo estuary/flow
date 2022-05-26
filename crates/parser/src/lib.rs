415e27b612654892008517e768722c823dba99ef
Pier-Olivier Thibault <pier-olivier@estuary.dev>
2022-05-26 10:35:11 -0400

    Parser: JSONSchema Generator (#490)
    * Parser: JSONSchema Generator
    
    Implements the skeleton of what could be used to generate JSONSchema
    from a JSON document. `schema_generator::generate(Metadata, &JSONValue)`
    generates a JSON Schema from the JSONValue document provided. It is
    expected that the document is a JSON object and will return an error if
    the JSON value received is not an object (it will error on an Array
    too).
    
    All validations are generated inline currently and only support scalar
    values. The tests here are built around the current implementation, they
    will change as more features are added.
    
    Also, the library itself is not being used anywhere and is inaccessible.
    Once the library is going to settle, it will be added as a command in
    `flow-parser` and will be usable from the CLI.
    
    * Improve the macro
    
    * Fix newbie rust idiomatic issues.
    
    * Improve tests so that it uses snapshot instead of checking each properties individually.

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -2,6 +2,7 @@ mod config;
 mod decorate;
 mod format;
 mod input;
+mod schema_generator;
 
 pub use self::config::{csv, Compression, ErrorThreshold, Format, JsonPointer, ParseConfig};
 pub use self::format::{parse, Output, ParseError, Parser};
