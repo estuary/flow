3e3610152cc2533b05f976a1fba13c32ad43cba1
Mahdi Dibaiee <mdibaiee@pm.me>
2022-06-09 15:29:12 +0000

    network-tunnel: switch to using openssh for tunnel

--- crates/network-tunnel/src/interface.rs
+++ crates/network-tunnel/src/interface.rs
@@ -74,8 +74,7 @@ mod test {
     fn test_network_config_parse_without_tunnel_type() {
         let config = "{
             \"sshForwarding\": {
-                \"sshEndpoint\": \"ssh://localhost:2222\",
-                \"user\": \"flow\",
+                \"sshEndpoint\": \"ssh://flow@localhost:2222\",
                 \"forwardHost\": \"localhost\",
                 \"forwardPort\": 5432,
                 \"privateKey\": \"\",
@@ -89,8 +88,7 @@ mod test {
         assert_eq!(
             result.unwrap(),
             NetworkTunnelConfig::SshForwarding(SshForwardingConfig {
-                ssh_endpoint: "ssh://localhost:2222".to_string(),
-                user: "flow".to_string(),
+                ssh_endpoint: "ssh://flow@localhost:2222".to_string(),
                 forward_host: "localhost".to_string(),
                 forward_port: 5432,
                 private_key: "".to_string(),
