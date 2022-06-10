3ec867d71b05d132636c1df1cb408d90df45d5a8
Mahdi Dibaiee <mdibaiee@pm.me>
2022-06-10 17:39:53 +0100

    network-tunnel: handle exit status of ssh tunnel process
    This reverts commit ecb071341d6082bfb4ea5c32dec562bc57161dd8.

--- crates/network-tunnel/src/errors.rs
+++ crates/network-tunnel/src/errors.rs
@@ -2,30 +2,15 @@ use base64::DecodeError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("SSH endpoint is invalid.")]
-    InvalidSshEndpoint,
-
-    #[error("SSH private key is invalid.")]
-    InvalidSshCredential,
-
-    #[error("Local port number of 0 is invalid")]
-    ZeroLocalPort,
-
     #[error("io operation error.")]
     IoError(#[from] std::io::Error),
-
-    #[error("openssl error.")]
-    OpenSslError(#[from] openssl::error::ErrorStack),
-
-    #[error("ssh_endpoint parse error. Expected format: ssh://<host_url_or_ip>[:port]")]
-    UrlParseError(#[from] url::ParseError),
-
-    #[error("IP parse error.")]
-    IpAddrParseError(#[from] std::net::AddrParseError),
 
     #[error("Json serialization error.")]
     JsonError(#[from] serde_json::Error),
 
     #[error("Failed to decode base64 content of OpenSSH key.")]
     DecodeError(#[from] DecodeError),
+
+    #[error("SSH forwarding network tunnel exit with non-zero exit code {0}")]
+    TunnelExitNonZero(String)
 }
