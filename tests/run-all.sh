c7616f1bbc4d7bd4c9eb22a53f51a5c592d912b7
Mahdi Dibaiee <mdibaiee@pm.me>
2022-06-14 11:17:06 +0000

    network-tunnel: try a different signal log for readiness of ssh tunnel
    Use the "Entering interactive session" signal instead of "Local forwarding listening"
    since there may be multiple addresses on which we have to listen (e.g. IPv6 and IPv4 localhost)
    and we might hit the first signal but not all the addresses are listened on.
    
    "Entering interactive session" on the other hand only happens after all desired ports are listened on.

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -3,14 +3,14 @@
 # fail fast
 set -e
 
+echo "running source-test-no-state"
+./run-end-to-end.sh source-test-no-state
+echo
 echo "running citi-bike-success"
 ./run-end-to-end.sh citi-bike-success
 echo
 echo "running source-test-exit-status"
 ./run-end-to-end.sh source-test-exit-status
-echo
-echo "running source-test-no-state"
-./run-end-to-end.sh source-test-no-state
 echo
 echo "running push-capture"
 ./run-end-to-end.sh push-capture
