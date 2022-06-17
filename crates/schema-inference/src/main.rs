8c49fb4eaadaca6c19a7737b8109b0806ffb0bc6
Alex Burkhart <alex@estuary.dev>
2022-06-17 13:10:08 -0400

    Wrap schema analysis in a CLI tool
    This tool has a single subcommand in the event that we want it to do
    more than it does today, but honestly, I'm not 100% sure this will stay
    a separate binary long term anyway. We've gone back and forth on this
    being a part of the parser, but I could also see it as part of
    schemalate. It's rather simple now, so it'd be easy to forklift
    somewhere else.

--- crates/schema-inference/src/main.rs
+++ crates/schema-inference/src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello world!");
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use clap::Parser;
+
+use schema_inference::analyze::{self};
+
+/// Reads JSON documents and infers a basic schema from its structure.
+#[derive(Debug, clap::Parser)]
+#[clap(author, name = "flow-schema-inference", version = env!("FLOW_VERSION"))]
+pub struct Args {
+    #[clap(subcommand)]
+    pub subcommand: Subcommand,
+}
+
+#[derive(Debug, clap::Args)]
+pub struct AnalyzeArgs {
+    /// Path to a file with the data to parse. If no file is given, reads
+    /// from stdin.
+    file: Option<PathBuf>,
+}
+
+impl AnalyzeArgs {
+    fn run(&self) -> Result<(), anyhow::Error> {
+        let input: Box<dyn BufRead> = match &self.file {
+            Some(path) => Box::new(BufReader::new(File::open(path)?)),
+            None => Box::new(std::io::stdin().lock()),
+        };
+
+        let schema = analyze::infer_schema(input)?;
+
+        serde_json::to_writer_pretty(std::io::stdout().lock(), &schema)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum Subcommand {
+    // Perform basic analysis over one or more documents and print the inferred
+    // schema to stdout.
+    Analyze(AnalyzeArgs),
+}
+
+impl Subcommand {
+    fn run(&self) -> Result<(), anyhow::Error> {
+        match self {
+            Subcommand::Analyze(args) => args.run(),
+        }
+    }
+}
+
+impl Display for Subcommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Subcommand::Analyze(_) => f.write_str("analyze"),
+        }
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+
+    args.subcommand.run()
 }
