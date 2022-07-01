1a2404c0d1d3d2bcd3437fc84b8883050f33eb5a
Phil <phil@estuary.dev>
2022-07-01 17:10:35 -0400

    Update citi-bike example for new csv parsing
    The parsing behavior has changed quite a bit, particularly for CSV
    files. This updates the citi-bike example to work with the new behavior
    by introducing a derivation after the capture from S3. This derivation
    transforms documents from the CSV into a structure that matches the
    original collection.

--- flow_generated/types/examples/citi-bike/rides.d.ts
+++ flow_generated/types/examples/citi-bike/rides.d.ts
@@ -29,3 +29,18 @@ export type Document = /* Ride within the Citi Bike system */ {
     gender?: /* Gender of the rider (Zero=unknown; 1=male; 2=female) */ 0 | 1 | 2;
     user_type?: /* Subscriber, or pay-as-you-go Customer */ null | 'Customer' | 'Subscriber';
 };
+
+// Generated from derivation register schema examples/citi-bike/rides.flow.yaml?ptr=/collections/examples~1citi-bike~1rides/derivation/register/schema.
+// Referenced from examples/citi-bike/rides.flow.yaml#/collections/examples~1citi-bike~1rides/derivation.
+export type Register = unknown;
+
+// Generated from transform fromCsvRides as a re-export of collection examples/citi-bike/csv-rides.
+// Referenced from examples/citi-bike/rides.flow.yaml#/collections/examples~1citi-bike~1rides/derivation/transform/fromCsvRides."
+import { Document as FromCsvRidesSource } from './csv-rides';
+export { Document as FromCsvRidesSource } from './csv-rides';
+
+// Generated from derivation examples/citi-bike/rides.flow.yaml#/collections/examples~1citi-bike~1rides/derivation.
+// Required to be implemented by examples/citi-bike/transform-csv-rides.ts.
+export interface IDerivation {
+    fromCsvRidesPublish(source: FromCsvRidesSource, register: Register, previous: Register): Document[];
+}
