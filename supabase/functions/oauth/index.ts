4eda89482d5eed9e1796171f05ab0ece7e4ce9f2
Mahdi Dibaiee <mdibaiee@pm.me>
2022-07-07 17:32:36 +0100

    merge functions into one with `operation` json parameter

---
+++ supabase/functions/oauth/index.ts
@@ -0,0 +1,23 @@
+import {serve} from "https://deno.land/std@0.131.0/http/server.ts"
+
+import {accessToken} from './access-token.ts';
+import {authURL} from './auth-url.ts';
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const request = await req.json()
+
+  if (request.operation == 'auth-url') {
+    return authURL(request);
+  }
+  else if (request.operation == 'access-token') {
+    return accessToken(request);
+  }
+  else {
+    return new Response(
+        JSON.stringify({"error" : "unknown_operation"}),
+        {headers : {"Content-Type" : "application/json"}, status : 400},
+    )
+  }
+})
