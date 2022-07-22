227415814202766f146e4f99462ed8c1622fccd4
Phil <phil@estuary.dev>
2022-07-22 16:35:32 -0400

    More tweaks to parser config
    - Changes the discriminant from `name` to `type`
    - Removes the `type: "string"` from the discriminant schemas, since that
      was causing problems in the UI.
    - Adds tests and some comments related to the representation

--- crates/parser/src/format/mod.rs
+++ crates/parser/src/format/mod.rs
@@ -109,11 +109,11 @@ pub fn parse(
 
 fn parser_for(format: Format) -> Box<dyn Parser> {
     match format {
-        Format::Auto(_) => character_separated::new_csv_parser(Default::default()),
-        Format::Json(_) => json::new_parser(),
+        Format::Auto => character_separated::new_csv_parser(Default::default()),
+        Format::Json => json::new_parser(),
         Format::Csv(csv_config) => character_separated::new_csv_parser(csv_config),
-        Format::W3cExtendedLog(_) => character_separated::new_w3c_extended_log_parser(),
-        Format::Avro(_) => avro::new_parser(),
+        Format::W3cExtendedLog => character_separated::new_w3c_extended_log_parser(),
+        Format::Avro => avro::new_parser(),
     }
 }
 
@@ -189,8 +189,8 @@ fn determine_format(config: &ParseConfig) -> Option<Format> {
 
 fn format_for_content_type(content_type: &str) -> Option<Format> {
     match content_type {
-        "application/json" => Some(Format::Json(Default::default())),
-        "text/json" => Some(Format::Json(Default::default())),
+        "application/json" => Some(Format::Json),
+        "text/json" => Some(Format::Json),
         "text/csv" => Some(Format::Csv(Default::default())),
         "text/tab-separated-values" => Some(Format::Csv(Default::default())),
         _ => None,
@@ -199,10 +199,10 @@ fn format_for_content_type(content_type: &str) -> Option<Format> {
 
 fn format_for_file_extension(extension: &str) -> Option<Format> {
     match extension {
-        "jsonl" | "json" => Some(Format::Json(Default::default())),
+        "jsonl" | "json" => Some(Format::Json),
         "csv" => Some(Format::Csv(Default::default())),
         "tsv" => Some(Format::Csv(Default::default())),
-        "avro" => Some(Format::Avro(Default::default())),
+        "avro" => Some(Format::Avro),
         _ => None,
     }
 }
@@ -333,7 +333,7 @@ mod test {
             content_type: Some("xml or something lol".to_string()),
             ..Default::default()
         };
-        assert_format_eq(Some(Format::Json(Default::default())), &conf);
+        assert_format_eq(Some(Format::Json), &conf);
         conf.filename = Some("nope.jason".to_string());
         assert_format_eq(None, &conf);
     }
@@ -345,9 +345,9 @@ mod test {
             content_type: Some("application/json".to_string()),
             ..Default::default()
         };
-        assert_format_eq(Some(Format::Json(Default::default())), &conf);
+        assert_format_eq(Some(Format::Json), &conf);
         conf.content_type = Some("text/json".to_string());
-        assert_format_eq(Some(Format::Json(Default::default())), &conf);
+        assert_format_eq(Some(Format::Json), &conf);
         conf.content_type = Some("wat".to_string());
         assert_format_eq(None, &conf);
     }
