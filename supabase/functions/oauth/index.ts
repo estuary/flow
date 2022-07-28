8b6720741a78882d91685ca2a6a0c54be607bedb
Travis Jenkins <travis@estuary.dev>
2022-07-28 11:47:25 -0400

    UI requested changes to OAuth edge functions (#42)
    * adding cors headers based on supabase example project
    
    * add redirect url and cors headers to auth url call
    
    * add cors response and headers
    
    * making redirect url optional
    
    * adding cors header to error response
    
    * need state returned. Adding cors headers to second call
    
    * adding redirect to access token call
    
    * adding uri encoding just in case

--- supabase/functions/oauth/index.ts
+++ supabase/functions/oauth/index.ts
@@ -1,27 +1,30 @@
-import {serve} from "https://deno.land/std@0.131.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 
-import {accessToken} from './access-token.ts';
-import {authURL} from './auth-url.ts';
-import {encryptConfig} from './encrypt-config.ts';
+import { accessToken } from "./access-token.ts";
+import { authURL } from "./auth-url.ts";
+import { encryptConfig } from "./encrypt-config.ts";
+import { corsHeaders } from "../_shared/cors.ts";
 
-console.log("Hello from Functions!")
+console.log("Hello from Functions!");
 
 serve(async (req) => {
-  const request = await req.json()
+  // This is needed if you're planning to invoke your function from a browser.
+  // Remember to add the corsHeaders on the other responses as well.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
 
-  if (request.operation == 'auth-url') {
+  const request = await req.json();
+  if (request.operation == "auth-url") {
     return authURL(request);
-  }
-  else if (request.operation == 'access-token') {
+  } else if (request.operation == "access-token") {
     return accessToken(request);
-  }
-  else if (request.operation == 'encrypt-config') {
+  } else if (request.operation == "encrypt-config") {
     return encryptConfig(request);
+  } else {
+    return new Response(JSON.stringify({ error: "unknown_operation" }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
   }
-  else {
-    return new Response(
-        JSON.stringify({"error" : "unknown_operation"}),
-        {headers : {"Content-Type" : "application/json"}, status : 400},
-    )
-  }
-})
+});
