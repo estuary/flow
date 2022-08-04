estuary/animated-carnival

oauth: fix casing of spec response fields

supabase/functions/oauth/access-token.ts
@@ -26,7 +26,7 @@ export async function accessToken(req: Record<string, any>) {
 
   const redirect_uri = "https://dashboard.estuary.dev/oauth";
 
-  const urlTemplate = Handlebars.compile(oauth2_spec.accessTokenUrlTemplate);
+  const urlTemplate = Handlebars.compile(oauth2_spec.access_token_url_template);
   const url = urlTemplate({
     redirect_uri,
     client_id : oauth2_client_id,
@@ -35,7 +35,8 @@ export async function accessToken(req: Record<string, any>) {
     ...params
   });
 
-  const bodyTemplate = Handlebars.compile(oauth2_spec.accessTokenBody);
+  const bodyTemplate =
+      Handlebars.compile(JSON.stringify(oauth2_spec.access_token_body_json));
   const body = bodyTemplate({
     redirect_uri,
     client_id : oauth2_client_id,
@@ -45,8 +46,9 @@ export async function accessToken(req: Record<string, any>) {
   });
 
   let headers = {};
-  if (oauth2_spec.accessTokenHeaders) {
-    const headersTemplate = Handlebars.compile(oauth2_spec.accessTokenHeaders);
+  if (oauth2_spec.access_token_headers_json) {
+    const headersTemplate = Handlebars.compile(
+        JSON.stringify(oauth2_spec.access_token_headers_json));
     headers = JSON.parse(headersTemplate({
       redirect_uri,
       client_id : oauth2_client_id,

estuary/flow

supabase/functions/oauth/access-token.ts
@@ -35,7 +35,8 @@ export async function accessToken(req: Record<string, any>) {
     ...params
   });
 
-  const bodyTemplate = Handlebars.compile(oauth2_spec.accessTokenBody);
+  const bodyTemplate =
+      Handlebars.compile(JSON.stringify(oauth2_spec.accessTokenBody));
   const body = bodyTemplate({
     redirect_uri,
     client_id : oauth2_client_id,
@@ -46,7 +47,8 @@ export async function accessToken(req: Record<string, any>) {
 
   let headers = {};
   if (oauth2_spec.accessTokenHeaders) {
-    const headersTemplate = Handlebars.compile(oauth2_spec.accessTokenHeaders);
+    const headersTemplate =
+        Handlebars.compile(JSON.stringify(oauth2_spec.accessTokenHeaders));
     headers = JSON.parse(headersTemplate({
       redirect_uri,
       client_id : oauth2_client_id,