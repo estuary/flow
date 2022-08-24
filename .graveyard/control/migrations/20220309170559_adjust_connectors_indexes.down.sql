7dc39f0ec92abdbddc9844ae5750f123a22d11cb
Johnny Graettinger <johnny@estuary.dev>
2022-08-24 16:13:25 +0000

    remove .graveyard

--- .graveyard/control/migrations/20220309170559_adjust_connectors_indexes.down.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX idx_connectors_name;
-DROP INDEX idx_connectors_type_name;
-
-CREATE UNIQUE INDEX idx_connectors_name ON connectors("name");
-CREATE INDEX idx_connectors_type_name ON connectors("type", "name");
