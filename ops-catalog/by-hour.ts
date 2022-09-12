d2f3cfeaf9745155676cbd52cb4aa6f3c515ce70
Johnny Graettinger <johnny@estuary.dev>
2022-09-12 16:39:17 +0000

    ops-catalog: introduce a per-tenant template of catalog specs
    The template includes:
    
     * ops/TENANT/logs: Raw logs.
     * ops/TENANT/stats: Raw stats.
     * ops/TENANT/stats-by-hour: Derivation which rolls up stats to their
       hour.
     * ops/TENANT/stats-by-hour-view: Materialization into the tenant's
       bespoke stats partition.
     * ops/TENANT/test/stats-by-hour: Test which verifies the stats-by-hour
       rollup.
    
    Include a template-local.flow.yaml and a template-prod.flow.yaml for
    local vs production workflows, which differ only by the materialization
    endpoint. Endpoints use a `stats_loader` login role which must exist.
    
    Issue #59

--- ops-catalog/by-hour.ts
+++ ops-catalog/by-hour.ts
@@ -1,18 +1,25 @@
-import { IDerivation, Document, Register, MinuteToHourSource } from 'flow/estuary/ops/task-stats/by-hour';
+import { IDerivation, Document, Register, ByHourSource } from 'flow/ops/TENANT/stats-by-hour';
 
-
-// Implementation for derivation derivations.flow.yaml#/collections/estuary~1ops~1task-stats~1by-hour/derivation.
+// Implementation for derivation template.flow.yaml#/collections/ops~1TENANT~1stats-by-hour/derivation.
 export class Derivation implements IDerivation {
-    minuteToHourPublish(
-        source: MinuteToHourSource,
-        _register: Register,
-        _previous: Register,
-    ): Document[] {
+    byHourPublish(source: ByHourSource, _register: Register, _previous: Register): Document[] {
         const ts = new Date(source.ts);
         ts.setUTCMilliseconds(0);
         ts.setUTCSeconds(0);
         ts.setUTCMinutes(0);
-        source.ts = ts.toISOString();
-        return [source];
+
+        return [
+            {
+                // Pass-through most of `source`...
+                ...source,
+                // But override `ts` with its truncated form.
+                ts: ts.toISOString(),
+                // And extend `source.shard` with a composed `split` property.
+                shard: {
+                    ...source.shard,
+                    split: source.shard.keyBegin + ':' + source.shard.rClockBegin,
+                },
+            },
+        ];
     }
 }
