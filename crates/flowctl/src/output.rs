38cb2e8ee1d2dbbcc5811854f282ccbfaeae4a30
Phil <phil@estuary.dev>
2022-09-27 18:18:04 -0400

    Add comments to flowctl CliOutput

--- crates/flowctl/src/output.rs
+++ crates/flowctl/src/output.rs
@@ -4,6 +4,7 @@ use std::io::{self, Write};
 
 #[derive(clap::Args, Clone, Debug, Default)]
 pub struct Output {
+    /// How to format CLI output
     #[clap(global = true, short, long, value_enum)]
     pub output: Option<OutputType>,
 }
@@ -18,12 +19,23 @@ pub enum OutputType {
     Table,
 }
 
+/// A trait for things that can be output from the CLI as either JSON, YAML, or a table.
+/// The body of this trait is focused on table output, since JSON and YAML are both handled
+/// by `Serialize`.
 pub trait CliOutput: Serialize {
+    /// Allows threading through an alternate representation of table output. An example is
+    /// the `--flows` option of `flowctl catalog list`, which adds additional columns to the table.
+    /// `type TableAlt = ();` is used to opt out of having an alternative representation.
     type TableAlt: Copy;
+    /// The type output from `into_table_rows`. Common types are `String` and `JsonCell`, or
+    /// anything implementing `std::fmt::Display`.
     type CellValue: Into<comfy_table::Cell>;
 
+    /// Returns the column headers of the table.
     fn table_headers(alt: Self::TableAlt) -> Vec<&'static str>;
 
+    /// Converts this item into a tablular representation. The returned cells must be in the
+    /// same order as the `table_headers`.
     fn into_table_row(self, alt: Self::TableAlt) -> Vec<Self::CellValue>;
 }
 
@@ -65,17 +77,21 @@ pub fn print_table<T: CliOutput>(
     Ok(())
 }
 
-pub fn to_table_row<T: Serialize>(value: T, columns: &[&str]) -> Vec<JsonCell> {
+/// Converts an item implementing `Serialize` into a table row by extracting values
+/// using the given list of JSON `pointers`. This function is often used to implement
+/// `CliOutput::into_table_row`.
+pub fn to_table_row<T: Serialize>(value: T, pointers: &[&str]) -> Vec<JsonCell> {
     let mut json = serde_json::to_value(value).expect("failed to serialize json");
 
-    let mut row = Vec::with_capacity(columns.len());
-    for column in columns {
+    let mut row = Vec::with_capacity(pointers.len());
+    for column in pointers {
         let val = json.pointer_mut(column).map(Value::take);
         row.push(JsonCell(val));
     }
     row
 }
 
+/// A wrapper around an `Option<Value>` to allow it to be converted into a table cell.
 pub struct JsonCell(pub Option<Value>);
 
 impl Into<comfy_table::Cell> for JsonCell {
