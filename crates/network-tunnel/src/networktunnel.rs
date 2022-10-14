d30c311458c446809685a8e584e91c99542afe17
Joseph Shearer <joseph@josephshearer.net>
2022-10-14 17:00:55 -0400

    fix: e2e tests failing because of conflicting ports (#717)
    * fix: e2e tests failing because of conflicting ports
    
    * Try using a specific network
    
    * try making it external
    
    * fix: Address the openssh server properly
    
    * fix: Turns out `external: true` is for manually-created networks
    
    * fix: Try using the `bridge` driver
    
    * tests: A bunch of changes that will hopefully fix CI
    
    * fix: typo in `docker-compose.yaml`
    
    * refactor: only use ipv4
    
    * fix: use the new style of tunnel config

--- crates/network-tunnel/src/networktunnel.rs
+++ crates/network-tunnel/src/networktunnel.rs
@@ -6,21 +6,22 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait NetworkTunnel: Send + Sync {
-    // Inspect and/or modify the endpoint spec for which this network tunnel is created.
-    // This takes place before the tunnel is started, and may also modify tunnel config
-    // if appropriate.
+    /// Inspect and/or modify the endpoint spec for which this network tunnel is created.
+    ///
+    /// This takes place before the tunnel is started, and may also modify tunnel config
+    /// if appropriate.
     fn adjust_endpoint_spec(
         &mut self,
         endpoint_spec: serde_json::Value,
     ) -> Result<serde_json::Value, Error>;
-    // Setup the network proxy server. Network proxy should be able to listen and accept requests after `prepare` is performed.
+    /// Setup the network proxy server. Network proxy should be able to listen and accept requests after `prepare` is performed.
     async fn prepare(&mut self) -> Result<(), Error>;
-    // Start a long-running task that serves and processes all proxy requests from clients.
+    /// Start a long-running task that serves and processes all proxy requests from clients.
     async fn start_serve(&mut self) -> Result<(), Error>;
-    // Cleanup the child process. This is called in cases of failure to make sure the child process
-    // is properly killed.
+    /// Cleanup the child process. This is called in cases of failure to make sure the child process
+    /// is properly killed.
     async fn cleanup(&mut self) -> Result<(), Error>;
 
-    // This is only used for testing purposes
+    /// This is only used for testing purposes
     fn as_any(&self) -> &dyn Any;
 }
