5f49e4c7df1d705d3b37fdb523c3078b785d6d3f
Phil <phil@estuary.dev>
2022-10-18 09:37:17 -0400

    WIP - stat verifier output timestamps

--- crates/flow-stat-verifier/src/main.rs
+++ crates/flow-stat-verifier/src/main.rs
@@ -1,12 +1,15 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::io::{self, Write};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+};
 use uuid::Uuid;
 
 #[derive(Deserialize, Serialize)]
 pub struct Meta {
     uuid: Uuid,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     ack: Option<bool>,
 }
 
@@ -18,26 +21,39 @@ pub struct Doc {
     shard: Value,
     #[serde(default)]
     ts: String,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Value::is_null")]
     capture: Value,
+
+    #[serde(default, skip_serializing_if = "Value::is_null")]
+    materialize: Value,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let mut uuid_conv = false;
+    for uuid_str in std::env::args().skip(1) {
+        let uuid = Uuid::parse_str(&uuid_str)?;
+        let parts = UuidParts::from(uuid);
+        serde_json::to_writer(io::stdout(), &parts)?;
+        println!("");
+        uuid_conv = true;
+    }
+    if uuid_conv {
+        return Ok(());
+    }
+
     let mut stdin = io::stdin().lock();
     let mut deser = serde_json::Deserializer::from_reader(&mut stdin).into_iter();
 
     let mut stats = Vec::with_capacity(4);
-    for result in deser {
+    while let Some(result) = deser.next() {
         let doc: Doc = result?;
-        if doc.meta.ack.is_some() {
-            if doc.meta.ack != Some(true) {
-                panic!("ack is false");
-            }
+        if doc.meta.ack == Some(true) {
             let issues = on_ack(doc.meta.uuid, &mut stats);
             if !issues.is_empty() {
                 serde_json::to_writer(
                     &mut io::stdout(),
                     &WeirdCrap {
+                        ack_offset: deser.byte_offset(),
                         ack: doc.meta.uuid,
                         ack_parts: UuidParts::from(doc.meta.uuid),
                         txn_docs: stats.drain(..).collect(),
@@ -57,6 +73,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
 
 #[derive(Serialize)]
 struct WeirdCrap {
+    ack_offset: usize,
     ack: Uuid,
     ack_parts: UuidParts,
     txn_docs: Vec<Doc>,
@@ -75,8 +92,18 @@ fn on_ack(ack: Uuid, docs: &[Doc]) -> Vec<Woah> {
 
     let mut woah = Vec::new();
 
-    for (i, doc) in docs.iter().enumerate() {
+    let mut acked_docs: Vec<&'_ Doc> = Vec::with_capacity(1);
+
+    for doc in docs.iter() {
         let doc_parts = UuidParts::from(doc.meta.uuid);
+        if doc_parts.flags != FLAG_CONTINUE_TXN {
+            woah.push(Woah {
+                uuid: doc.meta.uuid,
+                parts: doc_parts.clone(),
+                reason: "document flags != CONTINUE_TXN",
+            })
+        }
+
         if ack_parts.flags != FLAG_ACK_TXN {
             woah.push(Woah {
                 uuid: doc.meta.uuid,
@@ -95,20 +122,26 @@ fn on_ack(ack: Uuid, docs: &[Doc]) -> Vec<Woah> {
                 parts: doc_parts,
                 reason: "document was rolled back",
             });
-        } else if doc_parts.clock == ack_parts.clock {
+        } else {
+            acked_docs.push(doc);
+        }
+    }
+
+    if acked_docs.len() > 1 {
+        // This is what we're here for!
+        for doc in acked_docs {
             woah.push(Woah {
                 uuid: doc.meta.uuid,
-                parts: doc_parts,
-                reason: "doc clock == ack clock",
-            });
-        } else if i >= 1 {
-            // This is what we're here for!
-            woah.push(Woah {
-                uuid: doc.meta.uuid,
-                parts: doc_parts,
+                parts: UuidParts::from(doc.meta.uuid),
                 reason: "multiple stats documents in transaction",
             });
         }
+    } else if acked_docs.is_empty() {
+        woah.push(Woah {
+            uuid: ack,
+            parts: ack_parts.clone(),
+            reason: "Ack of 0 documents in this batch",
+        });
     }
     woah
 }
@@ -117,8 +150,9 @@ const FLAG_OUTSIDE_TXN: u16 = 0x00;
 const FLAG_CONTINUE_TXN: u16 = 0x01;
 const FLAG_ACK_TXN: u16 = 0x02;
 
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serialize, Clone, PartialEq)]
 pub struct UuidParts {
+    ts: String,
     clock: u64,
     producer_id: [u8; 6],
     flags: u16,
@@ -128,12 +162,31 @@ impl UuidParts {
     //fn is_acked_by(ack: )
 }
 
+fn timestamp(uuid_clock: u64) -> String {
+    use time::format_description::well_known::Rfc3339;
+
+    // UUID timestamps count from the gregorian calendar start, not the unix epoch.
+    // This number represents the difference between the two, in units of 100 nanos.
+    // We'll subract this from the uuid ts in order to translate into unix epoch time.
+    const G1582NS100: i128 = 122192928000000000;
+
+    // shift off the lowest 4 bits, which represent the sequence counter
+    let ts_100nanos = uuid_clock >> 4;
+    let unix_ts_100nanos = ts_100nanos as i128 - G1582NS100;
+
+    let ts = time::OffsetDateTime::from_unix_timestamp_nanos(unix_ts_100nanos * 100)
+        .expect("uuid clock could not be made into a timestamp");
+    ts.format(&Rfc3339).expect("failed to format ts")
+}
+
 impl From<Uuid> for UuidParts {
     fn from(uuid: Uuid) -> Self {
         let producer_id = get_producer_id(uuid);
         let clock = get_clock(uuid);
         let flags = get_flags(uuid);
+        let ts = timestamp(clock);
         UuidParts {
+            ts,
             clock,
             producer_id,
             flags,
@@ -157,7 +210,7 @@ fn get_clock(uuid: Uuid) -> u64 {
     */
     let uuid_bytes: &[u8] = uuid.as_ref();
 
-    let mut t = get_be_u32(&uuid_bytes[..4]) as u64;
+    let mut t = (get_be_u32(&uuid_bytes[..4]) as u64) << 4;
     t |= (get_be_u16(&uuid_bytes[4..6]) as u64) << 36;
     t |= (get_be_u16(&uuid_bytes[6..8]) as u64) << 52;
 
@@ -180,6 +233,33 @@ fn get_be_u32(bytes: &[u8]) -> u32 {
 
 fn get_be_u16(bytes: &[u8]) -> u16 {
     let mut u16_bytes = [0u8; 2];
-    &mut u16_bytes[..].copy_from_slice(bytes);
+    u16_bytes[..].copy_from_slice(bytes);
     u16::from_be_bytes(u16_bytes)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn uuid_is_parsed_correctly() {
+        /*
+        var uuidStr = "83fc2f81-cc5f-11e9-82aa-055f02a58010";
+        var expectedProducer = ProducerID([6]byte{5, 95, 2, 165, 128, 16})
+        require.Equal(t, expectedProducer, GetProducerID(parsed))
+
+        var expectedClock =  Clock(0x1e9cc5f83fc2f810)
+        const expectFlags = Flags(682) // 0b1010101010
+            */
+        let uuid_str = "83fc2f81-cc5f-11e9-82aa-055f02a58010";
+        let expected = UuidParts {
+            clock: 0x1e9cc5f83fc2f810u64,
+            producer_id: [5, 95, 2, 165, 128, 16],
+            flags: 682u16,
+        };
+
+        let parsed: Uuid = uuid_str.parse().unwrap();
+        let actual = UuidParts::from(parsed);
+        assert_eq!(expected, actual);
+    }
 }
