87dee6bb2a44247cf2f8965c29678dc38eb9c191
Phil <phil@estuary.dev>
2022-12-08 14:15:50 -0500

    Add `flowctl catalog test|publish` subcommands
    These subcommands provide a simpler UX for testing and publishing Flow
    specifications from a local directory. They each roll up the steps of
    creating a draft, pushing local specs into the draft, testing or
    publishing the draft, and deleting it on completion. The `publish`
    subcommand will additionally ask the user for confirmation before
    publication.

--- crates/flowctl/src/draft/author.rs
+++ crates/flowctl/src/draft/author.rs
@@ -1,6 +1,4 @@
-use crate::api_exec;
-use crate::catalog::SpecSummaryItem;
-use crate::source;
+use crate::{api_exec, catalog::SpecSummaryItem, controlplane, source};
 use serde::Serialize;
 
 #[derive(Debug, clap::Args)]
@@ -10,86 +8,84 @@ pub struct Author {
     source_args: source::SourceArgs,
 }
 
-pub async fn do_author(
-    ctx: &mut crate::CliContext,
-    Author { source_args }: &Author,
-) -> anyhow::Result<()> {
-    let cur_draft = ctx.config().cur_draft()?;
-    let specs = source_args.resolve_sources().await?;
+pub async fn upsert_draft_specs(
+    client: controlplane::Client,
+    draft_id: &str,
+    bundled_catalog: &models::Catalog,
+) -> anyhow::Result<Vec<SpecSummaryItem>> {
     let models::Catalog {
         collections,
         captures,
         materializations,
         tests,
         ..
-    } = crate::source::bundle(specs).await?;
-
+    } = bundled_catalog;
     // Build up the array of `draft_specs` to upsert.
     #[derive(Serialize, Debug)]
     struct DraftSpec<'a, P: serde::Serialize> {
         draft_id: &'a str,
         catalog_name: String,
         spec_type: &'static str,
-        spec: P,
+        spec: &'a P,
     }
 
     let mut body: Vec<u8> = Vec::new();
     body.push('[' as u8);
 
-    for (name, spec) in collections {
+    for (name, spec) in collections.iter() {
         if body.len() != 1 {
             body.push(',' as u8);
         }
         serde_json::to_writer(
             &mut body,
             &DraftSpec {
-                draft_id: &cur_draft,
-                catalog_name: name.into(),
+                draft_id,
+                catalog_name: name.to_string(),
                 spec_type: "collection",
                 spec,
             },
         )
         .unwrap();
     }
-    for (name, spec) in captures {
+    for (name, spec) in captures.iter() {
         if body.len() != 1 {
             body.push(',' as u8);
         }
         serde_json::to_writer(
             &mut body,
             &DraftSpec {
-                draft_id: &cur_draft,
-                catalog_name: name.into(),
+                draft_id,
+                catalog_name: name.to_string(),
                 spec_type: "capture",
                 spec,
             },
         )
         .unwrap();
     }
-    for (name, spec) in materializations {
+    for (name, spec) in materializations.iter() {
         if body.len() != 1 {
             body.push(',' as u8);
         }
         serde_json::to_writer(
             &mut body,
             &DraftSpec {
-                draft_id: &cur_draft,
-                catalog_name: name.into(),
+                draft_id,
+                catalog_name: name.to_string(),
                 spec_type: "materialization",
                 spec,
             },
         )
         .unwrap();
     }
-    for (name, steps) in tests {
+    for (name, steps) in tests.iter() {
         if body.len() != 1 {
             body.push(',' as u8);
         }
         serde_json::to_writer(
             &mut body,
             &DraftSpec {
-                draft_id: &cur_draft,
-                catalog_name: name.into(),
+                draft_id,
+                catalog_name: name.to_string(),
                 spec_type: "test",
                 spec: steps,
             },
@@ -99,13 +95,24 @@ pub async fn do_author(
     body.push(']' as u8);
 
     let rows: Vec<SpecSummaryItem> = api_exec(
-        ctx.controlplane_client()?
+        client
             .from("draft_specs")
             .select("catalog_name,spec_type")
             .upsert(String::from_utf8(body).expect("serialized JSON is always UTF-8"))
             .on_conflict("draft_id,catalog_name"),
     )
     .await?;
+    Ok(rows)
+}
+
+pub async fn do_author(
+    ctx: &mut crate::CliContext,
+    Author { source_args }: &Author,
+) -> anyhow::Result<()> {
+    let cur_draft = ctx.config().cur_draft()?;
+    let specs = source_args.resolve_sources().await?;
+    let catalog = crate::source::bundle(specs).await?;
+    let rows = upsert_draft_specs(ctx.controlplane_client()?, &cur_draft, &catalog).await?;
 
     ctx.write_all(rows, ())
 }
