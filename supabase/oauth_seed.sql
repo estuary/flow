4aa3871552a72dcc5578e37090384f72162f9ac0
Mahdi Dibaiee <mdibaiee@pm.me>
2022-12-16 14:52:45 +0000

    oauth: add seed for local oauth2 connectors

---
+++ supabase/oauth_seed.sql
@@ -0,0 +1,21 @@
+{
+	"data": "ENC[AES256_GCM,data:FgjPZNv/jBLc48jEHY6bQIinEmZe+tIMgV9lgg79qFinram+gJa0rvAxVmwMxKG3+LF9Fr3vtfR/dAl41/Ifz5e/Ni4t1ws/YqjVq9ytmbZxj9o+r7SJFdN3+QSrtpI4DAe8/s30SHui4zIop+2cfNz3+6iKdsng1TcUFZprRBDbcJ8uPISYsmn9cWorRH2+v1BqYkmu0TJbKxpdBbwfH6r9M/KmraQSlXUdP1PDm/Sa4Lh+YjOlYoBdgcY6eVYzu7Pqe30vTtjEbnNUQRNTajobik33wfrTIVd85Kvy961LhaiQbcHlNbI+ZAAKcrFdn86BSADRza0mGG3xFUgXvCp5KQjxU3inN8gLAnCEM/FBoi+qm9hEEJBbaOfEYf7bv+1IKNeDwz5Lj15kQaHC+RAznB21Zl1zPlaNM6qc4vLFbW+KaywYJpO7myO8mLPKGXAz5sevJBveuIQW951HPFrdKrLifGL/tcHmbBH2ap4xg6Ybjktk+FCYM+Mlhb9KK3qw8BFq5p8PMaZJCQ+k33+15xnz36jehCYtM7J7gBi3dhW9AP04duuzWJZ0hLHQnerBCwO9LnMAKsz/NihSSzXIHXkks46Y0x2G/vRQD21FjkGyeer5HtMYBAx6AxIuba23TnBOWAU2Yyio3jCzuRV7CICgs2wrzQNMxm9Bx4p7Jq82g+fcNA01v7FDwVeu6MuzjyuzjTc/1rfvrzH2gC/QiVU4HJSGZU3D2hzXTNFP4CIPQIjizsp2mp/phrKTE2NC/0NfewvFKqeEIXHDoqHnn5zO8LeIfCW0VV3AInE2kiujgTBg9EQqAbThVwUa7bA5HApyMVTBUYawGzL8MS3Jow0BYl7xPBMfKy4iQ854cn36C/ZcVpNFrfKBWE7mGQbj,iv:29awtQF3yebFOksk6AwuVSh9yWfQPFzpWhU6qxZSq3c=,tag:jf1MfU3lBUxKCoK5qxiTsw==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": [
+			{
+				"resource_id": "projects/estuary-sandbox/locations/global/keyRings/sops/cryptoKeys/sops-key",
+				"created_at": "2022-12-16T14:43:24Z",
+				"enc": "CiQAUgSTnm0aAbrPE1JqtkeKr1IKnPM6qqOTNeIxMg2CYZPSZT4SSQArfuq0V0hxcFjP9jk6buSjYJSlWWiaQczJ51BkoxB5RIR9xtKrbhkkI5KYf8GzdF2OKfHnDa/lhWo/f++2eQGmB+plZLRaEuI="
+			}
+		],
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"lastmodified": "2022-12-16T14:52:36Z",
+		"mac": "ENC[AES256_GCM,data:ZktxUJLBKJPW9ohHcAAMPs9VPOxUCqoT87tfhVDKUUB4SG49BDmm/SxB1ye4SazKV7JWD+YZIW2KcPAUcVHkw/SYFk9qqGMXEpFmuKqYEPn/7JNO30Tc66s4DZs8R36LZuK7+U/axLC1mYE8TWS5l0GzPZtpeUceohzSa+W8zXI=,iv:YDk6MBaAgXhi1BtUHCBaDlQ39KNew7d64D3gDpyV1II=,tag:GGlCchFy2RVyVL3p9IXDWQ==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.7.3"
+	}
+}
