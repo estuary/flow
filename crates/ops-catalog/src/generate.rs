813ce8ba50487b47bcb28cda5d69c1f484f8a301
Will Baker <wbaker@gmail.com>
2023-02-01 13:23:36 -0800

    ops-catalog: new ops-catalog monitoring process
    Creates a process for monitoring the ops catalog and publishing changes to ops catalog specs. It
    will converge the published set of specs for reporting based on the list of tenants in the system.
    
    The ops-catalog binary can be run as a long-lived process that will continuously poll the tenants
    table, re-compute specs based on handlebars templates, and then publish the specs using flowctl. For
    local development the `Generate` command is also available to generate a set of rendered specs based
    on a list of tenants provided over stdin.

---
+++ crates/ops-catalog/src/generate.rs
@@ -0,0 +1,26 @@
+use std::{io, path};
+
+use crate::{render::Renderer, TenantInfo};
+
+#[derive(Debug, clap::Args)]
+pub struct GenerateArgs {
+    /// Directory to output rendered templates. Will be created if it does not exist.
+    #[clap(long = "output-dir")]
+    output_dir: String,
+}
+
+impl GenerateArgs {
+    pub fn run(&self) -> anyhow::Result<()> {
+        let output_dir = path::Path::new(&self.output_dir);
+
+        let mut tenants: Vec<TenantInfo> = Vec::new();
+        for line in io::stdin().lines() {
+            tenants.push(serde_json::from_str(&line?)?);
+        }
+
+        let r = Renderer::new(true, true)?;
+        r.render(tenants, output_dir)?;
+
+        Ok(())
+    }
+}
