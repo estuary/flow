b4994b344afda5421ec5a0bfcd8c2b599acaef1c
Will Baker <wbaker@gmail.com>
2023-02-13 11:21:50 -0800

    ops-catalog: disable reporting materialization initially
    This is a tweak to the rollout/migration strategy for the new ops-catalog. Initially, the
    ops-catalog will publish the final reporting materialization in a disabled state. This, along with
    an additional step in the rollout, will allow us to create the level 1 and 2 derivations and have
    them catch up before removing the existing stats materialization and table. This will minimize the
    amount of stats downtime users see.

--- supabase/pending/new_ops_catalog_1_initial_migration.sql
+++ supabase/pending/new_ops_catalog_1_initial_migration.sql
@@ -1,49 +1,13 @@
--- Run this after deploying the updated agent.
+-- This migration can be run with no user-facing impact, and is the minimum needed for ops catalogs
+-- specs to be published with flowctl's new "don't publish the spec if it hasn't changed" machinery.
+-- It is intended to be run prior to deploying the new ops-catalog. It does not include clearing out
+-- the existing stats table, so ops-catalog should not yet be configured to publish the
+-- materialization.
 
 begin;
 
-drop table ops_catalog_template;
-drop function internal.create_ops_publication(tenant_prefix catalog_tenant, ops_user_id uuid);
 alter table live_specs add md5 text generated always as (md5(trim(spec::text))) stored;
 alter table tenants add l1_stat_rollup integer not null default 0;
-
--- Clear out the existing catalog_stats table and materialization metadata tables. The new
--- ops-catalog materialization will create new metadata tables. The catalog_stats table should be
--- created anew by re-running its migration prior to the new ops-catalog being deployed since the
--- connector would create the table differently than we want.
-drop schema catalog_stat_partitions;
-drop table catalog_stats, flow_checkpoints_v1, flow_materializations_v2;
-
--- Create a publication that will delete all the existing per-tenant ops catalogs.
-do $$
-declare
-    ops_user_id uuid;
-    current_tenant tenants;
-    new_draft_id flowid := internal.id_generator();
-    publication_id flowid := internal.id_generator();
-begin
-
-    select id into strict ops_user_id from auth.users where email = 'support@estuary.dev';
-    insert into drafts (id, user_id, detail) values
-    (new_draft_id, ops_user_id, 're-publishing ops catalog');
-    insert into publications (id, user_id, draft_id) values
-    (publication_id, ops_user_id, new_draft_id);
-
-    for current_tenant in
-        select * from tenants
-    loop
-        insert into draft_specs (draft_id, catalog_name, spec_type, spec)
-        select new_draft_id, catalog_name, null, null
-        from live_specs
-        where catalog_name in (
-            'ops/' || current_tenant.tenant || 'catalog-stats-view',
-            'ops/' || current_tenant.tenant || 'tests/catalog-stats',
-            'ops/' || current_tenant.tenant || 'catalog-stats'
-        );
-    end loop;
-
-end
-$$ language plpgsql;
 
 -- The live_specs_ext view must be dropped & re-created to include the new md5 column from
 -- live_specs. draft_specs_ext depends on that view, so it must also be dropped & re-created to
@@ -120,4 +84,3 @@ comment on view draft_specs_ext is
   'View of `draft_specs` extended with metadata of its live specification';
 
 commit;
-
