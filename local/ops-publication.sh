0468ccaf5a917fe7dcf7e9074f3293edceea4f53
Will Baker <wbaker@gmail.com>
2023-02-17 16:32:12 -0800

    ops-catalog: create ops catalog for local stack
    This will automatically create an ops catalog publication when starting a local stack. It's a bit
    tricky since there is a race between processing the publication itself and the onboarding directive
    for the ops account. To work around that the script for creating the ops publication basically does
    what the onboarding directive would do for grants and storage mappings, but does it ahead of time to
    make sure they exist when the agent processes the ops catalog publication.
    
    Issue #946

---
+++ local/ops-publication.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+bundled_ops_catalog="$(flowctl raw bundle --source ops-catalog/template-local.flow.yaml | sed "s/'/''/g")"
+
+cat << EOF
+begin;
+do \$\$
+declare
+    bundled_catalog_arg json := '${bundled_ops_catalog}';
+    ops_user_id uuid;
+    new_draft_id flowid := internal.id_generator();
+    publication_id flowid := internal.id_generator();
+begin
+    -- Identify user that owns ops specifications.
+    select id into strict ops_user_id from auth.users where email = 'support@estuary.dev';
+
+    -- These items must exist for the publication we are creating to succeed. The agent may decide
+    -- to process this publication prior to the onboarding directive for the ops tenant, so this makes
+    -- sure the publication can succeed in that case.
+    insert into user_grants (user_id, object_role, capability) values
+    (ops_user_id, 'ops.us-central1.v1/', 'admin')
+    on conflict do nothing;
+    insert into role_grants (subject_role, object_role, capability) values
+    ('ops.us-central1.v1/', 'ops.us-central1.v1/', 'write')
+    on conflict do nothing;
+    insert into storage_mappings (catalog_prefix, spec) values
+    ('ops.us-central1.v1/', '{"stores": [{"provider": "GCS", "bucket": "estuary-trial", "prefix": "collection-data/"}]}'),
+    ('recovery/ops.us-central1.v1/', '{"stores": [{"provider": "GCS", "bucket": "estuary-trial"}]}')
+    on conflict do nothing;
+
+    -- Create a draft of ops changes.
+    insert into drafts (id, user_id, detail) values
+    (new_draft_id, ops_user_id, 'publishing ops catalog for local development');
+
+    -- Queue a publication of the draft.
+    insert into publications (id, user_id, draft_id) values
+    (publication_id, ops_user_id, new_draft_id);
+
+    insert into draft_specs (draft_id, catalog_name, spec_type, spec)
+    select new_draft_id, "key", 'collection'::catalog_spec_type, "value"
+    from json_each(json_extract_path(bundled_catalog_arg, 'collections'))
+    union all
+    select new_draft_id, "key", 'materialization'::catalog_spec_type, "value"
+    from json_each(json_extract_path(bundled_catalog_arg, 'materializations'))
+
+	return;
+end \$\$
+language plpgsql;
+commit;
+EOF
