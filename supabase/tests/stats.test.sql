c547646599cbf1049bd8a48bf5529abb781469cc
Mahdi Dibaiee <mdibaiee@pm.me>
2023-02-25 11:00:11 +0000

    migrations: remove partitions from stats tests

--- supabase/tests/stats.test.sql
+++ supabase/tests/stats.test.sql
@@ -1,4 +1,3 @@
-/*
 create function tests.test_catalog_stats()
 returns setof text as $$
 begin
@@ -7,11 +6,6 @@ begin
     ('11111111-1111-1111-1111-111111111111', 'aliceCo/', 'admin'),
     ('22222222-2222-2222-2222-222222222222', 'bobCo/', 'admin')
   ;
-
-  -- For each tenant, we explicitly create a partition of `catalog_stats` for them.
-  create table catalog_stat_partitions.alice_stats partition of catalog_stats for values in ('aliceCo/');
-  create table catalog_stat_partitions.bob_stats   partition of catalog_stats for values in ('bobCo/');
-  create table catalog_stat_partitions.carol_stats partition of catalog_stats for values in ('carolCo/');
 
   -- The `stats_loader` user materializes directly into the catalog_stats table.
   set role stats_loader;
@@ -37,57 +31,6 @@ begin
       10, 2
     );
 
-  -- Cannot insert tenant stats into catalog_stats if no tenant partition exists.
-  return query select throws_like(
-    $i$
-    insert into catalog_stats (
-      catalog_name, grain, ts, flow_document,
-      bytes_written_by_me, docs_written_by_me,
-      bytes_read_by_me, docs_read_by_me,
-      bytes_written_to_me, docs_written_to_me,
-      bytes_read_from_me, docs_read_from_me
-    ) values
-      (
-        'frankCo/whoops', 'monthly', '2022-08-01T00:00:00Z', '{"frank":1}',
-        0, 0, 0, 0, 0, 0, 0, 0
-    );
-    $i$,
-    'no partition of relation "catalog_stats" found for row',
-    'you cannot insert a stat into catalog_stats without a matching partition table'
-  );
-
-  -- Stats added to catalog_stats are accessible from their partitions.
-  set role postgres;
-  return query select results_eq(
-    $i$ select catalog_name::text, grain::text, flow_document::text from catalog_stat_partitions.alice_stats $i$,
-    $i$ values ('aliceCo/hello','hourly','{"alice":1}') $i$,
-    'alice stats are in alice partition'
-  );
-  return query select results_eq(
-    $i$ select catalog_name::text, grain::text, flow_document::text from catalog_stat_partitions.bob_stats $i$,
-    $i$ values ('bobCo/world','daily','{"bob":1}') $i$,
-    'bob stats are in bob partition'
-  );
-
-  -- Cannot insert tenant stats into an incorrect partition.
-  return query select throws_like(
-    $i$
-    insert into catalog_stat_partitions.bob_stats (
-      catalog_name, grain, ts, flow_document,
-      bytes_written_by_me, docs_written_by_me,
-      bytes_read_by_me, docs_read_by_me,
-      bytes_written_to_me, docs_written_to_me,
-      bytes_read_from_me, docs_read_from_me
-    ) values
-      (
-        'aliceCo/hello', 'monthly', '2022-08-01T00:00:00Z', '{"alice":1}',
-        0, 0, 0, 0, 0, 0, 0, 0
-    );
-    $i$,
-    'new row for relation "bob_stats" violates partition constraint',
-    'you cannot insert an alice stat into the bob stats partition'
-  );
-
   -- Drop priviledge to `authenticated` and authorize as Alice.
   perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
 
@@ -98,4 +41,4 @@ begin
   );
 
 end;
-$$ language plpgsql;*/
+$$ language plpgsql;
