631bed9aab05d44f9782e7f5a20780420d10c909
Phil <phil@estuary.dev>
2023-03-21 17:22:27 -0400

    control-plane gateway_auth_token 403 if unauthorized
    This changes the behavior of the `gateway_auth_token` postgres function
    to make it less surprising. Previously, if you requested scopes that
    you weren't authorized to, the function would succeed and return a valid
    token that just didn't contain the unauthorized scopes. This changes
    it to respond with an HTTP 403 error if you request any scopes that
    you aren't authorized to.

--- supabase/migrations/14_gateway_auth.sql
+++ supabase/migrations/14_gateway_auth.sql
@@ -48,29 +48,40 @@ returns text as $$
 
 $$ language sql stable security definer;
 
-
-
 create function gateway_auth_token(variadic prefixes text[])
 returns table (token text, gateway_url text) as $$
+declare
+  -- The number of distinct prefixes (i.e. scopes) that were requested.
+  requested_prefixes int := (select count(distinct p) from unnest(prefixes) p);
+  -- The distinct prefixes, filtered by whether or not they are authorized.
+  authorized_prefixes text[];
+begin
+  
+  select array_agg(distinct p) into authorized_prefixes
+    from 
+      unnest(prefixes) as p
+      join auth_roles() as r on starts_with(p, r.role_prefix);
 
-  with authorized_prefixes as (
-    select p as prefix
-    from auth_roles() as r, unnest(prefixes) as p
-    where starts_with(p, r.role_prefix)
-  )
+  -- authorized_prefixes will be null when _none_ of the requested prefixes are authorized.
+  -- In that case the array_length comparison won't work, so we need an explicit null check.
+  if authorized_prefixes is null or array_length(authorized_prefixes, 1) != requested_prefixes then
+    -- errcode 28000 causes potgrest to return an HTTP 403
+    -- see: https://www.postgresql.org/docs/current/errcodes-appendix.html
+    -- and: https://postgrest.org/en/stable/errors.html#status-codes
+    raise 'you are not authorized for all of the requested scopes' using errcode = 28000;
+  end if;
 
-  select internal.sign_jwt(
+  return query select internal.sign_jwt(
     json_build_object(
       'exp', trunc(extract(epoch from (now() + interval '1 hour'))),
       'iat', trunc(extract(epoch from (now()))),
       'operation', 'read',
-      'prefixes', array_agg(distinct ap.prefix),
+      'prefixes', authorized_prefixes,
       'sub', auth_uid()
     )
-  ) as token, internal.gateway_endpoint_url() as gateway_url
-  from authorized_prefixes as ap
-
-$$ language sql stable security definer;
+  ) as token, internal.gateway_endpoint_url() as gateway_url;
+end;
+$$ language plpgsql stable security definer;
 
 comment on function gateway_auth_token is
   'gateway_auth_token returns a jwt that can be used with the Data Plane Gateway to interact directly with Gazette RPCs.';
