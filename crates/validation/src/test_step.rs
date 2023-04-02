200c7306bb8f5782b8e8f4f6ff4bd765727dcc5b
Johnny Graettinger <johnny@estuary.dev>
2023-04-02 21:43:11 +0000

    crates/validations (derive-refactor)
    Rework validation to account for changes to specification
    representations in tables. Top-level specs are now a fully-inlined
    concern, rather than being highly normalized across many tables of
    constituent parts.
    
    Introduce a mechanism for dynamically resolving referenced collections
    which are not explicitly included in the set of validated specs.
    This is used by flowctl to dynamically resolve named collections
    as-needed, but is not used by control-plane agent builds yet.
    
    Account for capture / derive / materialize protocol additions and changes.

--- crates/validation/src/test_step.rs
+++ crates/validation/src/test_step.rs
@@ -1,53 +1,52 @@
-use super::{collection, errors::Error, indexed, reference, schema};
-use itertools::Itertools;
+use super::{collection, errors::Error, indexed, reference, schema, Scope};
+use flow::test_spec::step::Type as StepType;
 use proto_flow::flow;
-use superslice::Ext;
 
-pub fn walk_all_test_steps(
+pub fn walk_all_tests(
     built_collections: &[tables::BuiltCollection],
-    resources: &[tables::Resource],
-    schema_shapes: &[schema::Shape],
-    test_steps: &[tables::TestStep],
+    tests: &[tables::Test],
     errors: &mut tables::Errors,
 ) -> tables::BuiltTests {
     let mut built_tests = tables::BuiltTests::new();
 
-    for (test, steps) in &test_steps.iter().group_by(|s| &s.test) {
-        let steps = steps.collect::<Vec<_>>();
-        let scope = &steps[0].scope;
+    for tables::Test {
+        scope,
+        test,
+        spec: steps,
+    } in tests
+    {
+        let scope = Scope::new(scope);
 
         indexed::walk_name(scope, "test", test, models::Test::regex(), errors);
 
-        let steps: Vec<_> = steps
-            .into_iter()
-            .map(|test_step| {
+        let steps = steps
+            .iter()
+            .enumerate()
+            .filter_map(|(step_index, test_step)| {
                 walk_test_step(
+                    scope.push_item(step_index),
                     built_collections,
-                    resources,
-                    schema_shapes,
+                    step_index,
                     test_step,
                     errors,
                 )
-                .into_iter()
             })
-            .flatten()
             .collect();
 
         built_tests.insert_row(
-            scope,
+            scope.flatten(),
             test,
             flow::TestSpec {
-                test: test.to_string(),
+                name: test.to_string(),
                 steps,
             },
         );
     }
 
     indexed::walk_duplicates(
-        test_steps
+        tests
             .iter()
-            .filter(|s| s.step_index == 0)
-            .map(|s| ("test", s.test.as_str(), &s.scope)),
+            .map(|s| ("test", s.test.as_str(), Scope::new(&s.scope))),
         errors,
     );
 
@@ -55,99 +54,105 @@ pub fn walk_all_test_steps(
 }
 
 pub fn walk_test_step(
+    scope: Scope,
     built_collections: &[tables::BuiltCollection],
-    resources: &[tables::Resource],
-    schema_shapes: &[schema::Shape],
-    test_step: &tables::TestStep,
+    step_index: usize,
+    test_step: &models::TestStep,
     errors: &mut tables::Errors,
 ) -> Option<flow::test_spec::Step> {
-    let tables::TestStep {
-        scope,
-        test,
-        step_index,
-        spec,
-        documents,
-    } = test_step;
+    // Decompose the test step into its parts.
+    let (step_type, collection, description, documents, selector) = match test_step {
+        models::TestStep::Ingest(models::TestStepIngest {
+            description,
+            collection,
+            documents,
+        }) => (StepType::Ingest, collection, description, documents, None),
 
-    let documents = &resources[resources.equal_range_by_key(&documents, |r| &r.resource)];
-    let documents: Vec<serde_json::Value> = match documents.first() {
-        Some(tables::Resource {
-            content_type: flow::ContentType::DocumentsFixture,
-            content_dom,
-            ..
-        }) => serde_json::from_str(content_dom.get()).expect(
-            "a DocumentsFixture resource is verified to be an array of objects during load",
+        models::TestStep::Verify(models::TestStepVerify {
+            description,
+            collection: models::Source::Collection(collection),
+            documents,
+        }) => (StepType::Verify, collection, description, documents, None),
+
+        models::TestStep::Verify(models::TestStepVerify {
+            description,
+            collection:
+                models::Source::Source(models::FullSource {
+                    name: collection,
+                    partitions,
+                }),
+            documents,
+        }) => (
+            StepType::Verify,
+            collection,
+            description,
+            documents,
+            partitions.as_ref(),
         ),
-        _ => Vec::new(),
+    };
+    let scope = match step_type {
+        StepType::Ingest => scope.push_prop("ingest"),
+        StepType::Verify => scope.push_prop("verify"),
     };
 
-    // Map to slices of documents which are ingested or verified by this step.
-    let (collection, ingest, verify) = match spec {
-        models::TestStep::Ingest(ingest) => (&ingest.collection, documents.as_slice(), &[] as &[_]),
-        models::TestStep::Verify(verify) => (&verify.collection, &[] as &[_], documents.as_slice()),
-    };
-
-    // Dereference test collection, returning early if not found.
-    let collection = match reference::walk_reference(
-        scope,
-        &format!("test {} step {}", test.as_str(), step_index),
+    let collection = reference::walk_reference(
+        scope.push_prop("collection"),
+        "this test step",
         "collection",
-        collection,
+        collection.as_str(),
         built_collections,
-        |c| (&c.collection, &c.scope),
+        |c| (&c.collection, Scope::new(&c.scope)),
         errors,
-    ) {
-        Some(s) => s,
-        None => return None,
-    };
+    )?;
+    let documents = serde_json::from_str::<Vec<serde_json::Value>>(documents.get())
+        .expect("a documents fixture is verified to be an array of objects during load");
 
-    // Pluck the collection schema Shape, which must exist but could be a placeholder.
-    // Prefer to validate against its read-schema, if there is one.
-    let schema_uri = if !collection.spec.read_schema_uri.is_empty() {
-        collection.spec.read_schema_uri.as_str()
-    } else {
-        collection.spec.write_schema_uri.as_str()
-    };
-    let shape =
-        &schema_shapes[schema_shapes.equal_range_by_key(&schema_uri, |s| s.schema.as_str())][0];
+    if let StepType::Ingest = step_type {
+        // Require that all documents validate for both writes and reads.
+        let mut write_schema = schema::Schema::new(&collection.spec.write_schema_json).ok();
+        let mut read_schema = Some(&collection.spec.read_schema_json)
+            .filter(|s| !s.is_empty())
+            .and_then(|schema| schema::Schema::new(schema).ok());
 
-    // Verify that any ingest documents conform to the collection schema.
-    if shape.index.fetch(&shape.schema).is_none() {
-        // Referential integrity error, which we've already reported.
-    } else {
-        for doc in ingest {
-            if let Err(err) = doc::Validation::validate(
-                &mut doc::RawValidator::new(&shape.index),
-                &shape.schema,
-                doc,
-            )
-            .unwrap()
-            .ok()
-            {
-                Error::IngestDocInvalid(err).push(scope, errors);
+        for (doc_index, doc) in documents.iter().enumerate() {
+            for schema in [&mut read_schema, &mut write_schema] {
+                if let Some(err) = schema
+                    .as_mut()
+                    .and_then(|s| s.validator.validate(None, doc).unwrap().ok().err())
+                {
+                    Error::IngestDocInvalid(err)
+                        .push(scope.push_prop("documents").push_item(doc_index), errors);
+                }
             }
         }
-    }
-
-    // Verify that any verified documents are ordered correctly w.r.t.
-    // the collection's key.
-    if verify
-        .iter()
-        .tuple_windows()
-        .map(|(lhs, rhs)| json::json_cmp_at(&collection.spec.key_ptrs, lhs, rhs))
-        .any(|ord| ord == std::cmp::Ordering::Greater)
-    {
-        Error::TestVerifyOrder.push(scope, errors);
-    }
+    } else {
+        // Verify that any verified documents are ordered correctly w.r.t.
+        // the collection's key.
+        for (doc_index, (lhs, rhs)) in documents.windows(2).map(|p| (&p[0], &p[1])).enumerate() {
+            if json::json_cmp_at(&collection.spec.key, lhs, rhs).is_gt() {
+                Error::TestVerifyOrder
+                    .push(scope.push_prop("documents").push_item(doc_index), errors);
+            }
+        }
+    };
 
     // Verify a provided partition selector is valid.
-    if let models::TestStep::Verify(models::TestStepVerify {
-        partitions: Some(selector),
-        ..
-    }) = spec
-    {
-        collection::walk_selector(scope, &collection.spec, &selector, errors);
+    if let Some(selector) = selector {
+        collection::walk_selector(
+            scope.push_prop("collection").push_prop("partitions"),
+            &collection.spec,
+            &selector,
+            errors,
+        );
     }
 
-    Some(assemble::test_step_spec(test_step, &documents))
+    Some(flow::test_spec::Step {
+        step_type: step_type as i32,
+        step_index: step_index as u32,
+        step_scope: scope.flatten().into(),
+        collection: collection.collection.to_string(),
+        docs_json_vec: documents.into_iter().map(|d| d.to_string()).collect(),
+        partitions: Some(assemble::journal_selector(&collection.collection, selector)),
+        description: description.clone(),
+    })
 }
