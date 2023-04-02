cb8ba1392355b68700fd593036e392f424800966
Johnny Graettinger <johnny@estuary.dev>
2023-04-02 21:06:42 +0000

    go/protocols/capture: derive-refactor
    Capture protocol is now a single bidi stream of Request / Response.
    
    Push-based captures are removed entirely (replaced with connector
    networking feature).
    
    Client is simplified -- it handles only a pull-based workflow now,
    not push as well.

--- go/protocols/capture/extensions.go
+++ go/protocols/capture/extensions.go
@@ -5,54 +5,37 @@ import (
 
 	pf "github.com/estuary/flow/go/protocols/flow"
 	pb "go.gazette.dev/core/broker/protocol"
-	"go.gazette.dev/core/consumer/protocol"
 )
 
-func (m *SpecRequest) Validate() error {
-	if _, ok := pf.EndpointType_name[int32(m.EndpointType)]; !ok {
-		return pb.NewValidationError("unknown EndpointType %v", m.EndpointType)
-	} else if len(m.EndpointSpecJson) == 0 {
-		return pb.NewValidationError("missing EndpointSpecJson")
-	}
+func (m *Request_Spec) Validate() error {
+	// ConnectorType and ConfigJson are optional.
 	return nil
 }
 
-func (m *SpecRequest) GetEndpointType() pf.EndpointType {
-	return m.EndpointType
-}
-func (m *SpecRequest) GetEndpointSpecPtr() *json.RawMessage {
-	return &m.EndpointSpecJson
-}
-
 // Validate returns an error if the SpecResponse isn't well-formed.
-func (m *SpecResponse) Validate() error {
-	if len(m.EndpointSpecSchemaJson) == 0 {
-		return pb.NewValidationError("missing EndpointSpecSchemaJson")
-	} else if len(m.ResourceSpecSchemaJson) == 0 {
-		return pb.NewValidationError("missing ResourceSpecSchemaJson")
+func (m *Response_Spec) Validate() error {
+	if m.Protocol != 3032023 {
+		return pb.NewValidationError("wrong Protocol (%d, should be 3032023)", m.Protocol)
+	} else if len(m.ConfigSchemaJson) == 0 {
+		return pb.NewValidationError("missing ConfigSchemaJson")
+	} else if len(m.ResourceConfigSchemaJson) == 0 {
+		return pb.NewValidationError("missing ResourceConfigSchemaJson")
 	} else if m.DocumentationUrl == "" {
 		return pb.NewValidationError("missing DocumentationUrl")
 	}
 	return nil
 }
 
-func (m *DiscoverRequest) Validate() error {
-	if _, ok := pf.EndpointType_name[int32(m.EndpointType)]; !ok {
-		return pb.NewValidationError("unknown EndpointType %v", m.EndpointType)
-	} else if len(m.EndpointSpecJson) == 0 {
-		return pb.NewValidationError("missing EndpointSpecJson")
+func (m *Request_Discover) Validate() error {
+	if _, ok := pf.CaptureSpec_ConnectorType_name[int32(m.ConnectorType)]; !ok {
+		return pb.NewValidationError("unknown ConnectorType %v", m.ConnectorType)
+	} else if len(m.ConfigJson) == 0 {
+		return pb.NewValidationError("missing ConfigJson")
 	}
 	return nil
 }
 
-func (m *DiscoverRequest) GetEndpointType() pf.EndpointType {
-	return m.EndpointType
-}
-func (m *DiscoverRequest) GetEndpointSpecPtr() *json.RawMessage {
-	return &m.EndpointSpecJson
-}
-
-func (m *DiscoverResponse) Validate() error {
+func (m *Response_Discovered) Validate() error {
 	for i, b := range m.Bindings {
 		if err := b.Validate(); err != nil {
 			return pb.NewValidationError("Bindings[%d]: %w", i, err)
@@ -61,25 +44,25 @@ func (m *DiscoverResponse) Validate() error {
 	return nil
 }
 
-func (m *DiscoverResponse_Binding) Validate() error {
+func (m *Response_Discovered_Binding) Validate() error {
 	if err := m.RecommendedName.Validate(); err != nil {
 		return pb.ExtendContext(err, "RecommendedName")
 	} else if len(m.DocumentSchemaJson) == 0 {
 		return pb.NewValidationError("missing DocumentSchemaJson")
-	} else if len(m.ResourceSpecJson) == 0 {
-		return pb.NewValidationError("missing ResourceSpecJson")
+	} else if len(m.ResourceConfigJson) == 0 {
+		return pb.NewValidationError("missing ResourceConfigJson")
 	}
 	return nil
 }
 
 // Validate returns an error if the ValidateRequest isn't well-formed.
-func (m *ValidateRequest) Validate() error {
-	if err := m.Capture.Validate(); err != nil {
+func (m *Request_Validate) Validate() error {
+	if err := m.Name.Validate(); err != nil {
 		return pb.ExtendContext(err, "Capture")
-	} else if _, ok := pf.EndpointType_name[int32(m.EndpointType)]; !ok {
-		return pb.NewValidationError("unknown EndpointType %v", m.EndpointType)
-	} else if len(m.EndpointSpecJson) == 0 {
-		return pb.NewValidationError("missing EndpointSpecJson")
+	} else if _, ok := pf.CaptureSpec_ConnectorType_name[int32(m.ConnectorType)]; !ok {
+		return pb.NewValidationError("unknown ConnectorType %v", m.ConnectorType)
+	} else if len(m.ConfigJson) == 0 {
+		return pb.NewValidationError("missing ConfigJson")
 	}
 
 	for i := range m.Bindings {
@@ -90,25 +73,18 @@ func (m *ValidateRequest) Validate() error {
 	return nil
 }
 
-func (m *ValidateRequest) GetEndpointType() pf.EndpointType {
-	return m.EndpointType
-}
-func (m *ValidateRequest) GetEndpointSpecPtr() *json.RawMessage {
-	return &m.EndpointSpecJson
-}
-
 // Validate returns an error if the ValidateRequest_Binding isn't well-formed.
-func (m *ValidateRequest_Binding) Validate() error {
+func (m *Request_Validate_Binding) Validate() error {
 	if err := m.Collection.Validate(); err != nil {
 		return pb.ExtendContext(err, "Collection")
-	} else if len(m.ResourceSpecJson) == 0 {
-		return pb.NewValidationError("missing EndpointSpecJson")
+	} else if len(m.ResourceConfigJson) == 0 {
+		return pb.NewValidationError("missing ResourceConfigJson")
 	}
 	return nil
 }
 
 // Validate returns an error if the ValidateResponse isn't well-formed.
-func (m *ValidateResponse) Validate() error {
+func (m *Response_Validated) Validate() error {
 	for i := range m.Bindings {
 		if err := m.Bindings[i].Validate(); err != nil {
 			return pb.ExtendContext(err, "Bindings[%d]", i)
@@ -118,7 +94,7 @@ func (m *ValidateResponse) Validate() error {
 }
 
 // Validate returns an error if the ValidateResponse_Binding isn't well-formed.
-func (m *ValidateResponse_Binding) Validate() error {
+func (m *Response_Validated_Binding) Validate() error {
 	if len(m.ResourcePath) == 0 {
 		return pb.NewValidationError("missing ResourcePath")
 	}
@@ -132,7 +108,7 @@ func (m *ValidateResponse_Binding) Validate() error {
 }
 
 // Validate returns an error if the ApplyRequest is malformed.
-func (m *ApplyRequest) Validate() error {
+func (m *Request_Apply) Validate() error {
 	if err := m.Capture.Validate(); err != nil {
 		return pb.ExtendContext(err, "Capture")
 	}
@@ -141,144 +117,78 @@ func (m *ApplyRequest) Validate() error {
 	return nil
 }
 
-func (m *ApplyRequest) GetEndpointType() pf.EndpointType {
-	return m.Capture.EndpointType
-}
-func (m *ApplyRequest) GetEndpointSpecPtr() *json.RawMessage {
-	return &m.Capture.EndpointSpecJson
-}
-
-func (m *ApplyResponse) Validate() error {
+func (m *Response_Applied) Validate() error {
 	// No validations to do.
 	return nil
 }
 
 // Validate returns an error if the Documents isn't well-formed.
-func (m *Documents) Validate() error {
-	if len(m.DocsJson) == 0 {
-		return pb.NewValidationError("expected DocsJson")
+func (m *Response_Captured) Validate() error {
+	if len(m.DocJson) == 0 {
+		return pb.NewValidationError("expected DocJson")
 	}
 	return nil
 }
 
 // Validate returns an error if the Acknowledge isn't well-formed.
-func (m *Acknowledge) Validate() error {
+func (m *Request_Acknowledge) Validate() error {
 	return nil // No-op.
 }
 
-// Validate returns an error if the PullRequest isn't well-formed.
-func (m *PullRequest) Validate() error {
-	var count int
-	if m.Open != nil {
-		if err := m.Open.Validate(); err != nil {
-			return pb.ExtendContext(err, "Opened")
-		}
-		count += 1
-	}
-	if m.Acknowledge != nil {
-		if err := m.Acknowledge.Validate(); err != nil {
-			return pb.ExtendContext(err, "Acknowledge")
-		}
-		count += 1
-	}
-
-	if count != 1 {
-		return pb.NewValidationError("expected one of Open, Acknowledge")
-	}
-	return nil
-}
-
 // Validate returns an error if the PullRequest_Open isn't well-formed.
-func (m *PullRequest_Open) Validate() error {
+func (m *Request_Open) Validate() error {
 	if err := m.Capture.Validate(); err != nil {
 		return pb.ExtendContext(err, "Capture")
-	} else if m.KeyEnd < m.KeyBegin {
-		return pb.NewValidationError("invalid key range (KeyEnd < KeyBegin)")
+	} else if err := m.Range.Validate(); err != nil {
+		return pb.ExtendContext(err, "Range")
 	}
-	// DriverCheckpointJson may be empty.
-	// Tail has no validations.
+	// StateJson may be empty.
 	return nil
 }
 
-// Validate returns an error if the PullResponse isn't well-formed.
-func (m *PullResponse) Validate() error {
-	var count int
-	if m.Opened != nil {
-		if err := m.Opened.Validate(); err != nil {
-			return pb.ExtendContext(err, "Opened")
+func (m *Response_Checkpoint) Validate() error {
+	if m.State != nil {
+		if err := m.State.Validate(); err != nil {
+			return pb.ExtendContext(err, "State")
 		}
-		count += 1
-	}
-	if m.Documents != nil {
-		if err := m.Documents.Validate(); err != nil {
-			return pb.ExtendContext(err, "Documents")
-		}
-		count += 1
-	}
-	if m.Checkpoint != nil {
-		if err := m.Checkpoint.Validate(); err != nil {
-			return pb.ExtendContext(err, "Checkpoint")
-		}
-		count += 1
-	}
-
-	if count != 1 {
-		return pb.NewValidationError("expected one of Opened, Documents, Checkpoint")
 	}
 	return nil
 }
 
 // Validate is currently a no-op.
-func (m *PullResponse_Opened) Validate() error {
+func (m *Response_Opened) Validate() error {
 	return nil // Opened has no fields.
 }
 
-// Validate returns an error if the PushRequest isn't well-formed.
-func (m *PushRequest) Validate() error {
+// Validate returns an error if the Request isn't well-formed.
+func (m *Request) Validate_() error {
 	var count int
+	if m.Spec != nil {
+		if err := m.Spec.Validate(); err != nil {
+			return pb.ExtendContext(err, "Spec")
+		}
+		count += 1
+	}
+	if m.Discover != nil {
+		if err := m.Discover.Validate(); err != nil {
+			return pb.ExtendContext(err, "Discover")
+		}
+		count += 1
+	}
+	if m.Validate != nil {
+		if err := m.Validate.Validate(); err != nil {
+			return pb.ExtendContext(err, "Validate")
+		}
+		count += 1
+	}
+	if m.Apply != nil {
+		if err := m.Apply.Validate(); err != nil {
+			return pb.ExtendContext(err, "Apply")
+		}
+		count += 1
+	}
 	if m.Open != nil {
 		if err := m.Open.Validate(); err != nil {
-			return pb.ExtendContext(err, "Open")
-		}
-		count += 1
-	}
-	if m.Documents != nil {
-		if err := m.Documents.Validate(); err != nil {
-			return pb.ExtendContext(err, "Documents")
-		}
-		count += 1
-	}
-	if m.Checkpoint != nil {
-		if err := m.Checkpoint.Validate(); err != nil {
-			return pb.ExtendContext(err, "Checkpoint")
-		}
-		count += 1
-	}
-
-	if count != 1 {
-		return pb.NewValidationError("expected one of Open, Documents, Checkpoint")
-	}
-	return nil
-}
-
-// Validate returns an error if the PushRequest_Open isn't well-formed.
-func (m *PushRequest_Open) Validate() error {
-	if m.Header != nil {
-		if err := m.Header.Validate(); err != nil {
-			return pb.ExtendContext(err, "Header")
-		}
-	}
-	if err := m.Capture.Validate(); err != nil {
-		return pb.ExtendContext(err, "Capture")
-	}
-	return nil
-}
-
-// Validate returns an error if the PushResponse isn't well-formed.
-func (m *PushResponse) Validate() error {
-	var count int
-	if m.Opened != nil {
-		if err := m.Opened.Validate(); err != nil {
 			return pb.ExtendContext(err, "Open")
 		}
 		count += 1
@@ -291,29 +201,76 @@ func (m *PushResponse) Validate() error {
 	}
 
 	if count != 1 {
-		return pb.NewValidationError("expected one of Opened, Acknowledge")
+		return pb.NewValidationError("expected one of Spec, Discover, Validate, Apply, Open, or Acknowledge")
 	}
 	return nil
 }
 
-// Validate returns an error if the PushResponse_Opened isn't well-formed.
-func (m *PushResponse_Opened) Validate() error {
-	if err := m.Status.Validate(); err != nil {
-		return pb.ExtendContext(err, "Status")
-	} else if err := m.Header.Validate(); err != nil {
-		return pb.ExtendContext(err, "Header")
+// Validate returns an error if the Response isn't well-formed.
+func (m *Response) Validate() error {
+	var count int
+	if m.Spec != nil {
+		if err := m.Spec.Validate(); err != nil {
+			return pb.ExtendContext(err, "Spec")
+		}
+		count += 1
+	}
+	if m.Discovered != nil {
+		if err := m.Discovered.Validate(); err != nil {
+			return pb.ExtendContext(err, "Discovered")
+		}
+		count += 1
+	}
+	if m.Validated != nil {
+		if err := m.Validated.Validate(); err != nil {
+			return pb.ExtendContext(err, "Validated")
+		}
+		count += 1
+	}
+	if m.Applied != nil {
+		if err := m.Applied.Validate(); err != nil {
+			return pb.ExtendContext(err, "Applied")
+		}
+		count += 1
+	}
+	if m.Opened != nil {
+		if err := m.Opened.Validate(); err != nil {
+			return pb.ExtendContext(err, "Opened")
+		}
+		count += 1
+	}
+	if m.Captured != nil {
+		if err := m.Captured.Validate(); err != nil {
+			return pb.ExtendContext(err, "Captured")
+		}
+		count += 1
+	}
+	if m.Checkpoint != nil {
+		if err := m.Checkpoint.Validate(); err != nil {
+			return pb.ExtendContext(err, "Checkpoint")
+		}
+		count += 1
 	}
 
-	if m.Status != protocol.Status_OK {
-		// Capture, KeyBegin, KeyEnd, and DriverCheckpointJson are unset.
-	} else if m.Capture == nil {
-		return pb.NewValidationError("missing Capture")
-	} else if err := m.Capture.Validate(); err != nil {
-		return pb.ExtendContext(err, "Capture")
-	} else if m.KeyEnd < m.KeyBegin {
-		return pb.NewValidationError("invalid key range (KeyEnd < KeyBegin)")
+	if count != 1 {
+		return pb.NewValidationError("expected one of Spec, Discovered, Validated, Applied, Opened, Captured, or Checkpoint")
 	}
-	// DriverCheckpointJson may be empty.
-
 	return nil
+}
+
+func (m *Request) InvokeConfig() (*json.RawMessage, string) {
+	switch {
+	case m.Spec != nil:
+		return &m.Spec.ConfigJson, m.Spec.ConnectorType.String()
+	case m.Discover != nil:
+		return &m.Discover.ConfigJson, m.Discover.ConnectorType.String()
+	case m.Validate != nil:
+		return &m.Validate.ConfigJson, m.Validate.ConnectorType.String()
+	case m.Apply != nil:
+		return &m.Apply.Capture.ConfigJson, m.Apply.Capture.ConnectorType.String()
+	case m.Open != nil:
+		return &m.Open.Capture.ConfigJson, m.Open.Capture.ConnectorType.String()
+	default:
+		panic("invalid request")
+	}
 }
