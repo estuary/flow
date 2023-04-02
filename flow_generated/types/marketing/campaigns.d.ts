94b5ffecf13e0f1ebe6b8c4459fd6e30f4d25e6f
Johnny Graettinger <johnny@estuary.dev>
2023-04-02 22:40:25 +0000

    flow_generated/ and related dead-code cleanups (derive-refactor)
    Remove vestigates of V1 derivation typescript generation.

--- flow_generated/types/marketing/campaigns.d.ts
+++
@@ -1,9 +0,0 @@
-// Generated from collection schema examples/marketing/schema.yaml#/$defs/campaign.
-// Referenced from examples/marketing/flow.yaml#/collections/marketing~1campaigns.
-export type Document = /* Configuration of a marketing campaign. */ {
-    campaign_id: number;
-};
-
-// The collection has one schema, used for both reads and writes.
-export type SourceDocument = Document;
-export type OutputDocument = Document;
