e99001439a7e030e48ab14e91a17c6139b663390
Mahdi Dibaiee <mdibaiee@pm.me>
2023-04-27 14:21:55 +0100

    locate-bin package for finding binaries beside executable or in PATH

---
+++ crates/locate-bin/src/lib.rs
@@ -0,0 +1,22 @@
+use std::path::PathBuf;
+use which::which;
+
+pub fn locate(binary: &str) -> anyhow::Result<PathBuf> {
+    // Look for binary alongside this program.
+    let this_program = std::env::args().next().unwrap();
+
+    tracing::debug!(%this_program, "attempting for find '{binary}'");
+    let mut bin = std::path::Path::new(&this_program)
+        .parent()
+        .unwrap()
+        .join(binary);
+
+    // Fall back to the $PATH.
+    if !bin.exists() {
+        bin = which(binary)?;
+    } else {
+        bin = bin.canonicalize().unwrap();
+    }
+    tracing::debug!(executable = %bin.display(), "resolved {binary}");
+    Ok(bin)
+}
