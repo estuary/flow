baba5fcbc1ef942433829fe9c296b823a9ff5e5b
Kiahna Tucker <77648584+kiahna-tucker@users.noreply.github.com>
2023-06-06 10:04:15 -0400

    Create multi-use directive to provision read access to the demo tenant (#1068)
    * Create a multi-use directive to provision read access to the demo tenant
    
    * Rollback local changes to the Tile file
    
    * Apply the prefix type annotation to the accept_demo_tenant directive user claim
    
    * Increase test coverage of accept demo tenant directive
    
    * Add descriptive comment to the accept demo tenant directive
    
    * Correct accept demo tenant directive test typo

--- crates/agent-sql/src/directives/beta_onboard.rs
+++ crates/agent-sql/src/directives/beta_onboard.rs
@@ -74,8 +74,7 @@ pub async fn provision_tenant(
         ),
         grant_to_tenant as (
             insert into role_grants (subject_role, object_role, capability, detail) values
-                ($2, $2, 'write', $3),      -- Tenant specs may write to other tenant specs.
-                ($2, 'demo/', 'read', $3)   -- Tenant may read `demo/` collections.
+                ($2, $2, 'write', $3)      -- Tenant specs may write to other tenant specs.
             on conflict do nothing
         ),
         create_storage_mappings as (
