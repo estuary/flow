fc34524af62b330668e22601108db10a7fe07a9a
Phil <phil@estuary.dev>
2023-06-08 14:59:16 -0400

    models: allow materialization bindings to be disabled
    Updates the models for materialization bindings to allow representing bindings
    that are disabled. Just like for captures, disabled bindings are not sent to
    connectors as part of any RPCs. They are effectively stripped out and not
    represented at all in the built specifications.  Unlike captures, disabling a
    materialization binding must be indicated by a `null` (or undefined)
    `resource`.

--- crates/sources/src/indirect.rs
+++ crates/sources/src/indirect.rs
@@ -331,13 +331,15 @@ fn indirect_materialization(
 
     for (index, models::MaterializationBinding { resource, .. }) in bindings.iter_mut().enumerate()
     {
-        indirect_dom(
-            scope,
-            resource,
-            format!("{base}.resource.{index}.config"),
-            resources,
-            threshold,
-        )
+        if !resource.is_null() {
+            indirect_dom(
+                scope,
+                resource,
+                format!("{base}.resource.{index}.config"),
+                resources,
+                threshold,
+            )
+        }
     }
 }
 
