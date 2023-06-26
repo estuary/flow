ff22efb70e4929160a36df462219b40ba35d6d0e
Mahdi Dibaiee <mdibaiee@pm.me>
2023-06-26 15:44:25 +0100

    flowctl/suggest-schema: use journal_reader from crate

--- crates/flowctl/src/raw/suggest_schema.rs
+++ crates/flowctl/src/raw/suggest_schema.rs
@@ -23,7 +23,7 @@ use tonic::{codegen::InterceptedService, transport::Channel};
 use std::{collections::BTreeMap, io::ErrorKind, time::{Duration, Instant}};
 use url::Url;
 
-use crate::{connector::docker_run, catalog::{fetch_live_specs, List, SpecTypeSelector, NameSelector, collect_specs}, dataplane::{self, fetch_data_plane_access_token}, collection::{CollectionJournalSelector, Partition}};
+use crate::{connector::docker_run, catalog::{fetch_live_specs, List, SpecTypeSelector, NameSelector, collect_specs}, dataplane::{self, fetch_data_plane_access_token}, collection::{CollectionJournalSelector, Partition, read::{journal_reader, ReadArgs}}};
 use crate::local_specs;
 use anyhow::anyhow;
 
@@ -102,8 +102,11 @@ pub async fn do_suggest_schema(
     let selector = CollectionJournalSelector {
         collection: collection.clone(),
         ..Default::default()
-    }.build_label_selector();
-    let reader = journal_reader(&mut data_plane_client, &selector).await?;
+    };
+    let reader = journal_reader(ctx, &ReadArgs {
+        selector,
+        ..Default::default()
+    }).await?;
 
     // Reader for the ops log of the task
     let ops_collection = "ops.us-central1.v1/logs".to_string();
@@ -117,14 +120,13 @@ pub async fn do_suggest_schema(
             value: "capture".to_string(),
         }],
         ..Default::default()
-    }.build_label_selector();
-
-    let client = ctx.controlplane_client().await?;
-    let mut data_plane_client =
-        dataplane::journal_client_for(client, vec![ops_collection]).await?;
+    };
 
     // Read log lines from the logs collection and filter "failed validation" documents
-    let log_reader = journal_reader(&mut data_plane_client, &selector).await?;
+    let log_reader = journal_reader(ctx, &ReadArgs {
+        selector,
+        ..Default::default()
+    }).await?;
     let mut log_stream = AsyncByteLines::new(BufReader::new(log_reader.compat())).into_stream();
     let mut log_invalid_documents = log_stream.try_filter_map(|log| async move {
         let parsed: Log = serde_json::from_slice(&log)?;
@@ -202,40 +204,4 @@ fn raw_schema_to_shape(schema: &Schema) -> anyhow::Result<Shape> {
 
 fn to_io_error<T: ToString>(message: T) -> std::io::Error {
     std::io::Error::new(ErrorKind::Other, message.to_string())
-}
-
-async fn journal_reader(
-    mut data_plane_client: &mut JournalClient<InterceptedService<Channel, AuthHeader>>,
-    selector: &broker::LabelSelector
-) -> anyhow::Result<Reader<ExponentialBackoff>> {
-    tracing::debug!(?selector, "journal label selector");
-
-    let mut journals = list_journals(&mut data_plane_client, &selector)
-        .await
-        .context("listing journals for collection read")?;
-    tracing::debug!(journal_count = journals.len(), selector = ?selector, "listed journals");
-    let maybe_journal = journals.pop();
-    if !journals.is_empty() {
-        // TODO: implement a sequencer and allow reading from multiple journals
-        anyhow::bail!("flowctl is not yet able to read from partitioned collections (coming soon)");
-    }
-
-    let journal = maybe_journal.ok_or_else(|| {
-        anyhow::anyhow!(
-            "collection '{:#?}' does not exist or has never been written to (it has no journals)",
-            selector
-        )
-    })?;
-
-    let read = JournalRead::new(journal.name.clone())
-        .starting_at(ReadStart::Offset(0))
-        .read_until(ReadUntil::WriteHead);
-
-    tracing::debug!(journal = %journal.name, "starting read of journal");
-
-    // It would seem unusual for a CLI to retry indefinitely, so limit the number of retries.
-    let backoff = ExponentialBackoff::new(5);
-    let reader = Reader::start_read(data_plane_client.clone(), read, backoff);
-
-    return Ok(reader);
 }
