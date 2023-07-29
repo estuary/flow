ca901a6f520ff5962868d5299be5a7ee00127988
Phil <phil@estuary.dev>
2023-07-29 01:19:30 -0400

    agent: create linked materialization publications
    Introduces support for linked materializations into the agent. As
    captures and materializations are published, the agent will create additional
    publications as needed in order to keep materializations in sync with their
    `sourceCapture`s.  We always use additional publications when modifying the
    materializations so that any automated changes are directly auditable using our
    existing mechanisms for that.
    
    The basic explanation for how this works is:
    We first identify any captures that may have been modified by the current
    publication, or that are used as the `sourceCapture` of a materialization that
    is being published. We use those capture names to query for materializations
    having any of them as a `sourceCapture`. The agent will then create an
    additional publication for each materialization, which updates the bindings of
    the materialization to match those of the capture. This is done after the rest
    of the publication and activation is complete.
    
    The precise meaning of "match" is:
    
    - For each enabled binding in the capture, ensure that there is a corresponding
      enabled binding in the materialization having the same collection name.
        - If there's an existing disabled materialization binding, then it will be enabled.
        - Otherwise, a new binding will be created with a `resource` config that's
          invented using the `x-collection-name` annotation from the connector's
          resource spec schema.
    - Bindings that are removed from the capture (or disabled) are _not_ removed or
      disabled in the materialization. Extra bindings are always left in place.
    
    There are no requirements that the user publishing the capture, or the capture
    itself, have any capabilities to any of the linked materializations. There _is_
    a requirement that the materialization spec have 'read' capability to the
    _prefix_ of the capture.  In other words, a materialization with
    `sourceCapture: acmeCo/foo/source-s3` must have read capability to all of
    `acmeCo/foo/`. This may turn out to be overly broad, but it seems easier to
    lift restrictions than to enforce new ones.

---
+++ crates/agent/src/publications/linked_materializations.rs
@@ -0,0 +1,397 @@
+use std::collections::{HashMap, HashSet};
+use std::fmt::Write;
+
+use crate::draft::Error;
+use crate::Id;
+
+use agent_sql::linked_materializations::{
+    get_linked_materializations, CaptureSpecRow, InvalidSourceCapture, ValidateSourceCaptureInput,
+};
+use agent_sql::{drafts, publications::SpecRow};
+use agent_sql::{Capability, CatalogType};
+use anyhow::Context;
+use models::{Collection, MaterializationBinding, MaterializationDef, Source};
+
+#[cfg(test)]
+mod test;
+
+/// Returns the set of all capture names to use in querying for linked materializations.
+/// This will include all captures that were:
+/// - updated or deleted by this publication
+/// - used as the `sourceCapture` of a materialization that was updated by this publication
+/// The `spec_rows` should include only the specs that were modified by the publication, not
+/// the "expanded" specs. The `draft_catalog` is only used to avoid parsing the materialization
+/// specs again.
+pub fn get_source_capture_names(
+    spec_rows: &[SpecRow],
+    draft_catalog: &models::Catalog,
+) -> Vec<String> {
+    let maybe_linked_captures = spec_rows
+        .iter()
+        .filter_map(|row| {
+            if row.live_type == Some(CatalogType::Capture) {
+                Some(row.catalog_name.clone())
+            } else if row.draft_type == Some(CatalogType::Materialization) {
+                if let Some(materialization) = draft_catalog
+                    .materializations
+                    .get(&models::Materialization::new(&row.catalog_name))
+                {
+                    materialization
+                        .source_capture
+                        .as_ref()
+                        .map(|sc| sc.to_string())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<std::collections::HashSet<_>>();
+    maybe_linked_captures.into_iter().collect()
+}
+
+/// Validates the `sourceCapture` field of any materializations in the draft.
+/// It ensures that the materialization spec is read-authorized to the prefix
+/// of the `sourceCapture`, and that it exists and is actually a capture.
+/// For example, given `sourceCapture: acmeCo/foo/bar`, we'd require that the
+/// materialization spec be read authorized to all of `acmeCo/foo/`. This is
+/// because the capture may discover new collections, which would be put under
+/// that prefix. This is not strictly required for correctness, since we'd still
+/// enforce authZ when the materialization is published with the new bindings.
+/// But without this check, we might leak information about collections that
+/// a user doesn't have access to, since they'd be able to see them in the
+/// draft_specs.
+pub async fn validate_source_captures(
+    txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+    draft_catalog: &models::Catalog,
+    spec_rows: &[SpecRow],
+) -> anyhow::Result<Vec<Error>> {
+    let mut errors = Vec::new();
+    let mut other_checks = Vec::new();
+
+    for row in spec_rows
+        .iter()
+        .filter(|r| r.draft_type == Some(CatalogType::Materialization))
+    {
+        let spec = draft_catalog
+            .materializations
+            .get(&models::Materialization::new(row.catalog_name.as_str()))
+            .expect("draft catalog must contain items from spec_rows");
+        let Some(source_capture) = spec.source_capture.as_ref() else {
+            continue;
+        };
+        let starting_error_count = errors.len();
+        let catalog_name = row.catalog_name.as_str();
+        let spec_capabilities = &row.spec_capabilities;
+        // Check the permissions before checking that the sourceCapture exists
+        // and is a capture, so we avoid leaking information about unauthorized
+        // entities.
+        if let Some(last_slash_index) = source_capture.rfind('/') {
+            let capture_prefix = &source_capture[..(last_slash_index + 1)];
+            if !spec_capabilities.iter().any(|cap| {
+                capture_prefix.starts_with(&cap.object_role) && cap.capability >= Capability::Read
+            }) {
+                errors.push(Error {
+                    catalog_name: row.catalog_name.clone(),
+                    detail: format!(
+                        "Specification '{catalog_name}' is not read-authorized to `sourceCapture` prefix: '{capture_prefix}'.\nAvailable grants are: {}",
+                        serde_json::to_string_pretty(&spec_capabilities.0).unwrap(),
+                    ),
+                    ..Default::default()
+                });
+            }
+        } else {
+            // The value of sourceCapture is invalid, since all catalog names must include
+            // at least one "/"
+            errors.push(Error {
+                catalog_name: catalog_name.to_string(),
+                detail: format!("Materialization '{catalog_name}' `sourceCapture` '{source_capture}' is not a valid catalog name"),
+                ..Default::default()
+            });
+        }
+
+        // If the draft doesn't contain the referenced capture, we'll need to
+        // query the database in order to validate that the sourceCapture is
+        // really a capture. We'll build up a list of capture-materialization
+        // pairs, so we can check them all in a single query. We only do this if
+        // we _didn't_ already add an authZ error.
+        if !draft_catalog.captures.contains_key(source_capture)
+            && errors.len() == starting_error_count
+        {
+            other_checks.push(ValidateSourceCaptureInput {
+                source_capture_name: source_capture.as_str().to_owned(),
+                materialization_name: row.catalog_name.clone(),
+            });
+        }
+    }
+
+    tracing::debug!(
+        ?other_checks,
+        "finished checking materialization sourceCapture permissions"
+    );
+    // We've already validated all the spec permissions, so all we're doing here
+    // is validating that the sourceCaptures exist and have a `spec_type` of `capture`.
+    let invalid =
+        agent_sql::linked_materializations::find_invalid_captures(txn, other_checks).await?;
+    for row in invalid {
+        let InvalidSourceCapture {
+            materialization_name,
+            source_capture_name,
+            live_type,
+        } = row;
+
+        let mut detail = format!(
+            "Materialization '{materialization_name}' has invalid `sourceCapture`: expected '{source_capture_name}' to be a Capture, but "
+        );
+        if let Some(ty) = live_type {
+            write!(&mut detail, "it is a {ty}").unwrap();
+        } else {
+            detail.push_str("it does not exist (or was deleted)");
+        }
+        errors.push(Error {
+            catalog_name: materialization_name,
+            scope: None,
+            detail,
+        });
+    }
+    Ok(errors)
+}
+
+/// Creates publications as needed in order to keep materializations in sync
+/// with their `sourceCaptures`. It first queries for any materializations
+/// having a `sourceCapture` that may have been updated by the current
+/// publication, as provided by the `maybe_linked_captures` argument. For each
+/// such materialization, it checks to ensure that the materialization bindings
+/// are in sync with the capture. If not, then it updates the bindings in the
+/// materialization spec and creates a new draft and publication.
+///
+/// Note that if you publish a materialization with a sourceCapture by itself
+/// (without the capture in the draft), then this function will, by design,
+/// create _another_ publication if the current bindings don't match those of
+/// the capture. Materialization bindings are only ever added or enabled. They
+/// are never disabled or removed when they are disabled or removed from the
+/// source capture.
+///
+/// This will query for individual `sourceCapture`s if they are not part of the
+/// `built_captures` from this publication.
+pub async fn create_linked_materialization_publications(
+    agent_user_email: &str,
+    built_captures: &tables::BuiltCaptures,
+    maybe_linked_captures: Vec<String>,
+    txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+) -> anyhow::Result<Vec<Id>> {
+    let n_captures = maybe_linked_captures.len();
+    let linked_materializations = get_linked_materializations(txn, maybe_linked_captures).await?;
+    tracing::debug!(%n_captures, n_linked_materializations = %linked_materializations.len(), "fetched linked materializations for captures");
+
+    let parsed_materializations = linked_materializations
+        .into_iter()
+        .map(|row| {
+            let spec: MaterializationDef = serde_json::from_str(row.materialization_spec.get())
+                .with_context(|| {
+                    format!(
+                        "parsing materialization spec of: '{}'",
+                        row.materialization_name
+                    )
+                })?;
+            Ok((row.materialization_name, row.last_pub_id, spec))
+        })
+        .collect::<anyhow::Result<Vec<(String, Id, MaterializationDef)>>>()?;
+
+    // Index all the captures in the build output by name for easy lookups.
+    let mut captures_by_name = built_captures
+        .iter()
+        .map(|row| (row.capture.as_str(), &row.spec))
+        .collect::<HashMap<_, _>>();
+
+    // Determine which capture specs we'll need but don't yet have available.
+    let query_captures = parsed_materializations
+        .iter()
+        .map(|(_, _, spec)| spec.source_capture.as_ref().unwrap().as_str())
+        .filter(|source_capture| !captures_by_name.contains_key(source_capture))
+        .collect::<HashSet<_>>();
+    // This is here so that the specs live long enough, since we're storing references
+    let moar_captures: Vec<CaptureSpecRow>;
+    if !query_captures.is_empty() {
+        let input = query_captures.into_iter().collect::<Vec<_>>();
+        moar_captures = agent_sql::linked_materializations::get_source_capture_specs(txn, &input)
+            .await
+            .with_context(|| format!("querying for sourceCaptures: {:?}", input))?;
+        for capture in moar_captures.iter() {
+            if let Some(spec) = capture.spec.as_ref() {
+                captures_by_name.insert(spec.0.name.as_str(), &spec.0);
+            }
+        }
+    }
+
+    let mut resource_ptr_cache = ResourcePointerCache::default();
+    let mut pub_ids = Vec::new();
+    for (materialization_name, last_pub_id, mut spec) in parsed_materializations {
+        // safe unwrap because we just queried for materializations having sourceCaptures.
+        let capture_name = spec.source_capture.as_ref().unwrap().as_str();
+        let maybe_capture_spec = captures_by_name.get(capture_name).map(|cs| *cs);
+
+        // Make the materialization bindings match those of the capture,
+        // and create a publication if there were any changes.
+        let was_updated = update_linked_materialization(
+            &mut resource_ptr_cache,
+            txn,
+            maybe_capture_spec,
+            &materialization_name,
+            &mut spec,
+        )
+        .await?;
+        let maybe_pub_id = if was_updated {
+            let pub_id = create_publication(
+                agent_user_email,
+                materialization_name.clone(),
+                spec,
+                last_pub_id,
+                txn,
+            )
+            .await?;
+            pub_ids.push(pub_id);
+            Some(pub_id)
+        } else {
+            None
+        };
+        tracing::debug!(%was_updated, materialization_name = %materialization_name, create_publication = ?maybe_pub_id, "checked linked materialization due to capture publication");
+    }
+    Ok(pub_ids)
+}
+
+/// Updates the bindings of a materialization spec to reflect those of the given capture.
+/// Bindings are matched based on the `target` of the capture binding and the `source` of the
+/// materialization binding. For each binding in the capture, a corresponding binding will be
+/// created (or enabled, if it already exists) in the materialization. The return value indicates
+/// whether the materialization spec was actually modified by this process, to allow avoiding
+/// unnecessary publications.
+async fn update_linked_materialization(
+    resource_pointer_cache: &mut ResourcePointerCache,
+    txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+    capture_spec: Option<&proto_flow::flow::CaptureSpec>,
+    materialization_name: &str,
+    materialization: &mut MaterializationDef,
+) -> anyhow::Result<bool> {
+    // if the capture spec is missing, it indicates a deletion, and all materialization bindings should be disabled.
+    let Some(capture_spec) = capture_spec else {
+        let mut disabled = 0;
+        for binding in materialization.bindings.iter_mut() {
+            if !binding.disable {
+                binding.disable = true;
+                disabled += 1;
+            }
+        }
+        if disabled > 0 {
+            tracing::info!(n_disabled = %disabled, "disabling all materialization bindings because the sourceCapture was deleted");
+        }
+        return Ok(disabled > 0);
+    };
+
+    // The set of collection names of the capture bindings.
+    // Note that the built spec never contains disabled bindings, so
+    // inclusion in this set indicates that the corresponding materialization
+    // binding should be enabled.
+    let mut capture_bindings = capture_spec
+        .bindings
+        .iter()
+        .map(|b| b.collection.as_ref().unwrap().name.as_str())
+        .collect::<std::collections::BTreeSet<_>>();
+
+    let mut changed = false;
+
+    // First enable any materialization bindings that are currently disabled, but exist in the capture.
+    for mat_binding in materialization.bindings.iter_mut() {
+        let desired_enable = capture_bindings.remove(mat_binding.source.collection().as_str());
+        if desired_enable && mat_binding.disable {
+            mat_binding.disable = false;
+            changed = true;
+        }
+    }
+
+    // Any remaining capture bindings will need to be added to the materialization.
+    for collection_name in capture_bindings {
+        changed = true; // We're definitely changing it
+
+        let models::MaterializationEndpoint::Connector(conn) = &materialization.endpoint else {
+            panic!("unexpected materialization endpoint type for '{materialization_name}'");
+        };
+        let mut resource_spec = serde_json::json!({});
+        let collection_name_ptr = resource_pointer_cache.get_pointer(txn, &conn.image).await?;
+        crate::resource_configs::update_materialization_resource_spec(
+            materialization_name,
+            &mut resource_spec,
+            &collection_name_ptr,
+            Some(collection_name.to_string()),
+        )?;
+
+        let binding = MaterializationBinding {
+            resource: models::RawValue::from_value(&resource_spec),
+            source: Source::Collection(Collection::new(collection_name)),
+            disable: false,
+            fields: Default::default(),
+            priority: Default::default(),
+        };
+        materialization.bindings.push(binding);
+    }
+
+    Ok(changed)
+}
+
+async fn create_publication(
+    agent_user_email: &str,
+    materialization_name: String,
+    materialization_spec: MaterializationDef,
+    last_pub_id: Id,
+    txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+) -> anyhow::Result<Id> {
+    let detail =
+        format!("system created publication in response to publication of the `sourceCapture`");
+    let draft_id = drafts::create(agent_user_email, detail.clone(), txn).await?;
+
+    drafts::upsert_spec(
+        draft_id,
+        &materialization_name,
+        materialization_spec,
+        agent_sql::CatalogType::Materialization,
+        Some(last_pub_id),
+        txn,
+    )
+    .await?;
+
+    let pub_id = agent_sql::publications::create_with_user_email(
+        txn,
+        agent_user_email,
+        draft_id,
+        false,
+        detail,
+    )
+    .await?;
+    Ok(pub_id)
+}
+
+/// A cache of JSON pointers corresponding to the `x-collection-name` schema annotation
+/// in resource spec schemas. This is populated lazily.
+#[derive(Debug, Default)]
+struct ResourcePointerCache {
+    pointers_by_image: HashMap<String, doc::Pointer>,
+}
+
+impl ResourcePointerCache {
+    async fn get_pointer(
+        &mut self,
+        txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+        image: &str,
+    ) -> anyhow::Result<doc::Pointer> {
+        if let Some(ptr) = self.pointers_by_image.get(image) {
+            Ok(ptr.clone())
+        } else {
+            let schema = crate::resource_configs::fetch_resource_spec_schema(image, txn).await?;
+            let ptr = crate::resource_configs::pointer_for_schema(schema.get())?;
+            self.pointers_by_image
+                .insert(image.to_string(), ptr.clone());
+            Ok(ptr)
+        }
+    }
+}
