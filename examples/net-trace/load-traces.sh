f020e5f62e5a412b39d97bc5795e20371d7e267c
Phil <phil@estuary.dev>
2023-08-18 10:40:15 -0400

    examples: delete remaining outdated examples

--- examples/net-trace/load-traces.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget --no-clobber https://storage.googleapis.com/estuaryflowexamples/network-flows.csv.gz
-
-# Pipe CSV rows into Flow's CSV WebSocket ingestion API.
-gzip  -cd  network-flows.csv.gz \
-      | pv --line-mode --quiet --rate-limit 5000 \
-      | websocat --protocol csv/v1 ws://localhost:8080/ingest/examples/net-trace/pairs
--- examples/source-schema/flow.ts
+++
@@ -1,8 +0,0 @@
-import { IDerivation, Document, Register, FromPermissiveSource } from 'flow/examples/source-schema/restrictive';
-
-// Implementation for derivation examples/source-schema/flow.yaml#/collections/examples~1source-schema~1restrictive/derivation.
-export class Derivation implements IDerivation {
-    fromPermissivePublish(source: FromPermissiveSource, _register: Register, _previous: Register): Document[] {
-        return [source];
-    }
-}
