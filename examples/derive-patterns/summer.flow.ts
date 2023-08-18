a90fbddde64da542b6cb6019c682defea5cc543f
Phil <phil@estuary.dev>
2023-08-18 09:49:19 -0400

    examples: fix derive-patterns examples

--- examples/derive-patterns/summer.flow.ts
+++ examples/derive-patterns/summer.flow.ts
@@ -1,8 +1,8 @@
-import { IDerivation, Document, Register, FromIntsSource } from 'flow/patterns/sums-db';
+import { IDerivation, Document, SourceFromInts } from 'flow/patterns/sums-reductions.ts';
 
 // Implementation for derivation examples/derive-patterns/summer.flow.yaml#/collections/patterns~1sums-db/derivation.
-export class Derivation implements IDerivation {
-    fromIntsPublish(source: FromIntsSource, _register: Register, _previous: Register): Document[] {
-        return [{ Key: source.Key, Sum: source.Int }];
+export class Derivation extends IDerivation {
+    fromInts(read: { doc: SourceFromInts }): Document[] {
+        return [{ Key: read.doc.Key, Sum: read.doc.Int }];
     }
 }
