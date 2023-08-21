489d89a8a74a04a86c69d7cd0e8a8fbd0c683c02
Phil <phil@estuary.dev>
2023-08-21 07:45:21 -0400

    update proto-gazette generated code
    Updates the checked-in proto-gazette code to reflect the changes introduced in #1150

--- crates/journal-client/src/read/uncommitted.rs
+++ crates/journal-client/src/read/uncommitted.rs
@@ -121,6 +121,7 @@ impl JournalRead {
             block: self.block,
             do_not_proxy: false,
             metadata_only: self.fetch_fragments && !needs_direct_read,
+            begin_mod_time: 0,
         }
     }
 }
@@ -314,7 +315,7 @@ impl<R: Retry> futures::io::AsyncRead for Reader<R> {
 /// Models the state of this simple state machine. Either we are opening a new
 /// `raw::Reader`, reading data from said reader, or waiting on a backoff to try again.
 enum State {
-    StartReq(BoxFuture<'static,Result<raw::Reader, Error>>),
+    StartReq(BoxFuture<'static, Result<raw::Reader, Error>>),
     Reading(raw::Reader),
     Backoff(Pin<Box<tokio::time::Sleep>>),
 }
@@ -351,7 +352,7 @@ fn read_err(err: &io::Error) -> Option<&Error> {
 fn start_new_read(
     client: Client,
     req: broker::ReadRequest,
-) -> BoxFuture<'static,Result<raw::Reader, Error>> {
+) -> BoxFuture<'static, Result<raw::Reader, Error>> {
     Box::pin(async move {
         let mut c = client;
         raw::start_read(&mut c, req).await
