0c03eec475535824f03749b2769edf7479b4a15b
Johnny Graettinger <johnny@estuary.dev>
2023-09-01 17:10:25 +0000

    crates/agent: perform in-process managed builds
    Instead of shelling out to `flowctl-go`.
    
    This is a fairly stright forward drop-in replacement with
    `build::managed_build`, dispatched to an isolated runtime::TokioContext
    so that in-process tracing events can also be gathered.
    
    However, this means the Go logrus package is no longer responsible for
    text-formatting logs and we need to present structured instances of
    `ops::Log` to the control-plane and UI. Eventually we'd like to directly
    pass-through structured logs into the `internal.log_lines` table, but
    for now I've introduced a reasonable ANSI-colored rendering of
    deeply structured logs (that, IMO, improves on logrus).
    
    When we're ready, we can rip this out and trivially pass-through JSON
    encodings of ops::Logs.

--- crates/agent/src/logs.rs
+++ crates/agent/src/logs.rs
@@ -1,3 +1,4 @@
+use proto_flow::ops;
 use sqlx::types::Uuid;
 use tokio::io::AsyncBufReadExt;
 use tracing::{debug, trace};
@@ -128,5 +129,113 @@ pub async fn serve_sink(
         tokens.clear();
         streams.clear();
         lines.clear();
+    }
+}
+
+/// ops_handler returns an ops::Log handler that dispatches to `tx`
+/// using the given `stream` and `token`.
+pub fn ops_handler(
+    tx: Tx,
+    stream: String,
+    token: Uuid,
+) -> impl Fn(&ops::Log) + Clone + Send + Sync + 'static {
+    let log_handler = move |log: &ops::Log| {
+        let Err(tokio::sync::mpsc::error::TrySendError::Full(line)) = tx.try_send(Line {
+            token: token.clone(),
+            stream: stream.clone(),
+            line: render_ops_log_for_ui(log),
+        }) else { return };
+
+        // Perform an expensive "move" of all other tasks scheduled on the
+        // current async executor thread, so that we can block until there's capacity.
+        let tx_clone = tx.clone();
+        _ = tokio::task::block_in_place(move || {
+            tokio::runtime::Handle::current().block_on(tx_clone.send(line))
+        });
+    };
+    log_handler
+}
+
+// TODO(johnny): This is a placeholder until all `internal.log_lines` can be JSON.
+// Then we'll pass everything through as JSON and let the UI handle structured presentation.
+fn render_ops_log_for_ui(log: &ops::Log) -> String {
+    use colored_json::{Color, ColorMode, ColoredFormatter, CompactFormatter, Style, Styler};
+    use ops::log::Level;
+    use std::fmt::Write;
+
+    // These colors are drawn from Go's logrus package, and are chosen to be consistent with it.
+    let red = Color::Red; // 31
+    let yellow = Color::Yellow; // 33;
+    let blue = Color::Fixed(36);
+    let gray = Color::Fixed(37);
+
+    let mut line = String::new();
+
+    let (level_txt, level_color) = match log.level() {
+        Level::Trace => ("TRACE", gray),
+        Level::Debug => ("DEBUG", gray),
+        Level::Info => (" INFO", blue),
+        Level::Warn => (" WARN", yellow),
+        Level::Error => ("ERROR", red),
+        Level::UndefinedLevel => ("UNDEFINED", red),
+    };
+
+    write!(
+        &mut line,
+        "{}: {: <30}", // Right-pad the message to 30 characters.
+        Style::new(level_color).dimmed().paint(level_txt),
+        log.message
+    )
+    .unwrap();
+
+    let f = ColoredFormatter::with_styler(
+        CompactFormatter {},
+        Styler {
+            key: Style::new(blue).italic(),
+            string_value: Style::default(),
+            string_include_quotation: false,
+            ..Default::default()
+        },
+    );
+
+    for (field, content_json) in &log.fields_json_map {
+        let content: serde_json::Value = serde_json::from_str(content_json).unwrap();
+
+        write!(
+            &mut line,
+            " {}={}",
+            Style::new(blue).italic().paint(field),
+            f.clone().to_colored_json(&content, ColorMode::On).unwrap(),
+        )
+        .unwrap();
+    }
+
+    line
+}
+
+#[cfg(test)]
+mod test {
+    use super::render_ops_log_for_ui;
+    use proto_flow::ops;
+
+    #[test]
+    fn test_log_rendering() {
+        let fixture = ops::Log {
+            level: ops::log::Level::Info as i32,
+            message: "The log message!".to_string(),
+            fields_json_map: [
+                ("number", "42"),
+                ("boolean", "true"),
+                ("object", "{\"key\":\"value\"}"),
+                ("array", "[1,2,true,\"false\"]"),
+            ]
+            .into_iter()
+            .map(|(key, value)| (key.to_string(), value.to_string()))
+            .collect(),
+            ..Default::default()
+        };
+
+        println!("{}", render_ops_log_for_ui(&fixture));
+        insta::assert_debug_snapshot!(render_ops_log_for_ui(&fixture), @r###""\u{1b}[2;38;5;36m INFO\u{1b}[0m: The log message!               \u{1b}[3;38;5;36marray\u{1b}[0m=\u{1b}[1m[\u{1b}[0m1,2,true,\"false\"\u{1b}[1m]\u{1b}[0m \u{1b}[3;38;5;36mboolean\u{1b}[0m=true \u{1b}[3;38;5;36mnumber\u{1b}[0m=42 \u{1b}[3;38;5;36mobject\u{1b}[0m=\u{1b}[1m{\u{1b}[0m\"\u{1b}[3;38;5;36mkey\u{1b}[0m\":\"value\"\u{1b}[1m}\u{1b}[0m""###);
     }
 }
