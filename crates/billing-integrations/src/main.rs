a8a955740be87d4116066d25712896d86f3fddde
Joseph Shearer <joseph@josephshearer.net>
2023-09-13 16:35:40 -0400

    feature: Implement `flowctl raw publish-stripe-invoices` (#1180)
    * feature: Implement `flowctl raw publish-stripe-invoices`
    
    * For the moment, invoices are created as drafts and have to be approved and sent/paid manually. A draft invoice can be edited, but not sent or paid.
    * One of the next steps is to display invoice PDFs or links to Stripe's hosted invoice page in the dashboard. Neither of these are available for draft invoices, so only invoices that are manually approved will show the PDF/hosted link.
    * These invoices are being entered as `collection_method: SendInvoice`, which _requires_ that the associated customer has an email that can receive the invoice. Customers that have already entered payment details will have this email set, but rather than skip all of the customers that haven't gone through that process, I decided to pick the email address of an `admin` user on their tenant. This is only the default, and can be changed once created later.
    
    * refactor into separate `billing-integrations` crate, and use `sqlx`

---
+++ crates/billing-integrations/src/main.rs
@@ -0,0 +1,35 @@
+use billing_integrations::Cli;
+use clap::Parser;
+use tracing_subscriber::{filter::LevelFilter, EnvFilter};
+
+fn main() -> Result<(), anyhow::Error> {
+    // Colorization support for Win 10.
+    #[cfg(windows)]
+    let _ = colored_json::enable_ansi_support();
+
+    let cli = Cli::parse();
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("failed to start runtime");
+
+    // Use reasonable defaults for printing structured logs to stderr.
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .compact()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .without_time()
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
+
+    let result = runtime.block_on(async move { cli.run().await });
+
+    // We must call `shutdown_background()` because otherwise an incomplete spawned future
+    // could block indefinitely.
+    runtime.shutdown_background();
+    result
+}
