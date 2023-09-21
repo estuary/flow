e98f808425cb4dd81b6d3e3acc3c761591cd249a
Joseph Shearer <joseph@josephshearer.net>
2023-09-21 00:09:05 -0400

    Move `billing_report_202308` and associated functions to `internal` schema.

--- supabase/migrations/30_manual_billing.sql
+++ supabase/migrations/30_manual_billing.sql
@@ -10,22 +10,288 @@ create table manual_bills (
   primary key (tenant, date_start, date_end)
 );
 
-alter table manual_bills enable row level security;
-
-create policy "Users must be authorized to their catalog tenant"
-  on manual_bills as permissive for select
-  using (exists(
-    select 1 from auth_roles('admin') r where tenant ^@ r.role_prefix
-  ));
-grant select on manual_bills to authenticated;
-
 comment on table manual_bills is
   'Manually entered bills that span an arbitrary date range';
 
+-- Move billing report gen to internal
+
+-- Drop the public functions
+drop function billing_report_202308(catalog_prefix, timestamptz);
+drop function tier_line_items(integer, integer[], text, text);
+
+-- Compute a JSONB array of line-items detailing usage under a tenant's effective tiers.
+create or replace function internal.tier_line_items(
+  -- Ammount of usage we're breaking out.
+  amount integer,
+  -- Effective tenant tiers as ordered pairs of (quantity, cents), followed
+  -- by a final unpaired cents for unbounded usage beyond the final quantity.
+  tiers integer[],
+  -- Descriptive name of the tiered thing ("Data processing").
+  name text,
+  -- Unit of the tier ("GB" or "hour").
+  unit text
+)
+returns jsonb as $$
+declare
+  o_line_items jsonb = '[]'; -- Output variable.
+  tier_count   integer;
+  tier_pivot   integer;
+  tier_rate    integer;
+begin
+
+  for idx in 1..array_length(tiers, 1) by 2 loop
+    tier_rate = tiers[idx];
+    tier_pivot = tiers[idx+1];
+    tier_count = least(amount, tier_pivot);
+    amount = amount - tier_count;
+
+    o_line_items = o_line_items || jsonb_build_object(
+      'description', format(
+        case
+          when tier_pivot is null then '%1$s (at %4$s/%2$s)'      -- Data processing (at $0.50/GB)
+          when idx = 1 then '%1s (first %3$s %2$ss at %4$s/%2$s)' -- Data processing (first 30 GBs at $0.50/GB)
+          else '%1$s (next %3$s %2$ss at %4$s/%2$s)'              -- Data processing (next 6 GBs at $0.25/GB)
+        end,
+        name,
+        unit,
+        tier_pivot,
+        (tier_rate / 100.0)::money
+      ),
+      'count', tier_count,
+      'rate', tier_rate,
+      'subtotal', tier_count * tier_rate
+    );
+  end loop;
+
+  return o_line_items;
+
+end
+$$ language plpgsql;
+
+
+-- Billing report which is effective August 2023.
+-- Removed authorization logic as it's now going to be handled in invoices_ext
+create or replace function internal.billing_report_202308(billed_prefix catalog_prefix, billed_month timestamptz)
+returns jsonb as $$
+declare
+  -- Auth checks
+  has_admin_grant boolean;
+  has_bypassrls boolean;
+
+  -- Output variables.
+  o_daily_usage   jsonb;
+  o_data_gb       numeric;
+  o_line_items    jsonb = '[]';
+  o_recurring_fee integer;
+  o_subtotal      integer;
+  o_task_hours    numeric;
+  o_trial_credit  integer;
+  o_trial_start   date;
+  o_trial_range   daterange;
+  o_billed_range  daterange;
+begin
+
+  -- Ensure `billed_month` is the truncated start of the billed month.
+  billed_month = date_trunc('month', billed_month);
+
+  with vars as (
+    select
+      t.data_tiers,
+      t.trial_start,
+      t.usage_tiers,
+      tstzrange(billed_month, billed_month  + '1 month', '[)') as billed_range,
+      case when t.trial_start is not null
+        then tstzrange(t.trial_start, t.trial_start + interval '1 month', '[)')
+        else 'empty' end as trial_range,
+      -- Reveal contract costs only when computing whole-tenant billing.
+      case when t.tenant = billed_prefix then t.recurring_usd_cents else 0 end as recurring_fee
+      from tenants t
+      where billed_prefix ^@ t.tenant -- Prefix starts with tenant.
+  ),
+  -- Roll up each day's incremental usage.
+  daily_stat_deltas as (
+    select
+      ts,
+      sum(bytes_written_by_me + bytes_read_by_me) / (1024.0 * 1024 * 1024) as data_gb,
+      sum(usage_seconds) / (60.0 * 60) as task_hours
+    from catalog_stats, vars
+      where catalog_name ^@ billed_prefix -- Name starts with prefix.
+      and grain = 'daily'
+      and billed_range @> ts
+      group by ts
+  ),
+  -- Map to cumulative daily usage.
+  -- Note sum(...) over (order by ts) yields the running sum of its aggregate.
+  daily_stats as (
+    select
+      ts,
+      sum(data_gb) over w as data_gb,
+      sum(task_hours) over w as task_hours
+    from daily_stat_deltas
+    window w as (order by ts)
+  ),
+  -- Extend with line items for each category for the period ending with the given day.
+  daily_line_items as (
+    select
+      daily_stats.*,
+      internal.tier_line_items(ceil(data_gb)::integer, data_tiers, 'Data processing', 'GB') as data_line_items,
+      internal.tier_line_items(ceil(task_hours)::integer, usage_tiers, 'Task usage', 'hour') as task_line_items
+    from daily_stats, vars
+  ),
+  -- Extend with per-category subtotals for the period ending with the given day.
+  daily_totals as (
+    select
+      daily_line_items.*,
+      data_subtotal,
+      task_subtotal
+    from daily_line_items,
+      lateral (select sum((li->>'subtotal')::numeric) as data_subtotal from jsonb_array_elements(data_line_items) li) l1,
+      lateral (select sum((li->>'subtotal')::numeric) as task_subtotal from jsonb_array_elements(task_line_items) li) l2
+  ),
+  -- Map cumulative totals to per-day deltas.
+  daily_deltas as (
+    select
+      ts,
+      data_gb       - (coalesce(lag(data_gb,         1) over w, 0)) as data_gb,
+      data_subtotal - (coalesce(lag(data_subtotal,   1) over w, 0)) as data_subtotal,
+      task_hours    - (coalesce(lag(task_hours,      1) over w, 0)) as task_hours,
+      task_subtotal - (coalesce(lag(task_subtotal,   1) over w, 0)) as task_subtotal
+      from daily_totals
+      window w as (order by ts)
+  ),
+  -- 1) Group daily_deltas into a JSON array
+  -- 2) Sum a trial credit from daily deltas that overlap with the trial period.
+  daily_array_and_trial_credit as (
+    select
+    jsonb_agg(jsonb_build_object(
+      'ts', ts,
+      'data_gb', data_gb,
+      'data_subtotal', data_subtotal,
+      'task_hours', task_hours,
+      'task_subtotal', task_subtotal
+    )) as daily_usage,
+    coalesce(sum(data_subtotal + task_subtotal) filter (where trial_range @>ts),0 ) as trial_credit
+    from daily_deltas, vars
+  ),
+  -- The last day captures the cumulative billed period.
+  last_day as (
+    select * from daily_line_items
+    order by ts desc limit 1
+  ),
+  -- If we're reporting for the whole tenant then gather billing adjustment line-items.
+  adjustments as (
+    select coalesce(jsonb_agg(
+      jsonb_build_object(
+        'description', detail,
+        'count', 1,
+        'rate', usd_cents,
+        'subtotal', usd_cents
+      )
+    ), '[]') as adjustment_line_items
+    from internal.billing_adjustments a
+    where a.tenant = billed_prefix and a.billed_month = billing_report_202308.billed_month
+  )
+  select into
+    -- Block of variables being selected into.
+    o_daily_usage,
+    o_data_gb,
+    o_line_items,
+    o_recurring_fee,
+    o_task_hours,
+    o_trial_credit,
+    o_trial_start,
+    o_trial_range,
+    o_billed_range
+    -- The actual selected columns.
+    daily_usage,
+    data_gb,
+    data_line_items || task_line_items || adjustment_line_items,
+    recurring_fee,
+    task_hours,
+    trial_credit,
+    trial_start,
+    trial_range,
+    billed_range
+  from daily_array_and_trial_credit, last_day, adjustments, vars;
+
+  -- Add line items for recurring service fee & free trial credit.
+  if o_recurring_fee != 0 then
+    o_line_items = jsonb_build_object(
+      'description', 'Recurring service charge',
+      'count', 1,
+      'rate', o_recurring_fee,
+      'subtotal', o_recurring_fee
+    ) || o_line_items;
+  end if;
+
+  -- Display a (possibly zero) free trial credit if the trial range overlaps the billed range
+  if o_trial_range && o_billed_range then
+    o_line_items = o_line_items || jsonb_build_object(
+      'description', format('Free trial credit (%s - %s)', lower(o_trial_range), (upper(o_trial_range) - interval '1 day')::date),
+      'count', 1,
+      'rate', -o_trial_credit,
+      'subtotal', -o_trial_credit
+    );
+  end if;
+
+  -- Roll up the final subtotal.
+  select into o_subtotal sum((l->>'subtotal')::numeric)
+    from jsonb_array_elements(o_line_items) l;
+
+  return jsonb_build_object(
+    'billed_month', billed_month,
+    'billed_prefix', billed_prefix,
+    'daily_usage', o_daily_usage,
+    'line_items', o_line_items,
+    'processed_data_gb', o_data_gb,
+    'recurring_fee', o_recurring_fee,
+    'subtotal', o_subtotal,
+    'task_usage_hours', o_task_hours,
+    'trial_credit', coalesce(o_trial_credit, 0),
+    'trial_start', o_trial_start
+  );
+
+end
+$$ language plpgsql volatile security definer;
+
+-- Note: have to redefine this to know about internal.billing_report
+-- Calculate the specified month's billing report for every tenant
+-- and save those reports to billing_historicals.
+create or replace function internal.freeze_billing_month(billed_month timestamptz)
+returns integer as $$
+declare
+    tenant_row record;
+    tenant_count integer = 0;
+begin
+    for tenant_row in select tenant as tenant_name from tenants loop
+        insert into billing_historicals
+        select
+            report->>'billed_prefix' as tenant,
+            (report->>'billed_month')::timestamptz as billed_month,
+            report
+        from internal.billing_report_202308(tenant_row.tenant_name, billed_month) as report
+        on conflict do nothing;
+
+        -- INSERT statements set FOUND true if at least one row is affected, false if no row is affected.
+        if found then
+          tenant_count = tenant_count + 1;
+        end if;
+    end loop;
+    return tenant_count;
+end
+$$ language plpgsql volatile;
+-- End internal billing report gen
 
 create or replace view invoices_ext as
-with admin_roles as (
-  select role_prefix from auth_roles('admin')
+with has_bypassrls as (
+  select exists(select 1 from pg_roles where rolname = current_role and rolbypassrls = true) as bypass
+),
+authorized_tenants as (
+  select tenants.tenant
+  from tenants
+  left join has_bypassrls on true
+  left join auth_roles('admin') on tenants.tenant ^@ auth_roles.role_prefix
+  where has_bypassrls.bypass or auth_roles.role_prefix is not null
 ),
 historical_bills as (
   select
@@ -33,16 +299,17 @@ historical_bills as (
     date_trunc('month', (report->>'billed_month')::timestamptz) + interval '1 month' - interval '1 day' as date_end,
     report->>'billed_prefix' as billed_prefix,
     report->'line_items' as line_items,
-    report->'subtotal' as subtotal
+    report->'subtotal' as subtotal,
+    report as extra
   from billing_historicals
   -- inner join should give only rows that match the join condition
-  inner join admin_roles on billing_historicals.tenant ^@ admin_roles.role_prefix
+  inner join authorized_tenants on billing_historicals.tenant ^@ authorized_tenants.tenant
 ),
 manual_bills as (
   select
     date_start,
     date_end,
-    tenant as billed_prefix,
+    manual_bills.tenant as billed_prefix,
     jsonb_build_array(
       jsonb_build_object(
         'description', manual_bills.description,
@@ -51,9 +318,10 @@ manual_bills as (
         'subtotal', manual_bills.usd_cents
       )
     ) as line_items,
-    to_jsonb(usd_cents) as subtotal
+    to_jsonb(usd_cents) as subtotal,
+    'null'::jsonb as extra
   from manual_bills
-  inner join admin_roles on manual_bills.tenant ^@ admin_roles.role_prefix
+  inner join authorized_tenants on manual_bills.tenant ^@ authorized_tenants.tenant
 ),
 current_month as (
   select
@@ -61,20 +329,21 @@ current_month as (
     date_trunc('month', (report->>'billed_month')::timestamptz) + interval '1 month' - interval '1 day' as date_end,
     report->>'billed_prefix' as billed_prefix,
     report->'line_items' as line_items,
-    report->'subtotal' as subtotal
-  from admin_roles, billing_report_202308(admin_roles.role_prefix, now()) as report
+    report->'subtotal' as subtotal,
+    report as extra
+  from authorized_tenants, internal.billing_report_202308(authorized_tenants.tenant, now()) as report
 ),
 combined as (
   select
-    h.date_start, h.date_end, h.billed_prefix, h.line_items, h.subtotal, 'usage' as invoice_type
+    h.date_start, h.date_end, h.billed_prefix, h.line_items, h.subtotal, h.extra, 'usage' as invoice_type
   from historical_bills h
   union all
   select
-    m.date_start, m.date_end, m.billed_prefix, m.line_items, m.subtotal, 'manual' as invoice_type
+    m.date_start, m.date_end, m.billed_prefix, m.line_items, m.subtotal, m.extra, 'manual' as invoice_type
   from manual_bills m
   union all
   select
-    c.date_start, c.date_end, c.billed_prefix, c.line_items, c.subtotal, 'current_month' as invoice_type
+    c.date_start, c.date_end, c.billed_prefix, c.line_items, c.subtotal, c.extra, 'current_month' as invoice_type
   from current_month c
 )
 select * from combined
--- supabase/tests/billing.test.sql
+++ supabase/tests/billing.test.sql
@@ -148,38 +148,45 @@ begin
   delete from user_grants;
   insert into user_grants (user_id, object_role, capability) values
     ('11111111-1111-1111-1111-111111111111', 'aliceCo/', 'admin'),
+    ('11111111-1111-1111-1111-111111111111', 'aliceCo_aa/', 'admin'),
+    ('11111111-1111-1111-1111-111111111111', 'aliceCo_bb/', 'admin'),
+    ('11111111-1111-1111-1111-111111111111', 'aliceCo_cc/', 'admin'),
     ('22222222-2222-2222-2222-222222222222', 'bobCo/', 'admin')
   ;
 
   insert into tenants (tenant, data_tiers, usage_tiers, recurring_usd_cents) values
     ('aliceCo/', '{30, 4, 25, 6, 20, 20, 15}', '{18, 40, 17, 60, 16, 200, 14}', 10000),
+    ('aliceCo_aa/', '{50, 30, 20}', '{15}', 0),
+    ('aliceCo_bb/', '{50, 30, 20}', '{15}', 0),
+    ('aliceCo_cc/', '{50, 30, 20}', '{15}', 0),
     ('bobCo/', default, default, default);
 
   insert into catalog_stats (
     catalog_name, grain, ts, flow_document, bytes_written_by_me, bytes_read_by_me, usage_seconds
   ) values
-    ('aliceCo/aa/hello', 'daily', '2022-08-01T00:00:00Z', '{}', 5.125 * 1024 * 1024 * 1024, 0, 3600 * 720),
-    ('aliceCo/aa/big',   'daily', '2022-08-01T00:00:00Z', '{}', 6::bigint * 1024 * 1024 * 1024, 7::bigint * 1024 * 1024 * 1024, 0),
-    ('aliceCo/aa/big',   'daily', '2022-08-30T00:00:00Z', '{}', 1::bigint * 1024 * 1024 * 1024, 2::bigint * 1024 * 1024 * 1024, 0),
-    ('aliceCo/bb/world', 'daily', '2022-08-01T00:00:00Z', '{}', 0, 22::bigint * 1024 * 1024 * 1024, 3600 * 18.375),
-    ('aliceCo/cc/round', 'daily', '2022-07-01T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-02T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-03T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-04T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-05T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-06T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-07T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-08T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-09T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-10T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-11T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-12T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-13T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-14T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-15T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-16T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-17T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
-    ('aliceCo/cc/round', 'daily', '2022-07-18T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0)
+    ('aliceCo/hello',    'daily', '2022-08-01T00:00:00Z', '{}', 43.125 * 1024 * 1024 * 1024, 0, 3600 * 738.375),
+    ('aliceCo_aa/hello', 'daily', '2022-08-01T00:00:00Z', '{}', 5.125 * 1024 * 1024 * 1024, 0, 3600 * 720),
+    ('aliceCo_aa/big',   'daily', '2022-08-01T00:00:00Z', '{}', 6::bigint * 1024 * 1024 * 1024, 7::bigint * 1024 * 1024 * 1024, 0),
+    ('aliceCo_aa/big',   'daily', '2022-08-30T00:00:00Z', '{}', 1::bigint * 1024 * 1024 * 1024, 2::bigint * 1024 * 1024 * 1024, 0),
+    ('aliceCo_bb/world', 'daily', '2022-08-01T00:00:00Z', '{}', 0, 22::bigint * 1024 * 1024 * 1024, 3600 * 18.375),
+    ('aliceCo_cc/round', 'daily', '2022-07-01T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-02T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-03T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-04T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-05T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-06T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-07T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-08T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-09T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-10T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-11T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-12T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-13T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-14T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-15T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-16T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-17T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0),
+    ('aliceCo_cc/round', 'daily', '2022-07-18T00:00:00Z', '{}', 0, 0.1 * 1024 * 1024 * 1024, 0)
   ;
 
   insert into internal.billing_adjustments (
@@ -191,10 +198,18 @@ begin
     ('bobCo/', '2022-08-01T00:00:00Z', 200, 'frank@estuary.dev', 'different tenant')
   ;
 
+  perform internal.freeze_billing_month('2022-08-01');
   -- We're authorized as Alice.
   perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
 
-  return query select is(billing_report_202308('aliceCo/', '2022-08-29T13:00:00Z') - 'daily_usage', '{
+  return query select is((select jsonb_agg(invoices.extra-'daily_usage') from (
+    select extra
+    from invoices_ext
+    where date_end < '2022-09-01'
+    and billed_prefix = 'aliceCo/'
+  ) as invoices),
+  jsonb_build_array(
+    '{
     "billed_month": "2022-08-01T00:00:00+00:00",
     "billed_prefix": "aliceCo/",
     "line_items": [
@@ -271,60 +286,18 @@ begin
     "task_usage_hours": 738.375,
     "trial_credit": 0,
     "trial_start": null
-  }'::jsonb);
-
-  set role postgres;
-
-  -- Use a simpler tier structure.
-  update tenants set
-    data_tiers = '{50, 30, 20}',
-    usage_tiers = '{15}'
-    where tenant = 'aliceCo/';
-
-  perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
-
-  -- Again, but now look at a narrower billed prefix.
-  -- Note that we don't see fixed cost or adjustment,
-  -- just rolled-up usage of the prefixed catalog tasks.
-  return query select is((billing_report_202308('aliceCo/aa/', '2022-08-29T13:00:00Z') - 'daily_usage'), '{
-    "billed_month": "2022-08-01T00:00:00+00:00",
-    "billed_prefix": "aliceCo/aa/",
-    "line_items": [
-      {
-        "rate": 50,
-        "count": 22,
-        "subtotal": 1100,
-        "description": "Data processing (first 30 GBs at $0.50/GB)"
-      },
-      {
-        "rate": 20,
-        "count": 0,
-        "subtotal": 0,
-        "description": "Data processing (at $0.20/GB)"
-      },
-      {
-        "rate": 15,
-        "count": 720,
-        "subtotal": 10800,
-        "description": "Task usage (at $0.15/hour)"
-      }
-    ],
-    "processed_data_gb": 21.125,
-    "recurring_fee": 0,
-    "subtotal": 11900,
-    "task_usage_hours": 720,
-    "trial_credit": 0,
-    "trial_start": null
-  }'::jsonb);
+  }'::jsonb
+  ));
 
   -- We're authorized as Bob.
   perform set_authenticated_context('22222222-2222-2222-2222-222222222222');
 
-  return query select throws_like(
-    $i$ select * from billing_report_202308('aliceCo/', '2022-08-29T13:00:00Z'); $i$,
-    'You are not authorized for the billed prefix aliceCo/',
-    'Attempting to fetch a report for aliceCo/ as Bob fails'
-  );
+  return query select is((select jsonb_agg(distinct invoices.billed_prefix) from (
+    select billed_prefix
+    from invoices_ext
+  ) as invoices),
+  jsonb_build_array('bobCo/'));
+
 
   set role postgres;
   -- Switch tiers so usage spills over
@@ -332,14 +305,22 @@ begin
   update tenants set
     trial_start='2022-08-15',
     data_tiers = '{50, 5, 20}'
-    where tenant = 'aliceCo/';
+    where tenant ^@ 'aliceCo_';
 
+  truncate table billing_historicals;
+  perform internal.freeze_billing_month('2022-08-01');
   perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
 
   -- aliceCo has a free trial set, and has free trial usage, so let's check that
-  return query select is(billing_report_202308('aliceCo/aa/', '2022-08-29T13:00:00Z'), '{
+  return query select is((select jsonb_agg(invoices.extra) from (
+    select extra
+    from invoices_ext
+    where date_end < '2022-09-01'
+    and billed_prefix = 'aliceCo_aa/'
+  ) as invoices),
+  jsonb_build_array('{
     "billed_month": "2022-08-01T00:00:00+00:00",
-    "billed_prefix": "aliceCo/aa/",
+    "billed_prefix": "aliceCo_aa/",
     "daily_usage": [
         {
             "data_gb": 18.125,
@@ -388,13 +369,19 @@ begin
     "task_usage_hours": 720,
     "trial_credit": 60,
     "trial_start": "2022-08-15"
-  }'::jsonb);
+  }'::jsonb));
 
   -- aliceCo/bb has a free trial set, but has no usage in the trial period
   -- this should result in a free trial credit line item of $0
-  return query select is(billing_report_202308('aliceCo/bb/', '2022-08-29T13:00:00Z'), '{
+  return query select is((select jsonb_agg(invoices.extra) from (
+    select extra
+    from invoices_ext
+    where date_end < '2022-09-01'
+    and billed_prefix = 'aliceCo_bb/'
+  ) as invoices),
+  jsonb_build_array('{
     "billed_month": "2022-08-01T00:00:00+00:00",
-    "billed_prefix": "aliceCo/bb/",
+    "billed_prefix": "aliceCo_bb/",
     "daily_usage": [
       {
         "ts": "2022-08-01T00:00:00+00:00",
@@ -436,11 +423,21 @@ begin
     "task_usage_hours": 18.375,
     "trial_start": "2022-08-15",
     "trial_credit": 0
-  }'::jsonb);
+  }'::jsonb));
+
+  set role postgres;
+  perform internal.freeze_billing_month('2022-07-01');
+  perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
 
   -- aliceCo/cc has a free trial set, but has no usage in the trial period
   -- and multiple days of fractional usage. Let's see if they round correctly
-  return query select is(billing_report_202308('aliceCo/cc/', '2022-07-20T13:00:00Z'), '{
+  return query select is((select jsonb_agg(invoices.extra) from (
+    select extra
+    from invoices_ext
+    where date_end < '2022-08-01'
+    and billed_prefix = 'aliceCo_cc/'
+  ) as invoices),
+  jsonb_build_array('{
    "subtotal":100,
    "line_items":[
       {
@@ -593,22 +590,25 @@ begin
    "trial_start":"2022-08-15",
    "billed_month":"2022-07-01T00:00:00+00:00",
    "trial_credit":0,
-   "billed_prefix":"aliceCo/cc/",
+   "billed_prefix":"aliceCo_cc/",
    "recurring_fee":0,
    "task_usage_hours":0.00000000000000000000,
    "processed_data_gb":1.79999999329447746270
-  }'::jsonb);
+  }'::jsonb));
 
   set role postgres;
-  perform internal.freeze_billing_month('2022-08-01');
 
   insert into manual_bills (tenant, usd_cents, description, date_start, date_end)
   values ('aliceCo/',12356,'Test manually entered bill','2022-10-12','2022-11-25');
 
+  truncate table billing_historicals;
+  perform internal.freeze_billing_month('2022-08-01');
+
   perform set_authenticated_context('11111111-1111-1111-1111-111111111111');
-  return query select is((select jsonb_agg(invoices.jsonified) from (
+  return query select is((select jsonb_agg(invoices.jsonified::jsonb-'extra') from (
     select row_to_json(invoices_ext) as jsonified
     from invoices_ext
+    where billed_prefix='aliceCo/'
   ) as invoices), jsonb_build_array(
     jsonb_build_object(
       'date_start', date_trunc('month', now()),
@@ -646,22 +646,52 @@ begin
                 "subtotal": 10000
             },
             {
-                "count": 5,
-                "description": "Data processing (first 5 GBs at $0.50/GB)",
-                "rate": 50,
-                "subtotal": 250
+                "count": 4,
+                "description": "Data processing (first 4 GBs at $0.30/GB)",
+                "rate": 30,
+                "subtotal": 120
             },
             {
-                "count": 39,
-                "description": "Data processing (at $0.20/GB)",
+                "count": 6,
+                "description": "Data processing (next 6 GBs at $0.25/GB)",
+                "rate": 25,
+                "subtotal": 150
+            },
+            {
+                "count": 20,
+                "description": "Data processing (next 20 GBs at $0.20/GB)",
                 "rate": 20,
-                "subtotal": 780
+                "subtotal": 400
             },
             {
-                "count": 739,
-                "description": "Task usage (at $0.15/hour)",
+                "count": 14,
+                "description": "Data processing (at $0.15/GB)",
                 "rate": 15,
-                "subtotal": 11085
+                "subtotal": 210
+            },
+            {
+                "count": 40,
+                "description": "Task usage (first 40 hours at $0.18/hour)",
+                "rate": 18,
+                "subtotal": 720
+            },
+            {
+                "count": 60,
+                "description": "Task usage (next 60 hours at $0.17/hour)",
+                "rate": 17,
+                "subtotal": 1020
+            },
+            {
+                "count": 200,
+                "description": "Task usage (next 200 hours at $0.16/hour)",
+                "rate": 16,
+                "subtotal": 3200
+            },
+            {
+                "count": 439,
+                "description": "Task usage (at $0.14/hour)",
+                "rate": 14,
+                "subtotal": 6146
             },
             {
                 "count": 1,
@@ -674,15 +704,9 @@ begin
                 "description": "An extra charge for some reason",
                 "rate": 350,
                 "subtotal": 350
-            },
-            {
-                "count": 1,
-                "description": "Free trial credit (2022-08-15 - 2022-09-14)",
-                "rate": -60,
-                "subtotal": -60
             }
         ],
-        "subtotal": 22155
+        "subtotal": 22066
     }'::jsonb
   ));
 
