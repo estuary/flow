e9d7d9868e30f71ff61c1b80979d688edcdbe887
Joseph Shearer <joseph@josephshearer.net>
2023-09-29 12:55:42 -0400

    Implement logic to kick tenants into the free trial
    Rather than writing `internal.set_new_free_trials()` with a CTE, I thought it would be nice to be able to take a look and see which tenants are slated to be kicked into the free trial next time the cron job runs, so I made `internal.new_free_trial_tenants` a view. Any reason this would be a bad idea?
    
    The criteria I came up with from coversations is: a tenant should be kicked into the trial if:
    * They are not already a paying customer (i.e they don't have a free trial set) AND
    * They have at least one active, not-disabled capture or materialization
    * One of the following is true
      * Daily task usage time exceeds 2*24h
      * Monthly usage time exceeds 2*30d
      * Monthly data used exceeds 10gb

---
+++ supabase/migrations/33_automate_trial_start.sql
@@ -0,0 +1,48 @@
+begin;
+
+create or replace view internal.new_free_trial_tenants as
+select
+    tenants.tenant as tenant,
+    max(daily_stats.usage_seconds / (60.0 * 60)) as max_daily_usage_hours,
+    max(monthly_stats.usage_seconds / (60.0 * 60)) as max_monthly_usage_hours,
+    max(ceil((monthly_stats.bytes_written_by_me + monthly_stats.bytes_read_by_me) / (1024.0 * 1024 * 1024))) as max_monthly_gb,
+    count(distinct live_specs.id) filter (where live_specs.spec_type = 'capture') as today_captures,
+    count(distinct live_specs.id) filter (where live_specs.spec_type = 'materialization') as today_materializations
+from catalog_stats_monthly as monthly_stats
+join catalog_stats_daily as daily_stats on daily_stats.catalog_name = monthly_stats.catalog_name
+join tenants on tenants.tenant = split_part(monthly_stats.catalog_name,'/',1)||'/'
+join live_specs on (live_specs.catalog_name = monthly_stats.catalog_name and (live_specs.spec #>> '{shards,disable}')::boolean is not true)
+where tenants.trial_start is null
+group by tenants.tenant
+having (
+  (
+    max(daily_stats.usage_seconds / (60.0 * 60)) > (2 * 24) or
+    max(monthly_stats.usage_seconds / (60.0 * 60)) > (24 * 30 * 2) or
+    max((monthly_stats.bytes_written_by_me + monthly_stats.bytes_read_by_me) / (1024.0 * 1024 * 1024)) > 10
+  ) and (
+    count(distinct live_specs.id) filter (where live_specs.spec_type = 'capture') > 0 or
+    count(distinct live_specs.id) filter (where live_specs.spec_type = 'materialization') > 0
+  )
+)
+order by max_daily_usage_hours desc, max_monthly_usage_hours desc, max_monthly_gb desc;
+
+create or replace function internal.set_new_free_trials()
+returns integer as $$
+declare
+    tenant_row record;
+    update_count integer = 0;
+begin
+    for tenant_row in select tenant from internal.new_free_trial_tenants loop
+      update tenants set trial_start = date_trunc('day', now())
+      where tenants.tenant = tenant_row.tenant;
+
+      -- INSERT statements set FOUND true if at least one row is affected, false if no row is affected.
+      if found then
+        update_count = update_count + 1;
+      end if;
+    end loop;
+    return update_count;
+end
+$$ language plpgsql volatile;
+
+commit;
