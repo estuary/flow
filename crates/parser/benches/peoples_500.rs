edb1750ed62e5b28b575c27f47274300d562713d
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-04 21:30:18 +0100

    parser: add benchmark

---
+++ crates/parser/benches/peoples_500.rs
@@ -0,0 +1,22 @@
+#[path="../tests/testutil.rs"] mod testutil;
+
+use criterion::{criterion_group, criterion_main, Criterion, black_box};
+
+use parser::ParseConfig;
+use testutil::{input_for_file, run_test};
+
+fn peoples_500(c: &mut Criterion) {
+    let path = "benches/data/people-500.csv";
+    let cfg = ParseConfig {
+        filename: Some(path.to_string()),
+        ..Default::default()
+    };
+
+    c.bench_function("peoples_500", |b| b.iter(|| {
+        let input = input_for_file(path);
+        run_test(&cfg, input);
+    }));
+}
+
+criterion_group!(benches, peoples_500);
+criterion_main!(benches);
