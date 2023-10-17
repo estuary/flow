62a4aa7b0b0de335986a3b025fc05b457caa9780
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-17 16:07:24 +0100

    Revert "Revert "parser: use time crate to parse, update benchmark to remove I/O""
    This reverts commit a9520ae927e9373dde68e6614cbfbcbab9f3b2e3.

--- crates/parser/tests/sanitize_test.rs
+++ crates/parser/tests/sanitize_test.rs
@@ -17,17 +17,17 @@ fn sanitize_datetime_to_rfc3339() {
     let output = run_test(&cfg, input);
     output.assert_success(1);
 
-    let expected_first_row = "2020-01-01T00:00:00+00:00";
+    let expected_first_row = "2020-01-01T00:00:00Z";
     for value in output.parsed[0].as_object().unwrap().values() {
-        assert_eq!(expected_first_row, DateTime::parse_from_rfc3339(value.as_str().unwrap()).unwrap().to_rfc3339())
+        assert_eq!(expected_first_row, value.as_str().unwrap())
     }
 }
 
 #[test]
-fn sanitize_datetime_to_rfc3339_iana_timezone() {
+fn sanitize_datetime_to_rfc3339_offset() {
     let path = "tests/examples/datetimes-naive.csv";
     let cfg = ParseConfig {
-        default_timezone: "America/New_York".to_string(),
+        default_offset: "-05:00".to_string(),
         filename: Some(path.to_string()),
         ..Default::default()
     };
@@ -38,7 +38,7 @@ fn sanitize_datetime_to_rfc3339_iana_timezone() {
 
     let expected_first_row = "2020-01-01T00:00:00-05:00";
     for value in output.parsed[0].as_object().unwrap().values() {
-        assert_eq!(expected_first_row, DateTime::parse_from_rfc3339(value.as_str().unwrap()).unwrap().to_rfc3339())
+        assert_eq!(expected_first_row, value.as_str().unwrap())
     }
 }
 
