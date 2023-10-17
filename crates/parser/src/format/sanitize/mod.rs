400dec90be9efb562ca2d03277eaa466c4e76f75
Mahdi Dibaiee <mdibaiee@pm.me>
2023-10-17 16:07:24 +0100

    Revert "Revert "parser: sanitize timestamps to RFC3339""
    This reverts commit 48096710ab8caa5f72a1911c9878a203b5d6d6fa.

---
+++ crates/parser/src/format/sanitize/mod.rs
@@ -0,0 +1,14 @@
+use crate::{ParseConfig, Output};
+
+pub mod datetime;
+
+#[derive(Debug, thiserror::Error)]
+pub enum SanitizeError {
+    #[error("sanitizing datetimes: {0}")]
+    DatetimeSanitizeError(#[from] datetime::DatetimeSanitizeError),
+}
+
+pub fn sanitize_output(config: &ParseConfig, output: Output) -> Result<Output, SanitizeError> {
+    datetime::sanitize_datetime(config, output).map_err(SanitizeError::DatetimeSanitizeError)
+}
+
