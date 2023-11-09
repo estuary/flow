16d2cca224be1694442a3f2ee67f3dce023233f8
Joseph Shearer <joseph@josephshearer.net>
2023-11-09 10:40:05 -0500

    Implement `flowctl raw oauth` to test connector oauth flow end-to-end

--- crates/flowctl/src/raw/mod.rs
+++ crates/flowctl/src/raw/mod.rs
@@ -9,6 +9,7 @@ use std::{
 mod capture;
 mod discover;
 mod materialize_fixture;
+mod oauth;
 mod spec;
 mod suggest_schema;
 
@@ -55,6 +56,8 @@ pub enum Command {
     Capture(capture::Capture),
     /// Get the spec output of a connector
     Spec(spec::Spec),
+    /// Test a connector's OAuth config
+    Oauth(oauth::Oauth),
     /// Suggest a schema that would alleviate document schema violations of a specific collection
     SuggestSchema(suggest_schema::SuggestSchema),
     /// Emit the Flow specification JSON-Schema.
@@ -149,6 +152,7 @@ impl Advanced {
             Command::Discover(args) => discover::do_discover(ctx, args).await,
             Command::Capture(args) => capture::do_capture(ctx, args).await,
             Command::Spec(args) => spec::do_spec(ctx, args).await,
+            Command::Oauth(args) => oauth::do_oauth(ctx, args).await,
             Command::SuggestSchema(args) => suggest_schema::do_suggest_schema(ctx, args).await,
             Command::JsonSchema => {
                 let schema = models::Catalog::root_json_schema();
