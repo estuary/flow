8d901859d20b867bb2ebeb725a48f275d1c34eb9
Phil <phil@estuary.dev>
2023-12-05 13:38:33 -0500

    agent: don't allow deleting collections that are being used
    Fixes a bug that allowed users to delete collections that are in use by an
    active task.  The `ControlPlane` struct, which resolves remote collections
    during the build, was incorrectly returning built specs for collections being
    deleted by the current publication.  The reason for this was that the
    `ControlPlane` is using a separate connection pool, and is thus unable to see
    the updates of the in-progress transaction.  This adds kind of a hacky
    workaround to identify collections that are being deleted by the current
    publication and prevent them from being resolved.  The end result is that users
    will now get an error if they try to delete collections that are referenced by
    another live spec.

--- crates/agent/src/publications/validation.rs
+++ crates/agent/src/publications/validation.rs
@@ -1,14 +1,33 @@
+use std::collections::HashSet;
+
 use futures::{FutureExt, TryFutureExt};
 
 #[derive(Clone)]
 pub struct ControlPlane {
     pool: Option<sqlx::PgPool>,
+    /// A kludge to make sure we don't resolve collection specs that are being
+    /// deleted by the current publication. Ideally, the validation logic would
+    /// be aware that these collections were being deleted, and wouldn't ask for
+    /// them in the first place. But this should work in the meantime.
+    deleted_collections: HashSet<String>,
 }
 
 impl ControlPlane {
     pub fn new(pool: Option<&sqlx::PgPool>) -> Self {
         Self {
             pool: pool.cloned(),
+            deleted_collections: HashSet::new(),
+        }
+    }
+
+    /// Returns a copy of the `ControlPlane` that will filter out the given collections
+    /// when resolving collections and inferred schemas. This is a hopefully temporary
+    /// hack to ensure that we don't allow deleting collections that are still being used
+    /// by an active task.
+    pub fn with_deleted_collections(&self, deleted_collections: HashSet<String>) -> Self {
+        Self {
+            pool: self.pool.clone(),
+            deleted_collections,
         }
     }
 }
@@ -19,9 +38,13 @@ impl validation::ControlPlane for ControlPlane {
         collections: Vec<models::Collection>,
     ) -> futures::future::BoxFuture<'a, anyhow::Result<Vec<proto_flow::flow::CollectionSpec>>> {
         let Some(pool) = self.pool.clone() else {
-            return validation::NoOpControlPlane.resolve_collections(collections)
+            return validation::NoOpControlPlane.resolve_collections(collections);
         };
-        let collections = collections.into_iter().map(Into::into).collect();
+        let collections = collections
+            .into_iter()
+            .filter(|c| !self.deleted_collections.contains(c.as_str()))
+            .map(Into::into)
+            .collect();
 
         agent_sql::publications::resolve_collections(collections, pool)
             .map_err(Into::into)
@@ -41,9 +64,13 @@ impl validation::ControlPlane for ControlPlane {
         anyhow::Result<std::collections::BTreeMap<models::Collection, validation::InferredSchema>>,
     > {
         let Some(pool) = self.pool.clone() else {
-            return validation::NoOpControlPlane.get_inferred_schemas(collections)
+            return validation::NoOpControlPlane.get_inferred_schemas(collections);
         };
-        let collections = collections.into_iter().map(Into::into).collect();
+        let collections = collections
+            .into_iter()
+            .filter(|c| !self.deleted_collections.contains(c.as_str()))
+            .map(Into::into)
+            .collect();
 
         agent_sql::publications::get_inferred_schemas(collections, pool)
             .map_err(Into::into)
