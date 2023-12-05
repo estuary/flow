4660789a1caf077800b41cb348be0765e435a11c
Johnny Graettinger <johnny@estuary.dev>
2023-12-05 22:41:30 +0000

    runtime: add `combine` RPC and use it instead of bindings.NewCombiner
    Combine is a non-production RPC used in testing and CLI contexts for
    combining over documents of one or more configured collections and
    yielding their combined results.
    
    With this commit, all uses of bindings.NewCombiner have been removed.

---
+++ crates/runtime/src/combine/mod.rs
@@ -0,0 +1,18 @@
+use futures::Stream;
+use proto_flow::runtime::{CombineRequest as Request, CombineResponse as Response};
+
+mod protocol;
+mod serve;
+
+pub trait RequestStream: Stream<Item = anyhow::Result<Request>> + Send + Unpin + 'static {}
+impl<T: Stream<Item = anyhow::Result<Request>> + Send + Unpin + 'static> RequestStream for T {}
+
+pub trait ResponseStream: Stream<Item = anyhow::Result<Response>> + Send + 'static {}
+impl<T: Stream<Item = anyhow::Result<Response>> + Send + 'static> ResponseStream for T {}
+
+struct Binding {
+    key: Vec<doc::Extractor>,
+    ser_policy: doc::SerPolicy,
+    uuid_ptr: Option<doc::Pointer>,
+    values: Vec<doc::Extractor>,
+}
