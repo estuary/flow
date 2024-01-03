f2e8a9465bd8e7fa37a6b03858475b4061a7a318
Johnny Graettinger <johnny@estuary.dev>
2024-01-03 19:32:28 +0000

    wip

--- crates/simd-doc/src/lib.rs
+++ crates/simd-doc/src/lib.rs
@@ -1,39 +1,49 @@
+use std::ops::DerefMut;
+
 #[cxx::bridge]
 mod ffi {
 
     extern "Rust" {
-        type Alloc;
-        type Node<'alloc>;
-        type Field<'alloc>;
+        // Context for this parsing.
+        type Context;
+        // Node which is being parsed.
+        type Node<'c>;
+        // Field of an Object Node.
+        type Field<'c>;
 
-        unsafe fn set_array<'alloc>(
-            alloc: &'alloc Alloc,
-            node: &mut Node<'alloc>,
+        // Set Node to an Array and return its items for initialization.
+        unsafe fn set_array<'c>(
+            ctx: &'c mut Context,
+            node: &mut Node<'c>,
             size: usize,
-        ) -> &'alloc mut [Node<'alloc>];
+        ) -> &'c mut [Node<'c>];
 
-        unsafe fn set_object<'alloc>(
-            alloc: &'alloc Alloc,
-            node: &mut Node<'alloc>,
+        // Set Node to an Object and return its fields for initialization.
+        unsafe fn set_object<'c>(
+            ctx: &'c mut Context,
+            node: &mut Node<'c>,
             size: usize,
-        ) -> &'alloc mut [Field<'alloc>];
+        ) -> &'c mut [Field<'c>];
 
-        unsafe fn set_field<'alloc>(
-            alloc: &'alloc Alloc,
-            field: &'alloc mut Field<'alloc>,
+        // Set the property of a Field and return is value Node for initialization.
+        unsafe fn set_field<'c>(
+            ctx: &'c mut Context,
+            field: &'c mut Field<'c>,
             property: &str,
-        ) -> &'alloc mut Node<'alloc>;
+        ) -> &'c mut Node<'c>;
 
+        // Sort already-initialized fields into property order.
         fn sort_fields(fields: &mut [Field<'_>]);
 
+        // Set Node as a scalar from the given value.
         fn set_bool(node: &mut Node<'_>, value: bool);
         fn set_f64(node: &mut Node<'_>, value: f64);
         fn set_i64(node: &mut Node<'_>, value: i64);
         fn set_null(node: &mut Node<'_>);
-        unsafe fn set_string<'alloc>(alloc: &'alloc Alloc, node: &mut Node<'alloc>, value: &str);
+        unsafe fn set_string<'c>(ctx: &'c mut Context, node: &mut Node<'c>, value: &str);
         fn set_u64(node: &mut Node<'_>, value: u64);
 
-        fn fizzle(sib: usize, node: &Node<'_>);
+        unsafe fn complete<'c>(ctx: &'c mut Context, offset: usize, node: &Node<'c>);
     }
 
     unsafe extern "C++" {
@@ -44,69 +54,85 @@ mod ffi {
 
         fn new_parser() -> UniquePtr<parser>;
 
-        fn parse_many(
-            parser: &mut UniquePtr<parser>,
+        fn parse_many<'c>(
+            ctx: &'c mut Context,
+            node: &mut Node<'c>,
             padded_vec: &mut [u8],
-            alloc: &Alloc,
-            node: &mut Node,
+            parser: &mut UniquePtr<parser>,
         ) -> Result<usize>;
     }
 }
 
 pub use ffi::*;
-use std::ops::DerefMut;
 
-pub struct Alloc(pub doc::Allocator);
-pub struct Node<'alloc>(pub doc::HeapNode<'alloc>);
-pub struct Field<'alloc>(pub doc::HeapField<'alloc>);
+pub fn do_it(
+    parser: &mut cxx::UniquePtr<parser>,
+    input: &mut Vec<u8>,
+) -> Result<Vec<(usize, doc::OwnedHeapNode)>, cxx::Exception> {
+    let input_len = input.len();
+    input.extend_from_slice(&[0; 64]);
+    input.truncate(input_len);
 
-fn set_array<'alloc>(
-    alloc: &'alloc Alloc,
-    node: &mut Node<'alloc>,
-    size: usize,
-) -> &'alloc mut [Node<'alloc>] {
-    let mut array = doc::BumpVec::with_capacity_in(size, &alloc.0);
+    let mut ctx = Context {
+        alloc: std::sync::Arc::new(doc::Allocator::with_capacity(input_len * 2)),
+        out: Vec::new(),
+    };
+    let mut node = Node(doc::HeapNode::Null);
+
+    let consumed = parse_many(&mut ctx, &mut node, &mut *input, parser)?;
+    input.drain(..consumed);
+
+    Ok(ctx.out)
+}
+
+struct Context {
+    alloc: std::sync::Arc<doc::Allocator>,
+    out: Vec<(usize, doc::OwnedHeapNode)>,
+}
+
+#[repr(transparent)]
+pub struct Node<'c>(pub doc::HeapNode<'c>);
+#[repr(transparent)]
+pub struct Field<'c>(pub doc::HeapField<'c>);
+
+fn set_array<'c>(ctx: &'c mut Context, node: &mut Node<'c>, size: usize) -> &'c mut [Node<'c>] {
+    let mut array = doc::BumpVec::with_capacity_in(size, &ctx.alloc);
     // Safety: parse_many() will fully initialize all `size` items,
     // or will throw an exception which entirely discards all work.
     unsafe { array.set_len(size) };
 
-    // Safety: Node has the same layout as doc::HeapNode.
+    // Safety: Node is repr(transparent) with its inner doc::HeapNode.
     // parse_many() will be responsible for its initialization.
-    let ffi_array: &'alloc mut [Node<'alloc>] = unsafe { std::mem::transmute(array.deref_mut()) };
+    let ffi_array: &'c mut [Node<'c>] = unsafe { std::mem::transmute(array.deref_mut()) };
 
     node.0 = doc::HeapNode::Array(array);
     ffi_array
 }
 
-fn set_object<'alloc>(
-    alloc: &'alloc Alloc,
-    node: &mut Node<'alloc>,
-    size: usize,
-) -> &'alloc mut [Field<'alloc>] {
-    let mut fields = doc::BumpVec::with_capacity_in(size, &alloc.0);
+fn set_object<'c>(ctx: &'c mut Context, node: &mut Node<'c>, size: usize) -> &'c mut [Field<'c>] {
+    let mut fields = doc::BumpVec::with_capacity_in(size, &ctx.alloc);
     // Safety: parse_many() will fully initialize all `size` fields,
     // or will throw an exception which entirely discards all work.
     unsafe { fields.set_len(size) };
 
-    // Safety: Field has the same layout as doc::HeapField.
+    // Safety: Field is repr(transparent) with its inner doc::HeapField.
     // parse_many() will be responsible for its initialization.
-    let ffi_fields: &'alloc mut [Field<'alloc>] =
-        unsafe { std::mem::transmute(fields.deref_mut()) };
+    let ffi_fields: &'c mut [Field<'c>] = unsafe { std::mem::transmute(fields.deref_mut()) };
 
     node.0 = doc::HeapNode::Object(fields);
     ffi_fields
 }
 
-fn set_field<'alloc>(
-    alloc: &'alloc Alloc,
-    field: &'alloc mut Field<'alloc>,
+fn set_field<'c>(
+    ctx: &'c mut Context,
+    field: &'c mut Field<'c>,
     property: &str,
-) -> &'alloc mut Node<'alloc> {
-    field.0.property = doc::BumpStr::from_str(property, &alloc.0);
+) -> &'c mut Node<'c> {
+    field.0.property = doc::BumpStr::from_str(property, &ctx.alloc);
 
-    // Safety: Node has the same layout as doc::HeapNode.
+    // Safety: Node is repr(transparent) with its inner doc::HeapNode.
     // parse_many() will be responsible for its initialization.
-    let ffi_node: &'alloc mut Node<'alloc> = unsafe { std::mem::transmute(&mut field.0.value) };
+    let ffi_node: &'c mut Node<'c> = unsafe { std::mem::transmute(&mut field.0.value) };
     ffi_node
 }
 
@@ -119,15 +145,19 @@ fn set_f64(node: &mut Node<'_>, value: f64) {
 }
 
 fn set_i64(node: &mut Node<'_>, value: i64) {
-    node.0 = doc::HeapNode::NegInt(value)
+    node.0 = if value < 0 {
+        doc::HeapNode::NegInt(value)
+    } else {
+        doc::HeapNode::PosInt(value as u64)
+    };
 }
 
 fn set_null(node: &mut Node<'_>) {
     node.0 = doc::HeapNode::Null;
 }
 
-fn set_string<'alloc>(alloc: &'alloc Alloc, node: &mut Node<'alloc>, value: &str) {
-    node.0 = doc::HeapNode::String(doc::BumpStr::from_str(value, &alloc.0));
+fn set_string<'c>(ctx: &'c mut Context, node: &mut Node<'c>, value: &str) {
+    node.0 = doc::HeapNode::String(doc::BumpStr::from_str(value, &ctx.alloc));
 }
 
 fn set_u64(node: &mut Node<'_>, value: u64) {
@@ -138,7 +168,8 @@ fn sort_fields(fields: &mut [Field<'_>]) {
     fields.sort_by(|l, r| l.0.property.cmp(&r.0.property));
 }
 
-fn fizzle(sib: usize, node: &Node<'_>) {
-    let foo = serde_json::to_string(&doc::SerPolicy::default().on(&node.0)).unwrap();
-    eprintln!("fizzle: {sib} {foo}");
+fn complete<'c>(ctx: &'c mut Context, offset: usize, node: &Node<'c>) {
+    let node =
+        unsafe { doc::OwnedHeapNode::new(std::mem::transmute_copy(&node.0), ctx.alloc.clone()) };
+    ctx.out.push((offset, node));
 }
--- crates/simd-doc/src/simd-doc.h
+++ crates/simd-doc/src/simd-doc.h
@@ -12,26 +12,28 @@ inline std::unique_ptr<dom::parser> new_parser()
     return std::make_unique<dom::parser>();
 }
 
-void baz(dom::element element,
-         const Alloc &alloc,
-         Node &node);
+void walk_element(Context &ctx, dom::element elem, Node &node);
 
+// Parse many JSON documents from `padded_vec`, calling back with each before starting the next.
+// Return the number of unconsumed remainder bytes.
 inline size_t parse_many(
-    std::unique_ptr<dom::parser> &parser,
+    Context &ctx,
+    Node &node,
     rust::Slice<uint8_t> padded_vec,
-    const Alloc &alloc,
-    Node &node)
+    std::unique_ptr<dom::parser> &parser)
 {
     dom::document_stream stream = parser->parse_many(padded_vec.data(), padded_vec.size(), padded_vec.size());
 
-    for (dom::document_stream::iterator it = stream.begin(); it != stream.end(); ++it) {
-        baz(*it, alloc, node);
-        fizzle(it.current_index(), node);
+    for (dom::document_stream::iterator it = stream.begin(); it != stream.end(); ++it)
+    {
+        walk_element(ctx, *it, node);
+        complete(ctx, it.current_index(), node);
     }
-    return stream.truncated_bytes();
+    return stream.size_in_bytes() - stream.truncated_bytes();
 }
 
-void baz(dom::element elem, const Alloc &alloc, Node &node)
+// Recursively walk a `dom::element`, initializing `node` with its structure.
+void walk_element(Context &ctx, dom::element elem, Node &node)
 {
     switch (elem.type())
     {
@@ -43,11 +45,12 @@ void baz(dom::element elem, const Alloc &alloc, Node &node)
         {
             throw std::out_of_range("array is too large");
         }
-        rust::Slice<Node>::iterator cur = set_array(alloc, node, arr.size()).begin();
+        rust::Slice<Node> items = set_array(ctx, node, arr.size());
+        rust::Slice<Node>::iterator it = items.begin();
 
         for (dom::element child : arr)
         {
-            baz(child, alloc, *(cur++));
+            walk_element(ctx, child, *(it++));
         }
         break;
     }
@@ -59,69 +62,53 @@ void baz(dom::element elem, const Alloc &alloc, Node &node)
         {
             throw std::out_of_range("object is too large");
         }
-        rust::Slice<Field> fields = set_object(alloc, node, obj.size());
-        rust::Slice<Field>::iterator cur = fields.begin();
+        rust::Slice<Field> fields = set_object(ctx, node, obj.size());
+        rust::Slice<Field>::iterator it = fields.begin();
 
-        std::string_view last_key;
+        // Track whether field properties are already sorted.
+        rust::Str last_property;
         bool must_sort = false;
 
         for (dom::key_value_pair child : obj)
         {
-            baz(
-                child.value,
-                alloc,
-                set_field(alloc, *(cur++), rust::Str(child.key.data(), child.key.size())));
+            rust::Str property(child.key.data(), child.key.size());
 
-            if (child.key < last_key) {
+            if (property < last_property)
+            {
                 must_sort = true;
             }
-            last_key = child.key;
+
+            walk_element(ctx, child.value, set_field(ctx, *(it++), property));
+            last_property = property;
         }
 
-        if (must_sort) {
+        // Restore the sorted invariant of doc::HeapNode::Object fields.
+        if (must_sort)
+        {
             sort_fields(fields);
         }
-
         break;
     }
     case dom::element_type::INT64:
-    {
-        int64_t value = elem;
-        if (value < 0)
-        {
-            set_i64(node, value);
-        }
-        else
-        {
-            set_u64(node, static_cast<uint64_t>(value));
-        }
+        set_i64(node, elem);
         break;
-    }
     case dom::element_type::UINT64:
-    {
         set_u64(node, elem);
         break;
-    }
     case dom::element_type::DOUBLE:
-    {
         set_f64(node, elem);
         break;
-    }
     case dom::element_type::STRING:
     {
         std::string_view str = elem;
-        set_string(alloc, node, rust::Str(str.data(), str.size()));
+        set_string(ctx, node, rust::Str(str.data(), str.size()));
         break;
     }
     case dom::element_type::BOOL:
-    {
         set_bool(node, elem);
         break;
-    }
-    case dom::element_type::NULL_VALUE:
-    {
+    default:
         set_null(node);
         break;
-    }
     }
 }
