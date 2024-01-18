c804bc24a5e1a508a858f83dbd2a5f74b2e1dcd7
Phil <phil@estuary.dev>
2024-01-18 12:15:26 -0500

    agent: allow evolutions to affect a specific set of materializations
    In cases where there are multiple materializations of a given collection, it's
    possible that only a subset of them will reject a proposed change as being
    incompatible.  The publications handler already identifies the specific subset
    of materializations that have rejected a proposed change.  This commit
    introduces an optional `materializations` parameter to the evolutions handler,
    which can be used to limit the effects of evolution to the given subset of
    materializations.  If this parameter is not provided, all connected
    materializations will be updated, just as before.  If it is provided, then only
    the given materializations will be updated to have the backfill counters
    incremented. The net effect is that we can avoid incrementing the backfill
    counters for materializations that haven't actually rejected a proposed change.
    
    The publications handler was also updated to use the `materializations`
    parameter when creating evolutions for automatic publications.

--- crates/agent/src/evolution/test.rs
+++ crates/agent/src/evolution/test.rs
@@ -286,3 +286,100 @@ async fn evolution_preserves_changes_already_in_the_draft() {
     // (even though it's already larger than the value in live_specs).
     insta::assert_debug_snapshot!(draft_specs);
 }
+
+#[tokio::test]
+#[serial_test::parallel]
+async fn evolution_affects_specific_materializations_when_requested() {
+    let mut conn = sqlx::postgres::PgConnection::connect(&FIXED_DATABASE_URL)
+        .await
+        .unwrap();
+    let mut txn = conn.begin().await.unwrap();
+
+    let draft_id = Id::from_hex("2230000000000000").unwrap();
+    let user_id = uuid::Uuid::parse_str("43a18a3e-5a59-11ed-9b6a-0242ac188888").unwrap();
+    sqlx::query(include_str!("test_setup.sql"))
+        .execute(&mut txn)
+        .await
+        .unwrap();
+
+    // Add another materialization, so we can assert that only the requested one gets updated
+    sqlx::query(r##"
+        with clear_draft as (
+            delete from draft_specs where draft_id = '2230000000000000'
+        ),
+        ls as (
+            insert into live_specs (id, catalog_name, spec, spec_type, last_build_id, last_pub_id) values
+            (
+              'b333000000000000', 'evolution/MaterializationD',
+              '{
+                    "bindings": [
+                      {"source": "evolution/CollectionA", "backfill": 9, "resource": {"targetThingy": "testTargetThingA"}},
+                      {"source": "evolution/CollectionC", "resource": {"targetThingy": "testTargetThingC"}}
+                    ],
+                    "endpoint": {"connector": {"image": "matImage:v1", "config": {}}}
+                }' :: json, 
+              'materialization', 'bbbbbbbbbbbbbbbb', 'bbbbbbbbbbbbbbbb'
+            )
+        ),
+        flows as (
+            insert into live_spec_flows (source_id, target_id, flow_type) values
+            (
+              'a100000000000000', 'b333000000000000',
+              'materialization'
+            ), 
+            (
+              'a200000000000000', 'b333000000000000',
+              'materialization'
+            )
+        ) select 1
+    "##).execute(&mut txn).await.unwrap();
+
+    let input = serde_json::value::to_raw_value(&serde_json::json!([
+        {"old_name": "evolution/CollectionA", "materializations": ["evolution/MaterializationD"]},
+    ]))
+    .unwrap();
+    let evolution_row = Row {
+        id: Id::from_hex("f100000000000000").unwrap(),
+        created_at: Utc::now(),
+        detail: None,
+        draft_id,
+        logs_token: uuid::Uuid::new_v4(),
+        updated_at: Utc::now(),
+        user_id,
+        collections: agent_sql::TextJson(input),
+        auto_publish: false,
+    };
+
+    let result = super::process_row(evolution_row, &mut txn)
+        .await
+        .expect("process row should succeed");
+
+    let JobStatus::Success {
+        evolved_collections,
+        publication_id,
+    } = result
+    else {
+        panic!("unexpected job status: {result:?}, expected success");
+    };
+    assert!(publication_id.is_none());
+
+    insta::assert_debug_snapshot!(evolved_collections);
+
+    let draft_specs = sqlx::query!(
+        r#"
+        select catalog_name, spec_type as "spec_type: CatalogType", spec, expect_pub_id as "expect_pub_id: Id"
+        from draft_specs
+        where draft_id = '2230000000000000'
+        order by catalog_name asc
+    "#
+    )
+    .fetch_all(&mut txn)
+    .await
+    .expect("querying draft_specs");
+
+    // We're looking for the new endpoint and resource configs, which ought to
+    // be preserved, and for the backfill counter to have been incremented still
+    // (even though it's already larger than the value in live_specs). Also looking
+    // for the expect_pub_id from the draft spec to be preserved.
+    insta::assert_debug_snapshot!(draft_specs);
+}
