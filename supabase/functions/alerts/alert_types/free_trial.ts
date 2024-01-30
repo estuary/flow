401967b9b591b7f5e621b0f6a097c0fb81b3d14c
Joseph Shearer <joseph@josephshearer.net>
2024-01-30 16:46:51 -0500

    Update condition naming to be more _condition_ focused rather than event focused

---
+++ supabase/functions/alerts/alert_types/free_trial.ts
@@ -0,0 +1,67 @@
+import { AlertRecord, EmailConfig } from "../index.ts";
+import { Recipient } from "../template.ts";
+import { commonTemplate } from "../template.ts";
+
+interface FreeTrialArguments {
+    // This feels like it should apply to all alert types, and doesn't belong here..
+    recipients: Recipient[];
+    trial_start: string;
+    trial_end: string;
+    tenant: string;
+    has_credit_card: boolean;
+}
+
+type FreeTrialRecord = AlertRecord<"free_trial", FreeTrialArguments>;
+
+const freeTrialStarted = (req: FreeTrialRecord): EmailConfig[] => {
+    return req.arguments.recipients.map((recipient) => ({
+        emails: [recipient.email],
+        subject: "Estuary Free Trial",
+        content: req.arguments.has_credit_card
+            // The only scenario in which your trial would start and you have a cc on file is we manually gave you back your trial
+            ? commonTemplate(
+                `
+                <mj-text>Your Estuary account <a class="identifier">${req.arguments.tenant}</a> has started its 30-day free trial. This trial will end on <strong>${req.arguments.trial_end}</strong>. Billing will begin accruing then.</mj-text>
+            `,
+                recipient,
+            )
+            : commonTemplate(
+                `
+                <mj-text>We hope you're enjoying Estuary Flow. Our free tier includes 10 GB / month and 2 connectors and your account <a class="identifier">${req.arguments.tenant}</a> has now exceeded that, so it has been transitioned to a 30 day free trial ending on <strong>${req.arguments.trial_end}</strong>.</mj-text>
+                <mj-text>Please add payment information in the next 30 days to continue using the platform. If you have any questions, feel free to reach out to <a href="mailto:support@estuary.dev">support@estuary.dev</a></mj-text>
+                <mj-button href="https://dashboard.estuary.dev/admin/billing">Add payment information</mj-button>
+            `,
+                recipient,
+            ),
+    }));
+};
+
+const freeTrialEnded = (req: FreeTrialRecord): EmailConfig[] => {
+    return req.arguments.recipients.map((recipient) => ({
+        emails: [recipient.email],
+        subject: req.arguments.has_credit_card ? "Estuary Flow: Paid Tier" : "Estuary Paid Tier: Enter Payment Info to Continue Access",
+        content: req.arguments.has_credit_card
+            ? commonTemplate(
+                `
+                <mj-text>Your Estuary account <a class="identifier">${req.arguments.tenant}</a> has started its 30-day free trial. This trial will end on <strong>${req.arguments.trial_end}</strong>. Billing will begin accruing then.</mj-text>
+            `,
+                recipient,
+            )
+            : commonTemplate(
+                `
+                <mj-text>We hope you are enjoying Estuary Flow. Your free trial for account <a class="identifier">${req.arguments.tenant}</a> is officially over.</mj-text>
+                <mj-text>Please add payment information immediately to continue using the platform. If you have any questions, feel free to reach out to <a href="mailto:support@estuary.dev">support@estuary.dev</a></mj-text>
+                <mj-button href="https://dashboard.estuary.dev/admin/billing">Add payment information</mj-button>
+            `,
+                recipient,
+            ),
+    }));
+};
+
+export const freeTrialEmail = (request: FreeTrialRecord): EmailConfig[] => {
+    if (request.resolved_at) {
+        return freeTrialEnded(request);
+    } else {
+        return freeTrialStarted(request);
+    }
+};
--- supabase/functions/alerts/alert_types/free_trial_ending.ts
+++ supabase/functions/alerts/alert_types/free_trial_ending.ts
@@ -1,12 +1,10 @@
 import { AlertRecord, EmailConfig } from "../index.ts";
+import { Recipient } from "../template.ts";
 import { commonTemplate } from "../template.ts";
 
 interface FreeTrialEnding {
     // This feels like it should apply to all alert types, and doesn't belong here..
-    recipients: {
-        email: string;
-        full_name: string | null;
-    }[];
+    recipients: Recipient[];
     trial_start: string;
     trial_end: string;
     tenant: string;
@@ -20,22 +18,25 @@ const freeTrialEnding = (req: FreeTrialEndingRecord): EmailConfig[] => {
         emails: [recipient.email],
         subject: "Estuary Flow: Paid Tier",
         content: req.arguments.has_credit_card
-            ? commonTemplate(`
-                <mj-text font-size="20px" color="#512d0b"><strong>Dear ${recipient.full_name},</strong></mj-text>
-                <mj-text font-size="17px" line-height="1.3">Just so you know, your free trial for <a class="identifier">${req.arguments.tenant}</a> will be ending on <strong>${req.arguments.trial_end}</strong>. Since you have already added a payment method, no action is required.</mj-text>
-                <mj-button background-color="#5072EB" color="white" href="https://dashboard.estuary.dev/admin/billing" padding="25px 0 0 0" font-weight="400" font-size="17px">📈 View your stats</mj-button>
-            `)
-            : commonTemplate(`
-                <mj-text font-size="20px" color="#512d0b"><strong>Dear ${recipient.full_name},</strong></mj-text>
-                <mj-text font-size="17px" line-height="1.3">Your free trial for <a class="identifier">${req.arguments.tenant}</a> is ending on <strong>${req.arguments.trial_end}</strong>, at which point your account will begin accruing usage. Please enter a payment method in order to continue using the platform after your trial ends. We'd be sad to see you go!</mj-text>
-                <mj-button background-color="#5072EB" color="white" href="https://dashboard.estuary.dev/admin/billing" padding="25px 0 0 0" font-weight="400" font-size="17px">💳 Enter a credit card</mj-button>
-            `),
+            ? commonTemplate(
+                `
+                <mj-text>Just so you know, your free trial for <a class="identifier">${req.arguments.tenant}</a> will be ending on <strong>${req.arguments.trial_end}</strong>. Since you have already added a payment method, no action is required.</mj-text>
+                <mj-button href="https://dashboard.estuary.dev/admin/billing">📈 View your stats</mj-button>
+            `,
+                recipient,
+            )
+            : commonTemplate(
+                `
+                <mj-text>Your free trial for <a class="identifier">${req.arguments.tenant}</a> is ending on <strong>${req.arguments.trial_end}</strong>, at which point your account will begin accruing usage. Please enter a payment method in order to continue using the platform after your trial ends. We'd be sad to see you go!</mj-text>
+                <mj-button href="https://dashboard.estuary.dev/admin/billing">Add payment information</mj-button>
+            `,
+                recipient,
+            ),
     }));
 };
 
 export const freeTrialEndingEmail = (request: FreeTrialEndingRecord): EmailConfig[] => {
     if (request.resolved_at) {
-        // Do we want to send a "cc confirmed" email when this alert stops firing?
         return [];
     } else {
         return freeTrialEnding(request);
