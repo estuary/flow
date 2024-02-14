252db564a9e0e6e8f9b9050bca2828a7cfdbe15e
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-02-14 12:21:31 -0500

    Preserve collections written by other active captures

--- supabase/migrations/43_draft_associated_collections.sql
+++ supabase/migrations/43_draft_associated_collections.sql
@@ -1,22 +1,33 @@
 create or replace function internal.get_collections_eligible_for_deletion(live_spec_id flowid)
 returns table(id flowid, catalog_name catalog_name, last_pub_id flowid) as $$
-declare
-  eligible_collection_ids flowid[];
 begin
 
-eligible_collection_ids := array(with target_collections as (
-    select target_id from live_spec_flows
-        where source_id = live_spec_id
+return query with target_collections as (
+  select target_id from live_spec_flows
+    where source_id = live_spec_id
+),
+collections_read as (
+  select target_collections.target_id from target_collections
+    join live_spec_flows lsf on target_collections.target_id = lsf.source_id
+),
+collections_written as (
+  select target_collections.target_id from target_collections
+    join live_spec_flows lsf on target_collections.target_id = lsf.target_id and lsf.source_id <> live_spec_id
+),
+ineligible_collections as (
+  select target_id from collections_read
+    union select target_id from collections_written
+),
+eligible_collections as (
+  select target_id from target_collections
+    except select target_id from ineligible_collections
 )
-select * from target_collections
-    where target_id not in (select source_id from live_spec_flows));
-
-return query select
-  live_specs.id,
-  live_specs.catalog_name,
-  live_specs.last_pub_id
-from live_specs
-where spec_type = 'collection' and live_specs.id = ANY(eligible_collection_ids);
+select
+  ls.id,
+  ls.catalog_name,
+  ls.last_pub_id
+from eligible_collections
+  join live_specs ls on eligible_collections.target_id = ls.id;
 
 end;
 $$ language plpgsql security definer;
