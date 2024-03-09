513fbc70177085e702dfae031321aca39ba410b4
Johnny Graettinger <johnny@estuary.dev>
2024-03-09 05:08:08 +0000

    gazette: new Stream-oriented client
    * Router for dynamic dispatch using a request Route.
    
    * read_json_lines() using simd-doc for the common case of reading JSON
      documents as quickly as possible.

---
+++ crates/gazette/src/lib.rs
@@ -0,0 +1,36 @@
+pub use proto_gazette::broker;
+
+pub mod journal;
+pub mod uuid;
+
+mod router;
+pub use router::Router;
+
+mod auth;
+pub use auth::Auth;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("invalid gRPC endpoint: '{0}'")]
+    InvalidEndpoint(String),
+    #[error(transparent)]
+    Transport(#[from] tonic::transport::Error),
+    #[error(transparent)]
+    Grpc(#[from] tonic::Status),
+    #[error("failed to fetch fragment from storage URL")]
+    FetchFragment(#[source] reqwest::Error),
+    #[error("failed to read fetched fragment from storage URL")]
+    ReadFragment(#[source] std::io::Error),
+    #[error("invalid bearer token")]
+    BearerToken(#[source] tonic::metadata::errors::InvalidMetadataValue),
+    #[error("unexpected broker status: {0:?}")]
+    BrokerStatus(broker::Status),
+    #[error("failed to parse document near journal offset {0}")]
+    Parsing(i64, #[source] std::io::Error),
+    #[error("{0}")]
+    Protocol(&'static str),
+    #[error(transparent)]
+    UUID(#[from] uuid::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
