11431ac211265ae8c806d0f8273f6ed6b08c9c80
Johnny Graettinger <johnny@estuary.dev>
2024-05-07 23:35:58 -0500

    gazette: add optional buffering to read_json_lines()
    In order to let callers perform hot loops over larger amounts of journal
    content without yielding.
    
    The inner stream is already boxed, so this is a very cheap place to add
    task-based polling and buffering of ReadResponses.
    
    ReadResponses contain bytes and have a bounded size (about 130K),
    which makes the total memory commitment fairly predictable.
    
    Also update for `next_offset`.

--- crates/gazette/src/journal/read_json_lines.rs
+++ crates/gazette/src/journal/read_json_lines.rs
@@ -1,25 +1,55 @@
 use super::Client;
 use crate::Error;
-use futures::{stream::BoxStream, StreamExt};
+use futures::{stream::BoxStream, SinkExt, StreamExt};
 use proto_gazette::broker;
 
 /// ReadJsonLine is the enumerated Item type of a read_json_lines() Stream.
 pub enum ReadJsonLine {
-    /// Doc is a document which was parsed at `offset` within the journal.
+    /// Doc is a parsed journal document and its `next_offset`, which is the
+    /// offset one beyond its last content or whitespace byte within the journal,
+    /// and is generally the offset of the *next* journal document.
+    ///
+    /// The [begin, end) extent of each document can be determined by retaining
+    /// each document's `next_offset`, which is its "end" offset and the "begin"
+    /// offset of the next document.
+    ///
+    /// Note that a Meta enum variant may reset the "begin" offset,
+    /// to the `begin` offset of its contained Fragment.
     Doc {
-        offset: i64,
         root: doc::OwnedArchivedNode,
+        next_offset: i64,
     },
     /// Meta is a metadata response which includes the Fragment currently being read.
     Meta(broker::ReadResponse),
 }
 
 impl Client {
-    pub fn read_json_lines(self, req: broker::ReadRequest) -> ReadJsonLines {
+    pub fn read_json_lines(self, req: broker::ReadRequest, buffer: usize) -> ReadJsonLines {
+        let inner = self.read(req);
+
+        // When buffered, use a tokio task to read up to `buffer` ReadResponses.
+        let inner = if buffer != 0 {
+            let (mut tx, rx) = futures::channel::mpsc::channel(buffer - 1);
+
+            tokio::spawn(async move {
+                tokio::pin!(inner);
+
+                while let Some(result) = inner.next().await {
+                    if let Err(_) = tx.send(result).await {
+                        break; // Read was cancelled.
+                    }
+                }
+            });
+
+            rx.boxed()
+        } else {
+            inner.boxed()
+        };
+
         ReadJsonLines {
             parsed: simd_doc::transcoded::OwnedIterOut::empty(),
             parser: simd_doc::Parser::new(),
-            inner: self.read(req).boxed(),
+            inner,
         }
     }
 }
@@ -43,8 +73,8 @@ impl futures::Stream for ReadJsonLines {
         let me = self.project();
 
         loop {
-            if let Some((offset, root)) = me.parsed.next() {
-                return Poll::Ready(Some(Ok(ReadJsonLine::Doc { offset, root })));
+            if let Some((root, next_offset)) = me.parsed.next() {
+                return Poll::Ready(Some(Ok(ReadJsonLine::Doc { root, next_offset })));
             }
 
             // Poll the inner stream for the next item
