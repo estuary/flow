35308cc8ed7d79248c31d83da95fc38ec7f3556d
Johnny Graettinger <johnny@estuary.dev>
2024-05-15 22:43:44 -0500

    crates/tables: DraftCatalog, LiveCatalog, and joins
    Broadly update tables to enable representing a potential state
    transition and transactional update of the global Estary catalog.
    
    LiveCatalog reflects a partial snapshot of the Estuary catalog,
    including current specification models and built specs,
    and tagged with the `last_pub_id` of their respective _last_
    publications.
    
    DraftCatalog reflects a desired change to be applied to the catalog
    in terms of specification models, optionally with `expect_pub_id` to
    which those models should be applied.
    
    Validations (to be renamed BuiltCatalog) reflects a validation outcome,
    and contains metadata as to whether the specification is updated,
    its model, its previous built specification, and the `expect_pub_id`
    which was currently effective as-of this build.
    
    BuiltCatalog thus encodes an optimistic transaction which can be applied
    by validating all `expect_pub_id` priors and then updating models and built
    specifications within the global control plane.
    
    Also add new routines to facilitate joins across tables, as we have more
    of them and now need to join across them.

---
+++ crates/tables/src/built.rs
@@ -0,0 +1,234 @@
+/// BuiltRow is a common trait of rows reflecting built specifications.
+pub trait BuiltRow: crate::Row {
+    type ModelDef: models::ModelDef;
+    type Validated;
+    type BuiltSpec;
+
+    // Build a new BuiltRow from its parts.
+    fn new(
+        catalog_name: Self::Key,
+        scope: url::Url,
+        expect_pub_id: models::Id,
+        model: Option<Self::ModelDef>,
+        validated: Option<Self::Validated>,
+        spec: Option<Self::BuiltSpec>,
+        previous_spec: Option<Self::BuiltSpec>,
+    ) -> Self;
+
+    // Name of this specification.
+    fn catalog_name(&self) -> &Self::Key;
+    // Scope of the built specification.
+    fn scope(&self) -> &url::Url;
+    // Expected last publication ID for optimistic concurrency.
+    fn expect_pub_id(&self) -> models::Id;
+    // Model of the built specification.
+    fn model(&self) -> Option<&Self::ModelDef>;
+    // Validated response which was used to build this spec.
+    fn validated(&self) -> Option<&Self::Validated>;
+    // Built specification, or None if it's being deleted.
+    fn spec(&self) -> Option<&Self::BuiltSpec>;
+    // Previous specification which is being modified or deleted,
+    // or None if unchanged OR this is an insertion.
+    fn previous_spec(&self) -> Option<&Self::BuiltSpec>;
+
+    // Is this specification unchanged (passed through) from its live specification?
+    fn is_unchanged(&self) -> bool {
+        !self.expect_pub_id().is_zero() && self.previous_spec().is_none()
+    }
+    fn is_insert(&self) -> bool {
+        self.expect_pub_id().is_zero()
+    }
+    fn is_update(&self) -> bool {
+        !self.expect_pub_id().is_zero() && self.previous_spec().is_some() && self.spec().is_some()
+    }
+    fn is_delete(&self) -> bool {
+        !self.expect_pub_id().is_zero() && self.previous_spec().is_some() && self.spec().is_none()
+    }
+}
+
+impl BuiltRow for crate::BuiltCapture {
+    type ModelDef = models::CaptureDef;
+    type Validated = proto_flow::capture::response::Validated;
+    type BuiltSpec = proto_flow::flow::CaptureSpec;
+
+    fn new(
+        catalog_name: Self::Key,
+        scope: url::Url,
+        expect_pub_id: models::Id,
+        model: Option<Self::ModelDef>,
+        validated: Option<Self::Validated>,
+        spec: Option<Self::BuiltSpec>,
+        previous_spec: Option<Self::BuiltSpec>,
+    ) -> Self {
+        Self {
+            capture: catalog_name,
+            scope,
+            expect_pub_id,
+            model,
+            validated,
+            spec,
+            previous_spec,
+        }
+    }
+    fn catalog_name(&self) -> &Self::Key {
+        &self.capture
+    }
+    fn scope(&self) -> &url::Url {
+        &self.scope
+    }
+    fn expect_pub_id(&self) -> models::Id {
+        self.expect_pub_id
+    }
+    fn model(&self) -> Option<&Self::ModelDef> {
+        self.model.as_ref()
+    }
+    fn validated(&self) -> Option<&Self::Validated> {
+        self.validated.as_ref()
+    }
+    fn spec(&self) -> Option<&Self::BuiltSpec> {
+        self.spec.as_ref()
+    }
+    fn previous_spec(&self) -> Option<&Self::BuiltSpec> {
+        self.previous_spec.as_ref()
+    }
+}
+
+impl BuiltRow for crate::BuiltCollection {
+    type ModelDef = models::CollectionDef;
+    type Validated = proto_flow::derive::response::Validated;
+    type BuiltSpec = proto_flow::flow::CollectionSpec;
+
+    fn new(
+        catalog_name: Self::Key,
+        scope: url::Url,
+        expect_pub_id: models::Id,
+        model: Option<Self::ModelDef>,
+        validated: Option<Self::Validated>,
+        spec: Option<Self::BuiltSpec>,
+        previous_spec: Option<Self::BuiltSpec>,
+    ) -> Self {
+        Self {
+            collection: catalog_name,
+            scope,
+            expect_pub_id,
+            model,
+            validated,
+            spec,
+            previous_spec,
+        }
+    }
+    fn catalog_name(&self) -> &Self::Key {
+        &self.collection
+    }
+    fn scope(&self) -> &url::Url {
+        &self.scope
+    }
+    fn expect_pub_id(&self) -> models::Id {
+        self.expect_pub_id
+    }
+    fn model(&self) -> Option<&Self::ModelDef> {
+        self.model.as_ref()
+    }
+    fn validated(&self) -> Option<&Self::Validated> {
+        self.validated.as_ref()
+    }
+    fn spec(&self) -> Option<&Self::BuiltSpec> {
+        self.spec.as_ref()
+    }
+    fn previous_spec(&self) -> Option<&Self::BuiltSpec> {
+        self.previous_spec.as_ref()
+    }
+}
+
+impl BuiltRow for crate::BuiltMaterialization {
+    type ModelDef = models::MaterializationDef;
+    type Validated = proto_flow::materialize::response::Validated;
+    type BuiltSpec = proto_flow::flow::MaterializationSpec;
+
+    fn new(
+        catalog_name: Self::Key,
+        scope: url::Url,
+        expect_pub_id: models::Id,
+        model: Option<Self::ModelDef>,
+        validated: Option<Self::Validated>,
+        spec: Option<Self::BuiltSpec>,
+        previous_spec: Option<Self::BuiltSpec>,
+    ) -> Self {
+        Self {
+            materialization: catalog_name,
+            scope,
+            expect_pub_id,
+            model,
+            validated,
+            spec,
+            previous_spec,
+        }
+    }
+    fn catalog_name(&self) -> &Self::Key {
+        &self.materialization
+    }
+    fn scope(&self) -> &url::Url {
+        &self.scope
+    }
+    fn expect_pub_id(&self) -> models::Id {
+        self.expect_pub_id
+    }
+    fn model(&self) -> Option<&Self::ModelDef> {
+        self.model.as_ref()
+    }
+    fn validated(&self) -> Option<&Self::Validated> {
+        self.validated.as_ref()
+    }
+    fn spec(&self) -> Option<&Self::BuiltSpec> {
+        self.spec.as_ref()
+    }
+    fn previous_spec(&self) -> Option<&Self::BuiltSpec> {
+        self.previous_spec.as_ref()
+    }
+}
+
+impl BuiltRow for crate::BuiltTest {
+    type ModelDef = models::TestDef;
+    type Validated = ();
+    type BuiltSpec = proto_flow::flow::TestSpec;
+
+    fn new(
+        catalog_name: Self::Key,
+        scope: url::Url,
+        expect_pub_id: models::Id,
+        model: Option<Self::ModelDef>,
+        _validated: Option<Self::Validated>,
+        spec: Option<Self::BuiltSpec>,
+        previous_spec: Option<Self::BuiltSpec>,
+    ) -> Self {
+        Self {
+            test: catalog_name,
+            scope,
+            expect_pub_id,
+            model,
+            spec,
+            previous_spec,
+        }
+    }
+    fn catalog_name(&self) -> &Self::Key {
+        &self.test
+    }
+    fn scope(&self) -> &url::Url {
+        &self.scope
+    }
+    fn expect_pub_id(&self) -> models::Id {
+        self.expect_pub_id
+    }
+    fn model(&self) -> Option<&Self::ModelDef> {
+        self.model.as_ref()
+    }
+    fn validated(&self) -> Option<&Self::Validated> {
+        None
+    }
+    fn spec(&self) -> Option<&Self::BuiltSpec> {
+        self.spec.as_ref()
+    }
+    fn previous_spec(&self) -> Option<&Self::BuiltSpec> {
+        self.previous_spec.as_ref()
+    }
+}
