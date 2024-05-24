ce7c69983317fb1870cc03409f9b0e5855bf57ff
Johnny Graettinger <johnny@estuary.dev>
2024-05-24 11:42:58 -0500

    crates/gazette: add a basic shard::Client with list & apply support
    Also add apply RPC to journal::Client.

--- crates/gazette/src/shard/mod.rs
+++ crates/gazette/src/shard/mod.rs
@@ -1,36 +1,29 @@
 use futures::FutureExt;
-use proto_gazette::broker;
+use proto_gazette::consumer;
 use std::sync::Arc;
 
-mod read;
-
-mod read_json_lines;
-pub use read_json_lines::{ReadJsonLine, ReadJsonLines};
-
 // SubClient is the routed sub-client of Client.
-type SubClient = proto_grpc::broker::journal_client::JournalClient<
+type SubClient = proto_grpc::consumer::shard_client::ShardClient<
     tonic::service::interceptor::InterceptedService<tonic::transport::Channel, crate::auth::Auth>,
 >;
 pub type Router = crate::Router<SubClient>;
 
 #[derive(Clone)]
 pub struct Client {
-    http: reqwest::Client,
     router: Arc<Router>,
 }
 
 impl Client {
-    pub fn new(http: reqwest::Client, router: Router) -> Self {
+    pub fn new(router: Router) -> Self {
         Self {
-            http,
             router: Arc::new(router),
         }
     }
 
     pub async fn list(
         &self,
-        req: broker::ListRequest,
-    ) -> Result<broker::ListResponse, crate::Error> {
+        req: consumer::ListRequest,
+    ) -> Result<consumer::ListResponse, crate::Error> {
         let mut client = self.router.route(None, false).await?;
 
         let resp = client
@@ -44,27 +37,12 @@ impl Client {
 
     pub async fn apply(
         &self,
-        req: broker::ApplyRequest,
-    ) -> Result<broker::ApplyResponse, crate::Error> {
+        req: consumer::ApplyRequest,
+    ) -> Result<consumer::ApplyResponse, crate::Error> {
         let mut client = self.router.route(None, false).await?;
 
         let resp = client
             .apply(req)
-            .await
-            .map_err(crate::Error::Grpc)?
-            .into_inner();
-
-        check_ok(resp.status(), resp)
-    }
-
-    pub async fn list_fragments(
-        &self,
-        req: broker::FragmentsRequest,
-    ) -> Result<broker::FragmentsResponse, crate::Error> {
-        let mut client = self.router.route(None, false).await?;
-
-        let resp = client
-            .list_fragments(req)
             .await
             .map_err(crate::Error::Grpc)?
             .into_inner();
@@ -85,7 +63,7 @@ impl crate::Router<SubClient> {
                         .connect()
                         .await?;
                     Ok(
-                        proto_grpc::broker::journal_client::JournalClient::with_interceptor(
+                        proto_grpc::consumer::shard_client::ShardClient::with_interceptor(
                             channel,
                             interceptor.clone(),
                         ),
@@ -99,25 +77,10 @@ impl crate::Router<SubClient> {
     }
 }
 
-fn check_ok<R>(status: broker::Status, r: R) -> Result<R, crate::Error> {
-    if status == broker::Status::Ok {
+fn check_ok<R>(status: consumer::Status, r: R) -> Result<R, crate::Error> {
+    if status == consumer::Status::Ok {
         Ok(r)
     } else {
-        Err(crate::Error::BrokerStatus(status))
+        Err(crate::Error::ConsumerStatus(status))
     }
-}
-
-async fn backoff(attempt: u32) {
-    use std::time::Duration;
-
-    if attempt == 0 {
-        return;
-    }
-    let dur = match attempt {
-        1 | 2 => Duration::from_millis(50),
-        3 | 4 => Duration::from_millis(100),
-        5 | 6 => Duration::from_secs(1),
-        _ => Duration::from_secs(5),
-    };
-    tokio::time::sleep(dur).await;
 }
