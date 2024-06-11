178b9a613b6ebbc44a50d61212d20fa6a3251d5f
Phil <phil@estuary.dev>
2024-06-11 17:03:22 -0400

    activate: Unassign failed shards during activation
    Brings the `activate` crate in line with the behavior of `flowctl-go api activate`,
    which unassigns failed shards after updating shard specs. This ensures
    that failed shards will get restarted after a new spec has been
    published.

--- crates/activate/src/lib.rs
+++ crates/activate/src/lib.rs
@@ -88,7 +88,7 @@ pub async fn activate_collection(
         converge_partition_journals(journal_client, collection, partition_template),
     )?;
 
-    todo!()
+    Ok(())
 }
 
 /// Activate a materialization into a data-plane.
@@ -151,6 +151,18 @@ async fn converge_task_shards(
     // We must create recovery logs before we create their shards.
     journal_client.apply(log_changes).await?;
     shard_client.apply(shard_changes).await?;
+
+    // Unassign any failed shards to get them running again after updating their specs
+    let shard_ids = shards.into_iter().map(|s| s.0).collect();
+    let unassign_req = consumer::UnassignRequest {
+        shards: shard_ids,
+        only_failed: true,
+        dry_run: false,
+    };
+    shard_client
+        .unassign(unassign_req)
+        .await
+        .context("unassigning failed shards")?;
 
     Ok(())
 }
@@ -524,24 +536,41 @@ mod test {
         ),)
     }
 
+    async fn managed_build(source: url::Url) -> build::Output {
+        use tables::CatalogResolver;
+        let file_root = std::path::Path::new("/");
+        let draft = build::load(&source, file_root).await;
+        if !draft.errors.is_empty() {
+            return build::Output::new(draft, Default::default(), Default::default());
+        }
+        let catalog_names = draft.all_spec_names().collect();
+        let live = build::NoOpCatalogResolver.resolve(catalog_names).await;
+        if !live.errors.is_empty() {
+            return build::Output::new(draft, live, Default::default());
+        }
+
+        build::validate(
+            models::Id::new([32; 8]), // pub_id
+            models::Id::new([1; 8]),  // build_id
+            true,                     // allow_local
+            "",                       // connector_network
+            true,                     // generate_ops_collections
+            ops::tracing_log_handler,
+            false, // don't no-op validations
+            false, // don't no-op validations
+            false, // don't no-op validations
+            &build::project_root(&source),
+            draft,
+            live,
+        )
+        .await
+    }
+
     #[tokio::test]
     async fn fixture_subtests() {
         let source = build::arg_source_to_url("./src/test.flow.yaml", false).unwrap();
 
-        let build::Output { built, .. } = build::managed_build(
-            models::Id::new([32; 8]),
-            models::Id::new([1; 8]),
-            true,
-            String::new(),
-            Box::new(build::NoOpCatalogResolver),
-            "/".into(),
-            ops::tracing_log_handler,
-            build::project_root(&source),
-            source,
-        )
-        .await
-        .into_result()
-        .unwrap();
+        let build::Output { built, .. } = managed_build(source).await.into_result().unwrap();
 
         let tables::BuiltCollection { spec, .. } = built
             .built_collections
