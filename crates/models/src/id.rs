ef7b19a8bf8d52a358fc91fb7e7d9b77822e82bb
Phil <phil@estuary.dev>
2024-06-12 07:50:22 -0400

    models: add `IdGenerator`
    Adds the `IdGenerator` to the models crate, which generates new unique
    `Id`s. This sets up future work for controllers, which will need to
    generate their own publication ids (rather than generating the ids in
    the database). The implementation here closely follows the one in
    `02_flowid.sql`, with a couple of differences:
    
    - `IdGenerator` allows using any shard id (the sql version always uses
      `0`)
    - `IdGenerator` resets the sequence counter to 0 whenever the timestamp
      increases, in order to ensure that `Id`s always increase
      monotonically.

--- crates/models/src/id.rs
+++ crates/models/src/id.rs
@@ -1,6 +1,13 @@
 use std::fmt::Write;
 use std::str::FromStr;
 
+// Estuary epoch is the first representable timestamp in generated IDs.
+// This could be zero, but subtracting |estuary_epoch| results in the
+// high bit being zero for the next ~34 years,
+// making ID representations equivalent for both signed and
+// unsigned 64-bit integers.
+const ESTUARY_EPOCH_MILLIS: u64 = 1_600_000_000_000;
+
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Id([u8; 8]);
 
@@ -13,6 +20,30 @@ impl Id {
     }
     pub fn is_zero(&self) -> bool {
         self.0 == [0u8; 8]
+    }
+    pub fn as_array(&self) -> [u8; 8] {
+        self.0
+    }
+
+    /// Constructs a new `Id` from the given parts, or panics if a part is out of range.
+    pub fn from_parts(timestamp: u64, seq: u16, shard: u16) -> Self {
+        assert!(seq <= (1 << 13) - 1, "sequence number out of range");
+        assert!(shard <= (1 << 10) - 1, "shard id out of range");
+        let int_val = timestamp << 23 | (seq as u64) << 10 | shard as u64;
+        Self::new(int_val.to_be_bytes())
+    }
+
+    /// Returns a tuple of (timestamp, sequence number, shard id)
+    pub fn into_parts(self) -> (u64, u16, u16) {
+        const SEQ_MASK: u64 = (1u64 << 13) - 1;
+        const SHARD_MASK: u64 = (1u64 << 10) - 1;
+
+        let int_val = u64::from_be_bytes(self.0);
+        let timestamp = int_val >> 23;
+
+        let seq = ((int_val >> 10) & SEQ_MASK) as u16;
+        let shard = (int_val & SHARD_MASK) as u16;
+        (timestamp, seq, shard)
     }
 }
 
@@ -31,7 +62,7 @@ impl std::fmt::Display for Id {
         let mut s = String::with_capacity(23);
 
         for (ind, b) in self.0.iter().enumerate() {
-            if ind != 0 {
+            if ind != 0 && f.alternate() {
                 s.push(':');
             }
             write!(&mut s, "{b:02x}").unwrap();
@@ -74,5 +105,80 @@ impl<'de> serde::Deserialize<'de> for Id {
         use serde::de::Error;
         let str_val = std::borrow::Cow::<'de, str>::deserialize(deserializer)?;
         Id::from_str(str_val.as_ref()).map_err(|err| D::Error::custom(format!("invalid id: {err}")))
+    }
+}
+
+/// Generates unique IDs that are compatible with the `flowid` generation in postgres.
+#[derive(Debug, Clone)]
+pub struct IdGenerator {
+    shard: u16,
+    seq: u16,
+    last_timestamp: u64,
+}
+impl IdGenerator {
+    /// Return a new generator with the given shard id.
+    pub fn new(shard: u16) -> Self {
+        Self {
+            shard,
+            seq: 0,
+            last_timestamp: 0,
+        }
+    }
+
+    /// Generate and return the next unique id.
+    pub fn next(&mut self) -> Id {
+        let mut timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64;
+        timestamp -= ESTUARY_EPOCH_MILLIS;
+        // Ensure that the timestamp is monotonically increasing, which is not guaranteed
+        // by the system time.
+        timestamp = timestamp.max(self.last_timestamp);
+
+        if timestamp == self.last_timestamp {
+            // Check if the sequence number would overflow, and increment the
+            // timestamp instead if it would. This is _really_ unlikely to
+            // happen in production scenarios, but handling it makes it easier
+            // to test id generation.
+            if self.seq >= (1 << 13) - 1 {
+                self.last_timestamp += 1;
+                timestamp += 1;
+                self.seq = 0;
+            } else {
+                self.seq += 1;
+            }
+        } else {
+            self.seq = 0;
+            self.last_timestamp = timestamp;
+        }
+        Id::from_parts(timestamp, self.seq, self.shard)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_id_generation() {
+        let mut gen = IdGenerator::new(789);
+
+        let mut prev_id = gen.next();
+        for i in 0..20000 {
+            let id = gen.next();
+            let (timestamp, seq, shard) = id.into_parts();
+            assert_eq!(gen.shard, shard, "shard mismatch");
+            assert_eq!(gen.last_timestamp, timestamp, "i: {i}, timestamp mismatch");
+            assert!(
+                id > prev_id,
+                "i: {i}, ids must increase monotonically, prev: {prev_id}, next: {id}, prev_parts: {:?}, next_parts: {:?}",
+                prev_id.into_parts(),
+                id.into_parts(),
+            );
+            let round_tripped = Id::from_parts(timestamp, seq, shard);
+            assert_eq!(id, round_tripped, "round trip failed at {i}");
+            prev_id = id;
+        }
     }
 }
