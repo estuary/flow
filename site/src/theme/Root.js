d6b24e87b26d224aae8bd71b8972e31708ab6b2f
Travis Jenkins <travis@estuary.dev>
2024-06-21 16:19:35 -0400

    commenting

--- site/src/theme/Root.js
+++ site/src/theme/Root.js
@@ -8,12 +8,17 @@ const REO_DOT_DEV_ID = "a2955ffcdd9029c";
 // Default implementation, that you can customize
 export default function Root({ children }) {
     useEffect(() => {
-        // Add Reo.dev to docs
-        const reoPromise = loadReoScript({ clientID:REO_DOT_DEV_ID });
+        // Load in reo.dev scripts
+        const reoPromise = loadReoScript({
+            clientID: REO_DOT_DEV_ID
+        });
         reoPromise.then(Reo => {
-            Reo.init({ clientID:REO_DOT_DEV_ID });
+            Reo.init({
+                clientID: REO_DOT_DEV_ID
+            });
         })
 
+        // Listen for messaging from dashboard for styling light/dark mode
         const handleMessageListener = (event) => {
             if (event.origin === 'http://localhost:3000' || event.origin === 'https://dashboard.estuary.dev') {
                 if (event.data?.type === MESSAGE_TYPE) {
