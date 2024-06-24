c571f92405b6ad7b740c0b1df7a7afe735c09485
Travis Jenkins <travis@estuary.dev>
2024-06-24 11:05:51 -0400

    docs: Add integration with reo.dev  (#1501)
    * Adding Reo dot Dev to the docs site
    
    * Adding Reo dot Dev to the docs site - new file
    
    * Going to use NPM to add reo to make things easier
    
    * Fixing audit issues
    
    * commenting
    
    * Updating browser list

--- site/src/theme/Root.js
+++ site/src/theme/Root.js
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react';
+import { loadReoScript } from 'reodotdev'
 
 const MESSAGE_TYPE = 'estuary.docs.hideNavBar';
-
 const BODY_CLASS = 'running-in-iframe';
+const REO_DOT_DEV_ID = "a2955ffcdd9029c";
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
     useEffect(() => {
+        // Load in reo.dev scripts
+        const reoPromise = loadReoScript({
+            clientID: REO_DOT_DEV_ID
+        });
+        reoPromise.then(Reo => {
+            Reo.init({
+                clientID: REO_DOT_DEV_ID
+            });
+        })
+
+        // Listen for messaging from dashboard for styling light/dark mode
         const handleMessageListener = (event) => {
             if (event.origin === 'http://localhost:3000' || event.origin === 'https://dashboard.estuary.dev') {
                 if (event.data?.type === MESSAGE_TYPE) {
