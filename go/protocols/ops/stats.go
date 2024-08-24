7ee292d89da5e76e5ed0aa35454280e8d5216c4f
Johnny Graettinger <johnny@estuary.dev>
2024-08-24 17:47:16 -0500

    go/protocols: authorization adapters and add ops collections to shard labels
    Add wrappers for authorization and verification of the NetworkProxy and
    Shuffler gRPC services, which parallel the implementations used in the
    Gazette project.
    
    Define new capability constants for AUTHORIZE, SHUFFLE, and
    NETWORK_PROXY.
    
    Remove ValidateLogsCollection and ValidateStatsCollection, which are no
    longer used.

--- go/protocols/ops/stats.go
+++ go/protocols/ops/stats.go
@@ -2,12 +2,8 @@ package ops
 
 import (
 	"encoding/json"
-	"fmt"
-	"reflect"
-	"strings"
 	"time"
 
-	pf "github.com/estuary/flow/go/protocols/flow"
 	"github.com/gogo/protobuf/jsonpb"
 )
 
@@ -23,30 +19,6 @@ func (s *Stats_DocsAndBytes) MarshalJSONPB(*jsonpb.Marshaler) ([]byte, error) {
 		DocsTotal:  s.DocsTotal,
 		BytesTotal: s.BytesTotal,
 	})
-}
-
-// StatsCollection returns the collection to which stats for the given task name are written.
-func StatsCollection(taskName string) pf.Collection {
-	return pf.Collection(fmt.Sprintf("ops/%s/stats", strings.Split(taskName, "/")[0]))
-}
-
-// ValidateStatsCollection sanity-checks that the given CollectionSpec is appropriate
-// for storing instances of Stats documents.
-func ValidateStatsCollection(spec *pf.CollectionSpec) error {
-	if !reflect.DeepEqual(
-		spec.Key,
-		[]string{"/shard/name", "/shard/keyBegin", "/shard/rClockBegin", "/ts"},
-	) {
-		return fmt.Errorf("CollectionSpec doesn't have expected key: %v", spec.Key)
-	}
-
-	if !reflect.DeepEqual(spec.PartitionFields, []string{"kind", "name"}) {
-		return fmt.Errorf(
-			"CollectionSpec doesn't have expected partitions 'kind' & 'name': %v",
-			spec.PartitionFields)
-	}
-
-	return nil
 }
 
 func (s *Stats) GoTimestamp() time.Time {
