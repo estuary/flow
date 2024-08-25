1a971b6775793e3304e864f95bb648daa7153e95
Johnny Graettinger <johnny@estuary.dev>
2024-08-25 12:11:04 -0500

    crates/gazette: refactor Router, error handling, and add watched lists
    Add list_watch() which returns a Stream of watched ListResponses.
    
    Refactor streaming error handling within the crate, to remove back-off
    timers and policy around which errors are terminal or may be retried.
    Instead, surface all errors in the response Stream, and let the client
    decide to retry by polling the Stream again, or cancel by dropping the
    Stream. Add Error::is_transient() to simplify client checks of common
    retry-able error conditions.
    
    Refactor Router to hold instances of tonic::Channel instead of clients.
    This enables easy re-use of a Router and its dialed Channels, and makes
    construction of a journal:: or shard::Client cheap. This is important
    because we expect to use dynamic authorization tokens with ephemeral
    Clients built around a long-lived Router.

---
+++ crates/gazette/src/journal/list.rs
@@ -0,0 +1,101 @@
+use super::{check_ok, Client};
+use crate::Error;
+use futures::TryStreamExt;
+use proto_gazette::broker;
+
+impl Client {
+    /// List journals that match the ListRequest.
+    pub async fn list(&self, mut req: broker::ListRequest) -> crate::Result<broker::ListResponse> {
+        assert!(!req.watch, "list() requires ListRequest.watch is not set");
+        let mut stream = self.start_list(&self.router, &req).await?;
+        recv_snapshot(&mut req, &mut stream).await
+    }
+
+    /// Watch journals that match the ListRequest, returning a Stream which yields
+    /// on every updated listing snapshot pushed by the Gazette broker.
+    pub fn list_watch(
+        self,
+        mut req: broker::ListRequest,
+    ) -> impl futures::Stream<Item = crate::Result<broker::ListResponse>> + 'static {
+        assert!(req.watch, "list_watch() requires ListRequest.watch is set");
+
+        coroutines::coroutine(move |mut co| async move {
+            loop {
+                let mut stream = match self.start_list(&self.router, &req).await {
+                    Ok(stream) => stream,
+                    Err(err) => {
+                        () = co.yield_(Err(err)).await;
+                        continue;
+                    }
+                };
+
+                loop {
+                    match recv_snapshot(&mut req, &mut stream).await {
+                        Ok(resp) => co.yield_(Ok(resp)).await,
+                        Err(err) => {
+                            if matches!(err, Error::UnexpectedEof if req.watch_resume.is_some()) {
+                                // Server stopped an ongoing watch. Expected and not an error.
+                            } else {
+                                co.yield_(Err(err)).await;
+                            }
+                            break; // Start new stream on next poll.
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    async fn start_list(
+        &self,
+        router: &crate::Router,
+        req: &broker::ListRequest,
+    ) -> crate::Result<tonic::Streaming<broker::ListResponse>> {
+        let mut client = self.into_sub(router.route(None, false).await?);
+        Ok(client.list(req.clone()).await?.into_inner())
+    }
+}
+
+async fn recv_snapshot(
+    req: &mut broker::ListRequest,
+    stream: &mut tonic::Streaming<broker::ListResponse>,
+) -> crate::Result<broker::ListResponse> {
+    let mut maybe_resp: Option<broker::ListResponse> = None;
+
+    loop {
+        let next = stream.try_next().await?;
+
+        match (maybe_resp.take(), next) {
+            // Completion of listing snapshot in a unary !watch request.
+            (Some(resp), None) if !req.watch => {
+                return Ok(resp);
+            }
+            // Unexpected EOF of a watch request.
+            (Some(_resp), None) => {
+                return Err(Error::UnexpectedEof);
+            }
+            // First response of listing snapshot.
+            (None, Some(next)) => {
+                let next = check_ok(next.status(), next)?;
+                req.watch_resume = next.header.clone();
+                maybe_resp = Some(next);
+            }
+            // Continued response of a listing snapshot.
+            (Some(mut resp), Some(next)) if !next.journals.is_empty() => {
+                resp.journals.extend(next.journals.into_iter());
+                maybe_resp = Some(resp);
+            }
+            // Completion of listing snapshot in an ongoing watch request.
+            (Some(resp), Some(_next)) if req.watch => {
+                return Ok(resp);
+            }
+            // !watch responses after the first should never be empty.
+            (Some(_resp), Some(_next)) => {
+                return Err(Error::Protocol(
+                    "unexpected empty ListResponse continutation in a !watch request",
+                ));
+            }
+            (None, None) => return Err(Error::UnexpectedEof),
+        }
+    }
+}
