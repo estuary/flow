66fcd8ad7459e1b8b2e80e9c4b310e2fa465c611
Johnny Graettinger <johnny@estuary.dev>
2024-08-25 14:36:51 -0500

    ops-catalog: update for federated data-planes
    See README for more discussion, but essentially:
    
    `data-plane-template.flow.yaml` is a per-data-plane template which
    install ops logs & stats collections, as well as L1 rollups processed
    within the data-plane.
    
    `reporting-L2-template.flow.yaml` is global rollups of L1 derivations.
    These templates have a stable name but automatically-generated bindings,
    one for each data-plane.
    
    `local-view.flow.yaml` materializes reporting for local stacks.
    The production materialization is no longer manged under version
    control.

--- ops-catalog/catalog-stats-rollup.ts
+++
@@ -1,8 +0,0 @@
-import { IDerivation, Document, SourceFromOpsUsCentral1V1 } from 'flow/ops.us-central1.v1/catalog-stats-L2.ts';
-
-// Implementation for derivation ops.us-central1.v1/catalog-stats-L2.
-export class Derivation extends IDerivation {
-    fromOpsUsCentral1V1(read: { doc: SourceFromOpsUsCentral1V1 }): Document[] {
-        return [read.doc]
-    }
-}
--- ops-catalog/flow_generated/typescript/ops/rollups/L1/BASE_NAME/catalog-stats.ts
+++ ops-catalog/flow_generated/typescript/ops/rollups/L1/BASE_NAME/catalog-stats.ts
@@ -1,5 +1,5 @@
 
-// Generated for published documents of derived collection ops.us-central1.v1/catalog-stats-L2.
+// Generated for published documents of derived collection ops/rollups/L1/BASE_NAME/catalog-stats.
 export type Document = /* Flow catalog task stats Statistics related to the processing of a Flow catalog. */ {
     catalogName: /* Name of the Flow catalog */ string;
     grain: /* Time grain that the stats are aggregated over */ "daily" | "hourly" | "monthly";
@@ -68,7 +68,7 @@ export type Document = /* Flow catalog task stats Statistics related to the proc
                     docsTotal: /* Total number of documents */ number;
                 };
                 out?: {
-                    bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
+                    bytesTotal?: /* Total number of bytes representing the JSON encoded documents */ number;
                     docsTotal: /* Total number of documents */ number;
                 };
                 right?: {
@@ -82,86 +82,83 @@ export type Document = /* Flow catalog task stats Statistics related to the proc
 };
 
 
-// Generated for read documents of sourced collection ops.us-central1.v1/catalog-stats-L1.
-export type SourceFromOpsUsCentral1V1 = /* Flow catalog task stats Statistics related to the processing of a Flow catalog. */ {
-    catalogName: /* Name of the Flow catalog */ string;
-    grain: /* Time grain that the stats are aggregated over */ "daily" | "hourly" | "monthly";
-    statsSummary: {
-        errors?: /* Total number of logged errors */ number;
-        failures?: /* Total number of shard failures */ number;
-        readByMe?: {
-            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-            docsTotal: /* Total number of documents */ number;
-        };
-        readFromMe?: {
-            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-            docsTotal: /* Total number of documents */ number;
-        };
-        usageSeconds?: /* Cumulative number of metered seconds of task usage */ number;
-        warnings?: /* Total number of logged warnings */ number;
-        writtenByMe?: {
-            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-            docsTotal: /* Total number of documents */ number;
-        };
-        writtenToMe?: {
-            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-            docsTotal: /* Total number of documents */ number;
-        };
+// Generated for read documents of sourced collection ops.us-central1.v1/logs.
+export type SourceLogs = /* Flow task logs Logs related to the processing of a Flow capture, derivation, or materialization */ {
+    fields?: /* Map of keys and values that are associated with this log entry. */ Record<string, unknown>;
+    level: "debug" | "error" | "info" | "trace" | "warn";
+    message?: string;
+    shard: /* Flow shard id Identifies a specific shard of a task, which may be the source of a log message or metrics */ {
+        keyBegin: /* The inclusive beginning of the shard's assigned key range */ string;
+        kind: /* The type of the catalog task */ "capture" | "derivation" | "materialization";
+        name: /* The name of the catalog task (without the task type prefix) */ string;
+        rClockBegin: /* The inclusive beginning of the shard's assigned rClock range */ string;
     };
-    taskStats?: {
-        capture?: /* Capture stats, organized by collection. The keys of this object are the collection names, and the values are the stats for that collection. */ {
-            [k: string]: {
-                out?: {
-                    bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-                    docsTotal: /* Total number of documents */ number;
-                };
-                right?: /* Documents fed into the combiner from the source */ {
-                    bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-                    docsTotal: /* Total number of documents */ number;
-                };
-            };
-        };
-        derive?: {
+    ts: /* Timestamp corresponding to the start of the transaction */ string;
+};
+
+
+// Generated for read documents of sourced collection ops.us-central1.v1/stats.
+export type SourceStats = /* Flow task stats Statistics related to the processing of a Flow capture, derivation, or materialization */ {
+    capture?: /* Capture stats, organized by collection. The keys of this object are the collection names, and the values are the stats for that collection. */ {
+        [k: string]: {
             out?: {
                 bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
                 docsTotal: /* Total number of documents */ number;
             };
-            published?: {
+            right?: /* Documents fed into the combiner from the source */ {
                 bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
                 docsTotal: /* Total number of documents */ number;
             };
-            transforms?: /* A map of each transform (transform name, not collection name) to stats for that transform */ {
-                [k: string]: /* Stats for a specific transform of a derivation, which will have an update, publish, or both. */ {
-                    input: /* The input documents that were fed into this transform. */ {
-                        bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-                        docsTotal: /* Total number of documents */ number;
-                    };
-                    source?: /* The name of the collection that this transform sources from */ string;
-                };
-            };
         };
-        interval?: {
-            uptimeSeconds: /* Number of seconds that the task shard is metered as having been running */ number;
-            usageRate?: /* Usage rate which adjusts `uptimeSeconds` to determine the task's effective usage */ number;
+    };
+    derive?: {
+        out?: {
+            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
+            docsTotal: /* Total number of documents */ number;
         };
-        materialize?: /* A map of each binding source (collection name) to combiner stats for that binding */ {
-            [k: string]: {
-                left?: {
+        published?: {
+            bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
+            docsTotal: /* Total number of documents */ number;
+        };
+        transforms?: /* A map of each transform (transform name, not collection name) to stats for that transform */ {
+            [k: string]: /* Stats for a specific transform of a derivation, which will have an update, publish, or both. */ {
+                input: /* The input documents that were fed into this transform. */ {
                     bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
                     docsTotal: /* Total number of documents */ number;
                 };
-                out?: {
-                    bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-                    docsTotal: /* Total number of documents */ number;
-                };
-                right?: {
-                    bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
-                    docsTotal: /* Total number of documents */ number;
-                };
+                source?: /* The name of the collection that this transform sources from */ string;
             };
         };
     };
-    ts: /* Timestamp of the catalog stat aggregate */ string;
+    interval?: {
+        uptimeSeconds: /* Number of seconds that the task shard is metered as having been running */ number;
+        usageRate?: /* Usage rate which adjusts `uptimeSeconds` to determine the task's effective usage */ number;
+    };
+    materialize?: /* A map of each binding source (collection name) to combiner stats for that binding */ {
+        [k: string]: {
+            left?: {
+                bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
+                docsTotal: /* Total number of documents */ number;
+            };
+            out?: {
+                bytesTotal?: /* Total number of bytes representing the JSON encoded documents */ number;
+                docsTotal: /* Total number of documents */ number;
+            };
+            right?: {
+                bytesTotal: /* Total number of bytes representing the JSON encoded documents */ number;
+                docsTotal: /* Total number of documents */ number;
+            };
+        };
+    };
+    openSecondsTotal?: /* Total time that the transaction was open before starting to commit */ number;
+    shard: /* Flow shard id Identifies a specific shard of a task, which may be the source of a log message or metrics */ {
+        keyBegin: /* The inclusive beginning of the shard's assigned key range */ string;
+        kind: /* The type of the catalog task */ "capture" | "derivation" | "materialization";
+        name: /* The name of the catalog task (without the task type prefix) */ string;
+        rClockBegin: /* The inclusive beginning of the shard's assigned rClock range */ string;
+    };
+    ts: /* Timestamp corresponding to the start of the transaction */ string;
+    txnCount?: /* Total number of transactions represented by this stats document */ number;
 };
 
 
@@ -184,5 +181,6 @@ export abstract class IDerivation {
         return {};
     }
 
-    abstract fromOpsUsCentral1V1(read: { doc: SourceFromOpsUsCentral1V1 }): Document[];
+    abstract logs(read: { doc: SourceLogs }): Document[];
+    abstract stats(read: { doc: SourceStats }): Document[];
 }
