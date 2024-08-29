fafac4116dc4490856ae690335a9c313ca72407c
Johnny Graettinger <johnny@estuary.dev>
2024-08-29 10:44:15 -0500

    supabase: additional production migration edits I left out
    These were already covered in my local end-to-end migration testing.

--- supabase/pending/data_planes_prod.sql
+++ supabase/pending/data_planes_prod.sql
@@ -1,6 +1,5 @@
 begin;
 
-
 create table data_planes (
   like internal._model including all,
 
@@ -76,10 +75,13 @@ returns table (role_prefix catalog_prefix, capability grant_capability) as $$
 $$ language sql stable;
 
 
-
 alter table discovers add column data_plane_name text not null default 'ops/dp/public/gcp-us-central1-c1';
 alter table publications add column data_plane_name text not null default 'ops/dp/public/gcp-us-central1-c1';
 
+-- Might need to run this again, to catch any new tenants created between migration and updated agent deployment.
+insert into role_grants (subject_role, object_role, capability)
+select tenant, 'ops/dp/public/', 'read' from tenants
+on conflict do nothing;
 
 do $$
 declare
@@ -97,7 +99,7 @@ begin
         ops_l2_stats_transform,
         broker_address,
         reactor_address,
-        hmac_keys,
+        hmac_keys
     ) values (
         'ops/dp/public/gcp-us-central1-c1',
         'gcp-us-central1-c1.dp.estuary-data.com',
@@ -105,19 +107,36 @@ begin
         'ops.us-central1.v1/stats',
         'ops.us-central1.v1/inferred-schemas/L1',
         'ops.us-central1.v1/catalog-stats-L1',
-        'from-ops.us-central1.v1'
+        'from-ops.us-central1.v1',
         'fromOps.us-central1.v1',
         'http://localhost:8080', -- TODO(johnny): K8s service
         'http://localhost:9000', -- TODO(johnny): K8s service
-        'c2VjcmV0,AA=='          -- TODO(johnny): replace with actual secret.
+        '{c2VjcmV0,AA==}'        -- TODO(johnny): replace with actual secret.
     );
 
     select id into cronut_id from data_planes where data_plane_name = 'ops/dp/public/gcp-us-central1-c1';
 
-    alter table live_specs add column data_plane_id flowid not null default cronut_id;
-
+    execute format('alter table live_specs add column data_plane_id flowid not null default %L', cronut_id);
 
 end $$;
 
+
+insert into role_grants (subject_role, object_role, capability) values
+  -- L1 roll-ups can read task logs & stats.
+  ('ops/rollups/L1/', 'ops/tasks/', 'read'),
+  -- L1 roll-ups tasks can write to themselves.
+  ('ops/rollups/L1/', 'ops/rollups/L1/', 'write'),
+  -- L2 roll-ups can read L1 roll-ups.
+  ('ops.us-central1.v1/', 'ops/rollups/L1/', 'read')
+  ;
+
+-- Ops collections are directed to estuary-flow-poc and not estuary-trial for $reasons.
+insert into storage_mappings (catalog_prefix, spec) values
+  ('ops/', '{"stores": [{"provider": "GCS", "bucket": "estuary-flow-poc", "prefix": "collection-data/"}]}'),
+  ('recovery/ops/', '{"stores": [{"provider": "GCS", "bucket": "estuary-flow-poc"}]}')
+;
+
+insert into user_grants (user_id, object_role, capability)
+select id, 'ops/', 'admin' from auth.users where email = 'support@estuary.dev';
 
 commit;
