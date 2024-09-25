71ba358584b5ac1de1fbfd90caa7921afcb21204
Joseph Shearer <joseph@josephshearer.net>
2024-09-25 11:40:05 -0400

    dekaf: Switch to jemalloc crate and expose some memory related metrics

--- crates/dekaf/src/metrics.rs
+++ crates/dekaf/src/metrics.rs
@@ -12,8 +12,20 @@ pub fn build_router(app: Arc<App>) -> axum::Router<()> {
     schema_router
 }
 
+fn record_jemalloc_stats() {
+    let stats = allocator::current_mem_stats();
+    metrics::gauge!("mem_allocated").set(stats.allocated as f64);
+    metrics::gauge!("mem_mapped").set(stats.mapped as f64);
+    metrics::gauge!("mem_metadata").set(stats.metadata as f64);
+    metrics::gauge!("mem_resident").set(stats.resident as f64);
+    metrics::gauge!("mem_retained").set(stats.retained as f64);
+    metrics::gauge!("mem_active").set(stats.active as f64);
+}
+
 #[tracing::instrument(skip_all)]
 async fn prometheus_metrics() -> (axum::http::StatusCode, String) {
+    record_jemalloc_stats();
+
     match prometheus::TextEncoder::new().encode_to_string(&prometheus::default_registry().gather())
     {
         Err(e) => (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
