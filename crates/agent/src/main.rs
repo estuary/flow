89b337e9b79bca8b464ddaef035438070cdd52d1
Johnny Graettinger <johnny@estuary.dev>
2024-09-30 15:53:41 -0500

    agent: support a NO_COLOR=1 environment variable
    Which disablses ANSI color logging output.

--- crates/agent/src/main.rs
+++ crates/agent/src/main.rs
@@ -60,6 +60,11 @@ fn main() -> Result<(), anyhow::Error> {
     // Use reasonable defaults for printing structured logs to stderr.
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_ansi(if matches!(std::env::var("NO_COLOR"), Ok(v) if v == "1") {
+            false
+        } else {
+            true
+        })
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
 
