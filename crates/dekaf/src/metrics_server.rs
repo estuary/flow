33b958870cf64c96ec8b16ae67c04ffa11fccb7a
Joseph Shearer <joseph@josephshearer.net>
2024-10-04 14:18:44 -0400

    dekaf: add more metrics and break out `dekaf_request_time` by request API key

--- crates/dekaf/src/metrics_server.rs
+++ crates/dekaf/src/metrics_server.rs
@@ -22,12 +22,12 @@ pub fn build_router() -> axum::Router<()> {
 
 fn record_jemalloc_stats() {
     let stats = allocator::current_mem_stats();
-    metrics::gauge!("mem_allocated").set(stats.allocated as f64);
-    metrics::gauge!("mem_mapped").set(stats.mapped as f64);
-    metrics::gauge!("mem_metadata").set(stats.metadata as f64);
-    metrics::gauge!("mem_resident").set(stats.resident as f64);
-    metrics::gauge!("mem_retained").set(stats.retained as f64);
-    metrics::gauge!("mem_active").set(stats.active as f64);
+    metrics::gauge!("dekaf_mem_allocated").set(stats.allocated as f64);
+    metrics::gauge!("dekaf_mem_mapped").set(stats.mapped as f64);
+    metrics::gauge!("dekaf_mem_metadata").set(stats.metadata as f64);
+    metrics::gauge!("dekaf_mem_resident").set(stats.resident as f64);
+    metrics::gauge!("dekaf_mem_retained").set(stats.retained as f64);
+    metrics::gauge!("dekaf_mem_active").set(stats.active as f64);
 }
 
 #[tracing::instrument(skip_all)]
