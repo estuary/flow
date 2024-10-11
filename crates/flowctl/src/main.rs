d5b3a04649a6f67f9c63f9fe77e9f4f748968b92
Phil <phil@estuary.dev>
2024-10-11 08:18:10 -0400

    flowctl: initialize rustls crypto provider
    Add the required initialization for the `rustls` crypto provider to the
    beginning of the agent main function, so that TLS will work again.

--- crates/flowctl/src/main.rs
+++ crates/flowctl/src/main.rs
@@ -4,6 +4,12 @@ use tracing_subscriber::{filter::LevelFilter, EnvFilter};
 fn main() -> Result<(), anyhow::Error> {
     let cli = flowctl::Cli::parse();
 
+    // Required in order for libraries to use `rustls` for TLS.
+    // See: https://docs.rs/rustls/latest/rustls/crypto/struct.CryptoProvider.html
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("failed to install default crypto provider");
+
     let env_filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::WARN.into()) // Otherwise it's ERROR.
         .from_env_lossy();
