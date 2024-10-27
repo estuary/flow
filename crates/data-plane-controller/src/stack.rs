0430e36552378a2b4dde28e4b637876e72652e4f
Johnny Graettinger <johnny@estuary.dev>
2024-10-27 17:06:56 -0500

    data-plane-controller: controller for managing data-plane life cycles
    The controller uses the recent `automations` crate to monitor structured
    changes to a bound data_planes and drive the data-plane to convergence.
    
    It's modeled as a CI/CD pipeline, where an indicated branch of our
    dry-dock repo (containing Pulumi and Ansible infrastructure) is deployed
    for the given data-plane.
    
    The controller performs the full lifecycle required for rolling updates:
    
    - `pulumi up` to create new resources or respond to replacements
    - Awaiting DNS propagation
    - Running the Ansible playbook to provision instances
    - Running `pulumi up` to reflect readiness of started instances
    - Awaiting DNS propagation (again)
    
    It also periodically refreshes a stack from remote providers to detect
    changed or deleted resources, such as EC2 instance replacements, and
    responds accordingly to heal the infrastructure.

---
+++ crates/data-plane-controller/src/stack.rs
@@ -0,0 +1,114 @@
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct PulumiStack {
+    #[serde(rename = "secretsprovider")]
+    pub secrets_provider: String,
+    #[serde(
+        default,
+        rename = "encryptedkey",
+        skip_serializing_if = "String::is_empty"
+    )]
+    pub encrypted_key: String,
+    pub config: PulumiStackConfig,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct PulumiStackConfig {
+    #[serde(rename = "est-dry-dock:model")]
+    pub model: DataPlane,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DataPlane {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub fqdn: Option<String>,
+
+    pub builds_root: url::Url,
+    pub builds_kms_keys: Vec<String>,
+    pub control_plane_api: url::Url,
+    pub data_buckets: Vec<url::Url>,
+    pub gcp_project: String,
+    pub ssh_subnets: Vec<ipnetwork::Ipv6Network>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub private_links: Vec<AWSPrivateLink>,
+    pub deployments: Vec<Deployment>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct AWSPrivateLink {
+    pub region: String,
+    pub az_ids: Vec<String>,
+    pub service_name: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    Etcd,
+    Gazette,
+    Reactor,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct Deployment {
+    pub role: Role,
+    pub template: serde_json::Value,
+    pub oci_image: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub oci_image_override: Option<String>,
+    pub desired: usize,
+    pub current: usize,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct PulumiExports {
+    pub ansible: AnsibleInventory,
+    pub control: ControlExports,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct AnsibleInventory {
+    pub all: AnsibleInventoryAll,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct AnsibleInventoryAll {
+    pub children: std::collections::BTreeMap<String, AnsibleRole>,
+    pub vars: std::collections::BTreeMap<String, serde_json::Value>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct AnsibleRole {
+    pub hosts: std::collections::BTreeMap<String, AnsibleHost>,
+    pub vars: std::collections::BTreeMap<String, serde_json::Value>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct AnsibleHost {
+    pub ansible_host: std::net::Ipv6Addr,
+    pub ansible_user: String,
+    pub host_fqdn: String,
+    pub local_cert_pem: String,
+    pub local_private_key_pem: String,
+    pub oci_image: String,
+    pub private_ip4: Option<std::net::Ipv4Addr>,
+    pub provider: String,
+    pub public_ip4: std::net::Ipv4Addr,
+    pub public_ip6: std::net::Ipv6Addr,
+    pub role: String,
+    pub role_fqdn: String,
+    pub starting: bool,
+    pub stopping: bool,
+    pub zone: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct ControlExports {
+    pub aws_iam_user_arn: String,
+    pub aws_link_endpoints: Vec<serde_json::Value>,
+    pub cidr_blocks: Vec<ipnetwork::IpNetwork>,
+    pub gcp_service_account_email: String,
+    pub hmac_keys: Vec<String>,
+    pub ssh_key: String,
+}
