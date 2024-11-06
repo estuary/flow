fbfc9894137a4859d7e70732289299af0e6d0887
Phil <phil@estuary.dev>
2024-11-06 16:50:57 -0500

    agent: refactor evolutions
    Refactors the `evolutions` module to decouple them from draft specs in the
    database. Previously, evolutions required a `drafts` row in the database, and
    wrote its results only into `draft_specs`. This presented a particular problem
    for controllers, which have their drafts only in memory. The new factoring
    accepts a `tables::DraftCatalog`, which will be modified with results of the
    evolution and returned as part of a result that includes a structured summary
    of the changes that were made. This design mimics that of `publications` and
    `discovers`.
    
    The tests of evolutions were also refactored to use the integration test harness.
    
    This also relaxes the restriction against supplying both `new_name` and
    `materializations` in an `EvolveRequest`, as there's seemingly no use for it.
    It is not incorrect to create a new version of a collection and leave some
    materializations reading from the old collection.

--- crates/agent/src/integration_tests/mod.rs
+++ crates/agent/src/integration_tests/mod.rs
@@ -3,6 +3,7 @@
 //! These tests cover end-to-end scenarios involving the control plane. The data plane and
 //! connectors are not exercised as part of these.
 mod dependencies_and_activations;
+mod evolutions;
 pub mod harness;
 mod locking_retries;
 mod null_bytes;
