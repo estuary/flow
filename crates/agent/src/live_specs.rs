e20736dcaba1e2f26442ca59637c8ef21452a91c
Phil <phil@estuary.dev>
2025-01-23 17:42:49 -0500

    agent: fetch live specs in chunks

--- crates/agent/src/live_specs.rs
+++ crates/agent/src/live_specs.rs
@@ -12,42 +12,49 @@ pub async fn get_live_specs(
     filter_capability: Option<Capability>,
     db: &sqlx::PgPool,
 ) -> anyhow::Result<tables::LiveCatalog> {
-    let rows = agent_sql::live_specs::fetch_live_specs(user_id, &names, db).await?;
     let mut live = tables::LiveCatalog::default();
-    for row in rows {
-        // Spec type might be null because we used to set it to null when deleting specs.
-        // For recently deleted specs, it will still be present.
-        let Some(catalog_type) = row.spec_type.map(Into::into) else {
-            continue;
-        };
-        let Some(model_json) = row.spec.as_deref() else {
-            continue;
-        };
-        if let Some(min_capability) = filter_capability {
-            if !row
-                .user_capability
-                .is_some_and(|actual_capability| actual_capability >= min_capability)
-            {
-                continue;
-            }
-        }
-        let built_spec_json = row.built_spec.as_ref().ok_or_else(|| {
-            tracing::warn!(catalog_name = %row.catalog_name, id = %row.id, "got row with spec but not built_spec");
-            anyhow::anyhow!("missing built_spec for {:?}, but spec is non-null", row.catalog_name)
-        })?.deref();
 
-        live.add_spec(
-            catalog_type,
-            &row.catalog_name,
-            row.id.into(),
-            row.data_plane_id.into(),
-            row.last_pub_id.into(),
-            row.last_build_id.into(),
-            model_json,
-            built_spec_json,
-            row.dependency_hash,
-        )
-        .with_context(|| format!("deserializing specs for {:?}", row.catalog_name))?;
+    // The query that's used by `fetch_live_specs` is pretty slow because of how
+    // it queries authZ capabilities for each name, even if it doesn't exist.
+    // Limit each individual query to 512 names to avoid statement timeouts when
+    // fetching a large number of specs.
+    for names_chunk in names.chunks(512) {
+        let rows = agent_sql::live_specs::fetch_live_specs(user_id, names_chunk, db).await?;
+        for row in rows {
+            // Spec type might be null because we used to set it to null when deleting specs.
+            // For recently deleted specs, it will still be present.
+            let Some(catalog_type) = row.spec_type.map(Into::into) else {
+                continue;
+            };
+            let Some(model_json) = row.spec.as_deref() else {
+                continue;
+            };
+            if let Some(min_capability) = filter_capability {
+                if !row
+                    .user_capability
+                    .is_some_and(|actual_capability| actual_capability >= min_capability)
+                {
+                    continue;
+                }
+            }
+            let built_spec_json = row.built_spec.as_ref().ok_or_else(|| {
+                tracing::warn!(catalog_name = %row.catalog_name, id = %row.id, "got row with spec but not built_spec");
+                anyhow::anyhow!("missing built_spec for {:?}, but spec is non-null", row.catalog_name)
+            })?.deref();
+
+            live.add_spec(
+                catalog_type,
+                &row.catalog_name,
+                row.id.into(),
+                row.data_plane_id.into(),
+                row.last_pub_id.into(),
+                row.last_build_id.into(),
+                model_json,
+                built_spec_json,
+                row.dependency_hash,
+            )
+            .with_context(|| format!("deserializing specs for {:?}", row.catalog_name))?;
+        }
     }
 
     Ok(live)
