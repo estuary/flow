fd3d933e1aac581392e13af2411c9b2ceecb1d6d
Phil <phil@estuary.dev>
2025-01-30 17:23:55 -0500

    agent: switch applied directives from handlers to automations
    Basically the same treatment as the others.
    
    I found it easier to switch the tests over to using the `TestHarness`, so we
    can re-use it's existing functionality for running automations tasks and
    clearing out the database before each test run.

--- crates/agent-sql/src/directives/mod.rs
+++ crates/agent-sql/src/directives/mod.rs
@@ -1,14 +1,14 @@
-use super::{Id, TextJson as Json};
-
-use chrono::prelude::*;
-use serde::Serialize;
-use serde_json::value::RawValue;
-use sqlx::types::Uuid;
-
 pub mod accept_demo_tenant;
 pub mod beta_onboard;
 pub mod grant;
 pub mod storage_mappings;
+
+use crate::TextJson;
+use chrono::{DateTime, Utc};
+use models::Id;
+use serde::Serialize;
+use serde_json::value::RawValue;
+use sqlx::types::Uuid;
 
 // Row is the dequeued task shape of an applied directive operation. We don't currently have a use
 // for background directive applications, so the `background` column is omitted.
@@ -18,19 +18,17 @@ pub struct Row {
     pub apply_updated_at: DateTime<Utc>,
     pub catalog_prefix: String,
     pub directive_id: Id,
-    pub directive_spec: Json<Box<RawValue>>,
+    pub directive_spec: TextJson<Box<RawValue>>,
     pub logs_token: Uuid,
-    pub user_claims: Json<Box<RawValue>>,
+    pub user_claims: TextJson<Box<RawValue>>,
     pub user_id: Uuid,
+    pub status_type: Option<String>,
 }
 
-#[tracing::instrument(level = "debug", skip(txn))]
-pub async fn dequeue(
+pub async fn fetch_directive(
+    task_id: Id,
     txn: &mut sqlx::Transaction<'_, sqlx::Postgres>,
-    allow_background: bool,
-) -> sqlx::Result<Option<Row>> {
-    // We don't currently have a use for `background` directive applications, but this function
-    // still handles them as if we may.
+) -> sqlx::Result<Row> {
     sqlx::query_as!(
         Row,
         r#"select
@@ -38,21 +36,19 @@ pub async fn dequeue(
             a.updated_at as "apply_updated_at",
             d.catalog_prefix as "catalog_prefix",
             d.id as "directive_id: Id",
-            d.spec as "directive_spec: Json<Box<RawValue>>",
+            d.spec as "directive_spec: TextJson<Box<RawValue>>",
             a.logs_token,
-            a.user_claims as "user_claims!: Json<Box<RawValue>>",
-            a.user_id as "user_id"
+            a.user_claims as "user_claims!: TextJson<Box<RawValue>>",
+            a.user_id as "user_id",
+            a.job_status->>'type' as "status_type"
         from directives as d
         join applied_directives as a on d.id = a.directive_id
-        -- The user must supply claims before we can dequeue the application.
-        where a.job_status->>'type' = 'queued' and a.user_claims is not null and (a.background = $1 or a.background = false)
-        order by a.background, a.id asc
-        limit 1
-        for update of a skip locked;
+        where a.id = $1::flowid
+        for update of a;
         "#,
-        allow_background
+        task_id as Id,
     )
-    .fetch_optional(txn)
+    .fetch_one(txn)
     .await
 }
 
@@ -72,7 +68,7 @@ where
         returning 1 as "must_exist";
         "#,
         id as Id,
-        Json(status) as Json<S>,
+        TextJson(status) as TextJson<S>,
     )
     .fetch_one(txn)
     .await?;
