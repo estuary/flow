4ed6987dd09f28ee3c2dfdcf061361215f801770
Phil <phil@estuary.dev>
2025-02-03 11:39:36 -0500

    agent: update handler to automations migration
    Updates the handler-automations migration to create tasks for any existing jobs
    that are still queued.  This ensures we don't miss any during the cut over.
    
    Also tweaks the trigger to set the `wake_at` and `inbox` as part of the insert,
    just as a minor simplification. They were only separate calls because the
    initial version of this used an `on conflict do nothing` to allow for a task to
    already exist. But now we require that the task does _not_ exist (and prevent
    updates to already-queued tasks) and so there's no reason to do this in two
    separate steps.

--- supabase/migrations/20250127152418_handler-automations.sql
+++ supabase/migrations/20250127152418_handler-automations.sql
@@ -1,20 +1,39 @@
-
 begin;
-
-
 
 create function internal.create_handler_task(task_id public.flowid, task_type integer) returns void
 language plpgsql security definer
 as $$
 begin
-    insert into internal.tasks (task_id, task_type)
-    values (task_id, task_type);
-
-    execute internal.send_to_task(task_id, '0000000000000000'::public.flowid, json_build_object(
-        'type', 'queued'
-    ));
+    insert into internal.tasks (task_id, task_type, wake_at, inbox)
+    values (task_id, task_type, now(), array[
+        json_build_array('0000000000000000', json_build_object('type', 'queued'))
+    ]);
 end;
 $$;
+comment on function internal.create_handler_task(task_id public.flowid, task_type integer) is
+'Create a task with the given task_id and task_type, which must not exist.
+The task is initially queued. Raises an error if the task already exists.';
+
+-- Create tasks for any existing jobs that are still queued.
+select internal.create_handler_task(p.id, 3)
+from public.publications p
+where p.job_status->>'type' = 'queued';
+
+select internal.create_handler_task(d.id, 4)
+from public.discovers d
+where d.job_status->>'type' = 'queued';
+
+select internal.create_handler_task(e.id, 5)
+from public.evolutions e
+where e.job_status->>'type' = 'queued';
+
+select internal.create_handler_task(ad.id, 6)
+from public.applied_directives ad
+where ad.job_status->>'type' = 'queued';
+
+select internal.create_handler_task(ct.id, 7)
+from public.connector_tags ct
+where ct.job_status->>'type' = 'queued';
 
 
 create function internal.create_publication_task() returns trigger
@@ -84,6 +103,5 @@ create trigger create_connector_tag_task after insert or update on public.connec
 for each row
 when (new.job_status->>'type' = 'queued')
 execute function internal.create_connector_tag_task();
-
 
 commit;
