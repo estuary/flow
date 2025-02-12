55332b021568291bea6f90c7e2f5647285af8e9d
Travis Jenkins <travis@estuary.dev>
2025-02-12 13:37:52 -0500

    docs: SEO update for the connector docs (#1933)
    * Adding script that handles updating connector titles
    
    * clean up and adding script as it wasn't running for some reason
    
    * clean up
    
    * cleaning up logs
    
    * Updating browser list
    
    * run `npm audit fix`
    
    * cleaned up was too aggressive
    
    * tweaks to logging
    
    * Make sure we do not update the connector index file
    
    * Handling more pages we do not want updates
    
    * commenting and cleaning up
    
    * controlling case
    
    * Updating logging
    
    * removing some extra space
    
    * keeping out put a bit more laid out
    
    * This was not auto running because of the camel case

---
+++ site/postBuildCustomizations.js
@@ -0,0 +1,50 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const outputDir = './build';
+const connectorsDir = `${outputDir}/reference/Connectors`
+const connector = 'Connector';
+const divider = ' | ';
+
+const updateAllConnectorPages = (params, titleAddition) => {
+    console.log('Customizing BEGIN')
+
+    let updateCount = 0;
+    fs.readdirSync(params, {
+        recursive: true,
+    }).forEach(file => {
+
+        if (file.includes('.html')) {
+            const fileFullPath = `${connectorsDir}/${file}`;
+            const $cheer = cheerio.load(fs.readFileSync(fileFullPath));
+            const $title = $cheer("title")
+            const titleText = $title.text();
+
+            if (
+                // Skip if we are on a specific "root" page
+                !titleText.toLowerCase().startsWith(connector.toLowerCase()) &&
+                !titleText.toLowerCase().startsWith('dekaf integrations'.toLowerCase()) && 
+                !titleText.toLowerCase().startsWith('materialization protocol'.toLowerCase()) && 
+
+                // Skip if it is already there
+                !titleText.toLowerCase().includes(titleAddition) && 
+
+                // Skip if plural version is there (ex: Capture Connectors)
+                !titleText.toLowerCase().includes(`${connector}s |`.toLowerCase())
+            ) {
+                const newTitle = titleText.replace(divider, titleAddition);
+                console.debug(`  - updating    : ${fileFullPath}`)
+                console.debug(`    - new title : ${newTitle}`)
+
+                $title.text(newTitle);
+                fs.writeFileSync(fileFullPath, $cheer.html());
+                updateCount += 1;
+            }
+        }
+    });
+    console.log(` - updated ${updateCount}`)
+    console.log('Customizing DONE')
+
+}
+
+updateAllConnectorPages(connectorsDir, ` ${connector}${divider}`);
