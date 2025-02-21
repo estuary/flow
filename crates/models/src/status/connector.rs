b4fc36dfac07e383272a5f59c0a20edb74f8ab1a
Phil <phil@estuary.dev>
2025-02-21 16:21:07 -0500

    agent: add connector_status to /status response
    Pulls in the new `connector_status` into the `/api/v1/status` response. This is
    represented just as an ops log, exactly as it is in the database.

---
+++ crates/models/src/status/connector.rs
@@ -0,0 +1,23 @@
+use crate::status::ShardRef;
+use chrono::{DateTime, Utc};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+/// The shape of a connector status, which matches that of an ops::Log.
+#[derive(Debug, Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct ConnectorStatus {
+    /// The shard that last updated the status
+    pub shard: ShardRef,
+    /// The time at which the status was last updated
+    #[schemars(schema_with = "crate::datetime_schema")]
+    pub ts: DateTime<Utc>,
+    /// The message is meant to be presented to users, and may use Markdown formatting.
+    pub message: String,
+    /// Arbitrary JSON that can be used to communicate additional details. The
+    /// specific fields and their meanings are entirely up to the connector.
+    #[serde(default)]
+    pub fields: serde_json::Map<String, serde_json::Value>,
+}
+
+crate::sqlx_json::sqlx_json!(ConnectorStatus);
