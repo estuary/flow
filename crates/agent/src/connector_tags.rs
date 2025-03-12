ac3d4251597284817a4c068f7538c6fe1287ae78
Johnny Graettinger <johnny@estuary.dev>
2025-03-12 15:53:24 -0500

    materialize.proto: officially replace resource_path_pointers with annotations
    We already had a defacto standard of using resource-config schema
    annotations to mark up the locations of injected platform variables.
    
    Formalize this by removing `resource_path_pointers` altogether (it was
    recently added and never populated), and replace it with a new routine
    (and tests) for validating and extracting doc::Pointers from the doc::Shape
    of the resource config schema.

--- crates/agent/src/connector_tags.rs
+++ crates/agent/src/connector_tags.rs
@@ -281,7 +281,6 @@ async fn spec_materialization(
         resource_config_schema_json,
         documentation_url,
         oauth2,
-        resource_path_pointers,
     } = spec;
 
     let oauth2 = if let Some(oa) = oauth2 {
@@ -289,13 +288,23 @@ async fn spec_materialization(
     } else {
         None
     };
+
+    let (resource_path_pointers, _delta_updates_pointer) =
+        validation::extract_materialization_resource_config_annotations(
+            &resource_config_schema_json,
+        )
+        .context("connector has an invalid resource_config_schema_json")?;
+
     Ok(ConnectorSpec {
         documentation_url,
         endpoint_config_schema: RawValue::from_string(config_schema_json)
             .context("parsing endpoint config schema")?,
         resource_config_schema: RawValue::from_string(resource_config_schema_json)
             .context("parsing resource config schema")?,
-        resource_path_pointers,
+        resource_path_pointers: resource_path_pointers
+            .iter()
+            .map(doc::Pointer::to_string)
+            .collect(),
         oauth2,
     })
 }
