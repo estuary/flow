c78f795323d28c7a43bb762ba94c7b45404c7bc6
Phil <phil@estuary.dev>
2025-03-12 08:31:41 -0400

    control-plane: add not null constraints to data_planes events columns
    These constraints were removed from the original `ops_catalog_status` migration
    in order to allow legacy agent processes to create new data planes.  This just
    breaks out those constraints to a separate migration.

--- supabase/migrations/20250204230547_ops_catalog_status.sql
+++ supabase/migrations/20250204230547_ops_catalog_status.sql
@@ -115,9 +115,6 @@ set
 where
     ops_l1_events_name is null and ops_l2_events_transform is null;
 
-alter table public.data_planes alter column ops_l1_events_name set not null;
-alter table public.data_planes alter column ops_l2_events_transform set not null;
-
 -- We need to update the `catalog_stats*` tables in order to allow a trailing
 -- slash in the `catalog_name` and add `txn_count`. This is a gross and annoying
 -- process because there's views that depend on that column, which all need to
