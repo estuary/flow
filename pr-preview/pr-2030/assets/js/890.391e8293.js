12474fc0be30cd23571d1aa9878247e4915ff08e
aeluce <aeluce@users.noreply.github.com>
2025-03-27 22:32:20 +0000

    Deploy preview for PR 2030 🛫

---
+++ pr-preview/pr-2030/assets/js/890.391e8293.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.id = 890;
+exports.ids = [890];
+exports.modules = {
+
+/***/ 10890:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   createInfoServices: () => (/* reexport safe */ _chunk_4YFB5VUC_mjs__WEBPACK_IMPORTED_MODULE_0__.v)
+/* harmony export */ });
+/* harmony import */ var _chunk_4YFB5VUC_mjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(97021);
+/* harmony import */ var _chunk_Y27MQZ3U_mjs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(19369);
+
+
+
+
+
+/***/ })
+
+};
+;
