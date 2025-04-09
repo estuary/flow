e504d77ac604220e77d836d82b822bf335373c3a
Phil <phil@estuary.dev>
2025-04-09 07:47:20 -0400

    models: renames and updates of sourceCapture
    This is part of #2010, which introduces most of the model changes.  The
    `sourceCapture` was renamed to `sources` to generalize it, so it's clearer that
    it can pertain to any newly added bindings, not just those from a source
    capture.
    
    Also renames `targetSchema` to `targetNaming`, and renames the pre-existing
    variants. `leaveEmpty` becomes `noSchema`, and `fromSourceName` becomes
    `withSchema`.
    
    In the end, something like:
    ```
    sourceCapture:
      capture: acmeCo/foo
      targetSchema: fromSourceName
    ``
    
    is now equivalent to:
    ```
    sources:
      capture: acmeCo/foo
      targetNaming: withSchema
    ```
    
    The `capture` is now allowed to be unspecified, allowing users to configure the
    `targetNaming` in cases where there is no source capture.

--- crates/agent/src/integration_tests/dependencies_and_activations.rs
+++ crates/agent/src/integration_tests/dependencies_and_activations.rs
@@ -1164,7 +1164,10 @@ async fn test_dependencies_and_controllers() {
         .unwrap()
         .as_materialization()
         .unwrap();
-    assert!(materialization_model.source_capture.is_none());
+    assert_eq!(
+        Some(&models::SourceType::Configured(models::SourceDef::default())),
+        materialization_model.source.as_ref()
+    );
 
     harness.run_pending_controller("owls/materialize").await;
     harness.control_plane().assert_activations(
