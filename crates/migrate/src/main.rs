7adecda367cb04ef70554f4594096b0a0294461c
Johnny Graettinger <johnny@estuary.dev>
2025-04-16 21:41:20 -0500

    migrate: prototype CLI for data-plane task migration
    The crate implements a minimal, useful CLI for performing live migration
    of tasks and collections from one data-plane to another.
    
    Over time we'll want to shift this crate towards a library, which is
    integrated with planned future-timepoint cordoning of authorizations
    granted by the Authorization API, to effect a fast, hot migration.

---
+++ crates/migrate/src/main.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+fn main() -> Result<(), anyhow::Error> {
+    // Required in order for libraries to use `rustls` for TLS.
+    // See: https://docs.rs/rustls/latest/rustls/crypto/struct.CryptoProvider.html
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("failed to install default crypto provider");
+
+    // Use reasonable defaults for printing structured logs to stderr.
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_ansi(if matches!(std::env::var("NO_COLOR"), Ok(v) if v == "1") {
+            false
+        } else {
+            true
+        })
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
+
+    let args = migrate::Args::parse();
+
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+
+    let result = runtime.block_on(runtime.spawn(async move { migrate::run(args).await }));
+
+    runtime.shutdown_timeout(std::time::Duration::from_secs(5));
+    result?
+}
