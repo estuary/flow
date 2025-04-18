6854570402b677d6a813732035c330e0fcf00954
Travis Jenkins <travis@estuary.dev>
2025-04-18 13:17:26 -0400

    Semantic versioning is now supported and probably safer

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.21.0";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@^2.0.0";
 
 export const supabaseClient = createClient(
     Deno.env.get("SUPABASE_URL")!,
