77b047af2ef25e06695a040a8b77afa6f317013b
Johnny Graettinger <johnny@estuary.dev>
2025-04-30 22:38:18 -0500

    data-plane-controller: refactor for better test-ability
    Use type-erased functions to run subcommands and log messages,
    and hoist dry-run mode into an implementation of a command runner which
    knows some of the details of controller subcommands (essentially: a
    lightweight mock).
    
    Refactor Controller::on_* callbacks to be pure functions, with all
    database interaction driven by controller::Executor prior to
    entering a Controller::on_poll call.
    
    This facilitates writing unit test loops driven by in-memory
    state, without DB interaction, using bespoke and test-integrated
    functions for tracking subcommands or collecting logs.
    
    Add an example test which covers stack initialization and a subsequent
    rolling release.
    
    Also add data-plane-controller as manually-enabled Tiltfile resource,
    to ease local integration testing. It works well with est_dry_dock
    and flowctl pointed at the local-stack database.

---
+++ crates/data-plane-controller/tests/test_initial_convergence_and_release.rs
@@ -0,0 +1,71 @@
+use data_plane_controller::{controller, stack};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
+
+mod util;
+use util::{initial_state, mock_emit_log_fn, mock_run_cmd_fn, TraceEntry};
+
+#[tokio::test]
+async fn test() {
+    let trace = Arc::new(Mutex::new(Vec::new()));
+
+    let controller = data_plane_controller::Controller {
+        dns_ttl: std::time::Duration::ZERO,
+        git_remote: "git@github.com:estuary/est-dry-dock.git".to_string(),
+        secrets_provider: "testing".to_string(),
+        state_backend: "file:///tmp/pulumi-test-state".parse().unwrap(),
+        emit_log_fn: mock_emit_log_fn(trace.clone()),
+        run_cmd_fn: mock_run_cmd_fn(trace.clone()),
+    };
+
+    let mut state: Option<stack::State> = None;
+    let mut inbox: VecDeque<(models::Id, Option<controller::Message>)> = VecDeque::new();
+    let mut maybe_checkout: Option<tempfile::TempDir> = None;
+    let mut row_state = initial_state();
+
+    inbox.push_back((
+        models::Id::zero(),
+        Some(controller::Message::Start(row_state.data_plane_id)),
+    ));
+    inbox.push_back((models::Id::zero(), Some(controller::Message::Enable)));
+
+    // An immediate release that's applied to the initial row state fixture.
+    let releases: Vec<stack::Release> = vec![stack::Release {
+        prev_image: "ghcr.io/gazette/broker:v1.2.3".to_string(),
+        next_image: "ghcr.io/gazette/broker:v5.6.7".to_string(),
+        step: 3,
+    }];
+
+    for _ in 0..50 {
+        let outcome = controller
+            .on_poll(
+                models::Id::new([42; 8]), // Task ID.
+                &mut state,
+                &mut inbox,
+                &mut maybe_checkout,
+                releases.clone(),
+                row_state.clone(),
+            )
+            .await
+            .expect("on_poll failed");
+
+        let mut trace = trace.lock().unwrap();
+
+        if let Some(exports) = outcome.publish_exports {
+            trace.push(TraceEntry::Exports(exports));
+        }
+        if let Some(stack) = outcome.publish_stack {
+            trace.push(TraceEntry::Stack(stack.clone()));
+            row_state.stack = stack;
+        }
+        trace.push(TraceEntry::Status(outcome.status));
+
+        if matches!(outcome.status, stack::Status::Idle) && outcome.sleep.as_secs() > 0 {
+            break;
+        }
+    }
+
+    insta::assert_json_snapshot!(trace.lock().unwrap().as_slice());
+}
