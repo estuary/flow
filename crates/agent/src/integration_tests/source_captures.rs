f38b01d7c0e4f575df5dbcc55cb96fee93371cd9
Phil Fried <phil@psfried.dev>
2025-05-19 15:32:32 -0400

    Revert "models: add new default materialization target naming"
    This reverts commit 2d6d0bb79f837ee8322022d66416aaed1e1273a5.

--- crates/agent/src/integration_tests/source_captures.rs
+++ crates/agent/src/integration_tests/source_captures.rs
@@ -171,7 +171,7 @@ async fn test_source_captures() {
     );
     let no_source_status = no_source_state.current_status.unwrap_materialization();
     assert_eq!(
-        Some("in response to deletion one or more depencencies, removed source Capture: 'ducks/notARealCapture'"),
+        Some("in response to deletion one or more depencencies, removed sourceCapture: \"ducks/notARealCapture\" because the capture was deleted"),
         no_source_status.publications.history[0].detail.as_deref()
     );
     assert!(no_source_status.source_capture.is_none());
@@ -320,7 +320,7 @@ async fn test_source_captures_collection_name() {
     );
     let no_source_status = no_source_state.current_status.unwrap_materialization();
     assert_eq!(
-        Some("in response to deletion one or more depencencies, removed source Capture: 'ducks/notARealCapture'"),
+        Some("in response to deletion one or more depencencies, removed sourceCapture: \"ducks/notARealCapture\" because the capture was deleted"),
         no_source_status.publications.history[0].detail.as_deref()
     );
     assert!(no_source_status.source_capture.is_none());
