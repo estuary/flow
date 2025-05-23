1b1f3ab41b1cba38059255187062b7f9f82b854e
Mahdi Dibaiee <mdibaiee@pm.me>
2025-05-23 13:05:25 +0100

    data-plane-controller: remove private_links transition check, add test for updating

--- crates/data-plane-controller/tests/test_private_links.rs
+++ crates/data-plane-controller/tests/test_private_links.rs
@@ -66,47 +66,18 @@ async fn test_private_links() {
         }
     }
 
-    insta::assert_json_snapshot!(trace.lock().unwrap().as_slice());
-}
+    row_state
+        .private_links
+        .push(stack::PrivateLink::AWS(stack::AWSPrivateLink {
+            az_ids: vec!["b".to_string(), "c".to_string()],
+            region: "us-west-2".to_string(),
+            service_name: "service-2".to_string(),
+        }));
 
-#[tokio::test]
-async fn test_private_links_inconsistent() {
-    let trace = Arc::new(Mutex::new(Vec::new()));
-
-    let controller = data_plane_controller::Controller {
-        dns_ttl: std::time::Duration::ZERO,
-        dry_dock_remote: "git@github.com:estuary/est-dry-dock.git".to_string(),
-        ops_remote: "git@github.com:estuary/ops.git".to_string(),
-        secrets_provider: "testing".to_string(),
-        state_backend: "file:///tmp/pulumi-test-state".parse().unwrap(),
-        emit_log_fn: mock_emit_log_fn(trace.clone()),
-        run_cmd_fn: mock_run_cmd_fn(trace.clone()),
-    };
-
-    let mut state: Option<stack::State> = None;
-    let mut inbox: VecDeque<(models::Id, Option<controller::Message>)> = VecDeque::new();
-    let mut checkouts: HashMap<String, tempfile::TempDir> = HashMap::new();
-    let mut row_state = initial_state();
-    row_state.stack.config.model.private_links =
-        vec![stack::PrivateLink::AWS(stack::AWSPrivateLink {
-            az_ids: vec!["b".to_string(), "a".to_string()],
-            region: "us-east-2".to_string(),
-            service_name: "service".to_string(),
-        })];
-    row_state.private_links = vec![stack::PrivateLink::AWS(stack::AWSPrivateLink {
-        az_ids: vec!["a".to_string(), "b".to_string()],
-        region: "us-west-2".to_string(),
-        service_name: "service".to_string(),
-    })];
-
-    inbox.push_back((
-        models::Id::zero(),
-        Some(controller::Message::Start(row_state.data_plane_id)),
-    ));
-    inbox.push_back((models::Id::zero(), Some(controller::Message::Enable)));
+    inbox.push_back((models::Id::zero(), Some(controller::Message::Converge)));
 
     for _ in 0..50 {
-        let output = controller
+        let outcome = controller
             .on_poll(
                 models::Id::new([42; 8]), // Task ID.
                 &mut state,
@@ -115,17 +86,8 @@ async fn test_private_links_inconsistent() {
                 Vec::new(),
                 row_state.clone(),
             )
-            .await;
-
-        if let Err(err) = output {
-            assert_eq!(
-                err.to_string(),
-                "cannot set both config.private_links and private_links, prefer the latter."
-            );
-            break;
-        };
-
-        let outcome = output.unwrap();
+            .await
+            .expect("on_poll failed");
 
         let mut trace = trace.lock().unwrap();
 
