c95fb2ce9837a0cef14aa7ae7f91c65a17185fb4
Travis Jenkins <travis@estuary.dev>
2025-06-06 12:06:59 -0400

    Moving creating clients to a single file to make it easier to keep the  version used in sync and ensure we are always aware there are two  clients being generated.

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,6 +1,18 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@^2.0.0?target=deno";
+import {createClient} from "npm:@supabase/supabase-js@^2.0.0";
 
 export const supabaseClient = createClient(
     Deno.env.get("SUPABASE_URL")!,
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );
+
+export const createSupabaseClientWithAuthorization = (authHeader: any) => {
+    return createClient(
+        Deno.env.get("SUPABASE_URL") ?? "",
+        Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+        {
+            global: {
+                headers: { Authorization: authHeader },
+            },
+        },
+    );
+}
