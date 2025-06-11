2eeba0cb10a03845e1a9ccd8a6e40f693f5cec6f
Mahdi Dibaiee <mdibaiee@pm.me>
2025-06-11 17:24:57 +0100

    agent: replace data_plane.hmac_keys with the decrypted value

--- crates/agent/src/proxy_connectors.rs
+++ crates/agent/src/proxy_connectors.rs
@@ -218,27 +218,18 @@ impl<L: runtime::LogHandler> ProxyConnectors<L> {
         let tables::DataPlane {
             reactor_address,
             hmac_keys,
-            encrypted_hmac_keys,
             data_plane_fqdn,
             ..
         } = data_plane;
 
         let mut metadata = gazette::Metadata::default();
 
-        let mut decrypted_hmac_keys = crate::decrypt_hmac_keys(&encrypted_hmac_keys)
-            .await
-            .context("decrypting HMAC keys")?;
-
-        if decrypted_hmac_keys.is_empty() {
-            decrypted_hmac_keys = hmac_keys.clone();
-        }
-
         metadata
             .signed_claims(
                 proto_flow::capability::PROXY_CONNECTOR,
                 data_plane_fqdn,
                 *CONNECTOR_TIMEOUT * 2,
-                &decrypted_hmac_keys,
+                &hmac_keys,
                 Default::default(),
                 task,
             )
