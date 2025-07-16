e6d9f02db3cbad14e5b1d53e1a257b006b632f22
Phil <phil@estuary.dev>
2025-07-16 08:52:50 -0400

    agent: log in json format by default

--- crates/agent/src/main.rs
+++ crates/agent/src/main.rs
@@ -77,6 +77,15 @@ struct Args {
     )]
     #[arg(value_parser = humantime::parse_duration)]
     heartbeat_timeout: std::time::Duration,
+
+    #[clap(long = "log-format", env = "LOG_FORMAT", default_value = "json")]
+    log_format: LogFormat,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, clap::ValueEnum)]
+enum LogFormat {
+    Text,
+    Json,
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -86,18 +95,19 @@ fn main() -> Result<(), anyhow::Error> {
         .install_default()
         .expect("failed to install default crypto provider");
 
-    // Use reasonable defaults for printing structured logs to stderr.
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_ansi(if matches!(std::env::var("NO_COLOR"), Ok(v) if v == "1") {
-            false
-        } else {
-            true
-        })
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
-
     let args = Args::parse();
+
+    // Use reasonable defaults for printing structured logs to stderr.
+    let builder = tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env());
+    if args.log_format == LogFormat::Json {
+        tracing::subscriber::set_global_default(builder.json().finish())
+            .expect("setting tracing default failed");
+    } else {
+        let no_color = matches!(std::env::var("NO_COLOR"), Ok(v) if v == "1");
+        tracing::subscriber::set_global_default(builder.with_ansi(!no_color).finish())
+            .expect("setting tracing default failed");
+    };
     tracing::info!(?args, "started!");
 
     let runtime = tokio::runtime::Builder::new_multi_thread()
