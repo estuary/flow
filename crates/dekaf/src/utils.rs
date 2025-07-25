3061d4bf4bd8e37e8ded37928aeaee82bfbb7564
Joseph Shearer <joseph@josephshearer.net>
2025-07-25 11:57:00 -0400

    Entirely remove user refresh token auth

--- crates/dekaf/src/utils.rs
+++ crates/dekaf/src/utils.rs
@@ -161,65 +161,6 @@ pub fn build_field_extractors(
     ))
 }
 
-pub fn build_LEGACY_field_extractors(
-    mut schema: doc::Shape,
-    deletions: DeletionMode,
-) -> anyhow::Result<(avro::Schema, Vec<(avro::Schema, CustomizableExtractor)>)> {
-    if matches!(deletions, DeletionMode::CDC) {
-        if let Some(meta) = schema
-            .object
-            .properties
-            .iter_mut()
-            .find(|prop| prop.name.to_string() == "_meta".to_string())
-        {
-            if let Err(idx) = meta
-                .shape
-                .object
-                .properties
-                .binary_search_by(|prop| prop.name.to_string().cmp(&"is_deleted".to_string()))
-            {
-                meta.shape.object.properties.insert(
-                    idx,
-                    doc::shape::ObjProperty {
-                        name: "is_deleted".into(),
-                        is_required: true,
-                        shape: doc::Shape {
-                            type_: json::schema::types::INTEGER,
-                            ..doc::Shape::nothing()
-                        },
-                    },
-                );
-            } else {
-                tracing::warn!(
-                    "This collection's schema already has a /_meta/is_deleted location!"
-                );
-            }
-        } else {
-            return Err(anyhow::anyhow!("Schema missing /_meta"));
-        }
-
-        let schema = avro::shape_to_avro(schema.clone());
-
-        Ok((
-            schema.clone(),
-            vec![(schema, CustomizableExtractor::RootExtractorWithIsDeleted)],
-        ))
-    } else {
-        let schema = avro::shape_to_avro(schema.clone());
-
-        Ok((
-            schema.clone(),
-            vec![(
-                schema,
-                CustomizableExtractor::Extractor(doc::Extractor::new(
-                    doc::Pointer::empty(),
-                    &doc::SerPolicy::noop(),
-                )),
-            )],
-        ))
-    }
-}
-
 pub fn fetch_all_collection_names(spec: &MaterializationSpec) -> anyhow::Result<Vec<String>> {
     spec.bindings
         .iter()
