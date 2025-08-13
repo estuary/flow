estuary/flow

commit 9b9560de838f15087b1e9879d7d76152ecaf24b7

dekaf: Don't transform schemas into canonical form in `/subjects/:subject/versions/latest`
Currently, `/schemas/ids/:id` returns the full-fat schema whereas `/subjects/:subject/versions/latest` returns the canonical form which is missing a lot of data.

crates/dekaf/src/registry.rs
@@ -107,9 +107,12 @@ async fn get_subject_latest(
             (value_id, &collection.value_schema)
         };
 
+        let serialized_schema =
+            serde_json::to_value(schema).context("Cannot parse Schema from JSON")?;
+
         Ok(serde_json::json!({
             "id": id,
-            "schema": schema.canonical_form(),
+            "schema": serialized_schema,
             "schemaType": "AVRO",
             "subject": subject,
             "version": 1,

commit acadbc162f8c45680b522e4205469ae7fcdcbf61

dekaf: Fix serialization of `get_subject_latest()` for Materialize
It looks like I broke this in https://github.com/estuary/flow/pull/2299. `Schema::canonical_form()` totally returns `String`, so we need to match that in order to be compliant with the confluent schema registry.

crates/dekaf/src/registry.rs
@@ -108,7 +108,7 @@ async fn get_subject_latest(
         };
 
         let serialized_schema =
-            serde_json::to_value(schema).context("Cannot parse Schema from JSON")?;
+            serde_json::to_string(schema).context("Cannot parse Schema from JSON")?;
 
         Ok(serde_json::json!({
             "id": id,