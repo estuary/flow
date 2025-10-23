f5b5d39e3f71190474a6e048574b673399c928e9
Johnny Graettinger <johnny@estuary.dev>
2025-10-23 11:53:03 -0500

    runtime: update CONNECTOR_INIT_IMAGE pin to `:v0.5.24-30-ga3eba41f95`
    This brings in support for Python derivations when running `flowctl`.

--- crates/runtime/src/container.rs
+++ crates/runtime/src/container.rs
@@ -18,7 +18,7 @@ const PORT_PUBLIC_LABEL_PREFIX: &str = "dev.estuary.port-public.";
 const PORT_PROTO_LABEL_PREFIX: &str = "dev.estuary.port-proto.";
 
 // TODO(johnny): Consider better packaging and versioning of `flow-connector-init`.
-const CONNECTOR_INIT_IMAGE: &str = "ghcr.io/estuary/flow:v0.5.23-6-gc4ce3f1eff";
+const CONNECTOR_INIT_IMAGE: &str = "ghcr.io/estuary/flow:v0.5.24-30-ga3eba41f95";
 const CONNECTOR_INIT_IMAGE_PATH: &str = "/usr/local/bin/flow-connector-init";
 
 /// Determines the protocol of an image. If the image has a `FLOW_RUNTIME_PROTOCOL` label,
