8658efa3d651282fc29271a3c3c23ff748399f28
Johnny Graettinger <johnny@estuary.dev>
2026-02-23 06:04:08 +0000

    shuffle::queue: scale-out second stage of distributed shuffles
    Queue RPCs join across and receive Enqueue and Flush requests from Slice
    RPCs. They merge and order recieved Enqueues, write to an on-disk queue
    file (still TODO), and respond to Slice Flush barriers.
    
    Queues build out the on-disk queued state which will be dequeued from
    using the checkpoint presented to the shuffle coordinator by the session
    RPC.

---
+++ crates/shuffle/src/queue/mod.rs
@@ -0,0 +1,20 @@
+use futures::stream::BoxStream;
+use proto_flow::shuffle;
+use tokio::sync::mpsc;
+
+mod actor;
+mod handler;
+mod heap;
+mod state;
+pub(crate) use handler::serve_queue;
+
+/// QueueJoin coordinates multiple Slice streams connecting to the same Queue.
+/// Each Queue member receives connections from all Slices (M connections total).
+pub(crate) struct QueueJoin {
+    members: Vec<
+        Option<(
+            BoxStream<'static, tonic::Result<shuffle::QueueRequest>>,
+            mpsc::Sender<tonic::Result<shuffle::QueueResponse>>,
+        )>,
+    >,
+}
