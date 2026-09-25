2211efd1556fd5a3a6e8fd254c8815cc66080c9c
Brian Bartman <brian.m.bartman@gmail.com>
2026-09-25 18:48:35 +0000

    Forgot to remove a pub from a function that is private.

--- crates/control-plane-api/src/envelope.rs
+++ crates/control-plane-api/src/envelope.rs
@@ -263,7 +263,7 @@ impl axum::extract::FromRequestParts<Arc<crate::App>> for Envelope {
     }
 }
 
-pub async fn parse_authorization_header(
+async fn parse_authorization_header(
     bearer: TypedHeader<Authorization<Bearer>>,
     state: &Arc<crate::App>,
 ) -> tonic::Result<MaybeControlClaims> {
